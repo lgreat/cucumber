9acb78197c527f584d240b2a552f994c3789a3e5
apeterson <apeterson>
2006-01-21 03:56:51 +0000

    city page

---
+++ src/java/gs/web/geo/NearbyCitiesController.java
@@ -0,0 +1,93 @@
+/*
+* Copyright (c) 2005 GreatSchools.net. All Rights Reserved.
+* $Id: NearbyCitiesController.java,v 1.1 2006/01/21 03:56:50 apeterson Exp $
+*/
+
+package gs.web.geo;
+
+import gs.data.geo.IGeoDao;
+import gs.data.geo.bestplaces.BpCity;
+import gs.data.state.State;
+import gs.web.ISessionFacade;
+import gs.web.SessionContextUtil;
+import gs.web.SessionFacade;
+import gs.web.util.Anchor;
+import gs.web.util.UnorderedListModel;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Provides...
+ *
+ * @author <a href="mailto:apeterson@greatschools.net">Andrew J. Peterson</a>
+ */
+public class NearbyCitiesController extends AbstractController {
+    private String _viewName;
+    private IGeoDao _geoDao;
+
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        Map model = new HashMap();
+
+        ISessionFacade context = SessionFacade.getInstance(request);
+        request.getParameter(SessionContextUtil.STATE_PARAM);
+
+        State state = context.getStateOrDefault();
+
+        String cityNameParam = request.getParameter("city");
+        if (StringUtils.isNotEmpty(cityNameParam) && state != null) {
+
+            BpCity city = _geoDao.findCity(state, cityNameParam);
+
+            if (city != null) {
+                model.put("city", city);
+
+                final int limit = 15;
+                List nearbyCities = _geoDao.findNearbyCities(city, limit);
+
+                model.put(UnorderedListModel.HEAD, "Cities Near " + city.getName());
+
+                List items = new ArrayList(limit);
+                for (int i = 0; i < limit; i++) {
+                    BpCity nearbyCity = (BpCity) nearbyCities.get(i);
+                    Anchor anchor = new Anchor("/test/city.page?state=" +
+                            nearbyCity.getState() +
+                            "&amp;city=" +
+                            nearbyCity.getName(),
+                            nearbyCity.getName());
+                    items.add(anchor);
+                }
+                model.put(UnorderedListModel.RESULTS, items);
+            }
+        }
+
+
+        ModelAndView modelAndView = new ModelAndView(_viewName, model);
+        return modelAndView;
+    }
+
+
+    public String getViewName() {
+        return _viewName;
+    }
+
+    public void setViewName(String viewName) {
+        _viewName = viewName;
+    }
+
+    public IGeoDao getGeoDao() {
+        return _geoDao;
+    }
+
+    public void setGeoDao(IGeoDao geoDao) {
+        _geoDao = geoDao;
+    }
+}
