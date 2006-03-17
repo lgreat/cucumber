41107a678d88cd7ba2b02e2bc5a403ae742ca057
apeterson <apeterson>
2006-03-17 05:33:43 +0000

    StateInterceptor experiment. (Successful!)

---
+++ src/java/gs/web/state/RequiresStateInterceptor.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2005 GreatSchools.net. All Rights Reserved.
+ * $Id: RequiresStateInterceptor.java,v 1.1 2006/03/17 05:33:43 apeterson Exp $
+ */
+
+package gs.web.state;
+
+import gs.web.SessionContextUtil;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Iterator;
+
+/**
+ * The purpose to guarantee that the user that is going to a
+ * specific URL has chosen a state first. We look in the URL for
+ * the state, and if it's not there, redirect to the "selectAState" page.
+ *
+ * @author Andrew J. Peterson <mailto:apeterson@greatschools.net>
+ */
+public class RequiresStateInterceptor
+        implements HandlerInterceptor,
+        ApplicationContextAware {
+
+    private static final Log _log = LogFactory.getLog(RequiresStateInterceptor.class);
+    private ApplicationContext _applicationContext;
+
+
+    public boolean preHandle(HttpServletRequest httpServletRequest,
+                             HttpServletResponse httpServletResponse,
+                             Object o) throws Exception {
+
+        if (httpServletRequest.getRequestURI().indexOf("selectAState.page") == -1) { // prevent recursion
+            String state = httpServletRequest.getParameter(SessionContextUtil.STATE_PARAM);
+            if (StringUtils.isEmpty(state) || state.length() < 2) {
+                String url = "http://" +
+                        httpServletRequest.getServerName() +
+                        ((httpServletRequest.getServerPort() != 80) ? ":" + httpServletRequest.getServerPort() : "") +
+                        httpServletRequest.getContextPath() +
+                        "/selectAState.page?prompt=Please+select+a+state+to+continue.&url=" +
+                        httpServletRequest.getContextPath() +
+                        httpServletRequest.getRequestURI();
+                if (httpServletRequest.getParameterMap().size() != 0) {
+                    url += "?";
+                    for (Iterator iter = httpServletRequest.getParameterMap().keySet().iterator(); iter.hasNext(); ) {
+                        String key = (String) iter.next();
+                        String value = httpServletRequest.getParameter(key);
+                        url += key + "=" + value;
+                        if (iter.hasNext()) {
+                            url += "%26";
+                        }
+                    }
+                }
+                final String redirect = httpServletResponse.encodeRedirectURL(url);
+                _log.info("Redirecting to " + url);
+                httpServletResponse.sendRedirect(redirect);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        // nothing
+    }
+
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        // nothing
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        _applicationContext = applicationContext;
+    }
+
+}
