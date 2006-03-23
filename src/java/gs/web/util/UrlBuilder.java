c4cc8abdc25b80e669f164e1919ee2c3bdc4642d
apeterson <apeterson>
2006-03-23 00:57:12 +0000

    Make building URLs easier.

---
+++ src/java/gs/web/util/UrlBuilder.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2005-2006 GreatSchools.net. All Rights Reserved.
+ * $Id: UrlBuilder.java,v 1.1 2006/03/23 00:56:56 apeterson Exp $
+ */
+
+package gs.web.util;
+
+import gs.data.content.Article;
+import gs.data.state.State;
+import org.apache.commons.lang.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+
+/**
+ * Provides a builder utility for our URLs.
+ *
+ * @author <a href="mailto:apeterson@greatschools.net">Andrew J. Peterson</a>
+ * @see UrlUtil#createBuilder(javax.servlet.http.HttpServletRequest)
+ * @see UrlUtil
+ */
+public class UrlBuilder {
+
+    private String _serverName;
+    private int _serverPort = 80;
+    private String _contextPath;
+
+    /**
+     * Path relative to the host/context.
+     */
+    private String _path;
+    private Map _parameters;
+    private boolean _perlPage = false;
+
+
+    /**
+     * Create a builder to the same page.
+     *
+     * @param request the current request
+     */
+    public UrlBuilder(HttpServletRequest request) {
+        _serverName = request.getServerName();
+        _serverPort = request.getServerPort();
+        _contextPath = request.getContextPath();
+        _path = request.getRequestURI();
+    }
+
+
+    /**
+     * Create a link to an article
+     *
+     * @param featured should the "featured" url be used instead of the normal one. This is
+     *                 for tracking.
+     */
+    public UrlBuilder(State s, Article article, boolean featured) {
+        _perlPage = true;
+        _contextPath = "";
+
+        // Calculate page to use
+        String page;
+        if (s.isSubscriptionState() && article.isInsider()) {
+            page = "showpartarticle";
+        } else {
+            if (featured) {
+                page = "showarticlefeature";
+            } else {
+                page = "showarticle";
+            }
+        }
+
+        // Calculate link
+        // TH: Commented this out because buildHref is noop with a null request
+        // link = buildHref(null, link, false, null);
+        _path = "/cgi-bin/" +
+                page +
+                "/" +
+                s.getAbbreviationLowerCase() +
+                "/" +
+                article.getId();
+    }
+
+    public void setPath(String path) {
+        _path = path;
+    }
+
+    public void addParametersFromRequest(HttpServletRequest request) {
+        if (_parameters == null) {
+            _parameters = new HashMap(request.getParameterMap());
+        } else {
+            _parameters.putAll(request.getParameterMap());
+        }
+    }
+
+    public void setParameter(String key, String value) {
+        if (_parameters == null) {
+            _parameters = new HashMap();
+        }
+        _parameters.put(key, new String[] {value});
+    }
+
+    public void removeParameter(String key) {
+        if (_parameters != null) {
+            _parameters.remove(key);
+        }
+    }
+
+    public String toString() {
+        return asSiteRelativeUrl();
+    }
+
+    public String asSiteRelativeUrl() {
+        StringBuffer sb = new StringBuffer();
+        if (!_perlPage && StringUtils.isNotEmpty(_contextPath)) {
+            sb.append(_contextPath);
+        }
+        sb.append(_path);
+        if (_parameters != null && _parameters.size() > 0) {
+            sb.append("?");
+            List keys = new ArrayList(_parameters.keySet());
+            Collections.sort(keys);
+            for (Iterator iter = keys.iterator(); iter.hasNext(); ) {
+                String key = (String) iter.next();
+                String[] values = (String[]) _parameters.get(key);
+                for (int i = 0; i < values.length; i++) {
+                    sb.append(key);
+                    sb.append("=" + values[i]);
+                    if (i < values.length && iter.hasNext()) {
+                        sb.append("&");
+                    }
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public Anchor asAnchor(String label) {
+        return null;
+    }
+
+    public String asFullUrl() {
+        String url = "http://" +
+                _serverName +
+                ((_serverPort != 80) ? ":" + _serverPort : "") +
+                asSiteRelativeUrl();
+        return url;
+    }
+
+
+}
---
+++ src/test/gs/web/util/UrlBuilderSaTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2005-2006 GreatSchools.net. All Rights Reserved.
+ * $Id: UrlBuilderSaTest.java,v 1.1 2006/03/23 00:57:12 apeterson Exp $
+ */
+
+package gs.web.util;
+
+import junit.framework.TestCase;
+import gs.data.content.Article;
+import gs.data.state.State;
+import gs.web.MockHttpServletRequest;
+
+/**
+ * Provides...
+ *
+ * @author <a href="mailto:apeterson@greatschools.net">Andrew J. Peterson</a>
+ */
+public class UrlBuilderSaTest extends TestCase {
+    public void testArticleLinkBuilder() {
+        Article article = new Article();
+        article.setId(new Integer(5));
+        article.setActive(true);
+        article.setInsider(false);
+        UrlBuilder builder12 = new UrlBuilder(State.CA, article, true);
+        assertEquals("/cgi-bin/showarticlefeature/ca/5", builder12.toString());
+        UrlBuilder builder11 = new UrlBuilder(State.CA, article, false);
+        assertEquals("/cgi-bin/showarticle/ca/5", builder11.toString());
+        UrlBuilder builder10 = new UrlBuilder(State.WY, article, true);
+        assertEquals("/cgi-bin/showarticlefeature/wy/5", builder10.toString());
+        UrlBuilder builder9 = new UrlBuilder(State.WY, article, false);
+        assertEquals("/cgi-bin/showarticle/wy/5", builder9.toString());
+
+        article.setInsider(true);
+        UrlBuilder builder8 = new UrlBuilder(State.CA, article, true);
+        assertEquals("/cgi-bin/showpartarticle/ca/5", builder8.toString());
+        UrlBuilder builder7 = new UrlBuilder(State.CA, article, false);
+        assertEquals("/cgi-bin/showpartarticle/ca/5", builder7.toString());
+        UrlBuilder builder6 = new UrlBuilder(State.WY, article, true);
+        assertEquals("/cgi-bin/showarticlefeature/wy/5", builder6.toString());
+        UrlBuilder builder5 = new UrlBuilder(State.WY, article, false);
+        assertEquals("/cgi-bin/showarticle/wy/5", builder5.toString());
+    }
+
+    public void testUrlBuilder() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setMethod("GET");
+        request.setProtocol("http");
+        request.setServerName("www.myserver.com");
+        request.setServerPort(80);
+        request.setRequestURI("/index.page");
+        UrlBuilder builder = new UrlBuilder(request);
+        assertEquals("/index.page", builder.asSiteRelativeUrl());
+        assertEquals("http://www.myserver.com/index.page", builder.asFullUrl());
+    }
+
+    public void testUrlBuilderParams() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setMethod("GET");
+        request.setProtocol("http");
+        request.setServerName("www.myserver.com");
+        request.setServerPort(80);
+        request.setRequestURI("/index.page");
+        request.setParameter("a", "1");
+        request.setParameter("b", "2");
+        UrlBuilder builder = new UrlBuilder(request);
+        builder.addParametersFromRequest(request);
+        assertEquals("/index.page?a=1&amp;b=2", builder.asSiteRelativeUrl());
+        assertEquals("http://www.myserver.com/index.page?a=1&amp;b=2", builder.asFullUrl());
+    }
+
+    public void testUrlBuilder8080() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setMethod("GET");
+        request.setProtocol("http");
+        request.setServerName("www.myserver.com");
+        request.setServerPort(8080);
+        request.setRequestURI("/index.page");
+        UrlBuilder builder = new UrlBuilder(request);
+        assertEquals("/index.page", builder.asSiteRelativeUrl());
+        assertEquals("http://www.myserver.com:8080/index.page", builder.asFullUrl());
+    }
+
+
+}
