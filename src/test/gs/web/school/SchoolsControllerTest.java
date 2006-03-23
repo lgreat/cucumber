b191d2a12602d976dc2115f705ea672e7b720b80
apeterson <apeterson>
2006-03-23 17:25:11 +0000

    GS-1658 see if the indexes will build and the unit tests will pass

--- src/test/gs/web/school/SchoolsControllerTest.java
+++ src/test/gs/web/school/SchoolsControllerTest.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2005-2006 GreatSchools.net. All Rights Reserved.
- * $Id: SchoolsControllerTest.java,v 1.3 2006/03/23 00:48:16 apeterson Exp $
+ * $Id: SchoolsControllerTest.java,v 1.4 2006/03/23 17:25:11 apeterson Exp $
  */
 
 package gs.web.school;
@@ -39,7 +39,7 @@ public class SchoolsControllerTest extends BaseControllerTestCase {
         _sessionContextUtil = (SessionContextUtil) getApplicationContext().getBean(SessionContextUtil.BEAN_ID);
     }
 
-    public void donttestByCity() throws Exception {
+    public void testByCity() throws Exception {
 
         MockHttpServletRequest request = getRequest();
         request.setParameter("state", "CA");
@@ -67,7 +67,31 @@ public class SchoolsControllerTest extends BaseControllerTestCase {
         assertEquals(null, request.getAttribute(SchoolsController.REQ_ATTR_SCHOOL_TYPE));
     }
 
-    public void testByDistrict() {
+    public void testByDistrict() throws Exception {
+        MockHttpServletRequest request = getRequest();
+        request.setParameter("state", "CA");
+        request.setParameter("district", "1");
+        _sessionContextUtil.prepareSessionContext(request, getResponse());
+
+        ModelAndView mav = _controller.handleRequestInternal(request, getResponse());
+
+        Map model = mav.getModel();
+        Map modelResults = (Map) model.get("results");
+
+        assertEquals(new Integer(10), modelResults.get(SchoolsController.MODEL_SCHOOLS_TOTAL));
+        assertEquals(new Integer(10), modelResults.get(SchoolsController.MODEL_PAGE_SIZE));
+        assertEquals(new Integer(10), modelResults.get(SchoolsController.MODEL_TOTAL));
+        List list = (List) modelResults.get(SchoolsController.MODEL_SCHOOLS);
+        for (int i = 0; i < list.size(); i++) {
+            School s = (School) list.get(i);
+            assertEquals("Wrong city for " + s, "Alameda", s.getPhysicalAddress().getCity());
+        }
+
+        // Check for other important stuff
+        assertEquals("1", request.getAttribute(SchoolsController.REQ_ATTR_PAGE));
+        assertEquals("Alameda City Unified", request.getAttribute(SchoolsController.REQ_ATTR_DISTNAME));
+        assertEquals(null, request.getAttribute(SchoolsController.REQ_ATTR_LEVEL_CODE));
+        assertEquals(null, request.getAttribute(SchoolsController.REQ_ATTR_SCHOOL_TYPE));
 
     }
 
