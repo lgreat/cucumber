17290780dc1434e33c248c2e21940ece04116344
thuss <thuss>
2006-04-24 18:19:07 +0000

    Enhanced logging in the OSIV filter to catch errors that go all the way up the stack

--- src/java/gs/web/util/OpenSessionInViewFilter.java
+++ src/java/gs/web/util/OpenSessionInViewFilter.java
@@ -3,12 +3,32 @@ package gs.web.util;
 import gs.data.dao.hibernate.ThreadLocalTransactionManager;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Cleans up database connections and hibernate sessions after the view has rendered
+ * Logs an error exception if there's a problem
  */
 public class OpenSessionInViewFilter implements Filter {
+
+    private static final Log _log = LogFactory.getLog(OpenSessionInViewFilter.class);
+
+    private static String _hostname = "unable to resolve";
+
+    static {
+        // Set the hostname for logging purposes
+        try {
+            java.net.InetAddress localMachine =
+                    java.net.InetAddress.getLocalHost();
+            _hostname = localMachine.getHostName();
+        } catch (java.net.UnknownHostException e) {
+            // No need to do anything
+        }
+    }
 
     public void doFilter(ServletRequest request,
                          ServletResponse response,
@@ -16,9 +36,38 @@ public class OpenSessionInViewFilter implements Filter {
             throws IOException, ServletException {
         try {
             chain.doFilter(request, response);
+        } catch (IOException e) {
+            logException(e, request);
+            throw e;
+        } catch (ServletException e) {
+            logException(e, request);
+            throw e;
+        } catch (RuntimeException e) {
+            logException(e, request);
+            throw e;
         } finally {
+            // Cleanup open database connections
             ThreadLocalTransactionManager.commitOrRollback();
         }
+    }
+
+    private void logException(Exception e, ServletRequest r) {
+        StringBuffer url = null;
+        String userAgent = null;
+        String remoteIp = null;
+        String referrer = null;
+        if (r instanceof HttpServletRequest) {
+            HttpServletRequest request = (HttpServletRequest) r;
+            // Reconstruct the URL
+            url = request.getRequestURL();
+            if (request.getQueryString() != null) url.append("?").append(request.getQueryString());
+            userAgent = request.getHeader("User-Agent");
+            remoteIp = request.getRemoteAddr();
+            referrer = request.getHeader("Referer");
+        }
+
+        _log.error("WEBSERVER: " + _hostname + " REQUEST: " + url + " REFERRER: " + referrer +
+                " REMOTEIP: " + remoteIp + " USER-AGENT: " + userAgent, e);
     }
 
     public void init(FilterConfig filterConfig) throws ServletException {
