f732173682bf2da1b49c8aacaa68d5f4fd2c1466
apeterson <apeterson>
2006-05-11 18:10:52 +0000

    GS-1980

--- src/java/gs/web/search/ResultsPager.java
+++ src/java/gs/web/search/ResultsPager.java
@@ -1,6 +1,7 @@
 package gs.web.search;
 
 import gs.data.school.ISchoolDao;
+import gs.data.school.School;
 import gs.data.search.Searcher;
 import gs.data.state.State;
 import gs.data.state.StateManager;
@@ -12,6 +13,7 @@ import org.apache.lucene.search.Hits;
 import org.apache.lucene.search.Query;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.orm.ObjectRetrievalFailureException;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,12 +23,15 @@ import java.util.List;
  * This class handles the organization of <code>Hits</code> into
  * page-ready parcels that can be retrieved using the <code>getPage(..)</code>
  * method.
+ *
  * @author Chris Kimm<mailto:chriskimm@greatschools.net>
  * @noinspection CanBeFinal
  */
 public class ResultsPager {
 
-    /** Spring bean id */
+    /**
+     * Spring bean id
+     */
     public static final String BEAN_ID = "resultsPager";
 
     private static ISchoolDao _schoolDao;
@@ -36,10 +41,14 @@ public class ResultsPager {
     private Query _explanationQuery;
 
 
-    /** Constant to indicate that the page has mixed results */
-    private static final int MIXED_PAGE  = 0;
+    /**
+     * Constant to indicate that the page has mixed results
+     */
+    private static final int MIXED_PAGE = 0;
 
-    /** Constant to indicate that the page has only school results */
+    /**
+     * Constant to indicate that the page has only school results
+     */
     private static final int SCHOOL_PAGE = 1;
 
     private static final StateManager _stateManager;
@@ -79,16 +88,15 @@ public class ResultsPager {
     /**
      * Returns page of hits encapsulated as a <code>List</code> of
      * <code>SearchResult</code> objects
-     * @param page
-     * @param pageSize
+     *
      * @return a <code>java.util.List</code> type
      */
     public List getResults(int page, int pageSize) {
-        return getPage(_hits, page,  pageSize, MIXED_PAGE);
+        return getPage(_hits, page, pageSize, MIXED_PAGE);
     }
 
     public List getSchools(int page, int pageSize) {
-        return getPage(_schoolHits, page,  pageSize, SCHOOL_PAGE);
+        return getPage(_schoolHits, page, pageSize, SCHOOL_PAGE);
     }
 
     public int getSchoolsTotal() {
@@ -98,12 +106,10 @@ public class ResultsPager {
 
     /**
      * This method returns a List of <code>SearchResults</code> of pageSize
-     * length based on the supplied page parameter.  Always returns a non-null
-     * <code>java.util.List</code> object
-     * @param hits
-     * @param page
-     * @param pageSize
-     * @return A sub<code>List</code> containing
+     * length based on the supplied page parameter.
+     *
+     * @param type set to MIXED_PAGE for a list of SearchResult objects, otherwise returns list of School objects
+     * @return non-null List object
      */
     List getPage(Hits hits, int page, int pageSize, int type) {
         List searchResults = new ArrayList();
@@ -113,7 +119,7 @@ public class ResultsPager {
             }
 
             int startIndex = pageSize > 0 ? (page - 1) * pageSize : 0;
-            int endIndex =   pageSize > 0 ? startIndex + pageSize : hits.length();
+            int endIndex = pageSize > 0 ? startIndex + pageSize : hits.length();
 
             if (startIndex > hits.length()) {
                 return null;
@@ -137,9 +143,16 @@ public class ResultsPager {
                         State state = _stateManager.getState(d.get("state"));
                         if (state != null) {
                             String id = d.get("id");
-                            if(StringUtils.isNotBlank(id)) {
-                                searchResults.add(getSchoolDao().getSchoolById(state,
-                                        Integer.valueOf(id)));
+                            if (StringUtils.isNotBlank(id)) {
+                                try {
+                                    final School schoolById = getSchoolDao().getSchoolById(state,
+                                            Integer.valueOf(id));
+                                    searchResults.add(schoolById);
+                                } catch (NumberFormatException e) {
+                                    _log.warn("Couldn't find school " + id + " in " + state, e);
+                                } catch (ObjectRetrievalFailureException e) {
+                                    _log.warn("Couldn't find school " + id + " in " + state, e);
+                                }
                             }
                         }
                     }
