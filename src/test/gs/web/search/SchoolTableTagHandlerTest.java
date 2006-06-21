5522258398777ed8c15b90c42a5c26b3fa1c9450
chriskimm <chriskimm>
2006-06-21 00:13:39 +0000

    Removed JDOM dep.

--- src/test/gs/web/search/SchoolTableTagHandlerTest.java
+++ src/test/gs/web/search/SchoolTableTagHandlerTest.java
@@ -5,30 +5,13 @@ import gs.web.jsp.MockPageContext;
 import gs.web.jsp.MockJspWriter;
 
 import javax.servlet.jsp.PageContext;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-
-import org.xml.sax.InputSource;
-//import org.w3c.dom.Document;
-import org.jdom.input.SAXBuilder;
-import org.jdom.Document;
-import org.jdom.Element;
-
 import java.io.StringReader;
-import java.util.List;
 
 
 /**
  * @author Chris Kimm <mailto:chriskimm@greatschools.net>
  */
 public class SchoolTableTagHandlerTest extends BaseTagHandlerTestCase {
-
-    private DocumentBuilder _builder;
-
-    protected void setUp() throws Exception {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        _builder = factory.newDocumentBuilder();
-    }
 
     public void testNoSchools() throws Exception {
         SchoolTableTagHandler tag = new SchoolTableTagHandler();
@@ -42,7 +25,5 @@ public class SchoolTableTagHandlerTest extends BaseTagHandlerTestCase {
         String string = "<x>"+writer.getOutputBuffer().toString() + "</x>";
         StringReader reader = new StringReader(string);
         //InputSource inputSource = new InputSource(reader);
-        SAXBuilder builder = new SAXBuilder();
-        Document doc = builder.build(reader);
     }
 }
