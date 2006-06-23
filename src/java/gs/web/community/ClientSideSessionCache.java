98bdfba53035a0e63356656c3c05878637614fa4
apeterson <apeterson>
2006-06-23 17:28:57 +0000

    GS-2115 add UserInfo for caching of user information via cookies

---
+++ src/java/gs/web/community/ClientSideSessionCache.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2005-2006 GreatSchools.net. All Rights Reserved.
+ * $Id: ClientSideSessionCache.java,v 1.1 2006/06/23 17:28:31 apeterson Exp $
+ */
+
+package gs.web.community;
+
+import gs.data.community.Subscription;
+import gs.data.community.SubscriptionProduct;
+import gs.data.community.User;
+import gs.data.state.State;
+import org.apache.commons.lang.StringUtils;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Provides summary information about the user. Can be initialized either from
+ * a user object or from a set of values. Normal usage is to create one when
+ * the user first logs in, and then persist it client-side as a
+ * session cookie. When a new request comes in, a ClientSideSessionCache will created
+ * from the cookie. This class is Externalizable, and that value is appropriate
+ * to stick in a cookie.
+ *
+ * @author <a href="mailto:apeterson@greatschools.net">Andrew J. Peterson</a>
+ */
+public class ClientSideSessionCache implements Externalizable {
+    private String _nickname;
+    private String _email;
+    private int _mslCount;
+    /**
+     * of {@link #createMssKey(gs.data.state.State, int)} values
+     * May be null.
+     */
+    private Set _mss;
+
+    /**
+     * of {@link SubscriptionProduct#getName() values}
+     * May be null.
+     */
+    private Set _nonMss;
+
+    private static final String COOKIE_LIST_DELIMETER = ",";
+
+    public ClientSideSessionCache() {
+    }
+
+    /** @noinspection FeatureEnvy*/
+    public ClientSideSessionCache(User user) {
+        _email = user.getEmail();
+        final String[] p = _email.split("@");
+        _nickname = p[0];
+
+        // Count their subscriptions
+        final Set subscriptions = user.getSubscriptions();
+        if (subscriptions != null) {
+            _mss = new HashSet();
+            _nonMss = new HashSet();
+            for (Iterator iter = subscriptions.iterator(); iter.hasNext();) {
+                Subscription sub = (Subscription) iter.next();
+                if (sub.getProduct().equals(SubscriptionProduct.MYSTAT)) {
+                    _mss.add(createMssKey(sub.getState(), sub.getSchoolId()));
+                } else {
+                    _nonMss.add(sub.getProduct().getName());
+                }
+            }
+        }
+
+        _mslCount = user.getFavoriteSchools() != null ? user.getFavoriteSchools().size() : 0;
+    }
+
+    /**
+     * Creates a unique name for the MSS subscription. Really just CA1234, concatenating
+     * the school ID on the back of the state.
+     */
+    private String createMssKey(State state, int schoolId) {
+        return state.getAbbreviation() + schoolId;
+    }
+
+    public int getMslCount() {
+        return _mslCount;
+    }
+
+    public String getEmail() {
+        return _email;
+    }
+
+    public String getNickname() {
+        return _nickname;
+    }
+
+    public boolean hasMssSubscription(State state, int schoolId) {
+        if (_mss == null) {
+            return false;
+        }
+        String mssHash = createMssKey(state, schoolId);
+        return _mss.contains(mssHash);
+    }
+
+    /**
+     * Returns true if the user has one (or more) of the given subscriptions.
+     * Doesn't check for "expiration" and other constraints. Returns true
+     * generically for MSS subscriptions, and those must be checked against
+     * a specific school using {@link #hasMssSubscription(gs.data.state.State, int)}.
+     */
+    public boolean hasSubscription(SubscriptionProduct product) {
+        if (SubscriptionProduct.MYSTAT.equals(product)) {
+            return _mss != null && !_mss.isEmpty();
+        } else {
+            return _nonMss != null && _nonMss.contains(product.getName());
+        }
+    }
+
+    public int getMssCount() {
+        return _mss == null ? 0 : _mss.size();
+    }
+
+    /* package visible for unit test access */
+    String getMssCookie() {
+        return _mss == null ? "" : StringUtils.join(_mss.iterator(), COOKIE_LIST_DELIMETER);
+    }
+
+
+    /* package visible for unit test access */
+    void setMssCookie(String cookie) {
+        String[] p = cookie.split(COOKIE_LIST_DELIMETER);
+        if (p.length > 0) {
+            _mss = new HashSet();
+            for (int i = 0; i < p.length; i++) {
+                String s = p[i];
+                if (s.length() > 2) {
+                    _mss.add(s);
+                }
+            }
+        } else {
+            _mss = null;
+        }
+    }
+
+    /* package visible for unit test access */
+    void setNonMssCookie(String cookie) {
+        String[] p = cookie.split(COOKIE_LIST_DELIMETER);
+        if (p.length > 0) {
+            _nonMss = new HashSet();
+            for (int i = 0; i < p.length; i++) {
+                String s = p[i];
+                SubscriptionProduct prod = SubscriptionProduct.getSubscriptionProduct(s);
+                if (prod != null) {
+                    _nonMss.add(prod.getName());
+                }
+            }
+        } else {
+            _nonMss = null;
+        }
+    }
+
+
+    /* package visible for unit test access */
+    String getNonMssCookie() {
+        return _nonMss == null ? "" : StringUtils.join(_nonMss.iterator(), COOKIE_LIST_DELIMETER);
+    }
+
+
+    public void writeExternal(ObjectOutput objectOutput) throws IOException {
+        objectOutput.writeUTF(_email);
+        objectOutput.writeUTF(_nickname);
+        objectOutput.writeUTF(getMssCookie());
+        objectOutput.writeUTF(getNonMssCookie());
+        objectOutput.writeInt(getMslCount());
+    }
+
+    public void readExternal(ObjectInput objectInput) throws IOException, ClassNotFoundException {
+        _email = objectInput.readUTF();
+        _nickname = objectInput.readUTF();
+        String mssCookie = objectInput.readUTF();
+        setMssCookie(mssCookie);
+        String nonMssCookie = objectInput.readUTF();
+        setNonMssCookie(nonMssCookie);
+        _mslCount = objectInput.readInt();
+    }
+
+}
---
+++ src/test/gs/web/community/ClientSideSessionCacheTest.java
@@ -0,0 +1,270 @@
+/*
+ * Copyright (c) 2005-2006 GreatSchools.net. All Rights Reserved.
+ * $Id: ClientSideSessionCacheTest.java,v 1.1 2006/06/23 17:28:48 apeterson Exp $
+ */
+
+package gs.web.community;
+
+import gs.data.community.FavoriteSchool;
+import gs.data.community.Subscription;
+import gs.data.community.SubscriptionProduct;
+import gs.data.community.User;
+import gs.data.state.State;
+import junit.framework.TestCase;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Tests ClientSideSessionCache class.
+ *
+ * @author <a href="mailto:apeterson@greatschools.net">Andrew J. Peterson</a>
+ * @noinspection FeatureEnvy,LocalVariableNamingConvention,OverlyLongMethod,MagicNumber
+ */
+public class ClientSideSessionCacheTest extends TestCase {
+
+    public void testCalculateNickname() {
+
+        User user = new User();
+        user.setEmail("blahblahblah@whatever.org");
+
+        ClientSideSessionCache clientSideSessionCache = new ClientSideSessionCache(user);
+
+        assertEquals("blahblahblah@whatever.org", clientSideSessionCache.getEmail());
+        assertEquals("blahblahblah", clientSideSessionCache.getNickname());
+
+        user.setEmail("wbeck@whatever.org");
+        clientSideSessionCache = new ClientSideSessionCache(user);
+        assertEquals("wbeck@whatever.org", clientSideSessionCache.getEmail());
+        assertEquals("wbeck", clientSideSessionCache.getNickname());
+    }
+
+    public void testFindsMsses() {
+        User user = new User();
+        user.setEmail("wbeck@gs.net");
+
+        // Manually add some subscriptions
+        Set subscriptions = new HashSet();
+
+        addMssSubscriptions(subscriptions);
+
+        user.setSubscriptions(subscriptions);
+
+        ClientSideSessionCache clientSideSessionCache = new ClientSideSessionCache(user);
+        assertEquals(2, clientSideSessionCache.getMssCount());
+        assertTrue(clientSideSessionCache.hasMssSubscription(State.CA, 1234));
+        assertTrue(clientSideSessionCache.hasMssSubscription(State.GA, 1234));
+        assertFalse(clientSideSessionCache.hasMssSubscription(State.CA, 1235));
+
+        // Cool, let's make sure it doesn't find non-MSS subscriptions and count them.
+        addPaAndMyFirstSubscriptions(subscriptions);
+
+        user.setSubscriptions(subscriptions);
+
+        clientSideSessionCache = new ClientSideSessionCache(user);
+        assertEquals(2, clientSideSessionCache.getMssCount());
+        assertTrue(clientSideSessionCache.hasMssSubscription(State.CA, 1234));
+        assertTrue(clientSideSessionCache.hasMssSubscription(State.GA, 1234));
+        assertFalse(clientSideSessionCache.hasMssSubscription(State.CA, 1235));
+
+
+    }
+
+    public void testFindsMsls() {
+        User user = new User();
+        user.setEmail("wbeck@gs.net");
+
+        // Manually add some schools
+        Set favoriteSchools = new HashSet();
+
+        FavoriteSchool school = new FavoriteSchool();
+        school.setState(State.CA);
+        school.setSchoolId(new Integer(1));
+        favoriteSchools.add(school);
+
+        FavoriteSchool school2 = new FavoriteSchool();
+        school2.setState(State.CA);
+        school2.setSchoolId(new Integer(5));
+        favoriteSchools.add(school2);
+
+        user.setFavoriteSchools(favoriteSchools);
+
+        ClientSideSessionCache clientSideSessionCache = new ClientSideSessionCache(user);
+        assertEquals(2, clientSideSessionCache.getMslCount());
+
+    }
+
+    public void testFindsNonMssSubscriptions() {
+        User user = new User();
+        user.setEmail("wbeck@gs.net");
+
+        ClientSideSessionCache clientSideSessionCache = new ClientSideSessionCache(user);
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.PARENT_ADVISOR));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.MY_FIRST_GRADER));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.BETA_GROUP));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.MY_SECOND_GRADER));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.MYSTAT));
+
+        // Manually add some subscriptions
+        Set subscriptions = new HashSet();
+
+        addMssSubscriptions(subscriptions);
+        user.setSubscriptions(subscriptions);
+
+        clientSideSessionCache = new ClientSideSessionCache(user);
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.PARENT_ADVISOR));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.MY_FIRST_GRADER));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.BETA_GROUP));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.MY_SECOND_GRADER));
+        assertTrue(clientSideSessionCache.hasSubscription(SubscriptionProduct.MYSTAT));
+
+        // Cool, let's make sure it doesn't find non-MSS subscriptions and count them.
+        addPaAndMyFirstSubscriptions(subscriptions);
+        user.setSubscriptions(subscriptions);
+
+        clientSideSessionCache = new ClientSideSessionCache(user);
+        assertTrue(clientSideSessionCache.hasSubscription(SubscriptionProduct.PARENT_ADVISOR));
+        assertTrue(clientSideSessionCache.hasSubscription(SubscriptionProduct.MY_FIRST_GRADER));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.BETA_GROUP));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.MY_SECOND_GRADER));
+        assertTrue(clientSideSessionCache.hasSubscription(SubscriptionProduct.MYSTAT));
+
+    }
+
+
+    public void testSetFromCookies() {
+        User user = new User();
+        user.setEmail("wbeck@gs.net");
+
+        ClientSideSessionCache clientSideSessionCache = new ClientSideSessionCache(user);
+        clientSideSessionCache.setMssCookie("CA10");
+        assertTrue(clientSideSessionCache.hasMssSubscription(State.CA, 10));
+        assertFalse(clientSideSessionCache.hasMssSubscription(State.CA, 1));
+        assertFalse(clientSideSessionCache.hasMssSubscription(State.GA, 10));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.PARENT_ADVISOR));
+        assertEquals(0, clientSideSessionCache.getMslCount());
+
+        clientSideSessionCache.setMssCookie("CA10,GA10");
+        assertTrue(clientSideSessionCache.hasMssSubscription(State.CA, 10));
+        assertFalse(clientSideSessionCache.hasMssSubscription(State.CA, 1));
+        assertTrue(clientSideSessionCache.hasMssSubscription(State.GA, 10));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.PARENT_ADVISOR));
+
+        clientSideSessionCache.setNonMssCookie("greatnews");
+        assertTrue(clientSideSessionCache.hasMssSubscription(State.CA, 10));
+        assertFalse(clientSideSessionCache.hasMssSubscription(State.CA, 1));
+        assertTrue(clientSideSessionCache.hasMssSubscription(State.GA, 10));
+        assertTrue(clientSideSessionCache.hasSubscription(SubscriptionProduct.PARENT_ADVISOR));
+
+        clientSideSessionCache.setNonMssCookie("greatnews,my1");
+        assertTrue(clientSideSessionCache.hasMssSubscription(State.CA, 10));
+        assertFalse(clientSideSessionCache.hasMssSubscription(State.CA, 1));
+        assertTrue(clientSideSessionCache.hasMssSubscription(State.GA, 10));
+        assertTrue(clientSideSessionCache.hasSubscription(SubscriptionProduct.PARENT_ADVISOR));
+        assertTrue(clientSideSessionCache.hasSubscription(SubscriptionProduct.MY_FIRST_GRADER));
+    }
+
+    public void testCalculatesCookies() {
+        User user = new User();
+        user.setEmail("wbeck@gs.net");
+
+        ClientSideSessionCache clientSideSessionCache = new ClientSideSessionCache(user);
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.PARENT_ADVISOR));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.MY_FIRST_GRADER));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.BETA_GROUP));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.MY_SECOND_GRADER));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.MYSTAT));
+
+        // Manually add some subscriptions
+        Set subscriptions = new HashSet();
+
+        addMssSubscriptions(subscriptions);
+        addPaAndMyFirstSubscriptions(subscriptions);
+        user.setSubscriptions(subscriptions);
+        clientSideSessionCache = new ClientSideSessionCache(user);
+        assertTrue("GA1234,CA1234".equals(clientSideSessionCache.getMssCookie()) || "CA1234,GA1234".equals(clientSideSessionCache.getMssCookie()));
+        assertTrue("my1,greatnews".equals(clientSideSessionCache.getNonMssCookie()) || "greatnews,my1".equals(clientSideSessionCache.getNonMssCookie()));
+    }
+
+    public void testCanExternalize() throws IOException, ClassNotFoundException {
+        // Create a simple one, and make sure it comes back
+        User user = new User();
+        user.setEmail("apeterson@gs.net");
+        ClientSideSessionCache clientSideSessionCache = new ClientSideSessionCache(user);
+        assertEquals(0, clientSideSessionCache.getMslCount());
+        assertEquals(0, clientSideSessionCache.getMssCount());
+        ClientSideSessionCache clientSideSessionCacheCopy = externalizeAndBringBack(clientSideSessionCache);
+        assertEquals("apeterson@gs.net", clientSideSessionCacheCopy.getEmail());
+        assertEquals("apeterson", clientSideSessionCacheCopy.getNickname());
+        assertEquals(0, clientSideSessionCacheCopy.getMslCount());
+        assertEquals(0, clientSideSessionCacheCopy.getMssCount());
+
+        // Create a rich user info object, with everything
+        Set subscriptions = new HashSet();
+        addMssSubscriptions(subscriptions);
+        addPaAndMyFirstSubscriptions(subscriptions);
+        user.setSubscriptions(subscriptions);
+
+        clientSideSessionCache = new ClientSideSessionCache(user);
+
+        // Make sure it's all here
+        assertTrue(clientSideSessionCache.hasSubscription(SubscriptionProduct.PARENT_ADVISOR));
+        assertTrue(clientSideSessionCache.hasSubscription(SubscriptionProduct.MY_FIRST_GRADER));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.BETA_GROUP));
+        assertFalse(clientSideSessionCache.hasSubscription(SubscriptionProduct.MY_SECOND_GRADER));
+        assertTrue(clientSideSessionCache.hasSubscription(SubscriptionProduct.MYSTAT));
+
+        // Bring it back
+        clientSideSessionCacheCopy = externalizeAndBringBack(clientSideSessionCache);
+
+        // Make sure it came back
+        assertTrue(clientSideSessionCacheCopy.hasSubscription(SubscriptionProduct.PARENT_ADVISOR));
+        assertTrue(clientSideSessionCacheCopy.hasSubscription(SubscriptionProduct.MY_FIRST_GRADER));
+        assertFalse(clientSideSessionCacheCopy.hasSubscription(SubscriptionProduct.BETA_GROUP));
+        assertFalse(clientSideSessionCacheCopy.hasSubscription(SubscriptionProduct.MY_SECOND_GRADER));
+        assertTrue(clientSideSessionCacheCopy.hasSubscription(SubscriptionProduct.MYSTAT));
+
+    }
+
+    private ClientSideSessionCache externalizeAndBringBack(ClientSideSessionCache clientSideSessionCache) throws IOException, ClassNotFoundException {
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        final ObjectOutputStream objectOutput = new ObjectOutputStream(byteArrayOutputStream);
+        clientSideSessionCache.writeExternal(objectOutput);
+        objectOutput.flush();
+        final byte[] bytes = byteArrayOutputStream.toByteArray();
+
+        ClientSideSessionCache clientSideSessionCacheCopy = new ClientSideSessionCache();
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        clientSideSessionCacheCopy.readExternal(new ObjectInputStream(byteArrayInputStream));
+        return clientSideSessionCacheCopy;
+    }
+
+    private void addPaAndMyFirstSubscriptions(Set subscriptions) {
+        Subscription sub3 = new Subscription();
+        sub3.setProduct(SubscriptionProduct.PARENT_ADVISOR);
+        sub3.setState(State.CA);
+        subscriptions.add(sub3);
+
+        Subscription sub4 = new Subscription();
+        sub4.setProduct(SubscriptionProduct.MY_FIRST_GRADER);
+        sub4.setState(State.CA);
+        subscriptions.add(sub4);
+    }
+
+
+    private void addMssSubscriptions(Set subscriptions) {
+        Subscription sub = new Subscription();
+        sub.setProduct(SubscriptionProduct.MYSTAT);
+        sub.setState(State.GA);
+        sub.setSchoolId(1234);
+        subscriptions.add(sub);
+
+        Subscription sub2 = new Subscription();
+        sub2.setProduct(SubscriptionProduct.MYSTAT);
+        sub2.setState(State.CA);
+        sub2.setSchoolId(1234);
+        subscriptions.add(sub2);
+    }
+
+}
