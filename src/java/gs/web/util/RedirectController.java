621baef50ed6c667f6794b5ac21b5044e462bf5d
apeterson <apeterson>
2006-07-07 00:37:32 +0000

    GS-2115 drawing issues

--- src/java/gs/web/util/RedirectController.java
+++ src/java/gs/web/util/RedirectController.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2005-2006 GreatSchools.net. All Rights Reserved.
- * $Id: RedirectController.java,v 1.1 2006/07/07 00:33:24 apeterson Exp $
+ * $Id: RedirectController.java,v 1.2 2006/07/07 00:35:25 apeterson Exp $
  */
 
 package gs.web.util;
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Redirects to the URL specified by p.
+ * Redirects to the URL specified by page. Useful for form processing.
  *
  * @author <a href="mailto:apeterson@greatschools.net">Andrew J. Peterson</a>
  */
