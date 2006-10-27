8720ed49773dee32b2507eecc6ac0c6af86c7d8a
dlee <dlee>
2006-10-27 18:48:52 +0000

    GS-2674 shutterfly

--- src/java/gs/web/promo/ShutterflyCardsController.java
+++ src/java/gs/web/promo/ShutterflyCardsController.java
@@ -1,26 +1,236 @@
 package gs.web.promo;
 
-import org.springframework.web.servlet.mvc.AbstractController;
+import gs.data.community.IUserDao;
+import gs.data.community.User;
+import gs.data.community.promo.IPromoDao;
+import gs.data.community.promo.Promo;
+import gs.data.community.promo.PromoCode;
+import gs.web.util.ReadWriteController;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * @author Chris Kimm <mailto:chriskimm@greatschools.net>
- *
- * This controller delivers three different views based on the following logic:
- *
- * <ol>
- * <li>Valid (members before 11/8/06) members who have not yet been sent a promo code
- *     are shown a success view and are sent an email with the promo code.</li>
- * <li>Members who have already received a promo code are shown an "already been
- *     redeemed" view</li>
- * <li>All other users are shown an "ineligible" view</li>
- * </ol>
+ *         <p/>
+ *         This controller delivers three different views based on the following logic:
+ *         <p/>
+ *         <ol>
+ *         <li>Valid (members before 11/8/06) members who have not yet been sent a promo code
+ *         are shown a success view and are sent an email with the promo code.</li>
+ *         <li>Members who have already received a promo code are shown an "already been
+ *         redeemed" view</li>
+ *         <li>All other users are shown an "ineligible" view</li>
+ *         </ol>
  */
-public class ShutterflyCardsController extends AbstractController {
-    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+public class ShutterflyCardsController extends AbstractController implements InitializingBean, ReadWriteController {
+
+    public static final String BEAN_ID = "/promo/shutterfly/cards.page";
+
+
+    protected static final String VIEW = "/promo/shutterfly/cards";
+    protected static final String PARAM_EMAIL = "email";
+
+    protected static final String IMAGE_LINK = "http://www.shutterfly.com/shop/product_c50285?cid=OMQ406GSCHL";
+
+    protected static final String MODEL_IMAGE_ALT = "imageAlt";
+    protected static final String MODEL_IMAGE_SRC = "imageSrc";
+
+
+    protected static final String ELIGIBLE_SRC = "/res/img/promo/shutterfly/eligible.jpg";
+    protected static final String INELIGIBLE_SRC = "/res/img/promo/shutterfly/ineligible.jpg";
+    protected static final String REDEEMED_SRC = "/res/img/promo/shutterfly/redeemed.jpg";
+
+    protected static final String ELIGIBLE_ALT = "Thank you for celebrating the holidays with Shutterfly! We have emailed you a promotional code good for 12 free 4x8 Shutterfly Holiday Cards. Hurry, this offer expires December 8, 2006.";
+    protected static final String INELIGIBLE_ALT = "Thank you for celebrating the holidays with Shutterfly! Unfortunately, this email address is ineligible for this offer, according to the terms of the offer outlined below.";
+    protected static final String REDEEMED_ALT = "Thank you for celebrating the holidays with Shutterfly! The email address you entered has already redeemed this offer. Limit one offer per customer.  See other ways Shutterfly can help you wrap up your holiday shopping!";
+
+    private static Promo PROMO;
+
+    private Log _log = LogFactory.getLog(getClass());
+
+    private IUserDao _userDao;
+    private IPromoDao _promoDao;
+    private JavaMailSender _mailSender;
+
+    public void afterPropertiesSet() throws Exception {
+        PROMO = _promoDao.findPromoById(1);
     }
+
+
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+                                                 HttpServletResponse response) throws Exception {
+
+        String email = request.getParameter(PARAM_EMAIL);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2006, Calendar.NOVEMBER, 8);
+
+
+        //Error checking
+        if (StringUtils.isBlank(email)) {
+            return processInelgibleRequest();
+        }
+
+        User user = _userDao.findUserFromEmail(email);
+
+        if (user == null) {
+            return processInelgibleRequest();
+        }
+
+        if (!user.getTimeAdded().before(calendar.getTime())) {
+            return processInelgibleRequest();
+        }
+
+
+        //start of promo processing
+        PromoCode promoCode = _promoDao.findPromoCode(user, PROMO);
+
+        if (null != promoCode) {
+            return processRequest(REDEEMED_ALT, REDEEMED_SRC, "redeemed code");
+        } else {
+            promoCode = _promoDao.assignPromoCode(user, PROMO);
+
+            //no more promo codes left
+            if (null == promoCode) {
+                _log.error("No more promo codes left for promo: " + PROMO.getName() + " user: " + email);                
+
+                return processRequest(REDEEMED_ALT, REDEEMED_SRC, "no more left");
+
+            } else {
+                //send email
+                try {
+                    _mailSender.send(createMessage(email, promoCode.getCode()));
+                } catch (MessagingException mess) {
+                    _log.warn("User " + email + " did not receive promo code: " + promoCode.getCode() , mess);
+                } catch (MailException me) {
+                    _log.warn("User " + email + " did not receive promo code: " + promoCode.getCode() , me);
+                }
+
+                return processRequest(ELIGIBLE_ALT, ELIGIBLE_SRC, "elgible");
+            }
+        }
+    }
+
+    private ModelAndView processInelgibleRequest() {
+        return processRequest(INELIGIBLE_ALT, INELIGIBLE_SRC, "inelgible");
+    }
+
+    private ModelAndView processRequest(String imageAlt, String imageSrc, String omnitureTracking) {
+        ModelAndView mv = new ModelAndView(VIEW);
+
+        mv.getModel().put(MODEL_IMAGE_ALT, imageAlt);
+        mv.getModel().put(MODEL_IMAGE_SRC, imageSrc);
+
+        return mv;
+    }
+
+    /**
+     * This is a utility method to created the <code>MimeMessage</code object from a stub
+     * MimeMessage.
+     *
+     * @return
+     * @throws javax.mail.MessagingException
+     */
+    MimeMessage createMessage(String email, String promoCode) throws MessagingException {
+
+        MimeMessageHelper helper = new MimeMessageHelper(_mailSender.createMimeMessage(), false, "UTF-8");
+        helper.setTo(email);
+        try {
+            helper.setFrom("beta@greatschools.net", "GreatSchools");
+        } catch (UnsupportedEncodingException uee) {
+            helper.setFrom("beta@greatschools.net");
+        }
+        helper.setSubject("Welcome to the GreatSchools Beta Group!");
+        helper.setSentDate(new Date());
+
+
+        String emailText = "<p>Thank you for celebrating the holidays with Shutterfly! Your promotional code good for 12 free 4x8 Shutterfly Holiday Cards is:</p>\n" +
+                "\n" +
+                "<p>$PROMO_CODE</p>\n" +
+                "\n" +
+                "<p><a href=\"http://www.shutterfly.com/greatschools?cid=OMQ406GSCHL\">Visit</a> Shutterfly today to enter your unique code!<p>\n" +
+                "\n" +
+                "<p>We wish you the best the season has to offer,</p>\n" +
+                "\n" +
+                "<p>- The Shutterfly and GreatSchools teams</p>\n" +
+                "\n" +
+                "<small>\n" +
+                "This special offer is only for GreatSchools.net users who subscribed to a GreatSchools newsletter before\n" +
+                "November 8, 2006. To receive this offer, user must have or create a valid Shutterfly account at <a\n" +
+                "    href=\"http://www.shutterfly.com/greatschools?cid=OMQ406GSCHL\">http://www.shutterfly.com/greatschools</a>. To\n" +
+                "redeem, a unique promotional code must be inserted and used by December 8, 2006. Credit cannot be transferred to\n" +
+                "other products or another account and cannot be combined with other offers, discounts or promotions. Users may\n" +
+                "order additional holiday cards at their own expense. Shipping charges will apply to any card order. Limit one\n" +
+                "per person. Shutterfly reserves the right to modify this offer should it be compromised in any manner including,\n" +
+                "but not limited to, fraudulent activity.\n" +
+                "</small>";
+
+        emailText = emailText.replaceAll("\\$PROMO_CODE", promoCode);
+
+        helper.setText(emailText, true);
+        return helper.getMimeMessage();
+    }
+
+    public IUserDao getUserDao() {
+        return _userDao;
+    }
+
+    public void setUserDao(IUserDao userDao) {
+        _userDao = userDao;
+    }
+
+    public IPromoDao getPromoDao() {
+        return _promoDao;
+    }
+
+    public void setPromoDao(IPromoDao promoDao) {
+        _promoDao = promoDao;
+    }
+
+
+    public JavaMailSender getMailSender() {
+        return _mailSender;
+    }
+
+    public void setMailSender(JavaMailSender mailSender) {
+        _mailSender = mailSender;
+    }
+
+    /*
+
+<p>Thank you for celebrating the holidays with Shutterfly! Your promotional code good for 12 free 4x8 Shutterfly Holiday Cards is:</p>
+
+<p>$PROMO_CODE</p>
+
+<p><a href="http://www.shutterfly.com/greatschools?cid=OMQ406GSCHL">Visit</a> Shutterfly today to enter your unique code!<p>
+
+<p>We wish you the best the season has to offer,</p>
+
+<p>- The Shutterfly and GreatSchools teams</p>
+
+<small>
+This special offer is only for GreatSchools.net users who subscribed to a GreatSchools newsletter before
+November 8, 2006. To receive this offer, user must have or create a valid Shutterfly account at <a
+    href="http://www.shutterfly.com/greatschools?cid=OMQ406GSCHL">http://www.shutterfly.com/greatschools</a>. To
+redeem, a unique promotional code must be inserted and used by December 8, 2006. Credit cannot be transferred to
+other products or another account and cannot be combined with other offers, discounts or promotions. Users may
+order additional holiday cards at their own expense. Shipping charges will apply to any card order. Limit one
+per person. Shutterfly reserves the right to modify this offer should it be compromised in any manner including,
+but not limited to, fraudulent activity.
+</small>
+     */
 }
--- src/test/gs/web/promo/ShutterflyCardsControllerTest.java
+++ src/test/gs/web/promo/ShutterflyCardsControllerTest.java
@@ -1,22 +1,185 @@
 package gs.web.promo;
 
+import gs.data.community.IUserDao;
+import gs.data.community.User;
+import gs.data.community.promo.IPromoDao;
+import gs.data.community.promo.Promo;
+import gs.data.community.promo.PromoCode;
 import gs.web.BaseControllerTestCase;
+import gs.web.util.MockJavaMailSender;
+import org.easymock.MockControl;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.mail.Address;
+import javax.mail.internet.MimeMessage;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author Chris Kimm <mailto:chriskimm@greatschools.net>
  */
 public class ShutterflyCardsControllerTest extends BaseControllerTestCase {
+    private ShutterflyCardsController _controller;
+    private MockJavaMailSender _sender;
 
     public void setUp() throws Exception {
         super.setUp();
+        _controller = (ShutterflyCardsController) getApplicationContext().getBean(ShutterflyCardsController.BEAN_ID);
+        _sender = new MockJavaMailSender();
+        _sender.setHost("mail.greatschools.net");
+        _controller.setMailSender(_sender);
+
     }
 
-    public void testEligibleUserNoPromo() throws Exception {
+    public void testUserMoreRecentJoinDate() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2006, Calendar.NOVEMBER, 9);
+        Date joinDate = calendar.getTime();
+
+        testPromoEligibilityByJoinDate(joinDate,
+                ShutterflyCardsController.INELIGIBLE_ALT,
+                ShutterflyCardsController.INELIGIBLE_SRC);
     }
+
+    public void testUserSameDayJoinDate() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2006, Calendar.NOVEMBER, 8);
+        Date joinDate = calendar.getTime();
+
+        testPromoEligibilityByJoinDate(joinDate,
+                ShutterflyCardsController.INELIGIBLE_ALT,
+                ShutterflyCardsController.INELIGIBLE_SRC);
+    }
+
+    public void testUserPriorToCutOffDate() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2006, Calendar.NOVEMBER, 7);
+        Date joinDate = calendar.getTime();
+
+        testPromoEligibilityByJoinDate(joinDate,
+                ShutterflyCardsController.ELIGIBLE_ALT,
+                ShutterflyCardsController.ELIGIBLE_SRC);
+
+        List messages = _sender.getSentMessages();
+        assertEquals(1, messages.size());
+
+        //test user got the email with promocode
+        MimeMessage msg = (MimeMessage) messages.get(0);
+
+        Address[] to = msg.getRecipients(MimeMessage.RecipientType.TO);
+        assertNotNull("Empty to field", to);
+        assertEquals("More than 1 to address", 1, to.length);
+        assertEquals("To field does not equal user's email", "user@greatschools.net", to[0].toString());
+
+        Address[] from = msg.getFrom();
+        assertNotNull("Empty from field", from);
+        assertEquals("More than 1 in from", 1, from.length);
+        assertEquals("From field not right", "GreatSchools <beta@greatschools.net>", from[0].toString());
+
+        assertEquals("Welcome to the GreatSchools Beta Group!", msg.getSubject());
+
+        String content = msg.getContent().toString();
+        assertTrue(content.indexOf("ABCDEFGHIJKLMN") > -1);
+        assertTrue(content.indexOf("http://www.shutterfly.com/greatschools?cid=OMQ406GSCHL") > -1);
+        assertTrue(content.indexOf("$PROMO_CODE") == -1);
+    }
+
+
+    //user has never received a promo before
+    private void testPromoEligibilityByJoinDate(Date joinDate,
+                                                String expectedImageAlt,
+                                                String expectedImageSrc) throws Exception {
+        MockHttpServletRequest request = getRequest();
+        request.addParameter(ShutterflyCardsController.PARAM_EMAIL, "user@greatschools.net");
+
+        User user = new User();
+        user.setEmail("user@greatschools.net");
+        user.setTimeAdded(joinDate);
+
+        PromoCode promoCode = new PromoCode("ABCDEFGHIJKLMN", new Promo());
+
+        _controller.setUserDao(getMockedUserDao(user));
+        _controller.setPromoDao(getMockedPromoDao(null, promoCode));
+
+        ModelAndView mv = _controller.handleRequestInternal(request, getResponse());
+
+        assertEquals("/promo/shutterfly/cards", mv.getViewName());
+        assertEquals(expectedImageAlt, (String) mv.getModel().get(ShutterflyCardsController.MODEL_IMAGE_ALT));
+        assertEquals(expectedImageSrc, (String) mv.getModel().get(ShutterflyCardsController.MODEL_IMAGE_SRC));
+    }
+
+
+    public void testUserWithRedeemedPromoCode() throws Exception {
+        MockHttpServletRequest request = getRequest();
+        request.addParameter(ShutterflyCardsController.PARAM_EMAIL, "user@greatschools.net");
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2006, Calendar.NOVEMBER, 7);
+        Date joinDate = calendar.getTime();
+
+        User user = new User();
+        user.setEmail("user@greatschools.net");
+        user.setTimeAdded(joinDate);
+
+        PromoCode promoCode = new PromoCode("code", new Promo());
+        promoCode.setMember(user);
+        promoCode.setAssigned(new Date());
+
+        _controller.setUserDao(getMockedUserDao(user));
+        _controller.setPromoDao(getMockedPromoDao(promoCode, null));
+
+        ModelAndView mv = _controller.handleRequestInternal(request, getResponse());
+        assertEquals("/promo/shutterfly/cards", mv.getViewName());
+
+        assertEquals(ShutterflyCardsController.REDEEMED_ALT,
+                (String) mv.getModel().get(ShutterflyCardsController.MODEL_IMAGE_ALT));
+        assertEquals(ShutterflyCardsController.REDEEMED_SRC,
+                (String) mv.getModel().get(ShutterflyCardsController.MODEL_IMAGE_SRC));
+
+    }
+
+
+    /**
+     * Return a mocked UserDao that returns the given user
+     */
+    private IUserDao getMockedUserDao(final User user) {
+        MockControl mockUserDaoControl = MockControl.createControl(IUserDao.class);
+        IUserDao userDao = (IUserDao) mockUserDaoControl.getMock();
+        userDao.findUserFromEmail(user.getEmail());
+        mockUserDaoControl.setReturnValue(user);
+        mockUserDaoControl.replay();
+
+        return userDao;
+    }
+
+    /**
+     * @param foundPromoCode    PromoCode that is returned by IPromoDao#findPromoCode
+     * @param assignedPromoCode PromoCode that is returned by IPromoDao#assignPromoCode
+     * @return
+     * @see gs.data.community.promo.IPromoDao#findPromoCode(gs.data.community.User,gs.data.community.promo.Promo)
+     * @see gs.data.community.promo.IPromoDao#assignPromoCode(gs.data.community.User,gs.data.community.promo.Promo)
+     */
+    private IPromoDao getMockedPromoDao(final PromoCode foundPromoCode, final PromoCode assignedPromoCode) {
+
+        MockControl mockPromoDaoControl = MockControl.createControl(IPromoDao.class);
+        IPromoDao promoDao = (IPromoDao) mockPromoDaoControl.getMock();
+        promoDao.findPromoCode(null, null);
+        mockPromoDaoControl.setDefaultReturnValue(foundPromoCode);
+        promoDao.assignPromoCode(null, null);
+        mockPromoDaoControl.setDefaultReturnValue(assignedPromoCode);
+        mockPromoDaoControl.replay();
+
+        return promoDao;
+    }
+
 
     public void testEligibleUserWithPromo() throws Exception {
+
     }
 
     public void testIneligibleUser() throws Exception {
+
     }
 }
