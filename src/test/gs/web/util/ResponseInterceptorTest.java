7342ab55136c00e999ce4ee26d1b6de796f1ab3b
cpickslay <cpickslay>
2007-01-02 18:17:39 +0000

    GS-2904 added test for cobrand cookie

--- src/test/gs/web/util/ResponseInterceptorTest.java
+++ src/test/gs/web/util/ResponseInterceptorTest.java
@@ -1,6 +1,9 @@
 package gs.web.util;
 
 import gs.web.BaseControllerTestCase;
+import gs.web.util.context.SessionContext;
+import gs.web.util.context.SessionContextInterceptor;
+import gs.web.util.context.SessionContextUtil;
 
 import javax.servlet.http.Cookie;
 
@@ -43,5 +46,34 @@ public class ResponseInterceptorTest extends BaseControllerTestCase {
         assertTrue(response.containsHeader(ResponseInterceptor.HEADER_CACHE_CONTROL));
         assertTrue(response.containsHeader(ResponseInterceptor.HEADER_PRAGMA));
         assertTrue(response.containsHeader(ResponseInterceptor.HEADER_EXPIRES));
+    }
+
+
+    // TODO: this test is fragile. shouldn't depend on sfgate being non-framed cobrand. Mock SessionContext
+    public void testPreHandleSetsCobrandCookie() throws Exception {
+        MockHttpServletRequest request = getRequest();
+        MockHttpServletResponse response = getResponse();
+
+        String requestedServer = "sfgate.greatschools.net";
+        request.setServerName(requestedServer);
+
+        // set up SessionContext and PageHelper
+        SessionContextUtil ctxUtil = (SessionContextUtil) getApplicationContext().getBean(SessionContextUtil.BEAN_ID);
+        SessionContextInterceptor sessionContextInterceptor = new SessionContextInterceptor();
+        sessionContextInterceptor.setSessionContextUtil(ctxUtil);
+        sessionContextInterceptor.preHandle(request, response, null);
+
+        assertTrue("preHandle should always return true", _interceptor.preHandle(request, response, null));
+
+        Cookie cobrandCookie = null;
+        Cookie cookies[] = response.getCookies();
+        assertNotNull("Expected to find cookies in response", cookies);
+        for (int i = 0; i < cookies.length; i++) {
+            if (ResponseInterceptor.COBRAND_COOKIE.equals(cookies[i].getName())) {
+                cobrandCookie = cookies[i];
+                break;
+            }
+        }
+        assertEquals("Unexpected cobrand cookie value", requestedServer, cobrandCookie.getValue());
     }
 }
