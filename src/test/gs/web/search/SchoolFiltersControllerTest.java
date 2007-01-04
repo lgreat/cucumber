f56d50d934f4dc172008d3c6ce2e89732616f8ec
thuss <thuss>
2007-01-04 01:38:49 +0000

    GS-2917

--- src/test/gs/web/search/SchoolFiltersControllerTest.java
+++ src/test/gs/web/search/SchoolFiltersControllerTest.java
@@ -4,7 +4,6 @@ import gs.web.BaseControllerTestCase;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
-import java.net.URLEncoder;
 
 /**
  * @author Chris Kimm <mailto:chriskimm@greatschools.net>
@@ -18,9 +17,9 @@ public class SchoolFiltersControllerTest extends BaseControllerTestCase {
         ModelAndView mAndV_noGL =
                 controller.handleRequestInternal(getRequest(), getResponse());
         List levels_noGL = (List)mAndV_noGL.getModel().get("levels");
-        assertEquals("<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e\">Elementary</a>", levels_noGL.get(0));
-        assertEquals("<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=m\">Middle</a>", levels_noGL.get(1));
-        assertEquals("<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=h\">High</a>", levels_noGL.get(2));
+        assertEquals("<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e\">Elementary</a>", levels_noGL.get(0));
+        assertEquals("<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=m\">Middle</a>", levels_noGL.get(1));
+        assertEquals("<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=h\">High</a>", levels_noGL.get(2));
 
 
         getRequest().addParameter(SchoolFiltersController.PARAM_LEVEL_CODE, "e");
@@ -30,14 +29,14 @@ public class SchoolFiltersControllerTest extends BaseControllerTestCase {
         ModelAndView mAndV =
                 controller.handleRequestInternal(getRequest(), getResponse());
         List levels = (List)mAndV.getModel().get("levels");
-        assertEquals("Elementary (<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=m&lc=h\">remove</a>)", levels.get(0));
-        assertEquals("Middle (<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e&lc=h\">remove</a>)", levels.get(1));
-        assertEquals("High (<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e&lc=m\">remove</a>)", levels.get(2));
+        assertEquals("Elementary (<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=m&amp;lc=h\">remove</a>)", levels.get(0));
+        assertEquals("Middle (<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e&amp;lc=h\">remove</a>)", levels.get(1));
+        assertEquals("High (<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e&amp;lc=m\">remove</a>)", levels.get(2));
 
         List types = (List)mAndV.getModel().get("types");
-        assertEquals("<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e&lc=m&lc=h&st=public\">Public</a>", types.get(0));
-        assertEquals("<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e&lc=m&lc=h&st=charter\">Charter</a>", types.get(1));
-        assertEquals("<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e&lc=m&lc=h&st=private\">Private</a>", types.get(2));
+        assertEquals("<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e&amp;lc=m&amp;lc=h&amp;st=public\">Public</a>", types.get(0));
+        assertEquals("<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e&amp;lc=m&amp;lc=h&amp;st=charter\">Charter</a>", types.get(1));
+        assertEquals("<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e&amp;lc=m&amp;lc=h&amp;st=private\">Private</a>", types.get(2));
     }
 
     public void testTypes() throws Exception {
@@ -46,9 +45,9 @@ public class SchoolFiltersControllerTest extends BaseControllerTestCase {
         getRequest().setQueryString("c=school&state=ca&city=South%20San%20Francisco&lc=e");
         ModelAndView mAndV = controller.handleRequestInternal(getRequest(), getResponse());
         List types = (List)mAndV.getModel().get("types");
-        assertEquals("<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e&st=public\">Public</a>", types.get(0));
-        assertEquals("<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e&st=charter\">Charter</a>", types.get(1));
-        assertEquals("<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e&st=private\">Private</a>", types.get(2));
+        assertEquals("<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e&amp;st=public\">Public</a>", types.get(0));
+        assertEquals("<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e&amp;st=charter\">Charter</a>", types.get(1));
+        assertEquals("<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e&amp;st=private\">Private</a>", types.get(2));
 
         getRequest().setQueryString("c=school&state=ca&city=South%20San%20Francisco&lc=e&st=public&st=charter&st=private");
         getRequest().addParameter("st", "public");
@@ -56,9 +55,9 @@ public class SchoolFiltersControllerTest extends BaseControllerTestCase {
         getRequest().addParameter("st", "private");
         ModelAndView mAndV2 = controller.handleRequestInternal(getRequest(), getResponse());
         List types2 = (List)mAndV2.getModel().get("types");
-        assertEquals("Public (<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e&st=charter&st=private\">remove</a>)", types2.get(0));
-        assertEquals("Charter (<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e&st=public&st=private\">remove</a>)", types2.get(1));
-        assertEquals("Private (<a href=\"/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e&st=public&st=charter\">remove</a>)", types2.get(2));
+        assertEquals("Public (<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e&amp;st=charter&amp;st=private\">remove</a>)", types2.get(0));
+        assertEquals("Charter (<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e&amp;st=public&amp;st=private\">remove</a>)", types2.get(1));
+        assertEquals("Private (<a href=\"/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e&amp;st=public&amp;st=charter\">remove</a>)", types2.get(2));
     }
 
     public void testContext() throws Exception {
@@ -68,9 +67,9 @@ public class SchoolFiltersControllerTest extends BaseControllerTestCase {
         getRequest().setContextPath("/gs-web");
         ModelAndView mAndV = controller.handleRequestInternal(getRequest(), getResponse());
         List types = (List)mAndV.getModel().get("types");
-        assertEquals("<a href=\"/gs-web/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e&st=public\">Public</a>", types.get(0));
-        assertEquals("<a href=\"/gs-web/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e&st=charter\">Charter</a>", types.get(1));
-        assertEquals("<a href=\"/gs-web/schools.page?c=school&state=ca&city=South%20San%20Francisco&lc=e&st=private\">Private</a>", types.get(2));
+        assertEquals("<a href=\"/gs-web/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e&amp;st=public\">Public</a>", types.get(0));
+        assertEquals("<a href=\"/gs-web/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e&amp;st=charter\">Charter</a>", types.get(1));
+        assertEquals("<a href=\"/gs-web/schools.page?c=school&amp;state=ca&amp;city=South%20San%20Francisco&amp;lc=e&amp;st=private\">Private</a>", types.get(2));
 
     }
 
@@ -81,9 +80,9 @@ public class SchoolFiltersControllerTest extends BaseControllerTestCase {
         getRequest().setParameter("dest", "/xxx.page");
         ModelAndView mAndV = controller.handleRequestInternal(getRequest(), getResponse());
         List types = (List)mAndV.getModel().get("types");
-        assertEquals("<a href=\"/xxx.page?state=CA&city=X&st=public\">Public</a>", types.get(0));
-        assertEquals("<a href=\"/xxx.page?state=CA&city=X&st=charter\">Charter</a>", types.get(1));
-        assertEquals("<a href=\"/xxx.page?state=CA&city=X&st=private\">Private</a>", types.get(2));
+        assertEquals("<a href=\"/xxx.page?state=CA&amp;city=X&amp;st=public\">Public</a>", types.get(0));
+        assertEquals("<a href=\"/xxx.page?state=CA&amp;city=X&amp;st=charter\">Charter</a>", types.get(1));
+        assertEquals("<a href=\"/xxx.page?state=CA&amp;city=X&amp;st=private\">Private</a>", types.get(2));
 
     }
 
