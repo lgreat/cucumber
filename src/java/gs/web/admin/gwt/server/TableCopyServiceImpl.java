2b7b99da009d22e7f74d81e8ffa796b24887c51b
cpickslay <cpickslay>
2007-01-16 21:27:29 +0000

    GS-2686 re-implemented without initializer code, which was confusing the serializer

--- src/java/gs/web/admin/gwt/server/TableCopyServiceImpl.java
+++ src/java/gs/web/admin/gwt/server/TableCopyServiceImpl.java
@@ -4,9 +4,27 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import gs.web.admin.gwt.client.TableCopyService;
 import gs.web.admin.gwt.client.TableData;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class TableCopyServiceImpl extends RemoteServiceServlet implements TableCopyService {
+    List gsList = new ArrayList();
+    List azList = new ArrayList();
+    TableData tableData = new TableData();
+
+
+    public TableCopyServiceImpl() {
+        gsList.add("table1");
+        gsList.add("table2");
+        azList.add("az_table1");
+        azList.add("az_table2");
+        tableData.addDatabase("gs_schooldb", gsList);
+        tableData.addDatabase("_az", azList);
+    }
+
     public TableData getTables(TableData.DatabaseDirection direction) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return tableData;
     }
 }
