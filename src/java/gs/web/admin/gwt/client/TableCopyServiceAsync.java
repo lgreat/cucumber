023568c7aafb7a801edf64f1543f6f9535134da1
cpickslay <cpickslay>
2007-01-16 21:26:30 +0000

    GS-2686 renamed variable

--- src/java/gs/web/admin/gwt/client/TableCopyServiceAsync.java
+++ src/java/gs/web/admin/gwt/client/TableCopyServiceAsync.java
@@ -4,5 +4,5 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 
 
 public interface TableCopyServiceAsync {
-    void getTables(TableData.DatabaseDirection direction, AsyncCallback callback);
+    void getTables(TableData.DatabaseDirection direction, AsyncCallback async);
 }
