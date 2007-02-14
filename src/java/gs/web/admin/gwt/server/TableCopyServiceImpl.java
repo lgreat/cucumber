b2049d67c288f6c9d48ce7df0fb8b4736a77d3c2
cpickslay <cpickslay>
2007-02-14 00:17:27 +0000

    GS-3018 added code to check wiki before copying

--- src/java/gs/web/admin/gwt/server/TableCopyServiceImpl.java
+++ src/java/gs/web/admin/gwt/server/TableCopyServiceImpl.java
@@ -26,39 +26,21 @@ public class TableCopyServiceImpl extends RemoteServiceServlet implements TableC
     private static final Log _log = LogFactory.getLog(TableCopyServiceImpl.class);
     private JdbcOperations _jdbcContext;
     private SessionFactory _sessionFactory;
+    private HttpClient _httpClient = new HttpClient();
+    private GetMethod _request = new GetMethod(TABLES_TO_MOVE_URL);
+
     public static final String DATABASE_COLUMN = "table_schema";
     public static final String TABLE_COLUMN = "table_name";
     public static final String TABLE_LIST_QUERY = "select " + DATABASE_COLUMN + ", " + TABLE_COLUMN + " from information_schema.tables " +
             "where table_schema not in ('information_schema', 'mysql') " +
             "order by table_schema, table_name;";
     public static final String COPY_TABLES_COMMAND = "/usr2/sites/main.dev/scripts/sysadmin/database/dumpcopy --yes ";
-    static final String TABLE_COPY_FAILURE_HEADER = "The following table(s) failed to copy:\n<br>";
+    public static final String TABLE_COPY_FAILURE_HEADER = "The following table(s) failed to copy:\n<br>";
     public static final String TABLES_TO_MOVE_URL = "http://wiki.greatschools.net/bin/view/Greatschools/TableToMove";
-    private HttpClient _httpClient = new HttpClient();
-    private GetMethod _request = new GetMethod(TABLES_TO_MOVE_URL);
     public static final String TABLES_FOUND_IN_TABLES_TO_MOVE_ERROR = "The following tables have already been copied.\n" +
             "Please check http://wiki.greatschools.net/bin/view/Greatschools/TableToMove before proceeding\n";
     public static final String TABLES_NOT_YET_MOVED_ERROR = "The following tables have not yet been copied from live -> dev.\n" +
             "Please check http://wiki.greatschools.net/bin/view/Greatschools/TableToMove before proceeding\n";
-
-    public void setJdbcContext(JdbcOperations context) {
-        this._jdbcContext = context;
-    }
-
-    public JdbcOperations getJdbcContext() {
-        if (_jdbcContext == null) {
-            _jdbcContext = new JdbcTemplate(new ThreadLocalHibernateDataSource(getSessionFactory()));
-        }
-        return _jdbcContext;
-    }
-
-    private SessionFactory getSessionFactory() {
-        return _sessionFactory;
-    }
-
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        _sessionFactory = sessionFactory;
-    }
 
     public TableData getTables(TableData.DatabaseDirection direction) {
 //        return populateTestData();
@@ -85,17 +67,30 @@ public class TableCopyServiceImpl extends RemoteServiceServlet implements TableC
         _log.info("Copying tables");
         long start = System.currentTimeMillis();
 
-        
+        // check status of tables on wiki
+        try {
+            String copyStatus = checkWikiForSelectedTables(direction, Arrays.asList(tableList));
+            if (copyStatus != null) {
+                ServiceException exception = new ServiceException(copyStatus);
+                throw exception;
+            }
+        } catch (IOException e) {
+            _log.error("Error getting wiki status", e);
+            throw new ServiceException("Error getting wiki status: " + e.getMessage());
+        }
+
+        // execute copy command
         String copyCommand = generateCopyCommand(direction, Arrays.asList(tableList));
         String copyOutput = null;
         try {
             copyOutput = executeCopyCommand(copyCommand);
             _log.info("Copy command output: " + copyOutput);
         } catch (IOException e) {
-            _log.error("Error executing dumpcopy: " + e.getMessage());
+            _log.error("Error executing dumpcopy", e);
             throw new ServiceException("Error copying tables: " + e.getMessage());
         }
 
+        // process comand output
         String errorText = parseCommandOutput(copyOutput);
         if (errorText != null) {
             _log.error("Error executing dumpcopy: " + errorText);
@@ -185,20 +180,6 @@ public class TableCopyServiceImpl extends RemoteServiceServlet implements TableC
         return error;
     }
 
-    private TableData populateTestData() {
-        List gsList = new ArrayList();
-        List azList = new ArrayList();
-        TableData tableData = new TableData();
-        gsList.add("table1");
-        gsList.add("table2");
-        azList.add("az_table1");
-        azList.add("az_table2");
-        tableData.addDatabase("gs_schooldb", gsList);
-        tableData.addDatabase("_az", azList);
-        tableData.setDirection(TableData.DEV_TO_STAGING);
-        return tableData;
-    }
-
     public String getDatabaseTableMatcher(String database, String table) {
         // match database and table in separate cells, only on the same line
         return "(?m)^.*" + database + ".*</td>\\s*<td.*" + table + ".*$";
@@ -271,6 +252,25 @@ public class TableCopyServiceImpl extends RemoteServiceServlet implements TableC
         return null;
     }
 
+    public void setJdbcContext(JdbcOperations context) {
+        this._jdbcContext = context;
+    }
+
+    public JdbcOperations getJdbcContext() {
+        if (_jdbcContext == null) {
+            _jdbcContext = new JdbcTemplate(new ThreadLocalHibernateDataSource(getSessionFactory()));
+        }
+        return _jdbcContext;
+    }
+
+    private SessionFactory getSessionFactory() {
+        return _sessionFactory;
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        _sessionFactory = sessionFactory;
+    }
+
     public void setHttpClient(HttpClient httpClient) {
         _httpClient = httpClient;
     }
@@ -283,4 +283,17 @@ public class TableCopyServiceImpl extends RemoteServiceServlet implements TableC
         _request = request;
     }
 
+    private TableData populateTestData() {
+        List gsList = new ArrayList();
+        List azList = new ArrayList();
+        TableData tableData = new TableData();
+        gsList.add("table1");
+        gsList.add("table2");
+        azList.add("az_table1");
+        azList.add("az_table2");
+        tableData.addDatabase("gs_schooldb", gsList);
+        tableData.addDatabase("_az", azList);
+        tableData.setDirection(TableData.DEV_TO_STAGING);
+        return tableData;
+    }
 }
