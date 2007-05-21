ba3efad23b5d95b5f0e7870dc3ff4eb70904c854
chriskimm <chriskimm>
2007-05-21 23:03:14 +0000

    GS-3410

--- src/webapp/res/js/parentReviewForm.js
+++ src/webapp/res/js/parentReviewForm.js
@@ -24,18 +24,17 @@ function setQuality(title, q) {
     savedTitle = title;
     quality = q;
 
-    /** For some reason a loop wasn't working to set these style attributes. */ 
-    document.getElementById('q_1').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px 0px;";
-    document.getElementById('q_2').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px 0px;";
-    document.getElementById('q_3').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px 0px;";
-    document.getElementById('q_4').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px 0px;";
-    document.getElementById('q_5').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px 0px;";
+    /** For some reason a loop wasn't working to set these style attributes. */
+    document.getElementById('q_1').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
+    document.getElementById('q_2').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
+    document.getElementById('q_3').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
+    document.getElementById('q_4').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
+    document.getElementById('q_5').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
 
     if (q > 0) { document.getElementById('q_1').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
     if (q > 1) { document.getElementById('q_2').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
     if (q > 2) { document.getElementById('q_3').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
     if (q > 3) { document.getElementById('q_4').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
     if (q > 4) { document.getElementById('q_5').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
-
     return false;
 }
