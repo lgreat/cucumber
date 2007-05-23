6e47c214e27a80dcfe52e4747459073eafe6edeb
chriskimm <chriskimm>
2007-05-23 01:13:07 +0000

    GS-3410

--- src/webapp/res/js/parentReviewForm.js
+++ src/webapp/res/js/parentReviewForm.js
@@ -1,5 +1,22 @@
-function setRatingTitle(text) {
+function setStars(rating) {
+    /** For some reason a loop wasn't working to set these style attributes. */
+    document.getElementById('q_1').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
+    document.getElementById('q_2').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
+    document.getElementById('q_3').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
+    document.getElementById('q_4').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
+    document.getElementById('q_5').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
+
+    if (rating > 0) { document.getElementById('q_1').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
+    if (rating > 1) { document.getElementById('q_2').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
+    if (rating > 2) { document.getElementById('q_3').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
+    if (rating > 3) { document.getElementById('q_4').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
+    if (rating > 4) { document.getElementById('q_5').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
+    return false;
+}
+
+function setRatingTitle(text, q) {
     document.getElementById('ratingTitle').innerHTML = text;
+    setStars(q);
     return false;
 }
 
@@ -10,7 +27,7 @@ function resetRatingTitle() {
     if (savedTitle == undefined) {
         savedTitle = "Rate this school";
     }
-    setRatingTitle(savedTitle);
+    setRatingTitle(savedTitle, quality);
 }
 
 function setSubmitFields() {
@@ -23,22 +40,29 @@ function setSubmitFields() {
     }
 }
 
-function setQuality(title, q) {
-
+function saveQuality(title, q) {
     savedTitle = title;
     quality = q;
-
-    /** For some reason a loop wasn't working to set these style attributes. */
-    document.getElementById('q_1').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
-    document.getElementById('q_2').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
-    document.getElementById('q_3').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
-    document.getElementById('q_4').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
-    document.getElementById('q_5').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
-
-    if (q > 0) { document.getElementById('q_1').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
-    if (q > 1) { document.getElementById('q_2').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
-    if (q > 2) { document.getElementById('q_3').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
-    if (q > 3) { document.getElementById('q_4').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
-    if (q > 4) { document.getElementById('q_5').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
+    setStars(q);
     return false;
+}
+
+function countWords(w){
+    var maxwords = 150;
+    var y = w.value;
+    var r = 0;
+    var a = y.replace('\n',' ');
+    a = y.replace('\t',' ');
+    var z;
+    for (z = 0; z < a.length; z++) {
+        if (a.charAt(z) == ' ' && a.charAt(z-1) != ' ') { r++; }
+ 	    if (r > maxwords) break;
+    }
+
+    if (r > maxwords) {
+        w.value = w.value.substr(0, z);
+        alert("Please keep your review to "+maxwords+" words or less.");
+        return false;
+    }
+    return true;
 }
