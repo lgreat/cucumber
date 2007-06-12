a039c9ab943ce3b2ea2f070aa489b7449d69d859
chriskimm <chriskimm>
2007-06-12 00:09:40 +0000

    GS-3475

---
+++ src/java/gs/web/util/IsValidEmailController.java
@@ -0,0 +1,34 @@
+package gs.web.util;
+
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+import gs.data.util.email.EmailUtils;
+
+/**
+ * This controller provides a web interface to gs.data.util.email.EmailUtils.isValidEmail();
+ *
+ * @author Chris Kimm <mailto:chriskimm@greatschools.net>
+ */
+public class IsValidEmailController implements Controller {
+
+    /** Spring BEAN id */
+    public static final String BEAN_ID = "/util/isValidEmail.page";
+
+    /**
+     * @see gs.data.util.email.EmailUtils
+     */
+    public ModelAndView handleRequest(HttpServletRequest request,
+                                      HttpServletResponse response) throws Exception {
+        String email = request.getParameter("email");
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.print(EmailUtils.isValidEmail(email));
+        out.flush();
+        return null;
+    }
+}
