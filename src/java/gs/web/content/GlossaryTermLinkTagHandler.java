a6b7d951c83c3a15fd447341569ed23776babb84
cpickslay <cpickslay>
2007-06-21 19:23:36 +0000

    GS-3474 added link tag for glossary terms

---
+++ src/java/gs/web/content/GlossaryTermLinkTagHandler.java
@@ -0,0 +1,21 @@
+package gs.web.content;
+
+import gs.web.jsp.link.LinkTagHandler;
+import gs.web.util.UrlBuilder;
+import gs.web.util.context.SessionContext;
+import gs.data.state.State;
+
+public class GlossaryTermLinkTagHandler extends LinkTagHandler {
+    private String _termId;
+
+    protected UrlBuilder createUrlBuilder() {
+        SessionContext sessionContext = getSessionContext();
+        State state = sessionContext.getStateOrDefault();
+
+        return new UrlBuilder(UrlBuilder.GLOSSARY_TERM, state, _termId);
+    }
+
+    public void setTermId(String termId) {
+        _termId = termId;
+    }
+}
