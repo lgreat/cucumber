1e1ac43f45777a68ae2eb08b84098c138363773a
dlee <dlee>
2007-07-31 17:28:55 +0000

    GS-3694 move some js into file

--- src/webapp/res/js/parentReviewForm.js
+++ src/webapp/res/js/parentReviewForm.js
@@ -66,3 +66,57 @@ function countWords(w){
     }
     return true;
 }
+
+//Uses yui to handle ajax call.  Callback object initialized in tag file and used in processResult
+var AjaxObject = {
+    handleSuccess:function(o){
+        this.processResult(o);
+    },
+
+    handleFailure:function(o){
+        var ul = this.getErrorNode();
+        var li = document.createElement('li');
+        var liText = document.createTextNode('Sorry your request could not be handled at this time. Please try again later.');
+        li.appendChild(liText);
+        ul.appendChild(li);
+    },
+
+    processResult:function(o){
+        var ul = this.getErrorNode();
+        var jsonResponse = o.responseText.parseJSON();
+        if (jsonResponse.status) {
+            document.getElementById('submitReview').disabled = true;
+            var email = document.getElementById('reviewEmail').value;
+            var successUrl = o.argument[0] + '&email='+email;
+            alert(successUrl);
+            showPopWin(successUrl, 344, 362, null, 'ratingPage');
+        } else {
+            for (var i=0;i<jsonResponse.errors.length;i++) {
+                var li = document.createElement('li');
+                var liText = document.createTextNode(jsonResponse.errors[i]);
+                li.appendChild(liText);
+                ul.appendChild(li);
+            }
+        }
+    },
+
+    startRequest:function(url,callback) {
+        setSubmitFields();
+        var formObject = document.getElementById('frmPRModule');
+        YAHOO.util.Connect.setForm(formObject);
+        YAHOO.util.Connect.asyncRequest('POST', url, callback, "");
+    },
+
+    getErrorNode:function() {
+        var ul = document.getElementById('frmErrors');
+        if (ul==null) {
+            ul = document.createElement('ul');
+            ul.id = "frmErrors";
+            var form = document.getElementById('frmPRModule');
+            ul = form.parentNode.insertBefore(ul,form);
+        } else {
+            while ( ul.hasChildNodes() ) { ul.removeChild(ul.firstChild)};
+        }
+        return ul;
+    }
+};
