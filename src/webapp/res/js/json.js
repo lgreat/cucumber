aca41f37c1ccdb9c288fb09bc53edc2edc3538da
dlee <dlee>
2007-08-02 23:25:37 +0000

    GS-3694 run through jsmin

--- src/webapp/res/js/json.js
+++ src/webapp/res/js/json.js
@@ -1,4 +1,3 @@
-
 if(!Object.prototype.toJSONString){(function(s){var m={'\b':'\\b','\t':'\\t','\n':'\\n','\f':'\\f','\r':'\\r','"':'\\"','\\':'\\\\'};s.parseJSON=function(filter){var j;function walk(k,v){var i;if(v&&typeof v==='object'){for(i in v){if(v.hasOwnProperty(i)){v[i]=walk(i,v[i]);}}}
 return filter(k,v);}
 if(/^[,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]*$/.test(this.replace(/\\./g,'@').replace(/"[^"\\\n\r]*"/g,''))){j=eval('('+this+')');return typeof filter==='function'?walk('',j):j;}
