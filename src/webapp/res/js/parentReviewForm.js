f8328be6205c350d2e9d09467c65776a5bc9dfd3
dlee <dlee>
2007-08-14 19:09:31 +0000

    GS-3753 updates to parent review module.  safari fix and css rewrite

--- src/webapp/res/js/parentReviewForm.js
+++ src/webapp/res/js/parentReviewForm.js
@@ -1,50 +1,40 @@
-function setStars(rating) {
-    /** For some reason a loop wasn't working to set these style attributes. */
-    document.getElementById('q_1').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
-    document.getElementById('q_2').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
-    document.getElementById('q_3').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
-    document.getElementById('q_4').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
-    document.getElementById('q_5').style.background="url(/res/img/school/review/emptyStars.gif) no-repeat 0 0;";
+function showStars(numStars) {
+    setDisplay(numStars);
+}
 
-    if (rating > 0) { document.getElementById('q_1').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
-    if (rating > 1) { document.getElementById('q_2').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
-    if (rating > 2) { document.getElementById('q_3').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
-    if (rating > 3) { document.getElementById('q_4').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
-    if (rating > 4) { document.getElementById('q_5').style.background="url(/res/img/school/review/stars.gif) no-repeat 0px -23px;"; }
+function setStars(numStars) {
+    document.getElementById('overallStarRating').value = numStars;
+    setDisplay(numStars);
     return false;
 }
 
-function setRatingTitle(text, q) {
-    document.getElementById('ratingTitle').innerHTML = text;
-    setStars(q);
-    return false;
+function resetStars() {
+    setDisplay(document.getElementById('overallStarRating').value);
 }
 
-var savedTitle;
-var quality = "decline";
+function setDisplay(numStars) {
+    document.getElementById('currentStarDisplay').style.width = 20*numStars + '%';
+    var title = '';
 
-function resetRatingTitle() {
-    if (savedTitle == undefined) {
-        savedTitle = "Rate this school";
+    switch (parseInt(numStars)) {
+        case 1: title = 'Unsatisfactory'; break;
+        case 2: title = 'Below Average'; break;
+        case 3: title = 'Average'; break;
+        case 4: title = 'Above Average'; break;
+        case 5: title = 'Excellent'; break;
+        default: title = 'Rate this school'; break;
     }
-    setRatingTitle(savedTitle, quality);
+    document.getElementById('ratingTitle').innerHTML = title;
 }
 
 function setSubmitFields() {
-    document.getElementById('confirm').value =
-        document.getElementById('reviewEmail').value;
-    document.getElementById('quality').value = quality;
-
+    document.getElementById('confirm').value = document.getElementById('reviewEmail').value;
+    if (document.getElementById('overallStarRating').value == '0') {
+        document.getElementById('overallStarRating').value = 'decline';
+    }
     if (document.getElementById('reviewText').value == 'Enter your review here') {
         document.getElementById('reviewText').value = "";
     }
-}
-
-function saveQuality(title, q) {
-    savedTitle = title;
-    quality = q;
-    setStars(q);
-    return false;
 }
 
 function countWords(w){
@@ -111,8 +101,8 @@ var AjaxObject = {
         if (ul==null) {
             ul = document.createElement('ul');
             ul.id = "frmErrors";
-            var form = document.getElementById('frmPRModule');
-            ul = form.parentNode.insertBefore(ul,form);
+            var txtArea = document.getElementById('reviewText');
+            ul = txtArea.parentNode.insertBefore(ul,txtArea);
         } else {
             while ( ul.hasChildNodes() ) { ul.removeChild(ul.firstChild)};
         }
