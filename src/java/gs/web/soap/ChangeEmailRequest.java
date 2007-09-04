d76ea128b5737fc4fc9b9ffdcf061e8b5cb079ba
aroy <aroy>
2007-09-04 22:50:04 +0000

    GS-3861 Implement SOAP service for change email. Changes were PHP side, but in the process of testing I refactored some soap code here

--- src/java/gs/web/soap/ChangeEmailRequest.java
+++ src/java/gs/web/soap/ChangeEmailRequest.java
@@ -7,6 +7,7 @@ import static org.apache.axis.Constants.XSD_STRING;
 import org.apache.axis.client.Call;
 
 import javax.xml.rpc.ParameterMode;
+import java.util.Map;
 
 /**
  * Provides Performs a SOAP changeEmailRequest.
@@ -15,12 +16,10 @@ import javax.xml.rpc.ParameterMode;
  */
 public class ChangeEmailRequest extends SoapRequest {
     public static final String DEFAULT_NAMESPACE_URI = "uri://www.greatschools.net/community/changeEmail/";
-    public static final String DEFAULT_TARGET = "http://aroy.dev.greatschools.net/cgi-bin/soap/soapServer.cgi";
     public static final int DEFAULT_TIMEOUT = 10000; // 10s in milliseconds
-    public static final boolean DISABLE_REQUEST = true;
 
     public ChangeEmailRequest() {
-        this(DEFAULT_TARGET);
+        this(null);
     }
 
     public ChangeEmailRequest(String target) {
@@ -30,18 +29,35 @@ public class ChangeEmailRequest extends SoapRequest {
     }
 
     public void changeEmailRequest(User user) throws SoapRequestException {
-        // quick hack to disable this class but still allow test cases to work
-        if (DISABLE_REQUEST && _mockCall == null) { return; }
+        // allows this class to be disabled (during development of community) but lets unit tests override
+        if (isDisabled()) { return; }
         try {
             Call call = setupCall("changeEmailRequest");
             Object[] params = setupParameters(call, user);
             Object ret = call.invoke(params);
 
-            if (ret != null) {
-                _log.warn("Exception generated on changeEmailRequest");
-                SoapRequestException e = (SoapRequestException) ret;
+            if (ret != null && ret instanceof Map) {
+                _log.warn("Exception generated on changeEmailRequest of class Map");
+                Map returnMap = (Map) ret;
+                SoapRequestException e = new SoapRequestException();
+                e.setErrorCode(String.valueOf(returnMap.get("errorCode")));
+                e.setErrorMessage(String.valueOf(returnMap.get("errorMessage")));
                 e.fillInStackTrace();
                 throw e;
+            } else if (ret != null && ret instanceof SoapRequestException) {
+                _log.warn("Exception generated on changeEmailRequest");
+                throw (SoapRequestException) ret;
+            } else if (ret != null && ret.toString() != null) {
+                if (ret.toString().equals(user.getId().toString())) {
+                    _log.info("changeEmailRequest successful on id " + ret);
+                } else {
+                    _log.warn("Exception generated on changeEmailRequest of class String");
+                    SoapRequestException e = new SoapRequestException();
+                    e.setErrorMessage(ret.toString());
+                    e.fillInStackTrace();
+                    throw e;
+                }
+            } else if (ret != null) {
             }
             _log.info("SOAP request to " + getTarget() + " successful");
         } catch (SoapRequestException e) {
