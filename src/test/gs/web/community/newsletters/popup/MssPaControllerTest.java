91b8e3cca8fc1254aa2d2bacf34b835dec699e2e
aroy <aroy>
2007-09-06 23:19:45 +0000

    I decided to refactor this test because it annoyed me that it contained a subclass of IUserDao for mocking purposes. Now it uses Easymock -- aroy

--- src/test/gs/web/community/newsletters/popup/MssPaControllerTest.java
+++ src/test/gs/web/community/newsletters/popup/MssPaControllerTest.java
@@ -1,35 +1,30 @@
 /**
  * Copyright (c) 2005 GreatSchools.net. All Rights Reserved.
- * $Id: MssPaControllerTest.java,v 1.18 2007/08/28 00:34:51 aroy Exp $
+ * $Id: MssPaControllerTest.java,v 1.19 2007/09/06 23:19:45 aroy Exp $
  */
 package gs.web.community.newsletters.popup;
 
 import gs.data.community.*;
-import gs.data.dao.hibernate.ThreadLocalTransactionManager;
 import gs.data.school.ISchoolDao;
+import gs.data.school.School;
 import gs.data.state.State;
 import gs.data.admin.IPropertyDao;
 import gs.web.BaseControllerTestCase;
-import gs.web.util.context.SessionContextUtil;
-import gs.web.util.validator.EmailValidator;
 import gs.web.util.validator.MaximumMssValidator;
-import gs.web.util.validator.SchoolIdValidator;
-import gs.web.util.validator.StateValidator;
 import org.apache.commons.lang.StringUtils;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.orm.ObjectRetrievalFailureException;
+import static org.easymock.EasyMock.*;
+import org.easymock.IAnswer;
 import org.springframework.validation.BindException;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.Validator;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.util.CookieGenerator;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import javax.servlet.http.Cookie;
 import java.util.*;
 
 /**
- * The purpose is ...
+ * The purpose is to test the MssPaController.
  *
  * @author David Lee <mailto:dlee@greatschools.net>
  */
@@ -38,50 +33,73 @@ public class MssPaControllerTest extends BaseControllerTestCase {
     private IUserDao _userDao;
     private ISubscriptionDao _subscriptionDao;
     private IPropertyDao _propertyDao;
+    private ISchoolDao _schoolDao;
+    private Validator _stateValidator;
+    private Validator _schoolIdValidator;
+    private NewsletterCommand _command;
+    private BindException _errors;
 
     protected void setUp() throws Exception {
         super.setUp();
 
-        _userDao = (IUserDao) getApplicationContext().getBean(IUserDao.BEAN_ID);
-        _subscriptionDao = (ISubscriptionDao) getApplicationContext().getBean(ISubscriptionDao.BEAN_ID);
-        _propertyDao = (IPropertyDao) getApplicationContext().getBean(IPropertyDao.BEAN_ID);
+        _userDao = createMock(IUserDao.class);
+        _subscriptionDao = createMock(ISubscriptionDao.class);
+        _propertyDao = createMock(IPropertyDao.class);
+        _schoolDao = createMock(ISchoolDao.class);
+        _stateValidator = createMock(Validator.class);
+        _schoolIdValidator = createMock(Validator.class);
 
         _controller = new MssPaController();
-        _controller.setApplicationContext(getApplicationContext());
         _controller.setUserDao(_userDao);
         _controller.setSubscriptionDao(_subscriptionDao);
-        _controller.setSchoolDao((ISchoolDao) getApplicationContext().getBean(ISchoolDao.BEAN_ID));
+        _controller.setPropertyDao(_propertyDao);
+        _controller.setSchoolDao(_schoolDao);
         _controller.setCommandClass(NewsletterCommand.class);
         _controller.setFormView("/community/newsletters/popup/mss/page1");
         _controller.setSuccessView("redirect:/community/newsletters/popup/mss/page2.page");
-        _controller.setPropertyDao(_propertyDao);
-        
-        _controller.setValidators(new Validator[]{
-                new SchoolIdValidator(),
-                new EmailValidator(),
-                new NewsletterCheckBoxValidator(),});
 
-        List onLoadValidators = new ArrayList();
-        onLoadValidators.add(new StateValidator());
-        onLoadValidators.add(new SchoolIdValidator());
+        List<Validator> onLoadValidators = new ArrayList<Validator>();
+        onLoadValidators.add(_stateValidator);
+        onLoadValidators.add(_schoolIdValidator);
         _controller.setOnLoadValidators(onLoadValidators);
 
+        _command = new NewsletterCommand();
+        _errors = new BindException(_command, "");
     }
 
-    public void testNoInputOnBindOnNewForm() {
-        NewsletterCommand command = new NewsletterCommand();
-        BindException errors = new BindException(command, "");
-        _controller.onBindOnNewForm(getRequest(), command, errors);
+    public void testBadInputOnBindOnNewForm() {
+        expect(_stateValidator.supports(NewsletterCommand.class)).andReturn(true);
+        _stateValidator.validate(_command, _errors);
+        replay(_stateValidator);
+        expect(_schoolIdValidator.supports(NewsletterCommand.class)).andReturn(true);
+        _schoolIdValidator.validate(_command, _errors);
+        replay(_schoolIdValidator);
+        replay(_schoolDao);
+
+        _errors.reject("foo"); // simulate an error
+        _controller.onBindOnNewForm(getRequest(), _command, _errors);
+        verify(_stateValidator);
+        verify(_schoolIdValidator);
+        verify(_schoolDao);
 
         //not passing in request parameters..should get errors
-        assertTrue(errors.hasErrors());
-        assertEquals("", command.getEmail());
+        assertTrue(_errors.hasErrors());
+        assertEquals("", _command.getEmail());
     }
 
     public void testGoodInputOnBindOnNewForm() {
-        NewsletterCommand command = new NewsletterCommand();
-        BindException errors = new BindException(command, "");
+        expect(_stateValidator.supports(NewsletterCommand.class)).andReturn(true);
+        _stateValidator.validate(_command, _errors);
+        replay(_stateValidator);
+        expect(_schoolIdValidator.supports(NewsletterCommand.class)).andReturn(true);
+        _schoolIdValidator.validate(_command, _errors);
+        replay(_schoolIdValidator);
 
+        School school = new School();
+        school.setId(1);
+        school.setName("My School");
+        expect(_schoolDao.getSchoolById(State.CA, 1)).andReturn(school);
+        replay(_schoolDao);
         final String email = "dlee@greatschools.net";
 
         User user = new User();
@@ -89,119 +107,139 @@ public class MssPaControllerTest extends BaseControllerTestCase {
         getSessionContext().setUser(user);
         getSessionContext().setEmail(email);
 
-        command.setSchoolId(1);
-        command.setState(State.CA);
-        _controller.onBindOnNewForm(getRequest(), command, errors);
+        _command.setSchoolId(1);
+        _command.setState(State.CA);
+        _controller.onBindOnNewForm(getRequest(), _command, _errors);
+        verify(_stateValidator);
+        verify(_schoolIdValidator);
+        verify(_schoolDao);
 
-        assertFalse(errors.hasErrors());
-        assertFalse(StringUtils.isEmpty(command.getSchoolName()));
-        assertEquals(email, command.getEmail());
-
-        command.setEmail(null);
-        getSessionContext().setUser(null);
-        getSessionContext().setEmail(null);
-        _controller.onBindOnNewForm(getRequest(), command, errors);
-        assertEquals("", command.getEmail());
+        assertFalse(_errors.hasErrors());
+        assertFalse(StringUtils.isEmpty(_command.getSchoolName()));
+        assertEquals(email, _command.getEmail());
+        assertEquals("My School", _command.getSchoolName());
     }
 
     public void testMaxedOutUserOnBindAndValidate() {
-        _controller.setUserDao(new MockMaxedOutMssUserDao());
-        NewsletterCommand command = new NewsletterCommand();
-        command.setMystat(true);
-        command.setEmail("anythingSinceWereUsingAMockDao");
+        _command.setMystat(true);
+        _command.setEmail("dlee@greatschools.net");
 
-        BindException errors = new BindException(command, "");
-        _controller.onBindAndValidate(getRequest(), command, errors);
+        User user = new User();
+        user.setEmail("dlee@greatschools.net");
+        Set<Subscription> subscriptions = new HashSet<Subscription>();
 
-        assertTrue(errors.hasErrors());
-        ObjectError error = errors.getGlobalError();
+        for (int i = 0; i < SubscriptionProduct.MAX_MSS_PRODUCT_FOR_ONE_USER; i++) {
+            Subscription sub = new Subscription();
+            sub.setProduct(SubscriptionProduct.MYSTAT);
+            sub.setSchoolId(i);
+            sub.setState(State.CA);
+            subscriptions.add(sub);
+        }
+        user.setSubscriptions(subscriptions);
+        expect(_userDao.findUserFromEmailIfExists("dlee@greatschools.net")).andReturn(user);
+        replay(_userDao);
+
+        _controller.onBindAndValidate(getRequest(), _command, _errors);
+        verify(_userDao);
+
+        assertTrue(_errors.hasErrors());
+        ObjectError error = _errors.getGlobalError();
 
         assertEquals(MaximumMssValidator.ERROR_CODE, error.getCode());
-        assertFalse(command.isMystat());
-
-        _controller.setUserDao(_userDao);
+        assertFalse(_command.isMystat());
     }
 
     public void testNonMaxedOutUserOnBindAndValidate() {
-        NewsletterCommand command = new NewsletterCommand();
-        command.setMystat(true);
-        command.setEmail("anythingSinceWereUsingAMockDao");
+        _command.setMystat(true);
+        _command.setEmail("dlee@greatschools.net");
 
-        BindException errors = new BindException(command, "");
-        _controller.onBindAndValidate(getRequest(), command, errors);
+        expect(_userDao.findUserFromEmailIfExists("dlee@greatschools.net")).andReturn(new User());
+        replay(_userDao);
 
-        assertFalse(errors.hasErrors());
+        _controller.onBindAndValidate(getRequest(), _command, _errors);
+        verify(_userDao);
+
+        assertFalse(_errors.hasErrors());
     }
 
     public void testGoodInputOnSubmit() {
-        NewsletterCommand command = new NewsletterCommand();
-        BindException errors = new BindException(command, "");
         final String email = "blahblahblah@greatschools.net";
         final int schoolId = 1;
 
-        command.setSchoolId(schoolId);
-        command.setState(State.CA);
-        command.setEmail(email);
-        command.setMystat(true);
-        command.setGn(true);
+        _command.setSchoolId(schoolId);
+        _command.setState(State.CA);
+        _command.setEmail(email);
+        _command.setMystat(true);
+        _command.setGn(true);
 
-        ModelAndView modelView = _controller.onSubmit(getRequest(), getResponse(), command, errors);
+        User user = new User();
+        user.setId(1);
+
+        expect(_userDao.findUserFromEmailIfExists(email)).andReturn(user);
+        replay(_userDao);
+        
+        _subscriptionDao.addNewsletterSubscriptions(isA(User.class), (List)anyObject());
+        replay(_subscriptionDao);
+
+        expect(_propertyDao.getProperty(IPropertyDao.CURRENT_ACADEMIC_YEAR)).andReturn("2007");
+        replay(_propertyDao);
+
+        ModelAndView modelView = _controller.onSubmit(getRequest(), getResponse(), _command, _errors);
+        verify(_userDao);
+        verify(_subscriptionDao);
+        verify(_propertyDao);
         Map model = modelView.getModel();
-        assertEquals(command.getState().toString(), model.get("state").toString());
-        assertEquals(command.getEmail(), model.get("email").toString());
-        assertEquals(String.valueOf(command.getSchoolId()), model.get("schoolId").toString());
-        assertEquals("true", (String) model.get("newuser"));
-
-        // repeat the request with the same email and make sure that "newuser" is empty
-        modelView = _controller.onSubmit(getRequest(), getResponse(), command, errors);
+        assertEquals(_command.getState().toString(), model.get("state").toString());
+        assertEquals(_command.getEmail(), model.get("email").toString());
+        assertEquals("2007", model.get("academicYear").toString());
+        assertEquals(String.valueOf(_command.getSchoolId()), model.get("schoolId").toString());
         assertTrue(StringUtils.isBlank((String) modelView.getModel().get("newuser")));
+//        assertEquals("true", (String) model.get("newuser"));
+        assertEquals("redirect:/community/newsletters/popup/mss/page2.page", modelView.getViewName());
+    }
 
-        _log.debug(modelView.getViewName());
-        assertEquals(modelView.getViewName(), "redirect:/community/newsletters/popup/mss/page2.page");
+    public void testOnSubmitWithNullUser() {
+        final String email = "blahblahblah@greatschools.net";
+        final int schoolId = 1;
 
-        assertEquals(modelView.getModel().get("academicYear"), _propertyDao.getProperty(IPropertyDao.CURRENT_ACADEMIC_YEAR));
+        _command.setSchoolId(schoolId);
+        _command.setState(State.CA);
+        _command.setEmail(email);
+        _command.setMystat(true);
+        _command.setGn(true);
 
-        ThreadLocalTransactionManager.commitOrRollback();
+        User user = new User();
+        user.setId(1);
 
-        User user = _userDao.findUserFromEmailIfExists(email);
-        try {
-            assertEquals("blahblahblah@greatschools.net", user.getEmail());
-            Set subscriptions = user.getSubscriptions();
-
-            Cookie cookie = getResponse().getCookie(SessionContextUtil.MEMBER_ID_COOKIE);
-            assertNotNull(cookie);
-            assertEquals(CookieGenerator.DEFAULT_COOKIE_MAX_AGE, cookie.getMaxAge());
-            assertEquals(CookieGenerator.DEFAULT_COOKIE_PATH, cookie.getPath());
-            assertEquals(cookie.getValue(), user.getId().toString());
-
-            boolean hasMss = false;
-            boolean hasGn = false;
-
-            assertEquals(2, subscriptions.size());
-
-            for (Iterator iter = subscriptions.iterator(); iter.hasNext();) {
-                Subscription sub = (Subscription) iter.next();
-                SubscriptionProduct product = sub.getProduct();
-
-                if (product.isNewsletter()) {
-                    if (product.getName().equals(SubscriptionProduct.MYSTAT.getName())) {
-                        if (sub.getSchoolId() == schoolId) {
-                            hasMss = true;
-                        }
-                    }
-
-                    if (product.getName().equals(SubscriptionProduct.PARENT_ADVISOR.getName())) {
-                        hasGn = true;
-                    }
-                }
+        expect(_userDao.findUserFromEmailIfExists(email)).andReturn(null);
+        _userDao.saveUser(isA(User.class));
+        // mock the saveUser call to set the User's id (required for the static PageHelper.setMemberCookie call)
+        expectLastCall().andAnswer(new IAnswer<Object>(){
+            public Object answer() throws Throwable {
+                User user = (User)getCurrentArguments()[0];
+                user.setId(1);
+                return user;
             }
+        });
+        replay(_userDao);
 
-            assertTrue(hasMss);
-            assertTrue(hasGn);
-        } finally {
-            _userDao.removeUser(user.getId());
-        }
-        assertNull(_userDao.findUserFromEmailIfExists(email));
+        _subscriptionDao.addNewsletterSubscriptions(isA(User.class), (List)anyObject());
+        replay(_subscriptionDao);
+
+        expect(_propertyDao.getProperty(IPropertyDao.CURRENT_ACADEMIC_YEAR)).andReturn("2007");
+        replay(_propertyDao);
+
+        ModelAndView modelView = _controller.onSubmit(getRequest(), getResponse(), _command, _errors);
+        verify(_userDao);
+        verify(_subscriptionDao);
+        verify(_propertyDao);
+        Map model = modelView.getModel();
+        assertEquals(_command.getState().toString(), model.get("state").toString());
+        assertEquals(_command.getEmail(), model.get("email").toString());
+        assertEquals("2007", model.get("academicYear").toString());
+        assertEquals(String.valueOf(_command.getSchoolId()), model.get("schoolId").toString());
+        assertEquals("Expect newuser to be set", "true",  modelView.getModel().get("newuser"));
+        assertEquals("redirect:/community/newsletters/popup/mss/page2.page", modelView.getViewName());
     }
 
     public void testSetHoverCookie() throws Exception {
@@ -211,65 +249,5 @@ public class MssPaControllerTest extends BaseControllerTestCase {
         assertEquals("Expected to find hover cookie", "hover", cookies[0].getName());
         assertEquals("Expected hover cookie path to be /", "/", cookies[0].getPath());
         assertEquals("Expected cookie to expire in 15 days", 60 * 60 * 24 * 15, cookies[0].getMaxAge());
-    }
-
-    private class MockMaxedOutMssUserDao implements IUserDao {
-
-        public void evict(User user) {
-            
-        }
-
-        public User findUserFromEmail(String string) throws ObjectRetrievalFailureException {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        public User findUserFromEmailIfExists(String string) {
-            User user = new User();
-            user.setEmail("dlee@greatschools.net");
-            Set subscriptions = new HashSet();
-
-            for (int i = 0; i < SubscriptionProduct.MAX_MSS_PRODUCT_FOR_ONE_USER; i++) {
-                Subscription sub = new Subscription();
-                sub.setProduct(SubscriptionProduct.MYSTAT);
-                sub.setSchoolId(i);
-                sub.setState(State.CA);
-                subscriptions.add(sub);
-            }
-
-            user.setSubscriptions(subscriptions);
-            return user;
-        }
-
-        public User findUserFromId(int i) throws ObjectRetrievalFailureException {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        public void saveUser(User user) throws DataIntegrityViolationException {
-            //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        public void updateUser(User user) throws DataIntegrityViolationException {
-            //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        public void removeUser(Integer integer) {
-            //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        public List findUsersModifiedSince(Date date) {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        public List findUsersModifiedBetween(Date date, Date date1) {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        public User findUserFromScreenNameIfExists(String screenName) {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        public List<User> findAllCommunityUsers() {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
     }
 }
