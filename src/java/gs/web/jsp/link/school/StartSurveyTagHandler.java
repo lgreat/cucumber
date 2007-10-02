aeb4de7760e6f13d46d8a6d457df2181222b79e1
chriskimm <chriskimm>
2007-10-02 20:31:12 +0000

    GS-4278

--- src/java/gs/web/jsp/link/school/StartSurveyTagHandler.java
+++ src/java/gs/web/jsp/link/school/StartSurveyTagHandler.java
@@ -3,8 +3,24 @@ package gs.web.jsp.link.school;
 import gs.web.util.UrlBuilder;
 
 public class StartSurveyTagHandler extends BaseSchoolTagHandler {
+
+    private String _cpn;
+
     protected UrlBuilder createUrlBuilder() {
         UrlBuilder builder = new UrlBuilder(getSchool(), UrlBuilder.SCHOOL_START_SURVEY);
+
+        if (_cpn != null) {
+            builder.addParameter("cpn", _cpn);
+        }
+        
         return builder;
+    }
+
+    public String getCpn() {
+        return _cpn;
+    }
+
+    public void setCpn(String cpn) {
+        this._cpn = cpn;
     }
 }
