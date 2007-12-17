79980e81e0e414986409687fec1ef1764b3a0047
chriskimm <chriskimm>
2007-12-17 06:40:44 +0000

    GS-5028

--- src/java/gs/web/survey/SurveyResultsController.java
+++ src/java/gs/web/survey/SurveyResultsController.java
@@ -3,8 +3,7 @@ package gs.web.survey;
 import gs.data.survey.ISurveyDao;
 import gs.data.survey.SurveyResults;
 import gs.data.school.School;
-import gs.data.state.StateManager;
-import gs.data.state.State;
+import gs.data.school.ISchoolDao;
 import gs.web.school.SchoolPageInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
@@ -20,19 +19,18 @@ public class SurveyResultsController extends AbstractController {
     public static final String BEAN_ID = "surveyResultsController";
 
     private ISurveyDao _surveyDao;
+    private ISchoolDao _schoolDao;
 
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
         // This controller is configured to be school-aware in pages-servlet.xml
         School school = (School) request.getAttribute(SchoolPageInterceptor.SCHOOL_ATTRIBUTE);
 
-        // Don't the get level from the school - in case it's a multi-level school
         String level = request.getParameter("level");
-
         SurveyResults results = getSurveyDao().getSurveyResultsForSchool(level, school);
+
         ModelAndView mAndV = new ModelAndView("survey/results");
         mAndV.getModel().put("results", results);
-        mAndV.getModel().put("schooler", school);  // todo: why isn't the school available in the request?
         return mAndV;
     }
 
@@ -42,5 +40,13 @@ public class SurveyResultsController extends AbstractController {
 
     public void setSurveyDao(ISurveyDao surveyDao) {
         _surveyDao = surveyDao;
+    }
+
+    public ISchoolDao getSchoolDao() {
+        return _schoolDao;
+    }
+
+    public void setSchoolDao(ISchoolDao schoolDao) {
+        _schoolDao = schoolDao;
     }
 }
--- src/test/gs/web/survey/SurveyResultsControllerTest.java
+++ src/test/gs/web/survey/SurveyResultsControllerTest.java
@@ -1,7 +1,11 @@
 package gs.web.survey;
 
 import gs.data.survey.SurveyResults;
+import gs.data.school.School;
+import gs.data.school.ISchoolDao;
+import gs.data.state.State;
 import gs.web.BaseControllerTestCase;
+import gs.web.school.SchoolPageInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
@@ -12,19 +16,33 @@ import java.util.Map;
 public class SurveyResultsControllerTest extends BaseControllerTestCase {
 
     private SurveyResultsController _controller;
+    private ISchoolDao _schoolDao;
 
     public void setUp() throws Exception {
         super.setUp();
         _controller = (SurveyResultsController)getApplicationContext().
                 getBean(SurveyResultsController.BEAN_ID);
+        _schoolDao = (ISchoolDao)getApplicationContext().
+                getBean(ISchoolDao.BEAN_ID);
     }
 
-    public void testGetResultsForNonExistentSchool() throws Exception {
-        //getRequest().setParameter("level", "h");
-        //ModelAndView mAndV = _controller.handleRequestInternal(getRequest(), getResponse());
-        //Map model = mAndV.getModel();
-        //SurveyResults results = (SurveyResults)model.get("results");
-        //assertEquals("SurveyResults should have 3 totat responses", 3, results.getTotalResponses());
+    public void testHandleRequest_SchoolWithResults() throws Exception {
+        School school = _schoolDao.getSchoolById(State.CA, 1);
+        getRequest().setAttribute(SchoolPageInterceptor.SCHOOL_ATTRIBUTE, school);
+        getRequest().setParameter("level", "h");
+        ModelAndView mAndV = _controller.handleRequestInternal(getRequest(), getResponse());
+        Map model = mAndV.getModel();
+        SurveyResults results = (SurveyResults)model.get("results");
+        assertEquals(3, results.getTotalResponses());
     }
 
+    public void testHandleRequest_SchoolWithNoResults() throws Exception {
+        School school = _schoolDao.getSchoolById(State.CA, 2);
+        getRequest().setAttribute(SchoolPageInterceptor.SCHOOL_ATTRIBUTE, school);
+        getRequest().setParameter("level", "h");
+        ModelAndView mAndV = _controller.handleRequestInternal(getRequest(), getResponse());
+        Map model = mAndV.getModel();
+        SurveyResults results = (SurveyResults)model.get("results");
+        assertEquals(0, results.getTotalResponses());
+    }
 }
