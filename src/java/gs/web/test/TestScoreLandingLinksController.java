56480dc3cbb5d57bbb55a37050a6b4da9170f68c
droy <droy>
2008-03-27 21:29:03 +0000

    GS-5565 Remove State instance variable and replace with a stateless passed around State.

--- src/java/gs/web/test/TestScoreLandingLinksController.java
+++ src/java/gs/web/test/TestScoreLandingLinksController.java
@@ -39,7 +39,6 @@ public class TestScoreLandingLinksController extends AbstractController {
     private StateManager _stateManager;
 
     private ITableDao _tableDao;
-    private State _state;
 
     private Logger _log = Logger.getLogger(TestScoreLandingLinksController.class);
 
@@ -63,20 +62,20 @@ public class TestScoreLandingLinksController extends AbstractController {
 
         String stateParam = request.getParameter(STATE_PARAM);
         if (stateParam != null) {
-            _state = _stateManager.getState(stateParam);
-            if (_state != null) {
+            State state = _stateManager.getState(stateParam);
+            if (state != null) {
                 if (!StringUtils.equals(request.getParameter(SHOW_FINDER_PARAM), "false")) {
-                    links.add(makeFindSchoolsLink());
+                    links.add(makeFindSchoolsLink(state));
                 }
 
                 String skip_tid = request.getParameter(SKIP_TID_PARAM);
                 if (skip_tid == null) {
                     skip_tid = "";
                 }
-                List<ITableRow> rows = getSpreadsheetRows();
+                List<ITableRow> rows = getSpreadsheetRows(state);
                 if (rows != null && !rows.isEmpty()) {
                     for (ITableRow row : rows) {
-                        Anchor anchor = makeLandingLink(row, skip_tid);
+                        Anchor anchor = makeLandingLink(state, row, skip_tid);
                         if (anchor != null) {
                             links.add(anchor);
                         }
@@ -90,30 +89,33 @@ public class TestScoreLandingLinksController extends AbstractController {
 
     /**
      * Get the rows for the current state
+     * @param state The state to get rows for
      * @return List of rows
      */
-    protected List<ITableRow> getSpreadsheetRows() {
-        String stateAbbrev = _state.getAbbreviation();
+    protected List<ITableRow> getSpreadsheetRows(State state) {
+        String stateAbbrev = state.getAbbreviation();
         return getTableDao().getRowsByKey(WORKSHEET_PRIMARY_ID_COL, stateAbbrev);
     }
 
     /**
      * Create an anchor for a specific test landing link
+     * @param state The state to make a link for
      * @param row Row of data containing details on the test
+     * @param skip_tid The test id of a test to ignore, if you are already on that tests page, for example
      * @return Anchor containing link to test landing page
      */
-    protected Anchor makeLandingLink(ITableRow row, String skip_tid) {
+    protected Anchor makeLandingLink(State state, ITableRow row, String skip_tid) {
         Anchor result = null;
         
         String datatype_id_str = row.getString(TABLE_DATA_TYPE_ID_COL);
         if (!StringUtils.isBlank(datatype_id_str) && !skip_tid.equals(datatype_id_str)) {
             String shortname = row.getString(TABLE_SHORT_NAME_COL);
             if (!StringUtils.isBlank(shortname) ) {
-                result = makeLandingLink(shortname, datatype_id_str);
+                result = makeLandingLink(state, shortname, datatype_id_str);
             } else {
                 String longname = row.getString(TABLE_LONG_NAME_COL);
                 if (!StringUtils.isBlank(longname)) {
-                    result = makeLandingLink(longname, datatype_id_str);
+                    result = makeLandingLink(state, longname, datatype_id_str);
                 }
             }
         }
@@ -123,28 +125,30 @@ public class TestScoreLandingLinksController extends AbstractController {
 
     /**
      * Create an anchor for a specific test landing link
+     * @param state The state to make a link for
      * @param testName The name of thest being linked to
      * @param datatype_id The datatype id of the test being linked to
      * @return Anchor containing the link to test landing page
      */
-    protected Anchor makeLandingLink(String testName, String datatype_id) {
+    protected Anchor makeLandingLink(State state, String testName, String datatype_id) {
         String testNameLower = testName.toLowerCase();
         if (!(testNameLower.endsWith(" ratings") || testNameLower.endsWith(" grades") ||
                 testNameLower.endsWith(" profiles") || testNameLower.endsWith(" scores"))) {
             testName += " results";
         }
-        String visibleText = _state.getLongName() + " " + testName;
-        UrlBuilder url_builder = new UrlBuilder(UrlBuilder.TEST_SCORE_LANDING, _state, datatype_id);
+        String visibleText = state.getLongName() + " " + testName;
+        UrlBuilder url_builder = new UrlBuilder(UrlBuilder.TEST_SCORE_LANDING, state, datatype_id);
         return new Anchor(url_builder.toString(), visibleText, _anchorStyleClass);
     }
 
     /**
      * Create an anchor to find more schools in the state
+     * @param state The state to make a finder link for
      * @return Anchor to find more schools
      */
-    protected Anchor makeFindSchoolsLink() {
-        String visibleText = "Find " + _state.getLongName() + " schools";
-        UrlBuilder url_builder = new UrlBuilder(UrlBuilder.RESEARCH, _state);
+    protected Anchor makeFindSchoolsLink(State state) {
+        String visibleText = "Find " + state.getLongName() + " schools";
+        UrlBuilder url_builder = new UrlBuilder(UrlBuilder.RESEARCH, state);
         return new Anchor(url_builder.toString(), visibleText, _anchorStyleClass);
     }
 
