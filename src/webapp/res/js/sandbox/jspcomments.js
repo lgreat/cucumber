ed223fea5608d315ef7aeace6829a67372f16738
chriskimm <chriskimm>
2008-04-10 20:55:47 +0000

    jspcomment sandbox stuff.

---
+++ src/webapp/res/js/sandbox/jspcomments.js
@@ -0,0 +1,32 @@
+function lessThan() {
+    var x = 0;
+    var y = 1;
+    if (x < y) {
+        alert("foo");
+    }
+}
+
+function logicalAnd() {
+    var x = true;
+    var y = true;
+    if (x && y) {
+        alert("bar");
+    }
+}
+
+function lessThan2() {
+    var x = 0;
+    var y = 1;
+
+//    if (x &lt; y) {  invalid javascript
+//        alert("foo");
+//    }
+}
+
+function logicalAnd2() {
+    var x = true;
+    var y = true;
+//    if (x &amp;&amp; y) { invalid javascript
+//        alert("bar");
+//    }
+}
