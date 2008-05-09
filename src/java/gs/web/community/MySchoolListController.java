7e5dd65d5a019e6e7181babf513f991e7453c102
chriskimm <chriskimm>
2008-05-09 00:58:43 +0000

    work on the skunkworks msl page

--- src/java/gs/web/community/MySchoolListController.java
+++ src/java/gs/web/community/MySchoolListController.java
@@ -2,7 +2,9 @@ package gs.web.community;
 
 import gs.data.community.IUserDao;
 import gs.data.community.User;
+import gs.data.community.FavoriteSchool;
 import gs.data.school.ISchoolDao;
+import gs.data.school.School;
 import gs.web.util.context.SessionContext;
 import gs.web.util.context.SessionContextUtil;
 import gs.web.util.list.AnchorListModel;
@@ -14,9 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.*;
 
 /**
- * Provide the "member" and "limit". Returns data in the AnchorListModel format.
- * 
- * @see AnchorListModel
+ * Controller for the MSL page.
  */
 public class MySchoolListController extends AbstractController {
 
@@ -25,20 +25,27 @@ public class MySchoolListController extends AbstractController {
     
     private String _viewName;
     private ISchoolDao _schoolDao;
-    private IUserDao _userDao;
-    public static final int DEFAULT_SCHOOL_LIMIT = 3;
 
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
         SessionContext context = SessionContextUtil.getSessionContext(request);
         User user = context.getUser();
         Map<String, Object> model = new HashMap<String, Object>();
         if (user != null) {
-            Set schools = user.getFavoriteSchools();
+            List<School> schools = getSchoolList(user.getFavoriteSchools());
             model.put("schools", schools);
         }
-        return new ModelAndView(_viewName, model);
+        return new ModelAndView(getViewName(), model);
     }
 
+    List<School> getSchoolList(Set<FavoriteSchool> favs) {
+        List<School> schools = new ArrayList<School>();
+        for (FavoriteSchool fav : favs) {
+            School s = getSchoolDao().getSchoolById(fav.getState(), fav.getSchoolId());
+            schools.add(s);
+        }
+        return schools;
+    }
+    
     public String getViewName() {
         return _viewName;
     }
@@ -53,13 +60,5 @@ public class MySchoolListController extends AbstractController {
 
     public void setSchoolDao(ISchoolDao schoolDao) {
         _schoolDao = schoolDao;
-    }
-
-    public IUserDao getUserDao() {
-        return _userDao;
-    }
-
-    public void setUserDao(IUserDao userDao) {
-        _userDao = userDao;
     }
 }
--- src/test/gs/web/community/MySchoolListControllerTest.java
+++ src/test/gs/web/community/MySchoolListControllerTest.java
@@ -1,21 +1,17 @@
-/*
- * Copyright (c) 2005-2006 GreatSchools.net. All Rights Reserved.
- * $Id: MySchoolListControllerTest.java,v 1.4 2008/04/30 21:02:42 chriskimm Exp $
- */
-
 package gs.web.community;
 
-import gs.data.community.IUserDao;
+import gs.data.community.FavoriteSchool;
 import gs.data.school.ISchoolDao;
+import gs.data.school.School;
+import gs.data.state.State;
 import gs.web.BaseControllerTestCase;
 import gs.web.GsMockHttpServletRequest;
 import gs.web.util.context.SessionContextUtil;
-import gs.web.util.list.AnchorListModel;
-import gs.web.util.context.SessionContextUtil;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
 import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * Tests MySchoolListController.
@@ -25,24 +21,59 @@ public class MySchoolListControllerTest extends BaseControllerTestCase {
     private MySchoolListController _controller;
     private SessionContextUtil _sessionContextUtil;
 
+    private static final String TEST_VIEW_NAME = "/community/msl.page";
+
     protected void setUp() throws Exception {
         super.setUp();
-
         _controller = new MySchoolListController();
         _controller.setApplicationContext(getApplicationContext());
-        _controller.setUserDao((IUserDao) getApplicationContext().getBean(IUserDao.BEAN_ID));
+        _controller.setViewName(TEST_VIEW_NAME);
         _controller.setSchoolDao((ISchoolDao) getApplicationContext().getBean(ISchoolDao.BEAN_ID));
-
         _sessionContextUtil = (SessionContextUtil) getApplicationContext().getBean(SessionContextUtil.BEAN_ID);
     }
 
     public void testMySchoolListController() throws Exception {
-
         GsMockHttpServletRequest request = getRequest();
+        request.setMethod("GET");
         request.setParameter(SessionContextUtil.MEMBER_PARAM, "1");
         _sessionContextUtil.prepareSessionContext(request, getResponse());
-        ModelAndView modelAndView = _controller.handleRequestInternal(request, getResponse());
-        Set schools = (Set) modelAndView.getModel().get("schools");
+        ModelAndView modelAndView = _controller.handleRequest(request, getResponse());
+        assertEquals(TEST_VIEW_NAME, modelAndView.getViewName());
+        List<School> schools = (List<School>) modelAndView.getModel().get("schools");
         assertEquals(4, schools.size());
+    }
+
+    public void testGetRequestNoUser() throws Exception {
+        getRequest().setMethod("GET");
+        ModelAndView mAndV = _controller.handleRequest(getRequest(), getResponse());
+        assertNull("no schools should be returned when there is no user", mAndV.getModel().get("schools"));
+    }
+
+    public void testGetSchoolList() throws Exception {
+        Set<FavoriteSchool> favoriteSchools = new HashSet<FavoriteSchool>();
+        favoriteSchools.add(makeFavoriteSchool(State.CA, 1));
+        favoriteSchools.add(makeFavoriteSchool(State.CA, 2));
+        favoriteSchools.add(makeFavoriteSchool(State.AK, 10));
+        favoriteSchools.add(makeFavoriteSchool(State.AK, 11));
+        List<School> schools = _controller.getSchoolList(favoriteSchools);
+        assertEquals("There should be 4 schools in the school list", 4, schools.size());
+        assertTrue(checkListContainsSchool(schools, State.CA, 1));
+    }
+
+    boolean checkListContainsSchool(List<School> schools, State state, int schoolId) {
+        for (School s : schools) {
+            if (s.getId().equals(schoolId) && s.getDatabaseState().equals(state)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static FavoriteSchool makeFavoriteSchool(State state, int school_id) {
+        FavoriteSchool fs = new FavoriteSchool();
+        fs.setId(1);
+        fs.setSchoolId(school_id);
+        fs.setState(state);
+        return fs;
     }
 }
