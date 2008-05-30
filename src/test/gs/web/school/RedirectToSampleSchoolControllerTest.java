e779ce7a50ab3b4c8b164badb7325986ec6e27d7
cpickslay <cpickslay>
2008-05-30 19:11:44 +0000

    removed data-dependency from test

--- src/test/gs/web/school/RedirectToSampleSchoolControllerTest.java
+++ src/test/gs/web/school/RedirectToSampleSchoolControllerTest.java
@@ -1,12 +1,16 @@
 /**
  * Copyright (c) 2005 GreatSchools.net. All Rights Reserved.
- * $Id: RedirectToSampleSchoolControllerTest.java,v 1.4 2007/03/05 23:27:48 dlee Exp $
+ * $Id: RedirectToSampleSchoolControllerTest.java,v 1.5 2008/05/30 19:11:44 cpickslay Exp $
  */
 package gs.web.school;
 
+import gs.data.school.ISchoolDao;
+import gs.data.school.School;
+import gs.data.state.State;
 import gs.web.BaseControllerTestCase;
 import gs.web.GsMockHttpServletRequest;
 import gs.web.util.context.SessionContextUtil;
+import static org.easymock.EasyMock.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -18,6 +22,7 @@ public class RedirectToSampleSchoolControllerTest extends BaseControllerTestCase
 
     private RedirectToSampleSchoolController _controller;
     private SessionContextUtil _sessionContextUtil ;
+    private ISchoolDao _schoolDao;
 
     protected void setUp() throws Exception {
         super.setUp();
@@ -25,9 +30,16 @@ public class RedirectToSampleSchoolControllerTest extends BaseControllerTestCase
                 getBean(RedirectToSampleSchoolController.BEAN_ID);
         _sessionContextUtil = (SessionContextUtil) getApplicationContext().
                 getBean(SessionContextUtil.BEAN_ID);
+        _schoolDao = createMock(ISchoolDao.class);
+        _controller.setSchoolDao(_schoolDao);
     }
 
    public void testGetSampleCASchool() throws Exception {
+       School school = new School();
+       school.setId(1);
+       expect(_schoolDao.getSampleSchool(State.CA)).andReturn(school);
+       replay(_schoolDao);
+
         GsMockHttpServletRequest request = getRequest();
         request.setParameter("state","CA");
         _sessionContextUtil.prepareSessionContext(getRequest(), getResponse());
@@ -40,7 +52,12 @@ public class RedirectToSampleSchoolControllerTest extends BaseControllerTestCase
    }
 
    public void testGetSampleAKSchool() throws Exception {
-        GsMockHttpServletRequest request = getRequest();
+       School school = new School();
+       school.setId(329);
+       expect(_schoolDao.getSampleSchool(State.AK)).andReturn(school);
+       replay(_schoolDao);
+
+       GsMockHttpServletRequest request = getRequest();
         request.setParameter("state","AK");
         _sessionContextUtil.prepareSessionContext(getRequest(), getResponse());
 
