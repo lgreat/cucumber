70158af4042c5465ad1b44d8b1ed0c70a11f157d
cpickslay <cpickslay>
2008-05-30 19:34:41 +0000

    removed data dependency from test

--- src/test/gs/web/survey/SurveyResultsControllerTest.java
+++ src/test/gs/web/survey/SurveyResultsControllerTest.java
@@ -1,11 +1,13 @@
 package gs.web.survey;
 
-import gs.data.survey.SurveyResults;
-import gs.data.school.School;
 import gs.data.school.ISchoolDao;
+import gs.data.school.School;
 import gs.data.state.State;
+import gs.data.survey.ISurveyDao;
+import gs.data.survey.SurveyResults;
 import gs.web.BaseControllerTestCase;
 import gs.web.school.SchoolPageInterceptor;
+import static org.easymock.EasyMock.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
@@ -17,6 +19,7 @@ public class SurveyResultsControllerTest extends BaseControllerTestCase {
 
     private SurveyResultsController _controller;
     private ISchoolDao _schoolDao;
+    private ISurveyDao _surveyDao;
 
     public void setUp() throws Exception {
         super.setUp();
@@ -24,26 +27,26 @@ public class SurveyResultsControllerTest extends BaseControllerTestCase {
                 getBean(SurveyResultsController.BEAN_ID);
         _schoolDao = (ISchoolDao)getApplicationContext().
                 getBean(ISchoolDao.BEAN_ID);
+        _surveyDao = createMock(ISurveyDao.class);
+        _controller.setSurveyDao(_surveyDao);
     }
 
     public void testHandleRequest_SchoolWithResults() throws Exception {
         School school = _schoolDao.getSchoolById(State.CA, 1);
         getRequest().setAttribute(SchoolPageInterceptor.SCHOOL_ATTRIBUTE, school);
         getRequest().setParameter("level", "h");
-        ModelAndView mAndV = _controller.handleRequestInternal(getRequest(), getResponse());
-        Map model = mAndV.getModel();
-        SurveyResults results = (SurveyResults)model.get("results");
-        assertEquals(3, results.getTotalResponses());
-    }
 
-    public void testHandleRequest_SchoolWithNoResults() throws Exception {
-        School school = _schoolDao.getSchoolById(State.CA, 2);
-        getRequest().setAttribute(SchoolPageInterceptor.SCHOOL_ATTRIBUTE, school);
-        getRequest().setParameter("level", "h");
+        SurveyResults expectedResults = new SurveyResults();
+        expect(_surveyDao.getSurveyResultsForSchool("h", school)).andReturn(expectedResults);
+        replay(_surveyDao);
+
         ModelAndView mAndV = _controller.handleRequestInternal(getRequest(), getResponse());
         Map model = mAndV.getModel();
         SurveyResults results = (SurveyResults)model.get("results");
-        assertEquals(0, results.getTotalResponses());
+        assertSame("Unexpected results", expectedResults, results);
+
+        verify(_surveyDao);
+        reset(_surveyDao);
     }
 
     public void testHandleRequest_specificPages() throws Exception {
