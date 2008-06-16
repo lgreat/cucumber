3ebbded2703fb3631270a1b6e411d4be5d3043e0
aroy <aroy>
2008-06-16 18:59:46 +0000

    GS-6483 Initial work on MSL page -- controller, PH for jspx/css, pages entry

--- src/java/gs/web/community/MySchoolListController.java
+++ src/java/gs/web/community/MySchoolListController.java
@@ -1,15 +1,14 @@
 package gs.web.community;
 
-import gs.data.community.IUserDao;
-import gs.data.community.User;
-import gs.data.community.FavoriteSchool;
 import gs.data.school.ISchoolDao;
 import gs.data.school.School;
-import gs.web.util.context.SessionContext;
-import gs.web.util.context.SessionContextUtil;
-import gs.web.util.list.AnchorListModel;
+import gs.data.community.IUserDao;
+import gs.data.community.FavoriteSchool;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.orm.ObjectRetrievalFailureException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,33 +18,38 @@ import java.util.*;
  * Controller for the MSL page.
  */
 public class MySchoolListController extends AbstractController {
+    protected static final Log _log = LogFactory.getLog(MySchoolListController.class);
 
     /** Spring bean id */
     public static final String BEAN_ID = "/community/msl.page";
     
     private String _viewName;
     private ISchoolDao _schoolDao;
+    private IUserDao _userDao;
 
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        SessionContext context = SessionContextUtil.getSessionContext(request);
-        User user = context.getUser();
         Map<String, Object> model = new HashMap<String, Object>();
-        if (user != null) {
-            List<School> schools = getSchoolList(user.getFavoriteSchools());
-            model.put("schools", schools);
-        }
+
         return new ModelAndView(getViewName(), model);
     }
 
-    List<School> getSchoolList(Set<FavoriteSchool> favs) {
+    public List<School> convertFavoriteSchoolsToSchools(List<FavoriteSchool> favoriteSchools) {
         List<School> schools = new ArrayList<School>();
-        for (FavoriteSchool fav : favs) {
-            School s = getSchoolDao().getSchoolById(fav.getState(), fav.getSchoolId());
-            schools.add(s);
+        if (favoriteSchools == null || favoriteSchools.isEmpty()) {
+            return schools;
         }
+            for (FavoriteSchool fave: favoriteSchools) {
+                try {
+                    schools.add(_schoolDao.getSchoolById(fave.getState(), fave.getSchoolId()));
+                } catch (ObjectRetrievalFailureException orfe) {
+                    _log.error("School in user's MSL doesn't exist in school table. User " + 
+                            fave.getUser().getId() + ", School " + fave.getState() + ":" + fave.getSchoolId());
+                    return new ArrayList<School>();
+                }
+            }
         return schools;
     }
-    
+
     public String getViewName() {
         return _viewName;
     }
@@ -60,5 +64,13 @@ public class MySchoolListController extends AbstractController {
 
     public void setSchoolDao(ISchoolDao schoolDao) {
         _schoolDao = schoolDao;
+    }
+
+    public IUserDao getUserDao() {
+        return _userDao;
+    }
+
+    public void setUserDao(IUserDao userDao) {
+        _userDao = userDao;
     }
 }
--- src/test/gs/web/community/MySchoolListControllerTest.java
+++ src/test/gs/web/community/MySchoolListControllerTest.java
@@ -1,91 +1,142 @@
 package gs.web.community;
 
-import gs.data.community.FavoriteSchool;
+import gs.data.community.IUserDao;
 import gs.data.community.User;
+import gs.data.community.FavoriteSchool;
 import gs.data.school.ISchoolDao;
 import gs.data.school.School;
 import gs.data.state.State;
 import gs.web.BaseControllerTestCase;
-import gs.web.util.context.SessionContextUtil;
-import static org.easymock.EasyMock.*;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashSet;
+import static org.easymock.EasyMock.*;
+import org.springframework.orm.ObjectRetrievalFailureException;
+
 import java.util.List;
-import java.util.Set;
+import java.util.ArrayList;
 
 /**
- * Tests MySchoolListController.
+ * test class for LoginController
+ *
+ * @author David Lee <mailto:dlee@greatschools.net>
  */
 public class MySchoolListControllerTest extends BaseControllerTestCase {
-
     private MySchoolListController _controller;
-    private SessionContextUtil _sessionContextUtil;
-
-    private static final String TEST_VIEW_NAME = "/community/msl.page";
+    private ISchoolDao _schoolDao;
+    private IUserDao _userDao;
+    private User _user;
 
     protected void setUp() throws Exception {
         super.setUp();
+
         _controller = new MySchoolListController();
-        _controller.setApplicationContext(getApplicationContext());
-        _controller.setViewName(TEST_VIEW_NAME);
-        _controller.setSchoolDao((ISchoolDao) getApplicationContext().getBean(ISchoolDao.BEAN_ID));
-        _sessionContextUtil = (SessionContextUtil) getApplicationContext().getBean(SessionContextUtil.BEAN_ID);
+
+        _schoolDao = createMock(ISchoolDao.class);
+        _userDao = createMock(IUserDao.class);
+
+        _controller.setSchoolDao(_schoolDao);
+        _controller.setUserDao(_userDao);
+
+        _user = new User();
+        _user.setId(1);
+        _user.setEmail("aroy@greatschools.net");
     }
 
-    public void testMySchoolListController() throws Exception {
-        ISchoolDao schoolDao = createMock(ISchoolDao.class);
-        _controller.setSchoolDao(schoolDao);
-        expect(schoolDao.getSchoolById(isA(State.class), isA(Integer.class))).andReturn(new School()).times(4);
-        replay(schoolDao);
+    public void testBasics() {
+        _controller.setViewName("view");
 
-        User user = new User();
-        _sessionContext.setUser(user);
-        user.setFavoriteSchools(createFourFavoriteSchools());
-
-        ModelAndView modelAndView = _controller.handleRequestInternal(_request, _response);
-        assertEquals(TEST_VIEW_NAME, modelAndView.getViewName());
-        List<School> schools = (List<School>) modelAndView.getModel().get("schools");
-        assertEquals(4, schools.size());
-        verify(schoolDao);
+        assertSame(_schoolDao, _controller.getSchoolDao());
+        assertSame(_userDao, _controller.getUserDao());
+        assertEquals("view", _controller.getViewName());
     }
 
-    public void testGetRequestNoUser() throws Exception {
-        getRequest().setMethod("GET");
-        ModelAndView mAndV = _controller.handleRequest(getRequest(), getResponse());
-        assertNull("no schools should be returned when there is no user", mAndV.getModel().get("schools"));
+    public void testConvertFavoriteSchoolsToSchoolsNull() {
+        List<School> schools = _controller.convertFavoriteSchoolsToSchools(null);
+        assertNotNull("Expect not null list out when given null list", schools);
+        assertEquals("Expect empty list out when given null list", 0, schools.size());
     }
 
-    public void testGetSchoolList() throws Exception {
-        Set<FavoriteSchool> favoriteSchools = createFourFavoriteSchools();
-        List<School> schools = _controller.getSchoolList(favoriteSchools);
-        assertEquals("There should be 4 schools in the school list", 4, schools.size());
-        assertTrue(checkListContainsSchool(schools, State.CA, 1));
+    public void testConvertFavoriteSchoolsToSchoolsEmptyList() {
+        List<FavoriteSchool> faves = new ArrayList<FavoriteSchool>();
+        List<School> schools = _controller.convertFavoriteSchoolsToSchools(faves);
+        assertNotNull("Expect not null list out when given empty list", schools);
+        assertEquals("Expect empty list out when given empty list", 0, schools.size());
     }
 
-    private Set<FavoriteSchool> createFourFavoriteSchools() {
-        Set<FavoriteSchool> favoriteSchools = new HashSet<FavoriteSchool>();
-        favoriteSchools.add(makeFavoriteSchool(State.CA, 1));
-        favoriteSchools.add(makeFavoriteSchool(State.CA, 2));
-        favoriteSchools.add(makeFavoriteSchool(State.AK, 10));
-        favoriteSchools.add(makeFavoriteSchool(State.AK, 11));
-        return favoriteSchools;
+    public void testConvertFavoriteSchoolsToSchoolsOneStateOneSchool() {
+        List<FavoriteSchool> faves = new ArrayList<FavoriteSchool>();
+        FavoriteSchool fave = new FavoriteSchool();
+        fave.setState(State.CA);
+        fave.setSchoolId(1);
+        faves.add(fave);
+
+        School school = new School();
+
+        expect(_schoolDao.getSchoolById(State.CA, 1)).andReturn(school);
+        replay(_schoolDao);
+        List<School> schools = _controller.convertFavoriteSchoolsToSchools(faves);
+        verify(_schoolDao);
+
+        assertNotNull("Expect not null list out when given favorite school list in", schools);
+        assertEquals("Expect same number of results as favorite schools", faves.size(), schools.size());
+        assertSame(school, schools.get(0));
     }
 
-    boolean checkListContainsSchool(List<School> schools, State state, int schoolId) {
-        for (School s : schools) {
-            if (s.getId().equals(schoolId) && s.getDatabaseState().equals(state)) {
-                return true;
+    public void testConvertFavoriteSchoolsToSchoolsTwoStatesOneSchoolEach() {
+        List<FavoriteSchool> faves = new ArrayList<FavoriteSchool>();
+        FavoriteSchool fave = new FavoriteSchool();
+        fave.setState(State.CA);
+        fave.setSchoolId(1);
+        faves.add(fave);
+
+        fave = new FavoriteSchool();
+        fave.setState(State.AK);
+        fave.setSchoolId(2);
+        faves.add(fave);
+
+        School school1 = new School();
+        School school2 = new School();
+
+        expect(_schoolDao.getSchoolById(State.CA, 1)).andReturn(school1);
+        expect(_schoolDao.getSchoolById(State.AK, 2)).andReturn(school2);
+        replay(_schoolDao);
+        List<School> schools = _controller.convertFavoriteSchoolsToSchools(faves);
+        verify(_schoolDao);
+
+        assertNotNull("Expect not null list out when given favorite school list in", schools);
+        assertEquals("Expect same number of results as favorite schools", faves.size(), schools.size());
+        boolean hasAK = false;
+        boolean hasCA = false;
+        for (School school: schools) {
+            if (school == school1) {
+                hasCA = true;
+            } else if (school == school2) {
+                hasAK = true;
             }
         }
-        return false;
+        assertTrue(hasCA);
+        assertTrue(hasAK);
     }
 
-    static FavoriteSchool makeFavoriteSchool(State state, int school_id) {
-        FavoriteSchool fs = new FavoriteSchool();
-        fs.setId(1);
-        fs.setSchoolId(school_id);
-        fs.setState(state);
-        return fs;
+    public void testConvertFavoriteSchoolsToSchoolsHandlesException() {
+        List<FavoriteSchool> faves = new ArrayList<FavoriteSchool>();
+        FavoriteSchool fave = new FavoriteSchool();
+        fave.setState(State.CA);
+        fave.setSchoolId(1);
+        fave.setUser(_user);
+        faves.add(fave);
+
+        expect(_schoolDao.getSchoolById(State.CA, 1)).andThrow(new ObjectRetrievalFailureException(School.class, 1));
+        replay(_schoolDao);
+        List<School> schools = null;
+        try {
+            schools = _controller.convertFavoriteSchoolsToSchools(faves);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("No exception should be thrown: " + e.getClass().getName());
+        }
+        verify(_schoolDao);
+
+        assertNotNull("Expect not null list out when exception thrown", schools);
+        assertEquals("Expect empty list out when exception thrown", 0, schools.size());
     }
 }
