f3f747c2e331245d7cef9285f6733e611d2043cf
yfan <yfan>
2008-06-24 18:35:15 +0000

    GS-6480 Added more JUnit test for MicrositePromoController. 100% methods, 100% lines covered.

--- src/test/gs/web/community/MicrositePromoControllerTest.java
+++ src/test/gs/web/community/MicrositePromoControllerTest.java
@@ -5,14 +5,17 @@ import static gs.web.community.CommunityQuestionPromoController.WORKSHEET_PRIMAR
 import static gs.web.community.CommunityQuestionPromoController.DEFAULT_CODE;
 import gs.web.util.google.GoogleSpreadsheetDao;
 import gs.web.util.list.AnchorListModel;
+import gs.web.util.list.Anchor;
 import gs.data.util.table.ITableDao;
 import gs.data.util.table.ITableRow;
+import gs.data.util.table.HashMapTableRow;
 //import static org.easymock.EasyMock.*;
 
 import static org.easymock.classextension.EasyMock.createMock;
 import static org.easymock.classextension.EasyMock.replay;
 import static org.easymock.classextension.EasyMock.verify;
 import static org.easymock.classextension.EasyMock.expect;
+import static org.easymock.classextension.EasyMock.reset;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -31,7 +34,11 @@ public class MicrositePromoControllerTest extends BaseControllerTestCase {
         _controller = (MicrositePromoController) getApplicationContext().getBean(MicrositePromoController.BEAN_ID);
         _tableDao = createMock(GoogleSpreadsheetDao.class);
         _controller.setTableDao(_tableDao);
-   }
+    }
+
+    public void testBasics() {
+        assertSame("Table DAO was not the same object", _tableDao, _controller.getTableDao());
+    }
 
     public void testHandleRequestInternal() throws Exception {
         // PARAM_PAGE not specified
@@ -48,6 +55,10 @@ public class MicrositePromoControllerTest extends BaseControllerTestCase {
         dao.setWorksheetUrl("google/od6");
 
         List<ITableRow> tableRowList = new ArrayList<ITableRow>();
+        HashMapTableRow hashMapTableRow = new HashMapTableRow();
+        hashMapTableRow.addCell(MicrositePromoController.SPREADSHEET_TEXT, "GreatSchools.net");
+        hashMapTableRow.addCell(MicrositePromoController.SPREADSHEET_URL, "http://www.greatschools.net");
+        tableRowList.add(hashMapTableRow);
         expect(dao.getRowsByKey(MicrositePromoController.SPREADSHEET_PAGE,
             getRequest().getParameter(MicrositePromoController.PARAM_PAGE))).andReturn(tableRowList);
         replay(dao);
@@ -61,6 +72,67 @@ public class MicrositePromoControllerTest extends BaseControllerTestCase {
         assertTrue("Model anchor list was not AnchorListModel object",
             modelAndView.getModel().get(MicrositePromoController.MODEL_ANCHOR_LIST) instanceof AnchorListModel);
 
+        AnchorListModel anchorListModel =
+            (AnchorListModel) modelAndView.getModel().get(MicrositePromoController.MODEL_ANCHOR_LIST);
+        Anchor firstAnchor = (Anchor) anchorListModel.getResults().get(0);
+        assertEquals("Anchor href was not GreatSchools.net", "GreatSchools.net", firstAnchor.getContents());
+        assertEquals("Anchor href was not http://www.greatschools.net", "http://www.greatschools.net", firstAnchor.getHref());
+
         verify(dao);
+    }
+
+    public void testInjectWorksheetName() {
+        GoogleSpreadsheetDao dao = (GoogleSpreadsheetDao) _tableDao;
+
+        getRequest().setServerName("dev.greatschools.net");
+        expect(dao.getWorksheetUrl()).andReturn("google/");
+        dao.setWorksheetUrl("google/" + _controller.getDevWorksheetName());
+        replay(dao);
+
+        _controller.injectWorksheetName(getRequest());
+        verify(dao);
+
+        reset(dao);
+
+        getRequest().setServerName("staging.greatschools.net");
+        expect(dao.getWorksheetUrl()).andReturn("google/");
+        dao.setWorksheetUrl("google/" + _controller.getStagingWorksheetName());
+        replay(dao);
+
+        _controller.injectWorksheetName(getRequest());
+        verify(dao);
+
+        reset(dao);
+
+        getRequest().setServerName("www.greatschools.net");
+        expect(dao.getWorksheetUrl()).andReturn("google/");
+        dao.setWorksheetUrl("google/" + _controller.getProductionWorksheetName());
+        replay(dao);
+
+        _controller.injectWorksheetName(getRequest());
+        verify(dao);
+
+        reset(dao);
+
+        getRequest().setServerName("dev.greatschools.net");
+        expect(dao.getWorksheetUrl()).andReturn("google/" + _controller.getDevWorksheetName());
+        replay(dao);
+
+        _controller.injectWorksheetName(getRequest());
+        verify(dao);
+    }
+
+    public void testGetWorksheet() {
+        getRequest().setServerName("dev.greatschools.net");
+        assertEquals("Worksheet name was not dev worksheet name for dev server name",
+            _controller.getDevWorksheetName(), _controller.getWorksheet(getRequest()));
+
+        getRequest().setServerName("staging.greatschools.net");
+        assertEquals("Worksheet name was not staging worksheet name for staging server name",
+            _controller.getStagingWorksheetName(), _controller.getWorksheet(getRequest()));
+
+        getRequest().setServerName("www.greatschools.net");
+        assertEquals("Worksheet name was not production worksheet name for production server name",
+            _controller.getProductionWorksheetName(), _controller.getWorksheet(getRequest()));
     }
 }
