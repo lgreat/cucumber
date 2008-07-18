6ccb6cb0703e0c2d18889bf2bb71228ca8e063c6
yfan <yfan>
2008-07-18 22:03:57 +0000

    GS-6708 Cleaned up code

--- src/java/gs/web/about/DownloadLogoController.java
+++ src/java/gs/web/about/DownloadLogoController.java
@@ -17,24 +17,34 @@ import java.io.BufferedOutputStream;
  * @author Young Fan
  */
 public class DownloadLogoController implements Controller {
+
+    final static String PARAM_SIZE = "size";
+    final static String PARAM_VAL_SMALL = "small";
+    final static String PARAM_VAL_MEDIUM = "medium";
+    final static String PARAM_VAL_LARGE = "large";
+    final static String IMAGE_PATH = "/res/img/logo/";
+    final static String SMALL_FILE_NAME = "logo_GS_201x38.gif";
+    final static String MEDIUM_FILE_NAME = "logo_GS_233x46.gif";
+    final static String LARGE_FILE_NAME = "logo_GS_304x58.gif";
+
+    public String chooseFileName(String size) {
+        if (PARAM_VAL_SMALL.equals(size)) {
+            return SMALL_FILE_NAME;
+        } else if (PARAM_VAL_MEDIUM.equals(size)) {
+            return MEDIUM_FILE_NAME;
+        } else {
+            return LARGE_FILE_NAME;
+        }
+    }
+
     public ModelAndView handleRequest(HttpServletRequest request,
                                                  HttpServletResponse response) throws Exception {
         // determine which size logo was requested
-        String size = request.getParameter("size");
+        String size = request.getParameter(PARAM_SIZE);
 
         // set the file name and file path
-        StringBuilder filePath = new StringBuilder("/res/img/logo/");
-        String fileName;
-        if ("small".equals(size)) {
-            fileName = "logo_GS_201x38.gif";
-            filePath.append(fileName);
-        } else if ("medium".equals(size)) {
-            fileName = "logo_GS_233x46.gif";
-            filePath.append(fileName);
-        } else {
-            fileName = "logo_GS_304x58.gif";
-            filePath.append(fileName);
-        }
+        String fileName = chooseFileName(size);
+        String filePath = IMAGE_PATH + fileName;
 
         // set response headers
         response.setContentType("image/gif");
