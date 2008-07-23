38cc07f9fd69077a5eab713b109fbc85409b39ca
yfan <yfan>
2008-07-23 17:49:44 +0000

    GS-6483 Wired up parent advisor subscription

--- src/java/gs/web/community/MySchoolListLoginController.java
+++ src/java/gs/web/community/MySchoolListLoginController.java
@@ -7,17 +7,21 @@ import org.springframework.validation.BindException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import gs.data.community.IUserDao;
-import gs.data.community.User;
+import gs.data.community.*;
+import gs.data.state.State;
 import gs.web.util.context.SessionContext;
 import gs.web.util.context.SessionContextUtil;
 import gs.web.util.PageHelper;
 import gs.web.util.ReadWriteController;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class MySchoolListLoginController extends SimpleFormController implements ReadWriteController {
 
     public static final String BEAN_NAME = "/community/mySchoolListLogin.page";
     public IUserDao _userDao;
+    private ISubscriptionDao _subscriptionDao;
 
     protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response,
                                     Object o, BindException e) throws Exception {
@@ -30,8 +34,29 @@ public class MySchoolListLoginController extends SimpleFormController implements
             getUserDao().saveUser(user);
             user = getUserDao().findUserFromEmail(email);
         }
+
+        if (request.getParameter("pa") != null) {
+            _subscriptionDao.addNewsletterSubscriptions(user, createSubscriptionList(user, request));
+        }
+
         PageHelper.setMemberCookie(request, response, user);
         return new ModelAndView(getSuccessView());
+    }
+
+    protected List<Subscription> createSubscriptionList(User user, HttpServletRequest request) {
+        // create a list of subscriptions and add this one to it
+        List<Subscription> subs = new ArrayList<Subscription>();
+
+        // create a new subscription, set user on it, set product to parent_advisor,
+        // set state to whatever is in sessioncontext
+        State state = SessionContextUtil.getSessionContext(request).getStateOrDefault();
+        Subscription subscription = new Subscription();
+        subscription.setUser(user);
+        subscription.setProduct(SubscriptionProduct.PARENT_ADVISOR);
+        subscription.setState(state);
+        subs.add(subscription);
+
+        return subs;
     }
 
     public IUserDao getUserDao() {
@@ -40,5 +65,13 @@ public class MySchoolListLoginController extends SimpleFormController implements
 
     public void setUserDao(IUserDao _userDao) {
         this._userDao = _userDao;
+    }
+
+    public ISubscriptionDao getSubscriptionDao() {
+        return _subscriptionDao;
+    }
+
+    public void setSubscriptionDao(ISubscriptionDao _subscriptionDao) {
+        this._subscriptionDao = _subscriptionDao;
     }
 }
--- src/test/gs/web/community/MySchoolListLoginControllerTest.java
+++ src/test/gs/web/community/MySchoolListLoginControllerTest.java
@@ -3,15 +3,20 @@ package gs.web.community;
 import gs.web.BaseControllerTestCase;
 import gs.web.util.context.SessionContext;
 import gs.web.util.context.SessionContextUtil;
-import gs.data.community.IUserDao;
-import gs.data.community.User;
+import gs.data.community.*;
+import gs.data.state.State;
 import org.springframework.web.servlet.ModelAndView;
 import static org.easymock.EasyMock.*;
+import static org.easymock.classextension.EasyMock.createMock;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class MySchoolListLoginControllerTest extends BaseControllerTestCase {
 
     MySchoolListLoginController _controller;
     IUserDao _mockUserDao;
+    ISubscriptionDao _mockSubscriptionDao;
     User _testUser;
 
     public void setUp() throws Exception {
@@ -26,6 +31,8 @@ public class MySchoolListLoginControllerTest extends BaseControllerTestCase {
         _controller.setFormView("/community/mySchoolListLogin");
         _controller.setSuccessView("/community/mySchoolList");
         _mockUserDao = createStrictMock(IUserDao.class);
+        _mockSubscriptionDao = createStrictMock(ISubscriptionDao.class);
+        _controller.setSubscriptionDao(_mockSubscriptionDao);
         _controller.setUserDao(_mockUserDao);
     }
 
@@ -52,13 +59,15 @@ public class MySchoolListLoginControllerTest extends BaseControllerTestCase {
         getRequest().setMethod("POST");
         expect(_mockUserDao.findUserFromEmailIfExists(_testUser.getEmail())).andReturn(_testUser);
         replay(_mockUserDao);
+        replay(_mockSubscriptionDao);
         ModelAndView mAndV = _controller.handleRequest(getRequest(), getResponse());
         verify(_mockUserDao);
+        verify(_mockSubscriptionDao);
         assertEquals("Expected the MSL page", _controller.getSuccessView(), mAndV.getViewName());
         assertEquals("Member cookie should now be in response", "1", getResponse().getCookie("MEMID").getValue());
     }
 
-    public void testSubmitWithUnkownEmail() throws Exception {
+    public void testSubmitWithUnknownEmail() throws Exception {
         assertNull("Member cookie should not be set", getResponse().getCookie("MEMID"));
         _testUser.setEmail("foo@flimflam.com");
         _testUser.setId(1234);
@@ -68,9 +77,40 @@ public class MySchoolListLoginControllerTest extends BaseControllerTestCase {
         _mockUserDao.saveUser(_testUser);
         expect(_mockUserDao.findUserFromEmail(_testUser.getEmail())).andReturn(_testUser);
         replay(_mockUserDao);
+        replay(_mockSubscriptionDao);
         ModelAndView mAndV = _controller.handleRequest(getRequest(), getResponse());
         verify(_mockUserDao);
+        verify(_mockSubscriptionDao);
         assertEquals("Expected the MSL page", _controller.getSuccessView(), mAndV.getViewName());
         assertEquals("Member cookie should be set set to user id", "1234", getResponse().getCookie("MEMID").getValue());
+    }
+
+    public void testSubmitWithKnownEmailAndParentAdvisor() throws Exception {
+        SessionContext sc = SessionContextUtil.getSessionContext(getRequest());
+        assertNull("Member id should not be set", sc.getUser());
+        getRequest().setParameter("email", _testUser.getEmail());
+        getRequest().setParameter("pa", "pa");
+        getRequest().setMethod("POST");
+        expect(_mockUserDao.findUserFromEmailIfExists(_testUser.getEmail())).andReturn(_testUser);
+
+        List<Subscription> subs = new ArrayList<Subscription>();
+        State state = SessionContextUtil.getSessionContext(getRequest()).getStateOrDefault();
+        Subscription sub = new Subscription();
+        sub.setUser(_testUser);
+        sub.setProduct(SubscriptionProduct.PARENT_ADVISOR);
+        sub.setState(state);
+        subs.add(sub);
+        _mockSubscriptionDao.addNewsletterSubscriptions(_testUser, subs);        
+
+        replay(_mockUserDao);
+        replay(_mockSubscriptionDao);
+
+        ModelAndView mAndV = _controller.handleRequest(getRequest(), getResponse());
+        verify(_mockUserDao);
+        verify(_mockSubscriptionDao);
+
+        assertEquals("Expected the MSL page", _controller.getSuccessView(), mAndV.getViewName());
+        assertEquals("Member cookie should now be in response", "1", getResponse().getCookie("MEMID").getValue());
+
     }
 }
