ebe5155a5f22c7da6618b092b9631451cf900c42
chriskimm <chriskimm>
2008-07-23 23:32:35 +0000

    GS-6483

--- src/java/gs/web/community/LogoutController.java
+++ src/java/gs/web/community/LogoutController.java
@@ -2,6 +2,8 @@ package gs.web.community;
 
 import org.springframework.web.servlet.mvc.AbstractController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+import org.apache.commons.lang.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,16 +16,21 @@ import gs.web.util.PageHelper;
  */
 public class LogoutController extends AbstractController {
 
-    public static final String DEFAULT_VIEW = "redirect:/";
+    public static final String DEFAULT_VIEW = "/";
 
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
         PageHelper.logout(request, response);
         return new ModelAndView(getRedirectView(request));
     }
 
-    protected String getRedirectView(HttpServletRequest request) {
-        //request.getParameter()
-        // todo
-        return DEFAULT_VIEW;
+    protected RedirectView getRedirectView(HttpServletRequest request) {
+        String redirect = request.getParameter("redirect");
+        RedirectView rView;
+        if (StringUtils.isNotBlank(redirect)) {
+            rView = new RedirectView(redirect);
+        } else {
+            rView = new RedirectView(DEFAULT_VIEW);
+        }
+        return rView;
     }
 }
