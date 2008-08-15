a17d657f52770ec40c16f457507aa1b9a693a7af
yfan <yfan>
2008-08-15 02:31:17 +0000

    GS-6868 Work in progress

--- src/test/gs/web/about/feedback/SubmitSchoolControllerTest.java
+++ src/test/gs/web/about/feedback/SubmitSchoolControllerTest.java
@@ -1,8 +1,18 @@
 package gs.web.about.feedback;
 
+import gs.web.BaseControllerTestCase;
+
 /**
  * @author
  */
-public class SubmitSchoolControllerTest {
+public class SubmitSchoolControllerTest extends BaseControllerTestCase {
     private SubmitSchoolController _controller;
+
+    public void setUp() {
+        // TODO-6868
+    }
+
+    public void testSomething() {
+        // TODO-6868
+    }
 }
