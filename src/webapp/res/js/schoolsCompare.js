3b92d862c85d2bdc48e9d317d0f4659a6a980700
aroy <aroy>
2008-08-20 17:14:03 +0000

    GS-6861 GS-6671 Do not allow multiple states, correctly determine state for compare

--- src/webapp/res/js/schoolsCompare.js
+++ src/webapp/res/js/schoolsCompare.js
@@ -66,13 +66,46 @@ function showInline(elementId) {
 }
 
 function evaluateCheckboxes() {
+    // first determine if they are trying to check multiple states
+    var numStates = countStates();
+    if (numStates > 1) {
+        alert("Sorry, you can only compare schools in the same state.");
+        // cancel check action
+        return false;
+    }
+    // then see how many they have checked
     var numChecked = countChecked();
+    // defaults to "Check to compare"
     setAllToDisplayLabel();
     if (numChecked == 1) {
+        // "Compare: check two or more"
         setCheckedToDisplayError();
     } else {
+        // "Compare now"
         setCheckedToDisplaySubmit();
     }
+    return true;
+}
+
+
+function countStates() {
+    var inputs = document.getElementsByTagName("input");
+    var stateMap = new Object;
+    var stateAb;
+    var numStates = 0;
+    for (var i=0; i < inputs.length; i++) {
+        var currentElem = inputs[i];
+        if (currentElem.id > '' && currentElem.id.indexOf(ID_PREFIX) > -1) {
+            if (currentElem.checked) {
+                stateAb = currentElem.value.substring(0, 2);
+                stateMap[stateAb] = 1;
+            }
+        }
+    }
+    for (stateAb in stateMap) {
+        numStates++;
+    }
+    return numStates;
 }
 
 /* --------- S O R T I N G &  P A G E  S E T T I N G S ----------------- */
