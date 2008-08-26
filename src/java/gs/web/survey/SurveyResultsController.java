073c76da7fa3b1895ce6eb25ff16499538c5718d
yfan <yfan>
2008-08-26 22:30:01 +0000

    GS-6979 Acceptance criteria #5

--- src/java/gs/web/survey/SurveyResultsController.java
+++ src/java/gs/web/survey/SurveyResultsController.java
@@ -4,12 +4,14 @@ import gs.data.survey.ISurveyDao;
 import gs.data.survey.SurveyResults;
 import gs.data.school.School;
 import gs.data.school.ISchoolDao;
+import gs.data.school.LevelCode;
 import gs.web.school.SchoolPageInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Set;
 
 /**
  * This controller delivers the survey results page.  Required query params:
@@ -30,6 +32,7 @@ public class SurveyResultsController extends AbstractController {
 
     private static final String VIEW_NAME = "survey/results";
     private static final String MODEL_NAME = "results";
+    private static final String MODEL_LEVEL = "level";
     private static final String LEVEL_PARAM = "level";
 
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -38,9 +41,21 @@ public class SurveyResultsController extends AbstractController {
         School school = (School) request.getAttribute(SchoolPageInterceptor.SCHOOL_ATTRIBUTE);
 
         String level = request.getParameter(LEVEL_PARAM);
+        String levelString = null;
+        Set<LevelCode.Level> levelCodes = school.getLevelCode().getIndividualLevelCodes();
+        if (levelCodes.size() > 1) {
+            LevelCode.Level lev = LevelCode.Level.getLevelCode(level);
+            if (LevelCode.Level.PRESCHOOL_LEVEL.equals(lev)) {
+                levelString = lev.getLongName();
+            } else {
+                levelString = lev.getLongName() + " school";
+            }
+        }
+
         SurveyResults results = getSurveyDao().getSurveyResultsForSchool(level, school);
         ModelAndView mAndV = new ModelAndView(VIEW_NAME);
         mAndV.getModel().put(MODEL_NAME, results);
+        mAndV.getModel().put(MODEL_LEVEL, levelString);
         return mAndV;
     }
 
