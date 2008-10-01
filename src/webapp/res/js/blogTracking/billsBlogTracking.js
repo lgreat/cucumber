676cdafb995bf151b22e9ff42937959572c5e02e
jnorton <jnorton>
2008-10-01 16:53:55 +0000

    GS-7139 javascript tracking code for blogs

---
+++ src/webapp/res/js/blogTracking/billsBlogTracking.js
@@ -0,0 +1,7 @@
+
+
+pageTracking.pageName =  "Bill's Blog";
+pageTracking.heirarchy = "Blog,Bill's Blog";
+pageTracking.server = "www.greatschools.net";
+
+pageTracking.send();
---
+++ src/webapp/res/js/blogTracking/sanFranciscoBlogTraking.js
@@ -0,0 +1,7 @@
+
+
+pageTracking.pageName =  "San Francisco";
+pageTracking.heirarchy = "Blog,San Francisco Blog, San Francisco";
+pageTracking.server = "www.greatschools.net";
+
+pageTracking.send();
