8b6d80e55105782a2249370cc261533154f9f246
yfan <yfan>
2008-10-01 23:46:25 +0000

    GS-7171 JUnit tests

--- src/test/gs/web/path/DirectoryStructureUrlFieldsTest.java
+++ src/test/gs/web/path/DirectoryStructureUrlFieldsTest.java
@@ -1,100 +1,83 @@
 package gs.web.path;
 
 import gs.web.BaseControllerTestCase;
+import gs.web.GsMockHttpServletRequest;
+import gs.data.school.SchoolType;
+import gs.data.school.LevelCode;
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import java.util.Set;
+import java.util.Arrays;
+import java.util.HashSet;
 
 /**
- * Created by IntelliJ IDEA.
- * User: youngfan
- * Date: Sep 25, 2008
- * Time: 4:15:52 PM
- * To change this template use File | Settings | File Templates.
+ * @author Young Fan
  */
 public class DirectoryStructureUrlFieldsTest extends BaseControllerTestCase {
-    public void setUp() {
 
+    public void testConstructor() throws Exception {
+        GsMockHttpServletRequest request = getRequest();
+
+        request.setRequestURI(null);
+
+        Set<SchoolType> schoolTypeSet = new HashSet<SchoolType>();
+
+        request.setRequestURI("/california/san-francisco/schools/");
+        DirectoryStructureUrlFields fields = new DirectoryStructureUrlFields(request);
+        assertEquals("Expected city name 'san francisco'", "san francisco", fields.getCityName());
+        assertNull("Expected school type null", fields.getSchoolTypesParams());
+        schoolTypeSet.clear();
+        assertEquals("Expected school type set to be empty", schoolTypeSet, fields.getSchoolTypes());
+        assertNull("Expected level code null", fields.getLevelCode());
+
+        request.setRequestURI("/california/cardiff_by_the_sea/schools/");
+        fields = new DirectoryStructureUrlFields(request);
+        assertEquals("Expected city name 'cardiff-by-the-sea'", "cardiff-by-the-sea", fields.getCityName());
+        assertNull("Expected school type null", fields.getSchoolTypesParams());
+        schoolTypeSet.clear();
+        assertEquals("Expected school type set to be empty", schoolTypeSet, fields.getSchoolTypes());
+        assertNull("Expected level code null", fields.getLevelCode());
+
+        request.setRequestURI("/california/san-francisco/public/schools/");
+        fields = new DirectoryStructureUrlFields(request);
+        assertEquals("Expected city name 'san francisco'", "san francisco", fields.getCityName());
+        assertTrue("Expected school type array to contain public",
+                Arrays.deepEquals(new String[]{SchoolType.PUBLIC.getSchoolTypeName()},
+                        fields.getSchoolTypesParams()));
+        schoolTypeSet.clear();
+        schoolTypeSet.add(SchoolType.PUBLIC);
+        assertEquals("Expected school type set to contain public", schoolTypeSet, fields.getSchoolTypes());
+        assertNull("Expected level code null", fields.getLevelCode());
+
+        request.setRequestURI("/california/san-francisco/public-private/schools/");
+        fields = new DirectoryStructureUrlFields(request);
+        assertEquals("Expected city name 'san francisco'", "san francisco", fields.getCityName());
+        assertTrue("Expected school type array to contain public and private",
+                Arrays.deepEquals(new String[]{SchoolType.PUBLIC.getSchoolTypeName(), SchoolType.PRIVATE.getSchoolTypeName()},
+                        fields.getSchoolTypesParams()));
+        schoolTypeSet.clear();
+        schoolTypeSet.add(SchoolType.PUBLIC);
+        schoolTypeSet.add(SchoolType.PRIVATE);
+        assertEquals("Expected school type set to contain public and private", schoolTypeSet, fields.getSchoolTypes());
+        assertNull("Expected level code null", fields.getLevelCode());
+
+        request.setRequestURI("/california/san-francisco/private-charter/elementary-schools/");
+        fields = new DirectoryStructureUrlFields(request);
+        assertEquals("Expected city name 'san francisco'", "san francisco", fields.getCityName());
+        assertTrue("Expected school type array to contain private and charter",
+                Arrays.deepEquals(new String[]{SchoolType.PRIVATE.getSchoolTypeName(), SchoolType.CHARTER.getSchoolTypeName()},
+                        fields.getSchoolTypesParams()));
+        schoolTypeSet.clear();
+        schoolTypeSet.add(SchoolType.PRIVATE);
+        schoolTypeSet.add(SchoolType.CHARTER);
+        assertEquals("Expected school type set to contain private and charter", schoolTypeSet, fields.getSchoolTypes());
+        assertEquals("Expected level code elementary", LevelCode.ELEMENTARY, fields.getLevelCode());
+
+        request.setRequestURI("/california/san-francisco/preschools/");
+        fields = new DirectoryStructureUrlFields(request);
+        assertEquals("Expected city name 'san francisco'", "san francisco", fields.getCityName());
+        assertNull("Expected school type null", fields.getSchoolTypesParams());
+        schoolTypeSet.clear();
+        assertEquals("Expected school type set to be empty", schoolTypeSet, fields.getSchoolTypes());
+        assertEquals("Expected level code preschools", LevelCode.PRESCHOOL, fields.getLevelCode());
     }
-
-    public void testConstructor() {
-        
-    }
-
-    /*
-     // TODO-7171 - convert this to test the new code
-     public void testGetFieldsFromNewStyleCityBrowseRequest() throws Exception {
-         GsMockHttpServletRequest request = getRequest();
-
-         request.setRequestURI(null);
-         boolean foundException = false;
-         try {
-             SchoolsController.getFieldsFromNewStyleCityBrowseRequest(request);
-         } catch (IllegalArgumentException e) {
-             foundException = true;
-         }
-         assertTrue("Expected IllegalArgumentException when request URI invalid", foundException);
-
-         Set<SchoolType> schoolTypeSet = new HashSet<SchoolType>();
-
-         request.setRequestURI("/california/san-francisco/schools/");
-         SchoolsController.CityBrowseFields fields = SchoolsController.getFieldsFromNewStyleCityBrowseRequest(request);
-         assertEquals("Expected city name 'san francisco'", "san francisco", fields.getCityName());
-         assertNull("Expected school type null", fields.getSchoolType());
-         schoolTypeSet.clear();
-         assertEquals("Expected school type set to be empty", schoolTypeSet, fields.getSchoolTypeSet());
-         assertNull("Expected level code null", fields.getLevelCode());
-
-         request.setRequestURI("/california/cardiff_by_the_sea/schools/");
-         fields = SchoolsController.getFieldsFromNewStyleCityBrowseRequest(request);
-         assertEquals("Expected city name 'cardiff-by-the-sea'", "cardiff-by-the-sea", fields.getCityName());
-         assertNull("Expected school type null", fields.getSchoolType());
-         schoolTypeSet.clear();
-         assertEquals("Expected school type set to be empty", schoolTypeSet, fields.getSchoolTypeSet());
-         assertNull("Expected level code null", fields.getLevelCode());
-
-         request.setRequestURI("/california/san-francisco/public/schools/");
-         fields = SchoolsController.getFieldsFromNewStyleCityBrowseRequest(request);
-         assertEquals("Expected city name 'san francisco'", "san francisco", fields.getCityName());
-         assertTrue("Expected school type array to contain public",
-                 Arrays.deepEquals(new String[]{SchoolType.PUBLIC.getSchoolTypeName()},
-                         fields.getSchoolType()));
-         schoolTypeSet.clear();
-         schoolTypeSet.add(SchoolType.PUBLIC);
-         assertEquals("Expected school type set to contain public", schoolTypeSet, fields.getSchoolTypeSet());
-         assertNull("Expected level code null", fields.getLevelCode());
-
-         request.setRequestURI("/california/san-francisco/public-private/schools/");
-         fields = SchoolsController.getFieldsFromNewStyleCityBrowseRequest(request);
-         assertEquals("Expected city name 'san francisco'", "san francisco", fields.getCityName());
-         assertTrue("Expected school type array to contain public and private",
-                 Arrays.deepEquals(new String[]{SchoolType.PUBLIC.getSchoolTypeName(), SchoolType.PRIVATE.getSchoolTypeName()},
-                         fields.getSchoolType()));
-         schoolTypeSet.clear();
-         schoolTypeSet.add(SchoolType.PUBLIC);
-         schoolTypeSet.add(SchoolType.PRIVATE);
-         assertEquals("Expected school type set to contain public and private", schoolTypeSet, fields.getSchoolTypeSet());
-         assertNull("Expected level code null", fields.getLevelCode());
-
-         request.setRequestURI("/california/san-francisco/private-charter/elementary-schools/");
-         fields = SchoolsController.getFieldsFromNewStyleCityBrowseRequest(request);
-         assertEquals("Expected city name 'san francisco'", "san francisco", fields.getCityName());
-         assertTrue("Expected school type array to contain private and charter",
-                 Arrays.deepEquals(new String[]{SchoolType.PRIVATE.getSchoolTypeName(), SchoolType.CHARTER.getSchoolTypeName()},
-                         fields.getSchoolType()));
-         schoolTypeSet.clear();
-         schoolTypeSet.add(SchoolType.PRIVATE);
-         schoolTypeSet.add(SchoolType.CHARTER);
-         assertEquals("Expected school type set to contain private and charter", schoolTypeSet, fields.getSchoolTypeSet());
-         assertEquals("Expected level code elementary", LevelCode.ELEMENTARY, fields.getLevelCode());
-
-         request.setRequestURI("/california/san-francisco/preschools/");
-         fields = SchoolsController.getFieldsFromNewStyleCityBrowseRequest(request);
-         assertEquals("Expected city name 'san francisco'", "san francisco", fields.getCityName());
-         assertNull("Expected school type null", fields.getSchoolType());
-         schoolTypeSet.clear();
-         assertEquals("Expected school type set to be empty", schoolTypeSet, fields.getSchoolTypeSet());
-         assertEquals("Expected level code preschools", LevelCode.PRESCHOOL, fields.getLevelCode());
-     }
-     */    
 }
