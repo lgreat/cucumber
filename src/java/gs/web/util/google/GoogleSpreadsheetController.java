ced4eae26f0bac1cd0198aa9ab23b91fe9611876
thuss <thuss>
2008-11-26 05:23:35 +0000

    GS-7584

---
+++ src/java/gs/web/util/google/GoogleSpreadsheetController.java
@@ -0,0 +1,34 @@
+package gs.web.util.google;
+
+import gs.data.util.table.ITableDao;
+import gs.data.util.table.ITableRow;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+/**
+ * @author thuss
+ */
+public class GoogleSpreadsheetController extends AbstractController {
+
+    private ITableDao _tableDao;
+    private String _viewName;
+    protected final String MODEL_ROWS = "rows";
+
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Map model = new HashMap();
+        model.put(MODEL_ROWS, _tableDao.getAllRows());
+        return new ModelAndView(_viewName, model);
+    }
+
+    public void setTableDao(ITableDao tableDao) {
+        _tableDao = tableDao;
+    }
+
+    public void setViewName(String viewName) {
+        this._viewName = viewName;
+    }
+}
