d8dd612cbb82cade17f8e754ab3b3322e3015c29
yfan <yfan>
2008-12-10 22:57:59 +0000

    GS-7649 Rolling back Java changes for setting welcome message field for User / list_member.

--- src/java/gs/web/util/NewSubscriberDetector.java
+++ src/java/gs/web/util/NewSubscriberDetector.java
@@ -2,12 +2,12 @@ package gs.web.util;
 
 import gs.data.community.User;
 import gs.data.community.Subscription;
-import gs.data.util.SubscriptionUtil;
 import gs.web.tracking.OmnitureTracking;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Set;
+import java.util.Collection;
 
 /**
  * Adds an omniture success event if the user had no previous newsletter subscriptions.
@@ -17,7 +17,7 @@ public class NewSubscriberDetector {
     public static void notifyOmnitureWhenNewNewsLetterSubscriber(User user,HttpServletRequest request, HttpServletResponse response){
         Set<Subscription> subs = user.getSubscriptions();
 
-        if (!SubscriptionUtil.userHasNewsLetterSubscriptions(subs)) {
+        if (!userHasNewsLetterSubscriptions(subs)) {
             OmnitureTracking ot = new OmnitureTracking(request, response);
             ot.addSuccessEvent(OmnitureTracking.SuccessEvent.NewNewsLetterSubscriber);
         }
@@ -26,8 +26,21 @@ public class NewSubscriberDetector {
     public static void notifyOmnitureWhenNewNewsLetterSubscriber(User user, OmnitureTracking ot){
         Set<Subscription> subs = user.getSubscriptions();
 
-        if (!SubscriptionUtil.userHasNewsLetterSubscriptions(subs)) {
+        if (!userHasNewsLetterSubscriptions(subs)) {
             ot.addSuccessEvent(OmnitureTracking.SuccessEvent.NewNewsLetterSubscriber);
         }
+    }
+
+    public static boolean userHasNewsLetterSubscriptions(Collection<Subscription> subs) {
+        boolean found = false;
+        if (subs != null){
+            for (Subscription subscription: subs){
+                if (subscription.getProduct().isNewsletter()){
+                    found = true;
+                    break;
+                }
+            }
+        }
+        return found;
     }
 }
