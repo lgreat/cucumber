831111233ad9c92fd98c4c329011841d29036b1d
yfan <yfan>
2008-12-31 23:16:43 +0000

    GS-7752 New page work in progress

---
+++ src/webapp/res/js/schoolChoice.js
@@ -0,0 +1,28 @@
+// Step 3
+
+function downloadWorksheet() {
+    var selector = document.getElementById('worksheet');
+    var path = selector.options[selector.selectedIndex].value;
+    alert(path);
+    var newWindow = window.open(path, '_blank');
+    newWindow.focus();
+    return false;
+}
+
+// Next
+
+function whatYourChildShouldKnow() {
+    var selector = document.getElementById('shouldKnow');
+    var articleId = selector.options[selector.selectedIndex].value;
+    window.location.href = '/cgi-bin/showarticle/' + articleId;
+    return false;
+}
+
+// GreatSchools Top 5
+
+function viewTopSchools() {
+    var selector = document.getElementById('top5State');
+    var longStateName = selector.options[selector.selectedIndex].innerHTML.toLowerCase();
+    window.location.href = '/top-high-schools/' + longStateName + '/';
+    return false;
+}
