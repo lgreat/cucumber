60c23c72776342a03d1ffce123ce64904688e67f
chriskimm <chriskimm>
2009-02-03 06:46:49 +0000

    GS-7921

--- src/java/gs/web/api/RegistrationController.java
+++ src/java/gs/web/api/RegistrationController.java
@@ -2,6 +2,10 @@ package gs.web.api;
 
 import org.springframework.web.servlet.mvc.SimpleFormController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import gs.data.api.IApiAccountDao;
 import gs.web.util.ReadWriteController;
@@ -9,13 +13,24 @@ import gs.web.util.ReadWriteController;
 /**
  * @author chriskimm@greatschools.net
  */
-public class RegistrationController extends SimpleFormController implements ReadWriteController {
+@Controller
+@RequestMapping("/api/registration.page")
+public class RegistrationController {
 
+    // Maps to the Spring form view
+    public static final String FORM_VIEW_NAME = "api/registration";
+
+    @Autowired
     private IApiAccountDao _apiAccountDao;
-    
-    @Override
-    protected ModelAndView onSubmit(Object o) throws Exception {
-        RegistrationCommand command = (RegistrationCommand)o;
-        return new ModelAndView(getSuccessView());
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String showForm() {
+        System.out.println ("_apiAccountDao: " + _apiAccountDao);
+        return "api/registration";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String processRegistration() {
+        return "api/registrationSuccess";
     }
 }
--- src/test/gs/web/api/RegistrationControllerTest.java
+++ src/test/gs/web/api/RegistrationControllerTest.java
@@ -1,21 +1,29 @@
 package gs.web.api;
 
-import gs.web.BaseControllerTestCase;
+//import gs.web.BaseControllerTestCase;
+import static org.junit.Assert.*;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.context.transaction.BeforeTransaction;
+
 
 /**
  * @author chriskimm
  */
-public class RegistrationControllerTest extends BaseControllerTestCase {
+public class RegistrationControllerTest { // extends BaseControllerTestCase {
 
     private RegistrationController _controller;
-
-    public void setUp() throws Exception {
-        super.setUp();
+    
+    @Before
+    public void init() throws Exception {
         _controller = new RegistrationController();
     }
 
+    @Test
     public void testShowForm() throws Exception {
-        // todo
+        String out = _controller.showForm();
+        assertEquals(RegistrationController.FORM_VIEW_NAME, out);
     }
 
     public void testFormSubmit() throws Exception {
