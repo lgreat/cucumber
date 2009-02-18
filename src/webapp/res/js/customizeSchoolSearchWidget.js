4e573373fde3fca0a6b43870e72db5d29fe44e96
npatury <npatury>
2009-02-18 03:11:12 +0000

    GS-7810 - Worked on making the changes to steps 1,2,3,4 visible immediately.

--- src/webapp/res/js/customizeSchoolSearchWidget.js
+++ src/webapp/res/js/customizeSchoolSearchWidget.js
@@ -12,6 +12,40 @@ function previewValid() {
     }
     return true;
 }
+
+
+function setIFrameSrc(){
+
+    var separator = "?";
+    var customizeForm = document.forms['customizeForm'];
+    var searchZipCode = customizeForm.searchQuery.value;
+    var textColor = customizeForm.textColor.value;
+    var borderColor = customizeForm.bordersColor.value;
+    var backgroundColor = customizeForm.backgroundColor.value;
+    var iFrameWidth = customizeForm.width.value -10;
+    var iFrameHeight = customizeForm.height.value -66;
+    var zoom = customizeForm.zoom.value;
+    //var path = "http://localhost:8080/schoolfinder/widget/customize.page";
+    var path = document.getElementById('widgetIFrame').src;
+    var url = path.substring(0,path.indexOf('?'));
+    var iFrameSrc = "http://localhost:8080/widget/schoolSearch.page";
+    var rval=iFrameSrc;
+    if (searchZipCode != '') {
+                rval += separator  + "searchQuery=" + searchZipCode;
+                separator = "&";
+            }
+       rval += separator  + "textColor=" +textColor;
+       rval += separator  + "bordersColor=" + borderColor;
+       rval += separator  + "width=" + iFrameWidth;
+       rval += separator  + "height=" + iFrameHeight;
+       rval += separator  + "zoom=" + zoom;
+    document.getElementById('widgetIFrame').src = rval;
+    document.getElementById('widgetIFrame').width = iFrameWidth;
+    document.getElementById('widgetIFrame').height = iFrameHeight;
+    document.getElementById('GS_schoolSearchWidget').style.width = iFrameWidth+2;
+    document.getElementById('GS_schoolSearchWidget').style.border = "solid 4px #"+ backgroundColor;
+}
+
 function inputsValid() {
     if (!previewValid()) {
         return false;
@@ -31,8 +65,10 @@ function inputsValid() {
 }
 
 function selectDim(width, height) {
+
     document.forms['customizeForm'].width.value=width;
     document.forms['customizeForm'].height.value=height;
+    setIFrameSrc();
 }
 
 function customDim() {
@@ -42,4 +78,6 @@ function customDim() {
 function updateColor(selectElement, inputElementId, hexValue) {
     selectElement.style.backgroundColor = "#" + hexValue;
     document.getElementById(inputElementId).value = hexValue;
+    setIFrameSrc();
+
 }
