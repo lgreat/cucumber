2e7039803dde79c3d950a9dfc378c7279fdbd0c6
npatury <npatury>
2009-02-23 22:02:47 +0000

    GS-7975

--- src/java/gs/web/about/DownloadLogoController.java
+++ src/java/gs/web/about/DownloadLogoController.java
@@ -27,6 +27,56 @@ public class DownloadLogoController implements Controller {
     final static String MEDIUM_FILE_NAME = "logo_GS_233x46.gif";
     final static String LARGE_FILE_NAME = "logo_GS_304x58.gif";
 
+    final static String IMAGE_PATH_BADGE = "/res/img/school/topSchools/badge/";
+    final static String PARAM_BADGE_TYPE_NAME = "badgeTypeAndName";
+
+    final static String PARAM_VAL_TOP_PERFORMING_HIGH_RES_JPEG = "topPerformingHighResJpg";
+    final static String PARAM_VAL_TOP_PERFORMING_LOW_RES_JPEG = "topPerformingLowResJpg";
+    final static String PARAM_VAL_TOP_PERFORMING_HIGH_RES_PNG = "topPerformingHighResPng";
+    final static String PARAM_VAL_TOP_PERFORMING_LOW_RES_PNG = "topPerformingLowResPng";
+    final static String PARAM_VAL_LOW_INCOME_HIGH_RES_JPEG = "lowIncomeHighResJpg";
+    final static String PARAM_VAL_LOW_INCOME_LOW_RES_JPEG = "lowIncomeLowResJpg";
+    final static String PARAM_VAL_LOW_INCOME_HIGH_RES_PNG = "lowIncomeHighResPng";
+    final static String PARAM_VAL_LOW_INCOME_LOW_RES_PNG = "lowIncomeLowResPng";
+    final static String PARAM_VAL_MOST_IMPROV_HIGH_RES_JPEG = "mostImprovedHighResJpg";
+    final static String PARAM_VAL_MOST_IMPROV_LOW_RES_JPEG = "mostImprovedLowResJpg";
+    final static String PARAM_VAL_MOST_IMPROV_HIGH_RES_PNG = "mostImprovedHighResPng";
+    final static String PARAM_VAL_MOST_IMPROV_LOW_RES_PNG = "mostImprovedLowResPng";
+    final static String PARAM_VAL_PUBLIC_SCHOOL_HIGH_RES_JPEG = "publicSchoolHighResJpg";
+    final static String PARAM_VAL_PUBLIC_SCHOOL_LOW_RES_JPEG = "publicSchoolLowResJpg";
+    final static String PARAM_VAL_PUBLIC_SCHOOL_HIGH_RES_PNG = "publicSchoolHighResPng";
+    final static String PARAM_VAL_PUBLIC_SCHOOL_LOW_RES_PNG = "publicSchoolLowResPng";
+    final static String PARAM_VAL_PRIVATE_SCHOOL_HIGH_RES_JPEG = "privateSchoolHighResJpg";
+    final static String PARAM_VAL_PRIVATE_SCHOOL_LOW_RES_JPEG = "privateSchoolLowResJpg";
+    final static String PARAM_VAL_PRIVATE_SCHOOL_HIGH_RES_PNG = "privateSchoolHighResPng";
+    final static String PARAM_VAL_PRIVATE_SCHOOL_LOW_RES_PNG = "privateSchoolLowResPng";
+
+    final static String TOP_PERFORMING_HIGH_RES_JPEG = "smbadge_highres_topperf.jpg";
+    final static String TOP_PERFORMING_LOW_RES_JPEG = "smbadge_lowres_topperf.jpg";
+    final static String TOP_PERFORMING_HIGH_RES_PNG = "smbadge_highres_topperf.png";
+    final static String TOP_PERFORMING_LOW_RES_PNG = "smbadge_lowres_topperf.png";
+
+    final static String LOW_INCOME_HIGH_RES_JPEG = "smbadge_highres_topperf_lowin.jpg";
+    final static String LOW_INCOME_LOW_RES_JPEG = "smbadge_lowres_topperf_lowin.jpg";
+    final static String LOW_INCOME_HIGH_RES_PNG = "smbadge_highres_topperf_lowin.png";
+    final static String LOW_INCOME_LOW_RES_PNG = "smbadge_lowres_topperf_lowin.png";
+
+    final static String MOST_IMPROV_HIGH_RES_JPEG = "smbadge_highres_improved.jpg";
+    final static String MOST_IMPROV_LOW_RES_JPEG = "smbadge_lowres_improved.jpg";
+    final static String MOST_IMPROV_HIGH_RES_PNG = "smbadge_highres_improved.png";
+    final static String MOST_IMPROV_LOW_RES_PNG = "smbadge_lowres_improved.png";
+
+    final static String PUBLIC_SCHOOL_HIGH_RES_JPEG = "smbadge_highres_pchoicepublic.jpg";
+    final static String PUBLIC_SCHOOL_LOW_RES_JPEG = "smbadge_lowres_pchoicepublic.jpg";
+    final static String PUBLIC_SCHOOL_HIGH_RES_PNG = "smbadge_highres_pchoicepublic.png";
+    final static String PUBLIC_SCHOOL_LOW_RES_PNG = "smbadge_lowres_pchoicepublic.png";
+
+    final static String PRIVATE_SCHOOL_HIGH_RES_JPEG = "smbadge_highres_pchoiceprivate.jpg";
+    final static String PRIVATE_SCHOOL_LOW_RES_JPEG = "smbadge_lowres_pchoiceprivate.jpg";
+    final static String PRIVATE_SCHOOL_HIGH_RES_PNG = "smbadge_highres_pchoiceprivate.png";
+    final static String PRIVATE_SCHOOL_LOW_RES_PNG = "smbadge_lowres_pchoiceprivate.png";
+
+
     public String chooseFileName(String size) {
         if (PARAM_VAL_SMALL.equals(size)) {
             return SMALL_FILE_NAME;
@@ -37,14 +87,70 @@ public class DownloadLogoController implements Controller {
         }
     }
 
+    public String chooseBadge(String typeAndName){
+
+         if(PARAM_VAL_TOP_PERFORMING_HIGH_RES_JPEG.equals(typeAndName)){
+             return TOP_PERFORMING_HIGH_RES_JPEG;
+         }else if(PARAM_VAL_TOP_PERFORMING_LOW_RES_JPEG.equals(typeAndName)){
+             return TOP_PERFORMING_LOW_RES_JPEG;
+         }else if(PARAM_VAL_TOP_PERFORMING_HIGH_RES_PNG.equals(typeAndName)){
+             return TOP_PERFORMING_HIGH_RES_PNG;
+         }else if(PARAM_VAL_TOP_PERFORMING_LOW_RES_PNG.equals(typeAndName)){
+             return TOP_PERFORMING_LOW_RES_PNG;
+         }else if(PARAM_VAL_LOW_INCOME_HIGH_RES_JPEG.equals(typeAndName)){
+             return LOW_INCOME_HIGH_RES_JPEG;
+         }else if(PARAM_VAL_LOW_INCOME_LOW_RES_JPEG.equals(typeAndName)){
+             return LOW_INCOME_LOW_RES_JPEG;
+         }else if(PARAM_VAL_LOW_INCOME_HIGH_RES_PNG.equals(typeAndName)){
+             return LOW_INCOME_HIGH_RES_PNG;
+         }else if(PARAM_VAL_LOW_INCOME_LOW_RES_PNG.equals(typeAndName)){
+             return LOW_INCOME_LOW_RES_PNG;
+         }else if(PARAM_VAL_MOST_IMPROV_HIGH_RES_JPEG.equals(typeAndName)){
+             return MOST_IMPROV_HIGH_RES_JPEG;
+         }else if(PARAM_VAL_MOST_IMPROV_LOW_RES_JPEG.equals(typeAndName)){
+             return MOST_IMPROV_LOW_RES_JPEG;
+         }else if(PARAM_VAL_MOST_IMPROV_HIGH_RES_PNG.equals(typeAndName)){
+             return MOST_IMPROV_HIGH_RES_PNG;
+         }else if(PARAM_VAL_MOST_IMPROV_LOW_RES_PNG.equals(typeAndName)){
+             return MOST_IMPROV_LOW_RES_PNG;
+         }else if(PARAM_VAL_PUBLIC_SCHOOL_HIGH_RES_JPEG.equals(typeAndName)){
+             return PUBLIC_SCHOOL_HIGH_RES_JPEG;
+         }else if(PARAM_VAL_PUBLIC_SCHOOL_LOW_RES_JPEG.equals(typeAndName)){
+             return PUBLIC_SCHOOL_LOW_RES_JPEG;
+         }else if(PARAM_VAL_PUBLIC_SCHOOL_HIGH_RES_PNG.equals(typeAndName)){
+             return PUBLIC_SCHOOL_HIGH_RES_PNG;
+         }else if(PARAM_VAL_PUBLIC_SCHOOL_LOW_RES_PNG.equals(typeAndName)){
+             return PUBLIC_SCHOOL_LOW_RES_PNG;
+         }else if(PARAM_VAL_PRIVATE_SCHOOL_HIGH_RES_JPEG.equals(typeAndName)){
+             return PRIVATE_SCHOOL_HIGH_RES_JPEG;
+         }else if(PARAM_VAL_PRIVATE_SCHOOL_LOW_RES_JPEG.equals(typeAndName)){
+             return PRIVATE_SCHOOL_LOW_RES_JPEG;
+         }else if(PARAM_VAL_PRIVATE_SCHOOL_HIGH_RES_PNG.equals(typeAndName)){
+             return PRIVATE_SCHOOL_HIGH_RES_PNG;
+         }else{
+             return PRIVATE_SCHOOL_LOW_RES_PNG;
+         }
+    }
+
     public ModelAndView handleRequest(HttpServletRequest request,
-                                                 HttpServletResponse response) throws Exception {
+                                                 HttpServletResponse response) throws Exception {            
         // determine which size logo was requested
         String size = request.getParameter(PARAM_SIZE);
+        String fileName = "";
+        String filePath = "";
 
-        // set the file name and file path
-        String fileName = chooseFileName(size);
-        String filePath = IMAGE_PATH + fileName;
+        //size parameter is passed only for the download of GreatSchools Logo and
+        // not for the downloads of Top Schools Bagdes.
+        if(size != null){
+            // set the file name and file path
+            fileName = chooseFileName(size);
+            filePath = IMAGE_PATH + fileName;
+        }else{
+           String type = request.getParameter(PARAM_BADGE_TYPE_NAME);
+           fileName = chooseBadge(type);
+           filePath = IMAGE_PATH_BADGE +fileName;
+        }
+
 
         // set response headers
         response.setContentType("image/gif");
