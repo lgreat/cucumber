0776d0a49dd426dc76b257b4ddb8e755cc4858e7
npatury <npatury>
2009-03-04 00:25:06 +0000

    GS-8036,GS-8058 - worked on the middle school landing page

--- src/java/gs/web/content/MiddleLandingPageController.java
+++ src/java/gs/web/content/MiddleLandingPageController.java
@@ -8,6 +8,8 @@ import java.util.Map;
 public class MiddleLandingPageController extends AbstractGradeLevelLandingPageController {
 
     protected void populateModel(Map<String, Object> model, HttpServletRequest request) {
+        loadTableRowsIntoModel(model,"ms_support_classroom");
+        loadTableRowsIntoModel(model,"ms_support_hallway");
         loadTableRowsIntoModel(model,"ms_college");
     }
 }
