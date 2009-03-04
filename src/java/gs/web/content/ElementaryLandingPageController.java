27ea8d72ceca1c29a4b1d9d3711a9846665cf09e
aroy <aroy>
2009-03-04 01:07:20 +0000

    GS-8057 find and compare module

--- src/java/gs/web/content/ElementaryLandingPageController.java
+++ src/java/gs/web/content/ElementaryLandingPageController.java
@@ -1,27 +1,11 @@
 package gs.web.content;
 
-import org.springframework.web.servlet.mvc.AbstractController;
-import org.springframework.web.servlet.ModelAndView;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import gs.data.util.table.ITableDao;
-import gs.data.util.table.ITableRow;
-import gs.data.util.NameValuePair;
 import gs.data.state.State;
-import gs.web.util.google.GoogleSpreadsheetDao;
-import gs.web.util.UrlUtil;
 import gs.web.util.context.SessionContextUtil;
 import gs.web.util.context.SessionContext;
 
 import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
 
 /**
  * @author Anthony Roy <mailto:aroy@greatschools.net>
@@ -34,7 +18,8 @@ public class ElementaryLandingPageController extends AbstractGradeLevelLandingPa
         loadTableRowsIntoModel(model,"3");
         loadTableRowsIntoModel(model,"4");
         loadTableRowsIntoModel(model,"5");
-         SessionContext context = SessionContextUtil.getSessionContext(request);
+
+        SessionContext context = SessionContextUtil.getSessionContext(request);
         String userCityName = "Los Angeles";
         State userState = context.getStateOrDefault();
         if (context.getCity() != null) {
