041a9f49768eaec0330341ab387b2e8e38c74d5e
chriskimm <chriskimm>
2009-03-17 23:48:18 +0000

    GS-7985

--- src/java/gs/web/api/ReportTagHandler.java
+++ src/java/gs/web/api/ReportTagHandler.java
@@ -1,7 +1,6 @@
 package gs.web.api;
 
 import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.params.HttpMethodParams;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.core.io.ClassPathResource;
@@ -33,19 +32,12 @@ public class ReportTagHandler extends SimpleTagSupport {
     public void doTag() throws JspException, IOException {
         getHttpClient().executeMethod(getMethod());
         String response = getMethod().getResponseBodyAsString();
-        //getJspContext().getOut().write(response);
-        //System.out.println ("response: " + response);
-        //Source xsltSource = new StreamSource(new StringReader(xslt));
         Source source = new StreamSource(new StringReader(response));
         Source xsltSource = getXsltSource();
-
-//        ClassPathResource cpr = new ClassPathResource("gs/web/api/reports.xsl");
-
         JspWriter out = getJspContext().getOut();
         StringWriter wr = new StringWriter();
 
         try {
-//            gs.data.util.XMLUtil.transform(source, cpr.getInputStream(), wr);
             gs.data.util.XMLUtil.transform(source, xsltSource, new StreamResult(wr));
             out.println(wr.getBuffer().toString());
         } catch (Exception e) {
@@ -75,22 +67,28 @@ public class ReportTagHandler extends SimpleTagSupport {
     String getReportUrl() {
         StringBuilder sb = new StringBuilder();
         sb.append("http://");
+
+        PageContext pc;
         if (getJspContext() instanceof PageContext) {
-            PageContext pc = (PageContext)getJspContext();
-            String host = pc.getRequest().getLocalName();
-            if (host.contains("dev.")) {
-                sb.append("api.dev.greatschols.net");
-            } else if (host.contains("staging.")) {
-                sb.append("api.staging.greatschols.net");
-            } else if (UrlUtil.isDeveloperWorkstation(host)) {
-                sb.append(host);
-                String port = String.valueOf(pc.getRequest().getLocalPort());
-                if (StringUtils.isNotBlank(port) && !"80".equals(port)) {
-                    sb.append(":").append(port);
-                }
-                sb.append("/apiservice");
-            }
+            pc = (PageContext)getJspContext();
+        } else {
+            pc = (PageContext) getJspContext().findAttribute(PageContext.PAGECONTEXT);
         }
+
+        String host = pc.getRequest().getLocalName();
+        if (host.contains("dev.")) {
+            sb.append("api.dev.greatschols.net");
+        } else if (host.contains("staging.")) {
+            sb.append("api.staging.greatschols.net");
+        } else if (UrlUtil.isDeveloperWorkstation(host)) {
+            sb.append(host);
+            String port = String.valueOf(pc.getRequest().getLocalPort());
+            if (StringUtils.isNotBlank(port) && !"80".equals(port)) {
+                sb.append(":").append(port);
+            }
+            sb.append("/apiservice");
+        }
+
         sb.append("/reports/").append(getType());
         return sb.toString();
     }
@@ -98,7 +96,6 @@ public class ReportTagHandler extends SimpleTagSupport {
     // Getter and Setter on the _method field is required for unit testing.
     GetMethod getMethod() {
         if (_method == null) {
-            System.out.println ("url: " + getReportUrl());
             _method = new GetMethod(getReportUrl());
             _method.setRequestHeader("Accept", "application/xml");
             _method.setQueryString("key=" + getKey());
