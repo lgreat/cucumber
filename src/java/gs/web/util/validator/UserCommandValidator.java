87110c90141badfb09025445f300dcdb451a5e4c
npatury <npatury>
2009-06-08 20:12:21 +0000

    GS-8290 - the terms error does not appear for the chooser registration hover

--- src/java/gs/web/util/validator/UserCommandValidator.java
+++ src/java/gs/web/util/validator/UserCommandValidator.java
@@ -88,9 +88,7 @@ public class UserCommandValidator implements IRequestAwareValidator {
         validateUsername(command, user, errors);
         validateGender(command, errors);
         //validateNumSchoolChildren(command, errors);
-        if (!command.isChooserRegistration()) {
-            validateTerms(command, errors);
-        }
+        validateTerms(command, errors);
         validatePassword(command, errors);
         if (command.isChooserRegistration()) {
             validateSchoolChoiceStateCity(command, errors);
