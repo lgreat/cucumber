ccaebb9e4428b9a983e0ce23b9e75dfbbd39b440
aroy <aroy>
2009-06-10 23:15:19 +0000

    GS-8291 Unhook old mss hover behavior, refactor mss hover js a bit for perl, change logic to trigger on first exit

---
+++ src/webapp/res/js/mssAutoHoverInterceptor.js
@@ -0,0 +1,43 @@
+// GS-8291
+// depends on submodal.js and cookie.js
+var mssAutoHoverInterceptor = {
+    interceptPercent: 0,
+    hoverName: '',
+    hoverUrl: '',
+    hoverHeight: 0,
+    hoverWidth: 0,
+    newHoverName: '',
+    cookieName: '',
+    cookieProperty: '',
+    userLoggedInCookieName: '',
+    ajaxRequest: null,
+    shouldIntercept: function(hoverName) {
+        if (this.hoverName != hoverName){
+            return false;
+        }
+        if (this.userIsLoggedIn(this.userLoggedInCookieName)){
+            return false;
+        }
+
+        var dataObject = subCookie.getObject(this.cookieName);
+        // if no cookie, set value to 1 and show hover
+        if (dataObject == undefined || dataObject[this.cookieProperty] == undefined){
+            subCookie.setObjectProperty(this.cookieName, this.cookieProperty, 1,1000);
+            return true;
+        }
+        // else no-op
+        return false;
+    },
+    init: function(){
+        this.hoverName = 'mssAutoHover';
+        this.cookieName = 'mssAutoHover';
+        this.cookieProperty = 'GS-8290';
+        this.userIsLoggedIn = GSType.submodal.interceptor.userIsLoggedIn;
+        this.userLoggedInCookieName = 'MEMID';
+    },
+    userShouldSeeAlternateHover: function(){return false;},
+    generateRandomNumber: function(){return 0;},
+    userIsLoggedIn: function(){return false;}
+};
+
+mssAutoHoverInterceptor.init();
