790ba9725ee1321c5d7a84ccb62d0d14f536469c
yfan <yfan>
2009-08-03 20:47:10 +0000

    GS-8430 B2S poll landing page - reversed order of 3rd chart, changed sidebar image, changed Special Report image, removed Quick Takeaways header text, changed article comments to use legacy article ID 10000

--- src/java/gs/web/backToSchool/BackToSchoolPollResultsController.java
+++ src/java/gs/web/backToSchool/BackToSchoolPollResultsController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import gs.data.content.ArticleComment;
 import gs.data.content.IArticleDao;
+import gs.web.util.UrlBuilder;
 
 public class BackToSchoolPollResultsController extends AbstractController {
     private IArticleDao _legacyArticleDao;
@@ -19,8 +20,13 @@ public class BackToSchoolPollResultsController extends AbstractController {
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
         Map<String, Object> model = new HashMap<String, Object>();
 
-        List<ArticleComment> comments = _legacyArticleDao.getArticleComments(10000L);
+        long legacyArticleId = 10000L;
+        List<ArticleComment> comments = _legacyArticleDao.getArticleComments(legacyArticleId);
         model.put("comments", comments);
+
+        UrlBuilder builder = new UrlBuilder(UrlBuilder.B2S_POLL_LANDING_PAGE);
+        model.put("contentUrl", builder.asFullUrl(request));
+        model.put("legacyArticleId", String.valueOf(legacyArticleId));
 
         return new ModelAndView(_viewName, model);
     }
