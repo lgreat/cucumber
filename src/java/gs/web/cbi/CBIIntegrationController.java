8d508aab161c0974bf43b85cec0eae43d2d00200
chriskimm <chriskimm>
2009-08-11 21:29:02 +0000

    New CBI Integration controller stub + fixes for errant check-in.

--- src/java/gs/web/cbi/CBIIntegrationController.java
+++ src/java/gs/web/cbi/CBIIntegrationController.java
@@ -7,14 +7,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import gs.data.integration.exacttarget.ExactTargetAPI;
+import gs.data.community.User;
 
 import java.io.PrintWriter;
 
 public class CBIIntegrationController implements Controller {
 
     /** Spring BEAN id */
-    public static final String BEAN_ID = "cbiIntegrationController";
+    public static final String BEAN_ID = "/cbi/integration.page";
 
+    private static final String KEY_PARAM = "key";
+    private static final String ACTION_PARAM = "action";
     private static final String SECRET_KEY = "cbounder";
 
     /** Keys to the actions supported by this controller */
@@ -26,11 +29,11 @@ public class CBIIntegrationController implements Controller {
                                       HttpServletResponse response) throws Exception {
         PrintWriter out = response.getWriter();
 
-        String secretKey = request.getParameter("key");
+        String secretKey = request.getParameter(KEY_PARAM);
         if (SECRET_KEY.equals(secretKey)) {
-            String action = request.getParameter("action");
+            String action = request.getParameter(ACTION_PARAM);
             if (SEND_TRIGGERED_EMAIL.equals(action)) {
-                out.print(sendTargetTriggeredEmail());
+                out.print(sendTargetTriggeredEmail(request));
             }
         }
         response.setContentType("text/plain");
@@ -38,8 +41,15 @@ public class CBIIntegrationController implements Controller {
         return null;
     }
 
-    protected String sendTargetTriggeredEmail() {
+    protected String sendTargetTriggeredEmail(HttpServletRequest request) {
+        String exactTargetKey = request.getParameter("etkey");
+        String emailAddress = request.getParameter("email");
+        User u = new User();
+        u.setEmail(emailAddress);
         StringBuilder response = new StringBuilder();
+        //getExactTargetAPI().sendTriggeredEmail(exactTargetKey , user);
+        // Nanditha, can you put whatever you think is appropriate in the response
+        // string.
         return response.toString();
     }
 
