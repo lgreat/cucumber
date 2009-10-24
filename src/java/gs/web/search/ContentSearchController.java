1b6aa84682545cfdadbc3dd15a50f8b52334c824
yfan <yfan>
2009-10-24 03:16:14 +0000

    GS-8876 article and community results

--- src/java/gs/web/search/ContentSearchController.java
+++ src/java/gs/web/search/ContentSearchController.java
@@ -8,11 +8,11 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.ArrayList;
+import java.util.*;
 
 import gs.web.util.UrlBuilder;
+import gs.data.search.ContentSearchResult;
+import gs.data.content.cms.ContentKey;
 
 /**
  * GS-8876
@@ -36,6 +36,7 @@ public class ContentSearchController extends AbstractController {
     public static final String MODEL_COMMUNITY_RESULTS = "communityResults";
     public static final String MODEL_URL = "url";
     public static final String MODEL_URL_WITHOUT_PAGE_NUM = "urlWithoutPageNum";
+    public static final String MODEL_CURRENT_DATE = "currentDate";
 
     public static final String PARAM_SEARCH_QUERY = "q";
     public static final String PARAM_PAGE = "page";
@@ -52,6 +53,7 @@ public class ContentSearchController extends AbstractController {
     public static final String SAMPLE_ALL_RESULTS = "allResults";
 
     public static final int PAGE_SIZE = 25;
+    public static final int ALL_RESULTS_PAGE_SIZE = 5;
 
     private String _viewName;
 
@@ -68,16 +70,10 @@ public class ContentSearchController extends AbstractController {
 
         if (isSample) {
             searchQuery = "friendship";
-            populateModelWithSampleResults(model, searchQuery, page, type, sample);
+            populateModelWithSampleResults(model, page, type, sample);
         } else {
             // TODO-8876 use Solr to search
             model.put(MODEL_SUGGESTED_SEARCH_QUERY, "friendship");
-            /*
-            model.put(MODEL_NUM_RESULTS, 0);
-            model.put(MODEL_NUM_ARTICLES, 0);
-            model.put(MODEL_NUM_DISCUSSIONS, 0);
-            model.put(MODEL_TOTAL_PAGES, 0);
-            */
         }
 
         model.put(MODEL_SEARCH_QUERY, searchQuery);
@@ -94,23 +90,32 @@ public class ContentSearchController extends AbstractController {
             type = null;
         }
         model.put(MODEL_TYPE, type);
+
+        // TODO-8876 see if we can get only ALL_RESULTS_PAGE_SIZE results instead of getting
+        // PAGE_SIZE results and then truncating
+        // also, this is a weird way of getting the right number of results
+        if (numArticles > 0 && numDiscussions > 0 && type == null) {
+            List<ContentSearchResult> articleResults = (List<ContentSearchResult>)model.get(MODEL_ARTICLE_RESULTS);
+            articleResults = articleResults.subList(0, Math.min(ALL_RESULTS_PAGE_SIZE, numArticles));
+            model.put(MODEL_ARTICLE_RESULTS, articleResults);
+
+            List<ContentSearchResult> communityResults = (List<ContentSearchResult>)model.get(MODEL_COMMUNITY_RESULTS);
+            communityResults = communityResults.subList(0, Math.min(ALL_RESULTS_PAGE_SIZE, numDiscussions));
+            model.put(MODEL_COMMUNITY_RESULTS, communityResults);
+        }
         
         model.put(MODEL_URL, getUrl(request, searchQuery, page, type, sample));
         model.put(MODEL_URL_WITHOUT_PAGE_NUM, getUrlWithoutPageNumber(request, searchQuery, type, sample));
 
         model.put(MODEL_TOTAL_PAGES, getTotalPages(numArticles, numDiscussions, type));
-
-        // TODO-8876 fix me!
-        model.put(MODEL_ARTICLE_RESULTS, new ArrayList());
-        model.put(MODEL_COMMUNITY_RESULTS, new ArrayList());
+        model.put(MODEL_CURRENT_DATE, new Date());
 
         return new ModelAndView(_viewName, model);
     }
 
-    protected void populateModelWithSampleResults(Map<String, Object> model, String searchQuery, int page, String filter, String sample) {
+    protected void populateModelWithSampleResults(Map<String, Object> model, int page, String type, String sample) {
         int numArticles;
         int numDiscussions;
-        int totalPages = 0;
         String pageTitlePrefix;
         if (SAMPLE_NO_ARTICLES.equals(sample)) {
             numArticles = 0;
@@ -123,9 +128,9 @@ public class ContentSearchController extends AbstractController {
         } else if (SAMPLE_ALL_RESULTS.equals(sample)) {
             numArticles = 68;
             numDiscussions = 173;
-            if (TYPE_ARTICLES.equals(filter)) {
+            if (TYPE_ARTICLES.equals(type)) {
                 pageTitlePrefix = "Article results for";
-            } else if (TYPE_COMMUNITY.equals(filter)) {
+            } else if (TYPE_COMMUNITY.equals(type)) {
                 pageTitlePrefix = "Community results for";
             } else {
                 pageTitlePrefix = "Results for";
@@ -134,11 +139,15 @@ public class ContentSearchController extends AbstractController {
             // SAMPLE_NO_RESULTS
             numArticles = 0;
             numDiscussions = 0;
-            totalPages = 0;
             pageTitlePrefix = null;
             model.put(MODEL_SUGGESTED_SEARCH_QUERY, "friendship");
-            model.put(MODEL_TOTAL_PAGES, 2);
         }
+
+        List<ContentSearchResult> articleResults = getResultsForPage(getSampleArticles(numArticles), page);
+        List<ContentSearchResult> communityResults = getResultsForPage(getSampleDiscussions(numDiscussions), page);
+
+        model.put(MODEL_ARTICLE_RESULTS, articleResults);
+        model.put(MODEL_COMMUNITY_RESULTS, communityResults);
 
         model.put(MODEL_NUM_RESULTS, String.valueOf(numArticles + numDiscussions));
         model.put(MODEL_NUM_ARTICLES, numArticles);
@@ -146,7 +155,47 @@ public class ContentSearchController extends AbstractController {
         model.put(MODEL_PAGE_TITLE_PREFIX, pageTitlePrefix);
     }
 
-    protected int getTotalPages(int numArticles, int numDiscussions, String type) {
+    protected static List<ContentSearchResult> getResultsForPage(List<ContentSearchResult> results, int page) {
+        int fromIndex = (page-1) * PAGE_SIZE;
+        int toIndex = page * PAGE_SIZE;
+        return results.subList(fromIndex, Math.min(toIndex, results.size()));
+    }
+
+    protected static List<ContentSearchResult> getSampleArticles(int numArticles) {
+        List<ContentSearchResult> articles = new ArrayList<ContentSearchResult>();
+        for (int i = 1; i <= numArticles; i++) {
+            ContentSearchResult result = new ContentSearchResult();
+            result.setContentKey(new ContentKey("Article",(long)i));
+            result.setResultSummary("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam urna diam, consectetur a volutpat sed, convallis in sem. Nam vitae tortor ultrices felis sodales ultricies. Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus placerat, nibh vitae aliquam vestibulum, leo felis auctor nisl, vulputate condimentum tellus lacus nec orci. Aliquam mollis, lacus sit amet cursus posuere, diam tortor placerat lectus, ac placerat massa mi et mi. Nulla at nibh erat, ut rutrum magna. Integer rhoncus enim non ipsum lobortis eu tristique quam fermentum. Integer tempus consectetur fringilla. Donec mattis vehicula mollis.");
+            result.setTitle("This is the title of article " + i);
+            result.setFullUri("article-full-uri");
+            articles.add(result);
+        }
+        return articles;
+    }
+
+    protected static List<ContentSearchResult> getSampleDiscussions(int numDiscussions) {
+        List<ContentSearchResult> discussions = new ArrayList<ContentSearchResult>();
+        for (int i = 1; i <= numDiscussions; i++) {
+            ContentSearchResult result = new ContentSearchResult();
+            result.setContentKey(new ContentKey("Discussion",(long)i));
+            result.setResultSummary("Vivamus ullamcorper accumsan convallis. Aliquam adipiscing, arcu eu adipiscing viverra, nulla tellus tempor dui, sit amet accumsan nibh ligula non nibh. Nam ornare congue rhoncus. In aliquet eleifend lectus vitae malesuada. Donec mattis nibh ac augue dapibus condimentum. Cras velit elit, dignissim id tempus et, imperdiet quis enim. Proin vitae velit risus. Etiam eget elit ut nibh adipiscing tincidunt. Cras augue lorem, egestas ut vestibulum a, suscipit quis turpis. Aliquam pretium mollis commodo. Morbi id turpis metus, in porttitor metus.");
+            result.setTitle("This is the title of discussion " + i);
+            result.setUsername("chriskimm");
+            result.setDiscussionBoardId(14L);
+            result.setDiscussionBoardName("Discussion Board Name");
+            result.setFullUri("discussion-full-uri");
+            result.setNumReplies((i * 7) % 19);
+            Calendar date = Calendar.getInstance();
+            // 15 days ago
+            date.add(Calendar.DATE, -15);
+            result.setDateCreated(date.getTime());
+            discussions.add(result);
+        }
+        return discussions;
+    }
+
+    protected static int getTotalPages(int numArticles, int numDiscussions, String type) {
         int totalPages;
         if (numArticles > 0 && numDiscussions > 0) {
             if (TYPE_ARTICLES.equals(type)) {
@@ -178,12 +227,12 @@ public class ContentSearchController extends AbstractController {
         return totalPages;
     }
 
-    protected String getUrlWithoutPageNumber(HttpServletRequest request, String searchQuery, String type, String sample) {
+    protected static String getUrlWithoutPageNumber(HttpServletRequest request, String searchQuery, String type, String sample) {
         UrlBuilder builder = new UrlBuilder(UrlBuilder.CONTENT_SEARCH, searchQuery, (Integer)null, type, sample);
         return builder.asSiteRelative(request);
     }
 
-    protected String getUrl(HttpServletRequest request, String searchQuery, int page, String type, String sample) {
+    protected static String getUrl(HttpServletRequest request, String searchQuery, int page, String type, String sample) {
         UrlBuilder builder = new UrlBuilder(UrlBuilder.CONTENT_SEARCH, searchQuery, page, type, sample);
         return builder.asSiteRelative(request);
     }
@@ -191,7 +240,7 @@ public class ContentSearchController extends AbstractController {
     /**
      * Extract the page number from the request. Defaults to 1.
      */
-    protected int getPageNumber(HttpServletRequest request) {
+    protected static int getPageNumber(HttpServletRequest request) {
         int page = 1;
         String pageParam = request.getParameter(PARAM_PAGE);
         if (pageParam != null) {
@@ -207,7 +256,7 @@ public class ContentSearchController extends AbstractController {
     /**
      * Extract the content type filter from the request. Defaults to null (no type filter).
      */
-    protected String getType(HttpServletRequest request) {
+    protected static String getType(HttpServletRequest request) {
         String type;
         String typeParam = request.getParameter(PARAM_TYPE);
         if (TYPE_COMMUNITY.equals(typeParam)) {
