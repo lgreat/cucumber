da6a24c923a5ad385336f0d88c47f3172b48ff10
yfan <yfan>
2009-10-28 00:41:59 +0000

    GS-8876 Work in progress - faceting for counts

--- src/java/gs/web/search/ContentSearchController.java
+++ src/java/gs/web/search/ContentSearchController.java
@@ -3,6 +3,9 @@ package gs.web.search;
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.lang.StringUtils;
+import org.apache.solr.client.solrj.response.FacetField;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
 import org.springframework.web.servlet.mvc.AbstractController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,6 +17,7 @@ import gs.web.util.UrlBuilder;
 import gs.data.search.ContentSearchResult;
 import gs.data.search.SolrService;
 import gs.data.content.cms.ContentKey;
+import gs.data.content.cms.CmsConstants;
 
 /**
  * GS-8876
@@ -82,11 +86,11 @@ public class ContentSearchController extends AbstractController {
         model.put(MODEL_PAGE_SIZE, PAGE_SIZE);
         model.put(MODEL_SAMPLE, sample);
 
-        int numArticles = (Integer)model.get(MODEL_NUM_ARTICLES);
+        long numArticles = (Long)model.get(MODEL_NUM_ARTICLES);
         if (TYPE_ARTICLES.equals(type) && numArticles == 0) {
             type = null;
         }
-        int numDiscussions = (Integer)model.get(MODEL_NUM_DISCUSSIONS);
+        long numDiscussions = (Long)model.get(MODEL_NUM_DISCUSSIONS);
         if (TYPE_COMMUNITY.equals(type) && numDiscussions == 0) {
             type = null;
         }
@@ -97,11 +101,11 @@ public class ContentSearchController extends AbstractController {
         // also, this is a weird way of getting the right number of results
         if (numArticles > 0 && numDiscussions > 0 && type == null) {
             List<ContentSearchResult> articleResults = (List<ContentSearchResult>)model.get(MODEL_ARTICLE_RESULTS);
-            articleResults = articleResults.subList(0, Math.min(ALL_RESULTS_PAGE_SIZE, numArticles));
+            articleResults = articleResults.subList(0, (int)Math.min(ALL_RESULTS_PAGE_SIZE, numArticles));
             model.put(MODEL_ARTICLE_RESULTS, articleResults);
 
             List<ContentSearchResult> communityResults = (List<ContentSearchResult>)model.get(MODEL_COMMUNITY_RESULTS);
-            communityResults = communityResults.subList(0, Math.min(ALL_RESULTS_PAGE_SIZE, numDiscussions));
+            communityResults = communityResults.subList(0, (int)Math.min(ALL_RESULTS_PAGE_SIZE, numDiscussions));
             model.put(MODEL_COMMUNITY_RESULTS, communityResults);
         }
         
@@ -115,13 +119,29 @@ public class ContentSearchController extends AbstractController {
     }
 
     protected void populateModelForQuery(Map<String, Object> model, int page, String type, String searchQuery) {
-        int numResults;
-        int numArticles;
-        int numDiscussions;
+        long numResults;
+        long numArticles = 0;
+        long numDiscussions = 0;
         String pageTitlePrefix;
         try {
             // TODO-8876 use Solr to search
-            List<ContentSearchResult> results = _solrService.getResults(searchQuery);
+            QueryResponse rsp = _solrService.getResults(searchQuery);
+            List<FacetField> facetFields = rsp.getFacetFields();
+
+            FacetField contentTypeFacet = rsp.getFacetField(SolrService.FIELD_CONTENT_TYPE);
+            for (FacetField.Count count : contentTypeFacet.getValues()) {
+                if (CmsConstants.ARTICLE_CONTENT_TYPE.equals(count.getName()) ||
+                    CmsConstants.ARTICLE_SLIDESHOW_CONTENT_TYPE.equals(count.getName()) ||
+                    CmsConstants.ASK_THE_EXPERTS_CONTENT_TYPE.equals(count.getName())) {
+                    numArticles += count.getCount();
+                } else if ("Discussion".equals(count.getName())) {
+                    numDiscussions = count.getCount(); 
+                }
+            }
+
+            SolrDocumentList docs = rsp.getResults();
+            List<ContentSearchResult> results = rsp.getBeans(ContentSearchResult.class);
+
             List<ContentSearchResult> articleResults = new ArrayList<ContentSearchResult>();
             List<ContentSearchResult> communityResults = new ArrayList<ContentSearchResult>();
             for (ContentSearchResult result : results) {
@@ -131,15 +151,15 @@ public class ContentSearchController extends AbstractController {
                     articleResults.add(result);
                 }
             }
-            numArticles = articleResults.size();
-            numDiscussions = communityResults.size();
+            //numArticles = articleResults.size();
+            //numDiscussions = communityResults.size();
             numResults = numArticles + numDiscussions;
 
             // TODO-8876 remove me
             //System.out.println("====== numArticles = " + numArticles + ", numDiscussions = " + numDiscussions + ", numResults = " + numResults);
 
             if (numResults == 0) {
-                // TODO-8876 use Solr to search
+                // TODO-8876 use Solr spell check plugin to suggest alternate query
                 model.put(MODEL_SUGGESTED_SEARCH_QUERY, "friendship");
             }
 
@@ -158,8 +178,8 @@ public class ContentSearchController extends AbstractController {
         }
     }
 
-    protected String getPageTitlePrefix(int numArticles, int numDiscussions, String type) {
-        int numResults = numArticles + numDiscussions;
+    protected String getPageTitlePrefix(long numArticles, long numDiscussions, String type) {
+        long numResults = numArticles + numDiscussions;
         if (numResults == 0) {
             return null;
         } else if (numResults == numArticles || numResults == numDiscussions) {
@@ -174,9 +194,9 @@ public class ContentSearchController extends AbstractController {
     }
 
     protected void populateModelForSample(Map<String, Object> model, int page, String type, String sample) {
-        int numResults;
-        int numArticles;
-        int numDiscussions;
+        long numResults;
+        long numArticles;
+        long numDiscussions;
         String pageTitlePrefix;
         if (SAMPLE_NO_ARTICLES.equals(sample)) {
             numArticles = 0;
@@ -215,9 +235,9 @@ public class ContentSearchController extends AbstractController {
         return results.subList(fromIndex, Math.min(toIndex, results.size()));
     }
 
-    protected static List<ContentSearchResult> getSampleArticles(int numArticles) {
+    protected static List<ContentSearchResult> getSampleArticles(long numArticles) {
         List<ContentSearchResult> articles = new ArrayList<ContentSearchResult>();
-        for (int i = 1; i <= numArticles; i++) {
+        for (long i = 1; i <= numArticles; i++) {
             ContentSearchResult result = new ContentSearchResult();
             result.setContentKey(new ContentKey("Article",(long)i));
             result.setResultSummary("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam urna diam, consectetur a volutpat sed, convallis in sem. Nam vitae tortor ultrices felis sodales ultricies. Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus placerat, nibh vitae aliquam vestibulum, leo felis auctor nisl, vulputate condimentum tellus lacus nec orci. Aliquam mollis, lacus sit amet cursus posuere, diam tortor placerat lectus, ac placerat massa mi et mi. Nulla at nibh erat, ut rutrum magna. Integer rhoncus enim non ipsum lobortis eu tristique quam fermentum. Integer tempus consectetur fringilla. Donec mattis vehicula mollis.");
@@ -228,9 +248,9 @@ public class ContentSearchController extends AbstractController {
         return articles;
     }
 
-    protected static List<ContentSearchResult> getSampleDiscussions(int numDiscussions) {
+    protected static List<ContentSearchResult> getSampleDiscussions(long numDiscussions) {
         List<ContentSearchResult> discussions = new ArrayList<ContentSearchResult>();
-        for (int i = 1; i <= numDiscussions; i++) {
+        for (long i = 1; i <= numDiscussions; i++) {
             ContentSearchResult result = new ContentSearchResult();
             result.setContentKey(new ContentKey("Discussion",(long)i));
             result.setResultSummary("Vivamus ullamcorper accumsan convallis. Aliquam adipiscing, arcu eu adipiscing viverra, nulla tellus tempor dui, sit amet accumsan nibh ligula non nibh. Nam ornare congue rhoncus. In aliquet eleifend lectus vitae malesuada. Donec mattis nibh ac augue dapibus condimentum. Cras velit elit, dignissim id tempus et, imperdiet quis enim. Proin vitae velit risus. Etiam eget elit ut nibh adipiscing tincidunt. Cras augue lorem, egestas ut vestibulum a, suscipit quis turpis. Aliquam pretium mollis commodo. Morbi id turpis metus, in porttitor metus.");
@@ -239,7 +259,6 @@ public class ContentSearchController extends AbstractController {
             result.setDiscussionBoardId(14L);
             result.setDiscussionBoardTitle("Discussion Board Name");
             result.setFullUri("discussion-full-uri");
-            result.setNumReplies((i * 7) % 19);
             Calendar date = Calendar.getInstance();
             // 15 days ago
             date.add(Calendar.DATE, -15);
@@ -249,8 +268,8 @@ public class ContentSearchController extends AbstractController {
         return discussions;
     }
 
-    protected static int getTotalPages(int numArticles, int numDiscussions, String type) {
-        int totalPages;
+    protected static long getTotalPages(long numArticles, long numDiscussions, String type) {
+        long totalPages;
         if (numArticles > 0 && numDiscussions > 0) {
             if (TYPE_ARTICLES.equals(type)) {
                 totalPages = numArticles / PAGE_SIZE;
