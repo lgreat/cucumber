a6c172600588ead58372ef88db40a7b4570c3879
eddie <eddie>
2009-11-03 21:01:35 +0000

    GS-8924 web service for fuzzy match controller

--- src/java/gs/web/util/FixedNameController.java
+++ src/java/gs/web/util/FixedNameController.java
@@ -8,8 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 
 import gs.data.school.SchoolType;
+import gs.data.school.Grades;
 import gs.data.state.State;
 import gs.data.util.ImportHelper;
+import gs.data.util.Address;
 
 
 /**
@@ -25,17 +27,35 @@ public class FixedNameController  implements Controller{
 
     private ImportHelper _importHelper;
 
+    final gs.data.util.Formatter formatter = new gs.data.util.Formatter();
+
     public ModelAndView handleRequest(HttpServletRequest request,
                                       HttpServletResponse response) throws Exception {
         response.setContentType("text/plain");
         String name = request.getParameter("name");
+        String street = request.getParameter("street");
+        String city = request.getParameter("city");
+        String stateName = request.getParameter("state");
+        State state = State.fromString(stateName);
+        String zipcode = request.getParameter("zipcode");
+        String phone = formatter.formatPhoneNumber(request.getParameter("phone"));
+        String level = new Grades(request.getParameter("level")).getCommaSeparatedString();
 
         try {
             String newSchoolName = getImportHelper().fixSchoolName(name, State.CA, SchoolType.CHARTER);
-
+            Address address = new Address(street,city,state,zipcode);
+            getImportHelper().fixAddress(address,state);
             if (newSchoolName != null) {
                 PrintWriter out = response.getWriter();
-                out.print(newSchoolName);
+                out.print(
+                                     "name:|:" + newSchoolName
+                                + ":|:street:|:" + address.getStreet()
+                                + ":|:city:|:" + address.getCity()
+                                + ":|:state:|:" + address.getState()
+                                + ":|:zipcode:|:" + address.getZip()
+                                + ":|:phone:|:" + phone
+                                + ":|:level:|:" + level
+                );
                 out.flush();
             }
         } catch (IllegalArgumentException e) {
