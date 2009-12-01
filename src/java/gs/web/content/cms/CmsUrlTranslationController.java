add63f1b30b57eb5a9cd87575c12c6d153a5f520
yfan <yfan>
2009-12-01 19:28:09 +0000

    GS-9123 Debugging output to diagnose 404 error on preview

--- src/java/gs/web/content/cms/CmsUrlTranslationController.java
+++ src/java/gs/web/content/cms/CmsUrlTranslationController.java
@@ -19,11 +19,14 @@ public class CmsUrlTranslationController extends AbstractController {
 
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
         if (!request.getParameterNames().hasMoreElements()) {
+            System.out.println("========= GS-9123 ======= A");
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return new ModelAndView("/status/error404.page");
         }
 
         String contentType = (String) request.getParameterNames().nextElement();
+
+        System.out.println("========= GS-9123 ======= B contentType = " + contentType);
 
         if ("Article".equals(contentType) || "AskTheExperts".equals(contentType) || "TopicCenter".equals(contentType) ||
             "ArticleSlide".equals(contentType) || "ArticleSlideshow".equals(contentType) || "DiscussionBoard".equals(contentType)) {
@@ -32,6 +35,7 @@ public class CmsUrlTranslationController extends AbstractController {
             try {
                 contentId = new Long(request.getParameter(contentType));
             } catch (NumberFormatException e) {
+                System.out.println("========= GS-9123 ======= C request.getParameter(contentType) = " + request.getParameter(contentType));
                 response.setStatus(HttpServletResponse.SC_NOT_FOUND);
                 return new ModelAndView("/status/error404.page");
             }
@@ -48,6 +52,8 @@ public class CmsUrlTranslationController extends AbstractController {
         if ("MostPopularContent".equals(contentType)) {
             return new ModelAndView("redirect:/content/cms/topicCenter.page");
         }
+
+        System.out.println("========= GS-9123 ======= D");
 
         response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         return new ModelAndView("/status/error404.page");
