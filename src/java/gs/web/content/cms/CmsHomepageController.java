df75195c01509612bd7e4bf963f6699be8265de4
yfan <yfan>
2010-02-10 01:14:25 +0000

    GS-9134 Work in progress for Raise Your Hand. Still working on submission behavior

--- src/java/gs/web/content/cms/CmsHomepageController.java
+++ src/java/gs/web/content/cms/CmsHomepageController.java
@@ -2,28 +2,47 @@ package gs.web.content.cms;
 
 import gs.data.cms.IPublicationDao;
 import gs.data.content.cms.CmsHomepage;
+import gs.data.content.cms.CmsDiscussionBoard;
+import gs.data.content.cms.ICmsDiscussionBoardDao;
 import gs.data.util.CmsUtil;
+import gs.data.admin.IPropertyDao;
+import gs.data.community.*;
+import gs.web.util.UrlBuilder;
+import gs.web.util.PageHelper;
+import gs.web.util.context.SessionContext;
+import gs.web.util.context.SessionContextUtil;
 import org.apache.log4j.Logger;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
-import org.springframework.ui.Model;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Collection;
+import java.util.*;
 
 public class CmsHomepageController extends AbstractController {
     private static final Logger _log = Logger.getLogger(CmsFeatureController.class);
 
     public static final String BEAN_ID = "/index.page";
 
+    public static final int RAISE_YOUR_HAND_NUM_REPLIES = 5;
+
+    public static final String MODEL_RAISE_YOUR_HAND_DISCUSSION = "ryhDiscussion";
+    public static final String MODEL_RAISE_YOUR_HAND_REPLIES = "ryhReplies";
+    public static final String MODEL_CURRENT_DATE = "currentDate";
+    public static final String MODEL_VALID_USER = "validUser";
+    public static final String MODEL_LOGIN_REDIRECT = "loginRedirectUrl";
+
     private IPublicationDao _publicationDao;
     private CmsContentLinkResolver _cmsFeatureEmbeddedLinkResolver;
     private String _viewName;
+    private IPropertyDao _propertyDao;
+    private ICmsDiscussionBoardDao _cmsDiscussionBoardDao;
+    private IDiscussionDao _discussionDao;
+    private IDiscussionReplyDao _discussionReplyDao;
+    private IUserDao _userDao;
 
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) {
+        Map<String, Object> model = new HashMap<String, Object>();
         CmsHomepage homepage = new CmsHomepage();
         if (CmsUtil.isCmsEnabled()) {
             Collection<CmsHomepage> homepages = _publicationDao.populateAllByContentType("Homepage", new CmsHomepage());
@@ -35,9 +54,54 @@ public class CmsHomepageController extends AbstractController {
                     throw new RuntimeException(e);
                 }
             }
+            model.put("homepage", homepage);
+
+            String raiseYourHandDiscussionId = _propertyDao.getProperty(IPropertyDao.RAISE_YOUR_HAND_DISCUSSION_ID);
+            if (raiseYourHandDiscussionId != null) {
+                try {
+                    int discussionId = Integer.parseInt(raiseYourHandDiscussionId);
+                    Discussion discussion = _discussionDao.findById(discussionId);
+                    if (discussion != null) {
+                        List<UserContent> userContents = new ArrayList<UserContent>();
+                        userContents.add(discussion);
+                        CmsDiscussionBoard discussionBoard = _cmsDiscussionBoardDao.get(discussion.getBoardId());
+                        List<DiscussionReply> replies = _discussionReplyDao.getRepliesForPage(
+                                discussion, 1, RAISE_YOUR_HAND_NUM_REPLIES,
+                                IDiscussionReplyDao.DiscussionReplySort.NEWEST_FIRST, false);
+                        userContents.addAll(replies);
+
+                        discussion.setDiscussionBoard(discussionBoard);
+                        _userDao.populateWithUsers(userContents);
+
+                        model.put(MODEL_RAISE_YOUR_HAND_DISCUSSION, discussion);
+                        model.put(MODEL_RAISE_YOUR_HAND_REPLIES, replies);
+                        model.put(MODEL_CURRENT_DATE, new Date());
+
+                        SessionContext sessionContext = SessionContextUtil.getSessionContext(request);
+                        User user = null;
+                        if (PageHelper.isMemberAuthorized(request)) {
+                            user = sessionContext.getUser();
+                            if (user != null) {
+                                model.put(MODEL_VALID_USER, user);
+                            }
+                        }
+                        if (user == null) {
+                            UrlBuilder urlBuilder = new UrlBuilder(UrlBuilder.LOGIN_OR_REGISTER, null, "/");
+                            model.put(MODEL_LOGIN_REDIRECT, urlBuilder.asSiteRelative(request));
+                        } else {
+                            model.put(MODEL_LOGIN_REDIRECT, "#");
+                        }
+                    } else {
+                        _log.warn("RAISE_YOUR_HAND_DISCUSSION_ID property specifies invalid discussion ID " + raiseYourHandDiscussionId);
+                    }
+
+                } catch (NumberFormatException e) {
+                    _log.warn("RAISE_YOUR_HAND_DISCUSSION_ID property specifies malformatted number " + raiseYourHandDiscussionId);
+                }
+            }
         }
 
-        return new ModelAndView(_viewName, "homepage", homepage);
+        return new ModelAndView(_viewName, model);
     }
 
     public void setPublicationDao(IPublicationDao publicationDao) {
@@ -58,5 +122,45 @@ public class CmsHomepageController extends AbstractController {
 
     public void setCmsFeatureEmbeddedLinkResolver(CmsContentLinkResolver cmsFeatureEmbeddedLinkResolver) {
         _cmsFeatureEmbeddedLinkResolver = cmsFeatureEmbeddedLinkResolver;
+    }
+
+    public IPropertyDao getPropertyDao() {
+        return _propertyDao;
+    }
+
+    public void setPropertyDao(IPropertyDao propertyDao) {
+        _propertyDao = propertyDao;
+    }
+
+    public ICmsDiscussionBoardDao getCmsDiscussionBoardDao() {
+        return _cmsDiscussionBoardDao;
+    }
+
+    public void setCmsDiscussionBoardDao(ICmsDiscussionBoardDao cmsDiscussionBoardDao) {
+        _cmsDiscussionBoardDao = cmsDiscussionBoardDao;
+    }
+
+    public IDiscussionDao getDiscussionDao() {
+        return _discussionDao;
+    }
+
+    public void setDiscussionDao(IDiscussionDao discussionDao) {
+        _discussionDao = discussionDao;
+    }
+
+    public IDiscussionReplyDao getDiscussionReplyDao() {
+        return _discussionReplyDao;
+    }
+
+    public void setDiscussionReplyDao(IDiscussionReplyDao discussionReplyDao) {
+        _discussionReplyDao = discussionReplyDao;
+    }
+
+    public IUserDao getUserDao() {
+        return _userDao;
+    }
+
+    public void setUserDao(IUserDao userDao) {
+        _userDao = userDao;
     }
 }
--- src/webapp/res/js/carousel/carousel.js
+++ src/webapp/res/js/carousel/carousel.js
@@ -1,6 +1,4 @@
 // JavaScript Document
-// required to avoid "$j" collisions with Prototype.js
-jQuery.noConflict();
 var $j = jQuery;
 
 $j(document).ready(function() {
