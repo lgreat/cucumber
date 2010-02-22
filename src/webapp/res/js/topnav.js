192823e286ede0a3ec6f7f6079c20ed9ab21b509
mwong <mwong>
2010-02-22 22:52:04 +0000

    GS-9528

--- src/webapp/res/js/topnav.js
+++ src/webapp/res/js/topnav.js
@@ -3,7 +3,11 @@
 jQuery(document).ready(function() {
 
     /* Subnav menus */
-
+    var srchArtTab = jQuery('#srch2').hasClass('active');
+    if(srchArtTab){
+        jQuery('#qNew').attr('style','width:318px')
+        jQuery('#stateSelector').attr('style','display:none');
+    }
     jQuery('#topnav_menusubnav > .nav_menu').hover(function() {
         jQuery(this).toggleClass('over');
         jQuery(this).children('.nav_group_heading:not(#GlobalNav_CommunityButton)').toggleClass('over');
@@ -27,7 +31,7 @@ jQuery(document).ready(function() {
     });
     /* search tab click functions */
     jQuery('#topnav_search .radLabel').click(function(){
-        jQuery(this).parent('li').toggleClass('active', true);
-        jQuery(this).parent('li').siblings().toggleClass('active', false);
+        jQuery(this).parent('li').removeClass('inactive').addClass('active');
+        jQuery(this).parent('li').siblings().removeClass('active').addClass('inactive');
     });
 });
