e9928e09621dc2b283cf5235ea7bea277674b7e4
aroy <aroy>
2010-03-17 21:49:52 +0000

    GS-9530 Refactor out javascript in preparation for porting to perl

---
+++ src/webapp/res/js/kindercareLeadGen.js
@@ -0,0 +1,74 @@
+jQuery(function() {
+    jQuery('#kindercareFirstNameError').hide();
+    jQuery('#kindercareLastNameError').hide();
+    jQuery('#kindercareEmailError').hide();
+    jQuery('#kindercareThankyou').hide();
+
+    function kindercareLeadGenSubmitHandler(data) {
+        jQuery('#kindercareSpinny').hide();
+        jQuery('#kindercareLeadGenForm').hide();
+        jQuery('#kindercareThankYou').show();
+        var cookieName = 'kindercare';
+        var dataObject = subCookie.getObject(cookieName);
+        // if no cookie, set value to 1 and show hover
+        if (dataObject == undefined || dataObject[kindercareCookieKey] == undefined){
+            subCookie.setObjectProperty(cookieName, kindercareCookieKey, 1, 9001);
+        }
+    }
+
+    jQuery('#kindercareLeadGenSubmit').click(function() {
+        if (validateLeadGenForm()) {
+            jQuery('#kindercareLeadGenSubmit').hide();
+            jQuery('#kindercareSpinny').show();
+            var params = {
+                firstName: jQuery('#kinder_firstname').val(),
+                lastName: jQuery('#kinder_lastname').val(),
+                email: jQuery('#kinder_email').val(),
+                informed: jQuery('#kinder_informed').is(':checked'),
+                offers: jQuery('#kinder_offers').is(':checked'),
+                schoolId: jQuery('#kinder_schoolId').val(),
+                state: jQuery('#kinder_state').val()
+            };
+
+            jQuery.post('/school/kindercareLeadGenAjax.page', params, kindercareLeadGenSubmitHandler);
+        } else {
+            jQuery('#kindercareIntroText').hide();
+        }
+
+        return false;
+    });
+
+    jQuery('#kindercareLeadGenClose').click(function() {
+        var params = {
+            schoolId: jQuery('#kinder_schoolId').val(),
+            state: jQuery('#kinder_state').val()
+        };
+        jQuery.post('/school/kindercareLeadGenAjax.page', params, kindercareLeadGenSubmitHandler);
+        jQuery('#kindercareLeadGen').hide();
+
+        return false;
+    });
+
+    function validateLeadGenForm() {
+        jQuery('#kindercareFirstNameError').hide();
+        jQuery('#kindercareLastNameError').hide();
+        jQuery('#kindercareEmailError').hide();
+
+        passed = true;
+
+        if (jQuery('#kinder_firstname').val() == '') {
+            jQuery('#kindercareFirstNameError').show();
+            passed = false;
+        }
+        if (jQuery('#kinder_lastname').val() == '') {
+            jQuery('#kindercareLastNameError').show();
+            passed = false;
+        }
+        if (jQuery('#kinder_email').val() == '') {
+            jQuery('#kindercareEmailError').show();
+            passed = false;
+        }
+
+        return passed;
+    }
+});
