cdbdd0d83904a904bb622053832348a7726cfcbd
rcox <rcox>
2010-03-19 22:49:42 +0000

    GS-9457 Reskin: Redesign grade pages & topic center template (front-end) - left navigation.

--- src/webapp/res/js/topiccenter/topicCenter2010.js
+++ src/webapp/res/js/topiccenter/topicCenter2010.js
@@ -6,12 +6,13 @@ var $j = jQuery;
 $j(document).ready(function() {
     //
     // Initialize (all twirly content closed)
-    var theTwirly = $j('ul.text2bold li span');
+    var theTwirly = $j('#topicbarGS .hasNested');
     //
     // On twirly click
     theTwirly.click(function () {
-        ($j(this).hasClass('twirlyClosed')) ? $j(this).removeClass('twirlyClosed').addClass('twirlyOpen') : $j(this).removeClass('twirlyOpen').addClass('twirlyClosed');
-        ($j(this).hasClass('twirlyClosed')) ? $j(this).parent().find('ul').slideUp(250) : $j(this).parent().find('ul').slideDown(250);
+        ($j('> span', this).hasClass('twirlyClosed')) ? $j('> span', this).removeClass('twirlyClosed').addClass('twirlyOpen') : $j('> span', this).removeClass('twirlyOpen').addClass('twirlyClosed');
+        ($j('> span', this).hasClass('twirlyClosed')) ? $j('> ul', this).slideUp(250) : $j('> ul', this).slideDown(250);
+        return false;
     });
 
     // GS-9690 - show 160x600 ad (and AD word above the ad), only if ad is running
