6af07f6c5b10ce4699bc065641f5dfb89c9fe561
npatury <npatury>
2010-05-12 20:18:11 +0000

    GS-9895 - suggest usernames

--- src/java/gs/web/community/registration/RegistrationAjaxController.java
+++ src/java/gs/web/community/registration/RegistrationAjaxController.java
@@ -32,10 +32,13 @@ public class RegistrationAjaxController implements Controller {
     private StateManager _stateManager;
 
     final public static String FORMAT_PARAM = "format";
+    final public static String JSONP_CALLBACK_PARAM = "jsoncallback";
     final public static String TYPE_PARAM = "type";
     final public static String EMAIL_PARAM = "email";
     final public static String USER_NAME_PARAM = "un";
+    final public static String USER_NAME_SUGGESTIONS = "suggest_un";
     final public static String FIRST_NAME_PARAM = "fn";
+    final public static String LAST_NAME_PARAM = "ln";
     final public static String CITY_TYPE = "city";
     final public static String CITY_OPTIONS_TYPE = "cityOptions";
     final public static String COUNTY_TYPE = "county";
@@ -44,6 +47,13 @@ public class RegistrationAjaxController implements Controller {
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         PrintWriter out = response.getWriter();
         String type = request.getParameter(TYPE_PARAM);
+         if (request.getParameter(FIRST_NAME_PARAM) != null
+                 && StringUtils.equals("true", request.getParameter(USER_NAME_SUGGESTIONS))
+                 && StringUtils.equals("json", request.getParameter(FORMAT_PARAM))) {
+                suggestUsername(request,out);
+                return null;
+         }
+
         if (StringUtils.equals("json", request.getParameter(FORMAT_PARAM))) {
             if (StringUtils.equals(CITY_TYPE, type)) {
                 outputCityJson(request, out);
@@ -107,6 +117,118 @@ public class RegistrationAjaxController implements Controller {
         } else {
             out.print("valid");
         }
+    }
+
+    protected void suggestUsername (HttpServletRequest request, PrintWriter out) {
+        String firstName = request.getParameter(FIRST_NAME_PARAM);
+        String lastName = request.getParameter(LAST_NAME_PARAM);
+        boolean cbcall = false;
+        if(request.getParameter(CBI_CALL) != null){
+            cbcall = true;
+        }
+        JSONObject rval = new JSONObject();
+        String username;
+        String suggestion="";
+        String usernames="";
+        if(StringUtils.isNotBlank(firstName)){
+            username = firstName;
+            if(StringUtils.isNotBlank(lastName)){
+                username += lastName.substring(0,1);
+            }
+            for(int i=0;i<3;i++){
+                if(i==0){
+                    usernames += constructUsername(username,false) + ",";
+                }else{
+                    usernames += constructUsername(username,true) + ",";
+                }
+            }
+            usernames = usernames.substring(0,usernames.length()-1);
+            suggestion = validateUsernames(usernames,cbcall);
+          }
+        try{
+            rval.put("suggestion", suggestion);
+        }catch(JSONException jsone){
+            _log.error("Error converting username suggestion to JSON: " + jsone, jsone);
+        }
+        String jsonCallbackParam = getSanitizedJsonpParam(request);
+        String res= jsonCallbackParam+"("+rval+");";
+        out.print(res);
+    }
+
+    protected String constructUsername(String name,boolean addRandom){
+       if(addRandom){
+           Long random = Math.round(Math.random()*100);
+           name = name + random;
+       } 
+       name = checkScreenameLength(name);
+       return name;
+    }
+
+    protected String checkScreenameLength(String screenName) {
+        int num = 1;
+        if(screenName.length()<UserCommandValidator.SCREEN_NAME_MINIMUM_LENGTH){
+          for(int i=screenName.length();i<UserCommandValidator.SCREEN_NAME_MINIMUM_LENGTH;i++){
+            num = num *10;
+          }
+          int diff = UserCommandValidator.SCREEN_NAME_MINIMUM_LENGTH - screenName.length() ;
+          long randomFiller = Math.round(Math.random()*num);
+          String paddedRandom =String.format("%0"+diff+"d",randomFiller);
+          screenName= screenName + paddedRandom;
+        }else if(screenName.length()>UserCommandValidator.SCREEN_NAME_MAXIMUM_LENGTH){
+          screenName = screenName.substring(0,12);
+          long random = Math.round(Math.random()*100);
+          screenName = screenName + random;
+        }
+        return screenName;
+    }
+
+    protected String validateUsernames(String uns,boolean cbcall) {
+        String suggestion="";
+        String names="";
+
+        Map<String,String> validNames=new LinkedHashMap();
+        if(StringUtils.isNotBlank(uns)){
+            String[] usernames = uns.split(",");
+            for(String username:usernames){
+                String validation = usernameValidator(username,cbcall);
+                if(validation.equals("valid")){
+                    validNames.put(username,validation);
+                    names = names+",'"+username+"'";
+                }
+            }
+            if(StringUtils.isNotBlank(names) && validNames.size() >0){
+                names = names.substring(1,names.length());
+                _userDao.findUsersFromScreenNameIfExists(names,validNames);
+                for(String key: validNames.keySet()){
+                    if(validNames.get(key).equals("valid")){
+                        suggestion = key;
+                        break;
+                    }
+                }
+            }
+        }
+        return suggestion;
+    }
+
+    protected String usernameValidator(String username,boolean cbcall){
+        String output;
+        UserCommandValidator validator = new UserCommandValidator();
+        if (username.length() < UserCommandValidator.SCREEN_NAME_MINIMUM_LENGTH || username.length() > UserCommandValidator.SCREEN_NAME_MAXIMUM_LENGTH ) {
+           output="invalid";
+        }else if(validator.screenNameHasInvalidCharacters(username,cbcall)){
+           output="invalidchars";
+        }else {
+           output="valid";
+        }
+        return output;
+    }
+
+    protected String getSanitizedJsonpParam(HttpServletRequest request) {
+        String jsonpParam = request.getParameter(JSONP_CALLBACK_PARAM);
+        if ( StringUtils.isEmpty(jsonpParam)) return null;
+        if ( StringUtils.length(jsonpParam) > 128 ) return null;
+        if ( !jsonpParam.matches("^jsonp\\d+$")) return null;
+        return jsonpParam;
     }
 
     protected void validateFirstName(HttpServletRequest request, PrintWriter out) {
