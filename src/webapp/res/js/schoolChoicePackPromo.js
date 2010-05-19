f58d87c059236c3f8260ac98a23dd2e1dd6a16e9
ssprouse <ssprouse>
2010-05-19 19:40:48 +0000

    GS-9184 New error messages / JS validation for Chooser pack module 2.0

--- src/webapp/res/js/schoolChoicePackPromo.js
+++ src/webapp/res/js/schoolChoicePackPromo.js
@@ -2,7 +2,7 @@ var $j = jQuery;
 
 $j(function() {
 
-    
+
 
     // clears the email field
     $j('#cemail').click(function () {
@@ -21,56 +21,73 @@ $j(function() {
         });
         var emailVal = $j('input#cemail').val();
         var termsChecked = document.getElementById('privacy_terms_checkbox').checked;
-        if (cks.length > 0) {
-            if (emailVal) {
-                $j.get("/util/isValidEmail.page", {email : emailVal},
-                        function (data) {
-                            if (data == 'true') {
-                                if (termsChecked) {
-                                    if (GS.showChooserTipSheetHover(emailVal, window.location.href)) {
-                                        $j.post("/promo/schoolChoicePackPromo.page",
-                                        {email : emailVal, levels : cks.join(','), pageName : clickCapture.pageName, redirectForConfirm : document.getElementById('redirectForConfirm').value},
-                                                function(datax) {
-                                                    // per GS-8301 don't set memid cookie here
-                                                    //                                                if (datax.createMemberCookie == 'y') {
-                                                    // must create cookie here even though it's in the controller already because
-                                                    // this is an ajax call and we must make sure next page is aware of
-                                                    // the MEMID. if only set in controller, the next page loaded may
-                                                    // not see that cookie.
-                                                    //                                                    createCookie("MEMID", datax.memid);
-                                                    //                                                }
-                                                    omnitureEventNotifier.clear();
-                                                    omnitureEventNotifier.successEvents = datax.omnitureTracking.successEvents;
-                                                    omnitureEventNotifier.eVars = datax.omnitureTracking.eVars;
-                                                    omnitureEventNotifier.send();
-                                                    if (datax.showRegistration == 'y') {
-                                                        //storeHrefOpenHover(datax.redirectEncoded,'/community/registration/popup/sendToDestination.page',datax.emailEncoded);
-                                                        //GSType.hover.joinHover.showJoinChooserTipSheet(emailVal);
-                                                    } else {
-                                                        $j("#form_panel").hide();
-                                                        $j("#confirm_panel").show();
-                                                    }
 
-                                                }, "json");
-                                    }
+        jQuery('#scpp_form .emailError').hide();
+        jQuery('#scpp_form .termsError').hide();
+        jQuery('#scpp_form .gradeError').hide();
 
-                                } else {
-                                    alert("Please accept the GreatSchools Privacy Policy and Terms of Use.");
-                                }
-                            } else {
-                                alert("Please enter a valid email address");
-                            }
-                        }, "text");
-            } else {
-                alert("Please enter your email address");
-            }
-        } else {
-            if (emailVal) {
-                alert("Please select the grade level(s) of the school(s) you are choosing");
-            } else {
-                alert("Please enter your email address and select the\ngrade level(s) of the school(s) you are choosing");
-            }
+        var validationPassed = true;
+
+        if (emailVal == "Enter email address" || !emailVal) {
+            jQuery('#scpp_form .emailError').show();
+            jQuery('#scpp_form .emailError .error').show();
+            validationPassed = false;
         }
+
+        if (!termsChecked) {
+            jQuery('#scpp_form .termsError').show();
+            jQuery('#scpp_form .termsError .error').show();
+            validationPassed = false;
+        }
+
+        if (!cks.length > 0) {
+            jQuery('#scpp_form .gradeError').show();
+            jQuery('#scpp_form .gradeError .error').show();
+            validationPassed = false;
+        }
+
+        if (!validationPassed) {
+            return false;
+        }
+
+        $j.get("/util/isValidEmail.page", {email : emailVal},
+                function (data) {
+                    if (data == 'true') {
+                        if (GS.showChooserTipSheetHover(emailVal, window.location.href)) {
+                            $j.post("/promo/schoolChoicePackPromo.page",
+                            {email : emailVal, levels : cks.join(','), pageName : clickCapture.pageName, redirectForConfirm : document.getElementById('redirectForConfirm').value},
+                                    function(datax) {
+                                        // per GS-8301 don't set memid cookie here
+                                        //                                                if (datax.createMemberCookie == 'y') {
+                                        // must create cookie here even though it's in the controller already because
+                                        // this is an ajax call and we must make sure next page is aware of
+                                        // the MEMID. if only set in controller, the next page loaded may
+                                        // not see that cookie.
+                                        //                                                    createCookie("MEMID", datax.memid);
+                                        //                                                }
+                                        omnitureEventNotifier.clear();
+                                        omnitureEventNotifier.successEvents = datax.omnitureTracking.successEvents;
+                                        omnitureEventNotifier.eVars = datax.omnitureTracking.eVars;
+                                        omnitureEventNotifier.send();
+                                        if (datax.showRegistration == 'y') {
+                                            //storeHrefOpenHover(datax.redirectEncoded,'/community/registration/popup/sendToDestination.page',datax.emailEncoded);
+                                            //GSType.hover.joinHover.showJoinChooserTipSheet(emailVal);
+                                        } else {
+                                            $j("#form_panel").hide();
+                                            $j("#confirm_panel").show();
+                                        }
+
+                                    }, "json");
+                        }
+
+                    } else {
+                        jQuery('#scpp_form .emailError').show();
+                        jQuery('#scpp_form .emailError .error').show();
+
+                    }
+                }, "text");
+
+
         return false;
     });
 
