54afeaadf2baebfd2eee0b2d0085e2333d70cf7f
yfan <yfan>
2010-05-19 01:14:45 +0000

    GS-10001 Updated media url

--- src/java/gs/web/util/UrlBuilder.java
+++ src/java/gs/web/util/UrlBuilder.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2005-2006 GreatSchools.org. All Rights Reserved.
- * $Id: UrlBuilder.java,v 1.216 2010/04/30 02:23:58 yfan Exp $
+ * $Id: UrlBuilder.java,v 1.217 2010/05/19 01:14:45 yfan Exp $
  */
 
 package gs.web.util;
@@ -688,7 +688,7 @@ public class UrlBuilder {
         } else if (SPECIAL_NEEDS.equals(page)) {
             _path = "/LD.topic?content=1541";
         } else if (MEDIA_CHOICES.equals(page)) {
-            _path = "/content/mediaChoices.page";
+            _path = "/media-choices.topic?content=2439";
         } else if (MOVING_WITH_KIDS.equals(page)) {
             _path = "/moving.topic?content=2220";
         } else if (COUNTDOWN_TO_COLLEGE.equals(page)) {
