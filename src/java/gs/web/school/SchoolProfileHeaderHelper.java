0e4723ec723552845883d818e3bbb33a5abb31c7
aroy <aroy>
2010-06-16 23:55:42 +0000

    GS-10154 Prototyping of the new school profile header

---
+++ src/java/gs/web/school/SchoolProfileHeaderHelper.java
@@ -0,0 +1,18 @@
+package gs.web.school;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.Map;
+
+/**
+ * @author Anthony Roy <mailto:aroy@greatschools.net>
+ */
+public class SchoolProfileHeaderHelper {
+    public static final String BEAN_ID = "schoolProfileHeaderHelper";
+    private static final Log _log = LogFactory.getLog(SchoolProfileHeaderHelper.class);
+
+    public void updateModel(Map<String, Object> model) {
+        
+    }
+}
