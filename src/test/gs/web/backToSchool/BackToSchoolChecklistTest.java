94790b586d78459e262b3771cbe249eadcf0d670
ssprouse <ssprouse>
2010-07-06 20:22:36 +0000

    GS-10060 Fixed bug with bts checklist emails getting sent twice, added test

--- src/test/gs/web/backToSchool/BackToSchoolChecklistTest.java
+++ src/test/gs/web/backToSchool/BackToSchoolChecklistTest.java
@@ -139,4 +139,61 @@ public class BackToSchoolChecklistTest extends BaseTestCase {
         verify(_exactTargetAPI);
     }
 
+
+    public void testAddChecklistItemSendsEmailsOnlyOnce() {
+        User user = new User();
+        user.setId(99999);
+        user.setEmail("ssprouse+test2@greatschools.org");
+
+        UserProfile profile = new UserProfile();
+        profile.setUser(user);
+        user.setUserProfile(profile);
+
+        BackToSchoolChecklist checklist = new BackToSchoolChecklist();
+        checklist.setUserDao(_userDao);
+        checklist.setExactTargetAPI(_exactTargetAPI);
+
+        Set<Subscription> subscriptions = new HashSet(1);
+        Subscription subscription = new Subscription();
+        subscription.setProduct(SubscriptionProduct.BTSTIP_E);
+        subscription.setUser(user);
+        subscriptions.add(subscription);
+        user.setSubscriptions(subscriptions);
+
+        BackToSchoolChecklist.BackToSchoolChecklistItem[] solutionSet = BackToSchoolChecklist.solutionSet;
+
+        for (int i = 0; i < solutionSet.length-1; i++) {
+            _userDao.updateUser(isA(User.class));
+        }
+
+        replay(_userDao);
+
+        for (int i = 0; i < solutionSet.length-1; i++) {
+            checklist.addChecklistItem(solutionSet[i].name(), user);
+        }
+
+        verify(_userDao);
+        reset(_userDao);
+
+        _userDao.updateUser(isA(User.class));
+        _exactTargetAPI.sendTriggeredEmail(eq(BackToSchoolChecklist.EXACT_TARGET_EMAIL_KEY), isA(User.class));
+
+        replay(_userDao);
+        replay(_exactTargetAPI);
+
+        checklist.addChecklistItem(solutionSet[solutionSet.length-1].name(), user);
+
+        verify(_userDao);
+        verify(_exactTargetAPI);
+
+        reset(_userDao);
+        reset(_exactTargetAPI);
+
+        replay(_userDao);
+        replay(_exactTargetAPI);
+        checklist.addChecklistItem(solutionSet[solutionSet.length-1].name(), user);
+        verify(_userDao);
+        verify(_exactTargetAPI);
+    }
+
 }
