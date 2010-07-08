4e405e2549e63c5384365628ba731b2d1dbb738b
aroy <aroy>
2010-07-08 22:04:06 +0000

    GS-10249 Handle number1expert

--- src/java/gs/web/school/PerlFetchController.java
+++ src/java/gs/web/school/PerlFetchController.java
@@ -1,11 +1,16 @@
 package gs.web.school;
 
 import gs.data.school.School;
+import gs.web.util.UrlBuilder;
 import gs.web.util.UrlUtil;
+import gs.web.util.context.SessionContext;
+import gs.web.util.context.SessionContextUtil;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
@@ -35,6 +40,16 @@ public class PerlFetchController extends AbstractSchoolController implements Con
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return new ModelAndView(VIEW_NOT_FOUND);
         }
+
+        // GS-3044 - number1expert cobrand specific code
+        SessionContext sessionContext = SessionContextUtil.getSessionContext(request);
+        if (sessionContext.isCobranded() && "number1expert".equals(sessionContext.getCobrand())) {
+            if (handleNumber1ExpertLeadGen(request, response, 
+                                           String.valueOf(school.getId()), sessionContext)) {
+                return null; // method forwards user to bireg
+            }
+        }
+
         String perlResponse;
         String href = getAbsoluteHref(school, request);
         String view = getViewName();
@@ -135,6 +150,45 @@ public class PerlFetchController extends AbstractSchoolController implements Con
         }
 
         return response.toString();
+    }
+
+    protected boolean handleNumber1ExpertLeadGen(HttpServletRequest request,
+                                                 HttpServletResponse response,
+                                                 String schoolIdStr,
+                                                 SessionContext sessionContext) throws IOException {
+        Cookie[] cookies = request.getCookies();
+        String agentId = null;
+        if (cookies != null) {
+            // Collect all the cookies
+            for (int i = 0; cookies.length > i; i++) {
+                // find the agent id cookie
+                if ("AGENTID".equals(cookies[i].getName())) {
+                    // store its value
+                    agentId = cookies[i].getValue();
+                }
+            }
+            // if there's no agent id, no lead gen necessary
+            if (agentId != null) {
+                boolean foundCookie = false;
+                String biregCookieName = "BIREG" + agentId;
+                for (int i = 0; cookies.length > i; i++) {
+                    if (biregCookieName.equals(cookies[i].getName())
+                            && StringUtils.isNotEmpty(cookies[i].getValue())
+                            && !cookies[i].getValue().equals("0")) {
+                        foundCookie = true;
+                    }
+                }
+                if (!foundCookie) {
+                    // send to bireg
+                    UrlBuilder urlBuilder = new UrlBuilder
+                            (UrlBuilder.GET_BIREG, sessionContext.getStateOrDefault(),
+                             new Integer(schoolIdStr), new Integer(agentId));
+                    response.sendRedirect(urlBuilder.asFullUrl(request));
+                    return true;
+                }
+            } // end if agentId != null
+        } // end if cookies != null
+        return false;
     }
 
     public String getViewName() {
