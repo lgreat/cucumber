82fb38da47669d9953a84b05234485d1ebfb550c
yfan <yfan>
2010-07-09 22:03:15 +0000

    GS-10239 Adding crufty(.greatschools.org) as non-cobrand server

--- src/java/gs/web/util/UrlUtil.java
+++ src/java/gs/web/util/UrlUtil.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2005 GreatSchools.org. All Rights Reserved.
- * $Id: UrlUtil.java,v 1.95 2010/07/01 16:27:09 aroy Exp $
+ * $Id: UrlUtil.java,v 1.96 2010/07/09 22:03:15 yfan Exp $
  */
 
 package gs.web.util;
@@ -81,6 +81,7 @@ public final class UrlUtil {
                 && !hostName.startsWith("admin")
                 && !hostName.startsWith("clone")
                 && !hostName.startsWith("cmsqa")
+                && !hostName.startsWith("crufty")
                 && !hostName.startsWith("dev")
                 && !hostName.startsWith("localhost")
                 && !hostName.startsWith("main.dev")
