94c51897b924a498b6b5aa71d7fcddb2333cd9ff
aroy <aroy>
2010-07-29 23:21:46 +0000

    GS-10256 Roll back changes from this issue

--- src/java/gs/web/community/registration/EmailVerificationEmail.java
+++ src/java/gs/web/community/registration/EmailVerificationEmail.java
@@ -1,8 +1,9 @@
 package gs.web.community.registration;
 
 import gs.data.community.User;
-import gs.data.integration.exacttarget.ExactTargetAPI;
 import gs.data.util.DigestUtil;
+import gs.data.util.email.EmailHelper;
+import gs.web.util.AbstractSendEmailBean;
 import gs.web.util.UrlBuilder;
 
 import javax.mail.MessagingException;
@@ -10,19 +11,21 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Anthony Roy <mailto:aroy@greatschools.net>
  */
-public class EmailVerificationEmail {
+public class EmailVerificationEmail extends AbstractSendEmailBean {
     public static final String BEAN_ID = "emailVerificationEmail";
-    public static final String VERIFICATION_EMAIL_KEY = "Acct_Verification";
-    public static final String SCHOOL_REVIEW_VERIFICATION_EMAIL_KEY = "Acct_Verification_SR";
-    public static final String CHANGED_VERIFICATION_EMAIL_KEY = "Acct_Email_Change";
-    public static final String VERIFICATION_LINK_ATTRIBUTE = "HTML__verifyLink";
-    private ExactTargetAPI _exactTargetAPI;
+    public static final String EMAIL_LOCATION =
+            "/gs/web/community/registration/emailVerificationEmail.txt";
+    public static final String EMAIL_PART_TWO_LOCATION =
+            "/gs/web/community/registration/emailVerificationEmail.txt";
+    public static final String EMAIL_CHANGED_EMAIL_LOCATION =
+            "/gs/web/community/registration/emailVerificationEmail-changedEmail.txt";
+    private String _verificationEmailSubject;
+    private String _verificationEmailPartTwoSubject;
+    private String _verificationEmailChangedEmailSubject;
 
     public void sendVerificationEmail(HttpServletRequest request, User user, String redirect)
             throws IOException, MessagingException, NoSuchAlgorithmException {
@@ -39,31 +42,16 @@ public class EmailVerificationEmail {
 
         String verificationLink = builder.asAbsoluteAnchor(request, builder.asFullUrl(request)).asATag();
 
-//        sendEmail(getVerificationEmailSubject(), EMAIL_LOCATION, user, verificationLink);
-        Map<String, String> emailAttributes = new HashMap<String, String>(1);
-        emailAttributes.put(VERIFICATION_LINK_ATTRIBUTE, verificationLink);
-        _exactTargetAPI.sendTriggeredEmail(VERIFICATION_EMAIL_KEY, user, emailAttributes);
+        sendEmail(getVerificationEmailSubject(), EMAIL_LOCATION, user, verificationLink);
+//        Map<String, String> emailAttributes = new HashMap<String, String>(1);
+//        emailAttributes.put(VERIFICATION_LINK_ATTRIBUTE, verificationLink);
+//        _exactTargetAPI.sendTriggeredEmail(VERIFICATION_EMAIL_KEY, user, emailAttributes);
     }
 
+    // rolling back GS-10256 so this method is equivalent to the above
     public void sendSchoolReviewVerificationEmail(HttpServletRequest request, User user, String redirect)
             throws IOException, MessagingException, NoSuchAlgorithmException {
-        String hash = DigestUtil.hashStringInt(user.getEmail(), user.getId());
-        Date now = new Date();
-        String nowAsString = String.valueOf(now.getTime());
-        hash = DigestUtil.hashString(hash + nowAsString);
-
-        UrlBuilder builder = new UrlBuilder(UrlBuilder.REGISTRATION_VALIDATION,
-                                            null,
-                                            hash + user.getId());
-        builder.addParameter("date", nowAsString);
-        builder.addParameter("redirect", redirect);
-
-        String verificationLink = builder.asAbsoluteAnchor(request, builder.asFullUrl(request)).asATag();
-
-//        sendEmail(getVerificationEmailSubject(), EMAIL_LOCATION, user, verificationLink);
-        Map<String, String> emailAttributes = new HashMap<String, String>(1);
-        emailAttributes.put(VERIFICATION_LINK_ATTRIBUTE, verificationLink);
-        _exactTargetAPI.sendTriggeredEmail(SCHOOL_REVIEW_VERIFICATION_EMAIL_KEY, user, emailAttributes);
+        sendVerificationEmail(request, user, redirect);
     }
 
     public void sendChangedEmailAddress(HttpServletRequest request, User user) throws IOException, MessagingException, NoSuchAlgorithmException {
@@ -80,17 +68,46 @@ public class EmailVerificationEmail {
 
         String verificationLink = builder.asAbsoluteAnchor(request, builder.asFullUrl(request)).asATag();
 
-//        sendEmail(getVerificationEmailChangedEmailSubject(), EMAIL_CHANGED_EMAIL_LOCATION, user, verificationLink);
-        Map<String, String> emailAttributes = new HashMap<String, String>(1);
-        emailAttributes.put(VERIFICATION_LINK_ATTRIBUTE, verificationLink);
-        _exactTargetAPI.sendTriggeredEmail(CHANGED_VERIFICATION_EMAIL_KEY, user, emailAttributes);
+        sendEmail(getVerificationEmailChangedEmailSubject(), EMAIL_CHANGED_EMAIL_LOCATION, user, verificationLink);
     }
 
-    public ExactTargetAPI getExactTargetAPI() {
-        return _exactTargetAPI;
+    protected void sendEmail(String subject,
+                             String emailLocation,
+                             User user,
+                             String verificationLink) throws IOException, MessagingException {
+        EmailHelper emailHelper = getEmailHelper();
+        emailHelper.setSubject(subject);
+        emailHelper.setToEmail(user.getEmail());
+        emailHelper.readHtmlFromResource(emailLocation);
+
+        emailHelper.addInlineReplacement("FIRST_NAME", user.getFirstName());
+        emailHelper.addInlineReplacement("VERIFICATION_LINK", verificationLink);
+        emailHelper.addInlineReplacement("NEW_EMAIL", user.getEmail());
+
+        emailHelper.send();
     }
 
-    public void setExactTargetAPI(ExactTargetAPI exactTargetAPI) {
-        _exactTargetAPI = exactTargetAPI;
+    public String getVerificationEmailSubject() {
+        return _verificationEmailSubject;
+    }
+
+    public void setVerificationEmailSubject(String verificationEmailSubject) {
+        _verificationEmailSubject = verificationEmailSubject;
+    }
+
+    public String getVerificationEmailPartTwoSubject() {
+        return _verificationEmailPartTwoSubject;
+    }
+
+    public void setVerificationEmailPartTwoSubject(String verificationEmailPartTwoSubject) {
+        _verificationEmailPartTwoSubject = verificationEmailPartTwoSubject;
+    }
+
+    public String getVerificationEmailChangedEmailSubject() {
+        return _verificationEmailChangedEmailSubject;
+    }
+
+    public void setVerificationEmailChangedEmailSubject(String verificationEmailChangedEmailSubject) {
+        _verificationEmailChangedEmailSubject = verificationEmailChangedEmailSubject;
     }
 }
