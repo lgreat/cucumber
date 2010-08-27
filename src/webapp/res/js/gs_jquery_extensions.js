df350bdefe5ab34d71a4e56837a6ccad0a330e33
rcox <rcox>
2010-08-27 00:08:55 +0000

    GS-10543 To add gs_jquery_extensions.js to GSWeb and include it in the combined.min.js

---
+++ src/webapp/res/js/gs_jquery_extensions.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+jQuery(document).ready(function() {
+    //
+    // extend the core jQuery with disableSelection
+    jQuery.fn.extend({
+        disableSelection : function() {
+            this.each(function() {
+                this.onselectstart = function() {
+                    return false;
+                };
+                this.unselectable = "on";
+                jQuery(this).css('-moz-user-select', 'none');
+                jQuery(this).css('-webkit-user-select', 'none');
+            });
+        }
+    });
+    //
+    // disable selection of button-1 class
+    jQuery('.button-1').disableSelection();
+    // disable selection of button-1-inactive class
+    jQuery('.button-1-inactive').disableSelection();
+});
