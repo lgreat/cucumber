cfd122c5e5305555af98d2594a8f34f007c377a5
ssprouse <ssprouse>
2010-09-30 22:21:08 +0000

    GS-10263 Increased cache-control to one week

--- src/test/gs/web/util/HttpCacheFilterTest.java
+++ src/test/gs/web/util/HttpCacheFilterTest.java
@@ -25,7 +25,7 @@ public class HttpCacheFilterTest extends BaseControllerTestCase {
         cacheFilter.doFilter(request, response, chain);
 
         // Verify that cache headers were set
-        assertEquals("public, max-age=" + (60 * 60 * 24 * 5), response.getHeader(HttpCacheInterceptor.HEADER_CACHE_CONTROL));
+        assertEquals("public, max-age=" + (60 * 60 * 24 * 7), response.getHeader(HttpCacheInterceptor.HEADER_CACHE_CONTROL));
         assertTrue(((Long) response.getHeader(HttpCacheInterceptor.HEADER_EXPIRES)) > 0);
         
         cacheFilter.destroy();
