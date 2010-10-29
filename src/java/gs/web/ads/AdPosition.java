3e0c18fdfc49a8ecb68bcc9ca72f34d6c7ec55ef
yfan <yfan>
2010-10-29 00:08:34 +0000

    GS-10708 Add ad slot needed on slide show pages, for Sony campaign

--- src/java/gs/web/ads/AdPosition.java
+++ src/java/gs/web/ads/AdPosition.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2005 GreatSchools.org. All Rights Reserved.
- * $Id: AdPosition.java,v 1.75 2010/09/27 21:31:34 yfan Exp $
+ * $Id: AdPosition.java,v 1.76 2010/10/29 00:08:34 yfan Exp $
  */
 package gs.web.ads;
 
@@ -148,6 +148,7 @@ public class AdPosition extends Enum {
     public static final AdPosition CustomSponsor_407x65 = new AdPosition("CustomSponsor_407x65", true); // GS-10414
     public static final AdPosition Community_Ad_300x50 = new AdPosition("Community_Ad_300x50", true); // GS-10357
     public static final AdPosition Sponsor_610x16 = new AdPosition("Sponsor_610x16", true); // GS-10540
+    public static final AdPosition Sponsor_BelowFold_610x38 = new AdPosition("Sponsor_BelowFold_610x38", true); // GS-10708
 
     private boolean _isGAMPosition;
     private String _baseName = null;
