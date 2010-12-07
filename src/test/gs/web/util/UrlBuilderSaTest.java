c7e3e9c8c4e7c19531bf165632624e015895bc6f
ssprouse <ssprouse>
2010-12-07 02:55:13 +0000

    GS-10701 Fixed additional unit tests for district home

--- src/test/gs/web/util/UrlBuilderSaTest.java
+++ src/test/gs/web/util/UrlBuilderSaTest.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2005-2006 GreatSchools.org. All Rights Reserved.
- * $Id: UrlBuilderSaTest.java,v 1.130 2010/12/07 02:54:28 ssprouse Exp $
+ * $Id: UrlBuilderSaTest.java,v 1.131 2010/12/07 02:55:13 ssprouse Exp $
  */
 
 package gs.web.util;
