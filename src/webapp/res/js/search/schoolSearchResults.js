d66f7ae3b8baf5cc476f2eb709297a518c4e7ab0
rcox <rcox>
2010-12-13 22:52:10 +0000

    GS-10965 Search : issues with more city/district hover in IE6 - fixed for 16.5

--- src/webapp/res/js/search/schoolSearchResults.js
+++ src/webapp/res/js/search/schoolSearchResults.js
@@ -130,5 +130,19 @@ GS.search.onMapMarkerClick = function(state, id) {
         return queryString;
     }
 
+jQuery(function () {
+if (jQuery.browser.msie && jQuery.browser.version.substr(0,1)<7) {
+jQuery('.js-trigger,.js-popup').mouseover(function(){
+    // do something
+    jQuery('#sort-by').hide();
+    jQuery('#page-size').hide();
+}).mouseout(function(){
+    // do something else
+    jQuery('#sort-by').show();
+    jQuery('#page-size').show();
+})
+}
+});
+
 
 
