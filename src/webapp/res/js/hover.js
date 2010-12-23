e04ac7fca2512ba769db861b62a18c6f4be58464
droy <droy>
2010-12-23 00:28:40 +0000

    GS-11043 Detect new qa servers in community cookie code

--- src/webapp/res/js/hover.js
+++ src/webapp/res/js/hover.js
@@ -932,7 +932,7 @@ GS.getServerName = function() {
     // check for staging
     if (location.hostname.match(/staging\.|staging$|clone/)) {
         serverName = 'staging';
-    } else if (location.hostname.match(/dev\.|dev$|\.office\.|cmsqa|cpickslay\.|localhost|127\.0\.0\.1|macbook/)) {
+    } else if (location.hostname.match(/dev\.|dev$|\.office\.|cmsqa|localhost|127\.0\.0\.1|macbook|qaapp-1\.|qaapp-2\.|qaadmin-1\.|qa\.|qa-preview\./)) {
         serverName = 'dev';
     }
 
