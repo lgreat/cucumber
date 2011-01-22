997281539e9edd46b615669dd5b63b38f7a7a0e6
ssprouse <ssprouse>
2011-01-22 02:40:33 +0000

    GS-10987 Added commented out code to be used after solr migration

--- src/java/gs/web/search/FieldConstraint.java
+++ src/java/gs/web/search/FieldConstraint.java
@@ -4,9 +4,18 @@ import gs.data.search.IndexField;
 import gs.data.search.Indexer;
 
 public enum FieldConstraint {
+    //before solr migration
     DISTRICT_ID(IndexField.DISTRICT),
     CITY(Indexer.CITY_KEYWORD),
     STATE(Indexer.STATE);
+    //
+
+
+    //after solr migration
+    /*DISTRICT_ID(SchoolIndexer.DISTRICT),
+    CITY(SchoolIndexer.ADDRESS_CITY_KEYWORD),
+    STATE(SchoolIndexer.PHYSICAL_STATE);*/
+    //
 
     private String _fieldName;
     FieldConstraint(String fieldName) {
--- src/java/gs/web/search/FieldSort.java
+++ src/java/gs/web/search/FieldSort.java
@@ -3,12 +3,24 @@ package gs.web.search;
 import gs.data.search.Indexer;
 
 public enum FieldSort {
+    //before solr migration
     SCHOOL_NAME_DESCENDING(Indexer.SORTABLE_NAME, true),
     SCHOOL_NAME_ASCENDING(Indexer.SORTABLE_NAME, false),
     GS_RATING_DESCENDING(Indexer.OVERALL_RATING, true),
     GS_RATING_ASCENDING(Indexer.OVERALL_RATING_SORTED_ASC, false),
     PARENT_RATING_DESCENDING(Indexer.PARENT_RATINGS_AVG_QUALITY, true),
     PARENT_RATING_ASCENDING(Indexer.COMMUNITY_RATING_SORTED_ASC, false);
+    //
+
+
+    //after solr migration
+    /*SCHOOL_NAME_DESCENDING(SchoolIndexer.SORTABLE_NAME, true),
+    SCHOOL_NAME_ASCENDING(SchoolIndexer.SORTABLE_NAME, false),
+    GS_RATING_DESCENDING(SchoolIndexer.OVERALL_RATING, true),
+    GS_RATING_ASCENDING(SchoolIndexer.OVERALL_RATING_SORTED_ASC, false),
+    PARENT_RATING_DESCENDING(SchoolIndexer.PARENT_RATINGS_AVG_QUALITY, true),
+    PARENT_RATING_ASCENDING(SchoolIndexer.COMMUNITY_RATING_SORTED_ASC, false);*/
+    //
 
     private String _field;
     private boolean _descending;
