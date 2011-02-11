a11db310645ab4fe8a3d54a101c35cef02bbd836
ssprouse <ssprouse>
2011-02-11 18:43:18 +0000

    GS-10987 - Switched sorting to work for solr migration

--- src/java/gs/web/search/FieldSort.java
+++ src/java/gs/web/search/FieldSort.java
@@ -1,25 +1,24 @@
 package gs.web.search;
 
-import gs.data.search.Indexer;
+import gs.data.search.indexers.documentBuilders.SchoolDocumentBuilder;
 
 public enum FieldSort {
     //before solr migration
-    SCHOOL_NAME_DESCENDING(Indexer.SORTABLE_NAME, true),
+    /*SCHOOL_NAME_DESCENDING(Indexer.SORTABLE_NAME, true),
     SCHOOL_NAME_ASCENDING(Indexer.SORTABLE_NAME, false),
     GS_RATING_DESCENDING(Indexer.OVERALL_RATING, true),
     GS_RATING_ASCENDING(Indexer.OVERALL_RATING_SORTED_ASC, false),
     PARENT_RATING_DESCENDING(Indexer.PARENT_RATINGS_AVG_QUALITY, true),
-    PARENT_RATING_ASCENDING(Indexer.COMMUNITY_RATING_SORTED_ASC, false);
+    PARENT_RATING_ASCENDING(Indexer.COMMUNITY_RATING_SORTED_ASC, false);*/
     //
 
-
     //after solr migration
-    /*SCHOOL_NAME_DESCENDING(SchoolDocumentBuilder.SCHOOL_SORTABLE_NAME, true),
+    SCHOOL_NAME_DESCENDING(SchoolDocumentBuilder.SCHOOL_SORTABLE_NAME, true),
     SCHOOL_NAME_ASCENDING(SchoolDocumentBuilder.SCHOOL_SORTABLE_NAME, false),
     GS_RATING_DESCENDING(SchoolDocumentBuilder.OVERALL_GS_RATING, true),
     GS_RATING_ASCENDING(SchoolDocumentBuilder.OVERALL_GS_RATING_SORTED_ASC, false),
     PARENT_RATING_DESCENDING(SchoolDocumentBuilder.COMMUNITY_RATING, true),
-    PARENT_RATING_ASCENDING(SchoolDocumentBuilder.COMMUNITY_RATING, false);*/
+    PARENT_RATING_ASCENDING(SchoolDocumentBuilder.COMMUNITY_RATING, false);
     //
 
     private String _field;
