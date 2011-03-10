266c248cef74df76c2292a7a7d6bf12fbbde34e9
ssprouse <ssprouse>
2011-03-10 21:13:31 +0000

    GS-10770 Added affiliation filter to FieldFilter

--- src/java/gs/web/search/FieldFilter.java
+++ src/java/gs/web/search/FieldFilter.java
@@ -1,18 +1,38 @@
 package gs.web.search;
 
-import gs.data.school.SchoolType;
-import gs.data.search.Searcher;
-import org.apache.commons.lang.StringUtils;
-
 public interface FieldFilter {
 
     public String getFilterName();
     public String getFilterType();
 
+    public enum AffiliationFilter implements FieldFilter {
+        RELIGOUS("religious"),
+        NONSECTARIAN("nonsectarian");
+
+        private String _filterName;
+        private static String _filterType = "Affiliation";
+
+        AffiliationFilter(String filterName) {
+            _filterName = filterName;
+}
+
+        public String getFilterName() {
+            return _filterName;
+        }
+
+        public void setFilterName(String filterName) {
+            _filterName = filterName;
+        }
+
+        public String getFilterType() {
+            return _filterType;
+        }
+    }
+
     public enum SchoolTypeFilter implements FieldFilter {
-        PRIVATE(Searcher.PRIVATE_SCHOOLS),
-        CHARTER(Searcher.CHARTER_SCHOOLS),
-        PUBLIC(Searcher.PUBLIC_SCHOOLS);
+        PRIVATE("private"),
+        CHARTER("charter"),
+        PUBLIC("public");
 
         private String _filterName;
         private static String _filterType = "SchoolType";
