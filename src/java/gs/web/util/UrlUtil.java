29281c337b5d631fc0a71c0a9cfefb904810ca07
yfan <yfan>
2011-04-06 22:49:30 +0000

    GS-11569 SITE: Remove and/or optimize ad calls to improve site performance - added Library_Article_Page_BelowFold_Top_300x125 to all topic centers, including p/e/m/h landing pages

--- src/java/gs/web/util/UrlUtil.java
+++ src/java/gs/web/util/UrlUtil.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2005 GreatSchools.org. All Rights Reserved.
- * $Id: UrlUtil.java,v 1.103 2011/04/06 19:16:49 cshumway Exp $
+ * $Id: UrlUtil.java,v 1.104 2011/04/06 22:49:30 yfan Exp $
  */
 
 package gs.web.util;
@@ -356,7 +356,8 @@ public final class UrlUtil {
 
     public static boolean isStagingServer(String hostName) {
         return hostName.indexOf("staging") != -1 ||
-                hostName.indexOf("clone") != -1;
+                hostName.indexOf("clone") != -1 ||
+                hostName.indexOf("willow") != -1;
     }
 
     public static boolean isQAServer(String hostName) {
