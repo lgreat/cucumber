53f170270ef701652b1df66472cfe8842e654d14
cshumway <cshumway>
2011-04-06 19:16:49 +0000

    Prevent sw1-{pub,pre}.greatschools.org from being a cobrand, also remove some old hosts

--- src/java/gs/web/util/UrlUtil.java
+++ src/java/gs/web/util/UrlUtil.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2005 GreatSchools.org. All Rights Reserved.
- * $Id: UrlUtil.java,v 1.102 2011/03/29 17:33:13 yfan Exp $
+ * $Id: UrlUtil.java,v 1.103 2011/04/06 19:16:49 cshumway Exp $
  */
 
 package gs.web.util;
@@ -81,8 +81,8 @@ public final class UrlUtil {
                 && !hostName.startsWith("maddy")
                 && !hostName.startsWith("admin")
                 && !hostName.startsWith("clone")
-                && !hostName.startsWith("cmsqa")
-                && !hostName.startsWith("crufty")
+                && !hostName.startsWith("sw1-pub")
+                && !hostName.startsWith("sw1-pre")
                 && !hostName.startsWith("dev")
                 && !hostName.startsWith("localhost")
                 && !hostName.startsWith("main.dev")
