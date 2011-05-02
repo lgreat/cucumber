10ba55ffc9cd0eb43707dbec769ca5fb090534f7
rcox <rcox>
2011-05-02 19:05:21 +0000

    GS-11702 All Perl Pages need Style Utility Header module - toggle tabs v2.

--- src/webapp/res/js/search/schoolAutocomplete.js
+++ src/webapp/res/js/search/schoolAutocomplete.js
@@ -1,13 +1,3 @@
-jQuery('#srch2').click(function() {
-    jQuery('#srch1').removeClass('active');
-    jQuery(this).addClass('active');
-});
-jQuery('#srch1').click(function() {
-    jQuery('#srch12').removeClass('active');
-    jQuery(this).addClass('active');
-});
-
-
 var GS = GS || {};
 
 GS.attachSchoolAutocomplete = function(domId) {
