e99caec146e48b34e55f3ce6eabfd18ea373d491
ssprouse <ssprouse>
2011-06-29 02:50:33 +0000

    GS-11651 Fixed bug with selections not being kept during pagination

--- src/webapp/res/js/content/cms/videoGallery.js
+++ src/webapp/res/js/content/cms/videoGallery.js
@@ -21,9 +21,7 @@ GS.content.cms.VideoGallery = function() {};
         data.confirm = "true";
         var queryString = window.location.search;
         var content = GS.uri.Uri.getFromQueryString("content");
-        var grades = GS.uri.Uri.getFromQueryString("grades");
         var subjects = GS.uri.Uri.getFromQueryString("subjects");
-        var topics = GS.uri.Uri.getFromQueryString("topics");
 
         //TODO: make a lot of the code in this method generic and put it elsewhere so it can easily be reused
 
@@ -32,43 +30,22 @@ GS.content.cms.VideoGallery = function() {};
             data.content = content;
             queryString = GS.uri.Uri.removeFromQueryString(queryString, "content");
         }
-        //Grades,subjects,topics are cms entered for the 'videos' subtopic on the topic center template.
-        //Therefore pass them through.
-        if (grades != undefined && grades != "") {
-            data.grades = grades;
-            queryString = GS.uri.Uri.removeFromQueryString(queryString, "grades");
-        }
 
         if (subjects != undefined && subjects != "") {
             data.subjects = subjects;
             queryString = GS.uri.Uri.removeFromQueryString(queryString, "subjects");
         }
 
-        if (topics != undefined && topics != "") {
-            data.topics = topics;
-            queryString = GS.uri.Uri.removeFromQueryString(queryString, "topics");
-        }
-
         //Add the user selected grades filter to the grades params.
         if ($("#js_gradesDropdown").val() != -1) {
-            if (data.grades != undefined && data.grades.length > 0) {
-                data.grades = data.grades + "," + $("#js_gradesDropdown").val();
-                queryString = GS.uri.Uri.removeFromQueryString(queryString, "grades");
-            } else {
-                data.grades = $("#js_gradesDropdown").val();
-                queryString = GS.uri.Uri.removeFromQueryString(queryString, "grades");
-            }
+            data.grades = $("#js_gradesDropdown").val();
+            queryString = GS.uri.Uri.removeFromQueryString(queryString, "grades");
         }
 
         //Add the user selected topics/subjects filter to the topics params.
         if ($("#js_topicsDropdown").val() != -1) {
-            if (data.topics != undefined && data.topics.length > 0) {
-                data.topics = data.topics + "," + $("#js_topicsDropdown").val();
-                queryString = GS.uri.Uri.removeFromQueryString(queryString, "topics");
-            } else {
-                data.topics = $("#js_topicsDropdown").val();
-                queryString = GS.uri.Uri.removeFromQueryString(queryString, "topics");
-            }
+            data.topics = $("#js_topicsDropdown").val();
+            queryString = GS.uri.Uri.removeFromQueryString(queryString, "topics");
         }
 
         $j('#spinner').show();
