5b09e91467559da3fa3c022836fabdafc814f3d9
aroy <aroy>
2011-08-24 18:31:10 +0000

    GS-12102 Refining skeleton for NBC quiz controller

--- src/java/gs/web/promo/NbcQuizController.java
+++ src/java/gs/web/promo/NbcQuizController.java
@@ -3,6 +3,7 @@ package gs.web.promo;
 import gs.data.json.JSONException;
 import gs.data.json.JSONObject;
 import gs.web.util.ReadWriteAnnotationController;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Enumeration;
 import java.util.regex.Pattern;
 
@@ -22,30 +22,59 @@ import java.util.regex.Pattern;
 @RequestMapping("/nbc/quiz.page")
 public class NbcQuizController implements ReadWriteAnnotationController {
     protected static final Log _log = LogFactory.getLog(NbcQuizController.class);
+
+    protected enum SaveStatus {
+        SUCCESS ("Success", "Success"),
+        ERROR_NO_CHILD_AGE ("Error", "Child age must be provided -- field " + PARAM_CHILD_AGE + " missing or empty"),
+        ERROR_SERVER ("Error", "Server error");
+
+        private final String _status;
+        private final String _message;
+        SaveStatus(String status, String message) {
+            _status = status;
+            _message = message;
+        }
+        public void writeToJSON(JSONObject o) throws JSONException {
+            o.put("status", _status);
+            o.put("message", _message);
+        }
+        public String toString() {
+            return "SaveStatus{" +_status + ":" + _message + "}";
+        }
+    }
+
     public static final String PARAM_CHILD_AGE = "childAge";
     public static final String PARAM_PARENT_AGE = "parentAge";
     public static final String PARAM_ZIP = "zip";
-    public static final Pattern PATTERN_PARAM_ANSWER = Pattern.compile("q(\\d+)");
+    public static final Pattern PATTERN_PARAM_ANSWER = Pattern.compile("^q(\\d+)$");
 
     @RequestMapping(method = RequestMethod.POST)
     /**
-     * saveResponse
+     * saveStatus = saveResponse
      * getAggregate
-     * writeOut(aggregate)
+     * writeOut(saveStatus, aggregate)
+     *
+     * Contract on response status:
+     * 200 - output includes valid aggregate data, status field, and status message
+     * 500 - no guaranteed output
      */
     public void submit(HttpServletRequest request, HttpServletResponse response) {
         response.setContentType("application/json");
         try {
-            saveResponse(request); // does not throw
-            JSONObject aggregateData = getAggregate(); // does not throw
+            SaveStatus saveStatus = saveResponse(request); // should not throw or return null
+            JSONObject aggregateData = getAggregate(); // should not throw
             if (aggregateData != null) {
-                // writeOut(aggregateData, response)
-                aggregateData.write(response.getWriter()); // throws on error writing to response
+                // combine aggregateData with saveStatus and print out
+                JSONObject output = new JSONObject();
+                output.put("aggregate", aggregateData);
+                saveStatus.writeToJSON(output);
+                output.write(response.getWriter()); // throws on error writing to response
             } else {
+                // no aggregate means return 500
                 response.setStatus(500);
-                response.getWriter().write("{error:\"Cannot determine aggregate\"}"); // throws ioexception
             }
         } catch (Exception e) {
+            // there was a failure writing out. Return a 500
             response.setStatus(500);
             _log.error("Error writing to response: " + e, e);
         }
@@ -58,46 +87,49 @@ public class NbcQuizController implements ReadWriteAnnotationController {
      *
      * @param request HttpServletRequest containing quiz response
      */
-    public void saveResponse(HttpServletRequest request) {
+    public SaveStatus saveResponse(HttpServletRequest request) {
         try {
-            Object responseObject = parseResponseObject(request);
-            if (responseObject != null) {
-                // _dao.save(responseObject)
-            }
+            Object quizTaken = parseQuizTaken(request);
+            // _dao.save(quizTaken)
+            return SaveStatus.SUCCESS;
+        } catch (ParseQuizTakenException pqte) {
+            return pqte.getSaveStatus();
         } catch (Exception e) {
-            _log.error("Error saving quiz response: " + e, e);
+            // could be thrown by _dao.save
+            // In which case there is no meaningful message we can communicate to view, so just log it
+            _log.error("Error saving quiz taken: " + e, e);
+            return SaveStatus.ERROR_SERVER;
         }
     }
 
     /**
      * parse static parameters
      * parse dynamic parameters
-     * return populated hibernate object
+     * return populated hibernate QuizTaken object
      *
-     * @param request HttpServletRequest containing quiz response
-     * @return hibernate object containing quiz response, or null on error
+     * @param request HttpServletRequest containing quiz taken data
+     * @return QuizTaken object containing quiz response, never null
+     * @throws ParseQuizTakenException on any error constructing the QuizTaken object
      */
-    public Object parseResponseObject(HttpServletRequest request) {
-        try {
-            // parse static parameters
-            String childAge = request.getParameter(PARAM_CHILD_AGE);
-            String parentAge = request.getParameter(PARAM_PARENT_AGE);
-            String zip = request.getParameter(PARAM_ZIP);
-
-            // parse dynamic parameters
-            Enumeration paramNames = request.getParameterNames();
-            while (paramNames.hasMoreElements()) {
-                String questionKey = String.valueOf(paramNames.nextElement());
-                if (PATTERN_PARAM_ANSWER.matcher(questionKey).matches()) {
-                    String questionResponse = request.getParameter(questionKey);
-                }
-            }
-            return new Object();
-        } catch (Exception e) {
-            _log.error("Error parsing response object: " + e, e);
+    public Object parseQuizTaken(HttpServletRequest request) throws ParseQuizTakenException {
+        Object rval = new Object();
+        // parse static parameters
+        String childAge = request.getParameter(PARAM_CHILD_AGE);
+        if (StringUtils.isBlank(childAge)) { // sample validation
+            throw new ParseQuizTakenException(SaveStatus.ERROR_NO_CHILD_AGE);
         }
+        String parentAge = request.getParameter(PARAM_PARENT_AGE);
+        String zip = request.getParameter(PARAM_ZIP);
 
-        return null;
+        // parse dynamic parameters
+        Enumeration paramNames = request.getParameterNames();
+        while (paramNames.hasMoreElements()) {
+            String questionKey = String.valueOf(paramNames.nextElement());
+            if (PATTERN_PARAM_ANSWER.matcher(questionKey).matches()) {
+                String questionResponse = request.getParameter(questionKey);
+            }
+        }
+        return rval;
     }
 
     /**
@@ -110,12 +142,32 @@ public class NbcQuizController implements ReadWriteAnnotationController {
         try {
             // if (cache) return cached aggregate
             // aggregate = _dao.fetchAggregate()
+            JSONObject aggregate = new JSONObject();
             // if (aggregate != null) cache(aggregate)
-            // return aggregate
-            return new JSONObject();
+            aggregate.put("Parent Type A", "80"); // for debugging
+            aggregate.put("Parent Type B", "20"); // for debugging
+            return aggregate;
         } catch (Exception e) {
             _log.error("Error constructing aggregate data: " + e, e);
         }
         return null;
+    }
+
+    /**
+     * Thrown on any sort of error parsing the request params or constructing the QuizTaken object.
+     * Field saveStatus indicates the error.
+     */
+    class ParseQuizTakenException extends Exception {
+        private SaveStatus _saveStatus;
+        protected ParseQuizTakenException(SaveStatus status) {
+            super();
+            _saveStatus = status;
+        }
+        public SaveStatus getSaveStatus() {
+            return _saveStatus;
+        }
+        public String toString() {
+            return _saveStatus._message;
+        }
     }
 }
--- src/test/gs/web/promo/NbcQuizControllerTest.java
+++ src/test/gs/web/promo/NbcQuizControllerTest.java
@@ -17,4 +17,59 @@ public class NbcQuizControllerTest extends BaseControllerTestCase {
     public void testBasics() {
         assertNotNull(_controller);
     }
+
+    private void replayAllMocks() {
+        super.replayMocks();
+    }
+
+    private void verifyAllMocks() {
+        super.verifyMocks();
+    }
+
+    public void testParseResponseObjectNoChild() {
+        // Test that no child age field results in an exception
+
+        replayAllMocks();
+        try {
+            _controller.parseQuizTaken(getRequest());
+            fail("Expect validation error when no parameters supplied");
+        } catch (NbcQuizController.ParseQuizTakenException pqte) {
+            assertEquals(NbcQuizController.SaveStatus.ERROR_NO_CHILD_AGE, pqte.getSaveStatus());
+        }
+        verifyAllMocks();
+    }
+
+    public void xtestParseResponseObjectTypeConversionFailure() {
+        // DELETE THIS TEST IF NO TYPE CONVERSION OCCURS
+        // Test that a failure converting a request param into another type is thrown
+
+        // TODO set some fields on request, including one that is typed incorrectly
+
+        replayAllMocks();
+        try {
+            _controller.parseQuizTaken(getRequest());
+            fail("Error converting parameter should throw exception");
+        } catch (NbcQuizController.ParseQuizTakenException pqte) {
+            // TODO assert that the save status is the right one
+            //assertEquals(NbcQuizController.SaveStatus.??, pqte.getSaveStatus());
+        }
+        verifyAllMocks();
+    }
+
+    public void testParseResponseObject() {
+        // Test that a valid set of response fields results in a return value
+
+        // set fields on request
+        getRequest().setParameter(NbcQuizController.PARAM_CHILD_AGE, "3");
+        replayAllMocks();
+        Object rval = null;
+        try {
+            rval = _controller.parseQuizTaken(getRequest());
+        } catch (NbcQuizController.ParseQuizTakenException pqte) {
+            fail("Unexpected exception on valid set of parameters: " + pqte);
+        }
+        verifyAllMocks();
+        assertNotNull("Expect quiz response object when valid fields are provided", rval);
+        // TODO assert rval fields match request fields
+    }
 }
