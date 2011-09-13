def468cdfcb9a0ee8a6a7d1f9c8fd95edfac5dfa
yfan <yfan>
2011-09-13 00:48:22 +0000

    GS-12144 Worksheets: Add meta data to worksheets gallery pages - work in progress

--- src/java/gs/web/content/cms/WorksheetGalleryController.java
+++ src/java/gs/web/content/cms/WorksheetGalleryController.java
@@ -51,6 +51,13 @@ public class WorksheetGalleryController extends CmsTopicCenterController2010 {
     public static Map<String,String> GRADE_CHOICES = new LinkedHashMap<String,String>();
     public static Map<String,String> SUBJECT_CHOICES = new LinkedHashMap<String,String>();
 
+    public static Map<String,String> META_KEYWORDS_SUBJECT_ONLY = new HashMap<String,String>();
+    public static Map<String,String> META_KEYWORDS_GRADE_ONLY = new HashMap<String,String>();
+    public static Map<String,String> META_GRADE_KEY_TO_NUMERIC_NAME = new HashMap<String,String>();
+    public static Map<String,String> META_GRADE_KEY_TO_SHORT_NAME = new HashMap<String,String>();
+    public static Map<String,String> META_DESCRIPTION_PK_K_GRADE_SUBJECT = new HashMap<String,String>();
+    public static Map<String,String> META_KEYWORDS_PK_K_GRADE_SUBJECT = new HashMap<String,String>();
+
     public static String SUBJECT_CHOICES_KEY = "subjectChoices";
     public static String GRADE_CHOICES_KEY = "gradeChoices";
     public static String REQUESTED_GRADE_KEY = "requestedGrade";
@@ -68,9 +75,11 @@ public class WorksheetGalleryController extends CmsTopicCenterController2010 {
     public static final int WORKSHEET_DEFAULT_PAGE_SIZE = 6;
 
     public static final String MODEL_WORKSHEET_RESULTS = "worksheetResults";
+    public static final String MODEL_META_DESCRIPTION = "metaDescription";
+    public static final String MODEL_META_KEYWORDS = "metaKeywords";
 
     public static final String WORKSHEETS_PATH = "/worksheets";
-    
+
     static {
 
         WORKSHEET_GALLERY_PAGINATION_CONFIG = new PaginationConfig(
@@ -113,6 +122,55 @@ public class WorksheetGalleryController extends CmsTopicCenterController2010 {
         SUBJECT_URL_COMPONENT_LOOKUP.put("math",CmsConstants.MATH_CATEGORY_ID);
         SUBJECT_URL_COMPONENT_LOOKUP.put("reading",CmsConstants.READING_CATEGORY_ID);
         SUBJECT_URL_COMPONENT_LOOKUP.put("writing",CmsConstants.WRITING_CATEGORY_ID);
+
+        // GS-12144 START constants for meta keywords & meta descriptions
+
+        META_KEYWORDS_SUBJECT_ONLY.put("math", "math worksheets, math worksheet, mathematics worksheet, maths worksheet, worksheets for math");
+        META_KEYWORDS_SUBJECT_ONLY.put("reading", "reading worksheets, reading comprehension worksheets, language worksheets, nouns worksheets, grammar worksheets");
+        META_KEYWORDS_SUBJECT_ONLY.put("writing", "writing worksheets, writing worksheet");
+
+        META_GRADE_KEY_TO_NUMERIC_NAME.put("kindergarten","kindergarten");
+        META_GRADE_KEY_TO_NUMERIC_NAME.put("first-grade","1st grade");
+        META_GRADE_KEY_TO_NUMERIC_NAME.put("second-grade","2nd grade");
+        META_GRADE_KEY_TO_NUMERIC_NAME.put("third-grade","3rd grade");
+        META_GRADE_KEY_TO_NUMERIC_NAME.put("fourth-grade","4th grade");
+        META_GRADE_KEY_TO_NUMERIC_NAME.put("fifth-grade","5th grade");
+
+        META_GRADE_KEY_TO_SHORT_NAME.put("kindergarten","kindergarten");
+        META_GRADE_KEY_TO_SHORT_NAME.put("first-grade","first grade");
+        META_GRADE_KEY_TO_SHORT_NAME.put("second-grade","second grade");
+        META_GRADE_KEY_TO_SHORT_NAME.put("third-grade","third grade");
+        META_GRADE_KEY_TO_SHORT_NAME.put("fourth-grade","fourth grade");
+        META_GRADE_KEY_TO_SHORT_NAME.put("fifth-grade","fifth grade");
+
+        META_KEYWORDS_GRADE_ONLY.put("preschool", "preschool worksheets, preschool worksheet, pre school worksheets, worksheets for preschool kids");
+        META_KEYWORDS_GRADE_ONLY.put("kindergarten", "worksheet for kindergarten, kindergarten worksheets, worksheets for kindergarten, kindergarten worksheet");
+
+        for (String gradeKey : META_GRADE_KEY_TO_SHORT_NAME.keySet()) {
+            if (!META_KEYWORDS_GRADE_ONLY.containsKey(gradeKey)) {
+                META_KEYWORDS_GRADE_ONLY.put(gradeKey,
+                        "worksheets for " + META_GRADE_KEY_TO_NUMERIC_NAME.get(gradeKey) + "rs, " +
+                        META_GRADE_KEY_TO_NUMERIC_NAME.get(gradeKey) + " worksheets, " +
+                        META_GRADE_KEY_TO_SHORT_NAME.get(gradeKey) + " worksheets, " +
+                        "worksheets for " + META_GRADE_KEY_TO_SHORT_NAME.get(gradeKey));
+            }
+        }
+
+        META_DESCRIPTION_PK_K_GRADE_SUBJECT.put("preschool/math", "Free printable preschool math worksheets to help your child practice numbers, counting, and addition.");
+        META_DESCRIPTION_PK_K_GRADE_SUBJECT.put("preschool/reading", "Free printable preschool reading worksheets to help your child practice the alphabet, letter sounds, and new words.");
+        META_DESCRIPTION_PK_K_GRADE_SUBJECT.put("preschool/writing", "Free printable preschool writing worksheets to help your child practice tracing and coloring shapes and letters.");
+        META_KEYWORDS_PK_K_GRADE_SUBJECT.put("preschool/math", "preschool math worksheets");
+        META_KEYWORDS_PK_K_GRADE_SUBJECT.put("preschool/reading", "preschool reading worksheets, alphabet worksheets");
+        META_KEYWORDS_PK_K_GRADE_SUBJECT.put("preschool/writing", "preschool writing worksheets, coloring worksheets");
+
+        META_DESCRIPTION_PK_K_GRADE_SUBJECT.put("kindergarten/math", "Free printable kindergarten math worksheets to help your child practice addition, matching, and identifying shapes and patterns.");
+        META_DESCRIPTION_PK_K_GRADE_SUBJECT.put("kindergarten/reading", "Free printable kindergarten reading worksheets to help your child with spelling, language, grammar, and more.");
+        META_DESCRIPTION_PK_K_GRADE_SUBJECT.put("kindergarten/writing", "Free printable kindergarten writing worksheets to help your child practice tracing and writing shapes and letters.");
+        META_KEYWORDS_PK_K_GRADE_SUBJECT.put("kindergarten/math", "kindergarten math worksheets");
+        META_KEYWORDS_PK_K_GRADE_SUBJECT.put("kindergarten/reading", "kindergarten reading worksheets, grammar worksheets");
+        META_KEYWORDS_PK_K_GRADE_SUBJECT.put("kindergarten/writing", "kindergarten writing worksheets");
+
+        // GS-12144 END constants for meta keywords & meta descriptions
     }
 
     //=========================================================================
@@ -172,12 +230,65 @@ public class WorksheetGalleryController extends CmsTopicCenterController2010 {
         //start adding content for middle area of page
         addPageSpecificContentToModel(request, topicCenter, model);
 
+        // IMPORTANT: must be called after addPageSpecificContentToModel
+        addMetaDataToModel(model);
 
         if(isAjaxRequest(request) && StringUtils.isNotBlank(request.getParameter("requestType")) && request.getParameter("requestType").equals("ajax")){
            return new ModelAndView("/content/cms/worksheetGalleryTable", model);
         } else {
             return new ModelAndView("/content/cms/worksheetGallery", model);
         }
+    }
+
+    /**
+     * must be called after addPageSpecificContentToModel
+     * @param model
+     */
+    public static void addMetaDataToModel(Map<String, Object> model) {
+        String metaDescription = null;
+        String metaKeywords = null;
+
+        String requestedGradeKey = (String)model.get(REQUESTED_GRADE_KEY);
+        String requestedSubjectKey = (String)model.get(REQUESTED_SUBJECT_KEY);
+
+        // worksheet gallery not filtered by grade, so just use topic center's CMS-entered meta description and keywords
+        if (requestedGradeKey == null && requestedSubjectKey == null) {
+            return;
+        }
+
+        if (requestedGradeKey == null && requestedSubjectKey != null) {
+            // subject-specific gallery
+            metaDescription = "Free " + requestedSubjectKey + " worksheets you can print and use with children in preschool through fifth grade.";
+            metaKeywords = META_KEYWORDS_SUBJECT_ONLY.get(requestedSubjectKey);
+        } else if (requestedGradeKey != null && requestedSubjectKey == null) {
+            // grade-specific gallery
+            if ("preschool".equals(requestedGradeKey)) {
+                metaDescription = "Free printable preschool worksheets to help your child develop early math, reading, and writing skills.";
+                metaKeywords = META_KEYWORDS_GRADE_ONLY.get(requestedGradeKey);
+            } else if ("elementary-school".equals(requestedGradeKey)) {
+                metaDescription = "Free elementary worksheets for kids in grades K-5; support learning with a supply of fun printables.";
+                metaKeywords = "elementary worksheets, worksheets for kids";
+            } else {
+                metaDescription = "Free printable " + META_GRADE_KEY_TO_NUMERIC_NAME.get(requestedGradeKey) + " worksheets to help your whiz kid practice math, reading, and writing skills.";
+                metaKeywords = META_KEYWORDS_GRADE_ONLY.get(requestedGradeKey);
+            }
+        } else {
+            // grade- and subject-specific gallery
+            if ("preschool".equals(requestedGradeKey) || "kindergarten".equals(requestedGradeKey)) {
+                metaDescription = META_DESCRIPTION_PK_K_GRADE_SUBJECT.get(requestedGradeKey + "/" + requestedSubjectKey);
+                metaKeywords = META_KEYWORDS_PK_K_GRADE_SUBJECT.get(requestedGradeKey + "/" + requestedSubjectKey);
+            } else {
+                metaDescription = "Free printable " + META_GRADE_KEY_TO_NUMERIC_NAME.get(requestedGradeKey) + " " +
+                        requestedSubjectKey + " worksheets to help your child practice skills while having fun.";
+                metaKeywords = META_GRADE_KEY_TO_NUMERIC_NAME.get(requestedGradeKey) + " " + requestedSubjectKey + " worksheets, " +
+                        META_GRADE_KEY_TO_SHORT_NAME.get(requestedGradeKey) + " " + requestedSubjectKey + " worksheets";
+            }
+        }
+
+        // TODO-12144 what about /worksheets/elementary-school/<subject>/ or /worksheets/ ???
+
+        model.put(MODEL_META_DESCRIPTION, metaDescription);
+        model.put(MODEL_META_KEYWORDS, metaKeywords);
     }
 
     public void addGoogleAdKeywordsIfNeeded(HttpServletRequest request, CmsTopicCenter topicCenter) {
@@ -206,7 +317,7 @@ public class WorksheetGalleryController extends CmsTopicCenterController2010 {
         String requestedSubject = null;
 
         //we need to locate requested grade and/or subject if they exist. Give priority to querystring params over URL structure
-        
+
         requestedGradeId = request.getParameter(GRADE_ID_REQUEST_PARAM);
         if (StringUtils.isNotBlank(requestedGradeId)) {
             //TODO: should probably use a bidirectional map to do this
@@ -254,7 +365,7 @@ public class WorksheetGalleryController extends CmsTopicCenterController2010 {
                 }
             }
         }
-        
+
         GsSolrQuery query = new GsSolrQuery();
         query.filter(DocumentType.CMS_FEATURE);
         query.filter(CmsFeatureFields.FIELD_CONTENT_TYPE, CmsConstants.WORKSHEET_CONTENT_TYPE);
---
+++ src/test/gs/web/content/cms/WorksheetGalleryControllerTest.java
@@ -0,0 +1,130 @@
+package gs.web.content.cms;
+
+import junit.framework.TestCase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WorksheetGalleryControllerTest extends TestCase {
+    public void testAddMetaDataToModel() throws Exception {
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        model.clear();
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertNull(model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertNull(model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_SUBJECT_KEY, "math");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free math worksheets you can print and use with children in preschool through fifth grade.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("math worksheets, math worksheet, mathematics worksheet, maths worksheet, worksheets for math", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_SUBJECT_KEY, "reading");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free reading worksheets you can print and use with children in preschool through fifth grade.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("reading worksheets, reading comprehension worksheets, language worksheets, nouns worksheets, grammar worksheets", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_SUBJECT_KEY, "writing");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free writing worksheets you can print and use with children in preschool through fifth grade.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("writing worksheets, writing worksheet", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_GRADE_KEY, "preschool");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free printable preschool worksheets to help your child develop early math, reading, and writing skills.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("preschool worksheets, preschool worksheet, pre school worksheets, worksheets for preschool kids", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_GRADE_KEY, "kindergarten");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free printable kindergarten worksheets to help your whiz kid practice math, reading, and writing skills.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("worksheet for kindergarten, kindergarten worksheets, worksheets for kindergarten, kindergarten worksheet", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_GRADE_KEY, "first-grade");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free printable 1st grade worksheets to help your whiz kid practice math, reading, and writing skills.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("worksheets for 1st graders, 1st grade worksheets, first grade worksheets, worksheets for first grade", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_GRADE_KEY, "third-grade");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free printable 3rd grade worksheets to help your whiz kid practice math, reading, and writing skills.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("worksheets for 3rd graders, 3rd grade worksheets, third grade worksheets, worksheets for third grade", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_GRADE_KEY, "preschool");
+        model.put(WorksheetGalleryController.REQUESTED_SUBJECT_KEY, "math");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free printable preschool math worksheets to help your child practice numbers, counting, and addition.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("preschool math worksheets", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_GRADE_KEY, "preschool");
+        model.put(WorksheetGalleryController.REQUESTED_SUBJECT_KEY, "reading");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free printable preschool reading worksheets to help your child practice the alphabet, letter sounds, and new words.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("preschool reading worksheets, alphabet worksheets", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_GRADE_KEY, "preschool");
+        model.put(WorksheetGalleryController.REQUESTED_SUBJECT_KEY, "writing");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free printable preschool writing worksheets to help your child practice tracing and coloring shapes and letters.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("preschool writing worksheets, coloring worksheets", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_GRADE_KEY, "kindergarten");
+        model.put(WorksheetGalleryController.REQUESTED_SUBJECT_KEY, "math");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free printable kindergarten math worksheets to help your child practice addition, matching, and identifying shapes and patterns.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("kindergarten math worksheets", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_GRADE_KEY, "kindergarten");
+        model.put(WorksheetGalleryController.REQUESTED_SUBJECT_KEY, "reading");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free printable kindergarten reading worksheets to help your child with spelling, language, grammar, and more.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("kindergarten reading worksheets, grammar worksheets", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_GRADE_KEY, "kindergarten");
+        model.put(WorksheetGalleryController.REQUESTED_SUBJECT_KEY, "writing");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free printable kindergarten writing worksheets to help your child practice tracing and writing shapes and letters.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("kindergarten writing worksheets", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_GRADE_KEY, "elementary-school");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free elementary worksheets for kids in grades K-5; support learning with a supply of fun printables.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("elementary worksheets, worksheets for kids", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_GRADE_KEY, "second-grade");
+        model.put(WorksheetGalleryController.REQUESTED_SUBJECT_KEY, "math");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free printable 2nd grade math worksheets to help your child practice skills while having fun.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("2nd grade math worksheets, second grade math worksheets", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_GRADE_KEY, "third-grade");
+        model.put(WorksheetGalleryController.REQUESTED_SUBJECT_KEY, "reading");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free printable 3rd grade reading worksheets to help your child practice skills while having fun.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("3rd grade reading worksheets, third grade reading worksheets", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        model.clear();
+        model.put(WorksheetGalleryController.REQUESTED_GRADE_KEY, "fifth-grade");
+        model.put(WorksheetGalleryController.REQUESTED_SUBJECT_KEY, "writing");
+        WorksheetGalleryController.addMetaDataToModel(model);
+        assertEquals("Free printable 5th grade writing worksheets to help your child practice skills while having fun.", model.get(WorksheetGalleryController.MODEL_META_DESCRIPTION));
+        assertEquals("5th grade writing worksheets, fifth grade writing worksheets", model.get(WorksheetGalleryController.MODEL_META_KEYWORDS));
+
+        // TODO-12144 what about /worksheets/elementary-school/<subject>/ or /worksheets/ ???
+    }
+}
