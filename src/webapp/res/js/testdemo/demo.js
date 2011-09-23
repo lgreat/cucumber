75cb03d91d935616930cf0d0572a1c079e05c1a7
ssprouse <ssprouse>
2011-09-23 23:31:26 +0000

    More jsTestDriver stuff

--- src/webapp/res/js/testdemo/demo.js
+++ src/webapp/res/js/testdemo/demo.js
@@ -6,3 +6,7 @@ GS.Person.prototype.speak = function(words) {
     console.log("GS.Person.speak called with: " + words);
     return words;
 };
+
+GS.Person.prototype.usingGoodBrowser = function() {
+    return navigator.appVersion.indexOf("Chrome") > -1;
+}
--- src/webapp/test/js/testdemo/demo_test.js
+++ src/webapp/test/js/testdemo/demo_test.js
@@ -1,8 +1,14 @@
 PersonTest = TestCase("PersonTest");
 
+PersonTest.prototype.setUp = function() {
+  this.person = new GS.Person();
+};
+
 PersonTest.prototype.testSpeaks = function() {
-  var person = new GS.Person();
-  assertEquals("Should returned Hellow World!", "Hello World!", person.speak("Hello World!"));
+  assertEquals("Should returned Hellow World!", "Hello World!", this.person.speak("Hello World!"));
   //assertNull("Should have been null", person.speak("Hello World!")); this test will fail
 };
 
+PersonTest.prototype.testUsingGoodBrowser = function() {
+  assertTrue("Only a good browser is supported!", this.person.usingGoodBrowser());
+};
