7c962e679b597c289b74180549f2f506f3638ba6
ssprouse <ssprouse>
2011-12-22 01:24:23 +0000

    GS-12064 fixed accidental checkin of invalid page call

--- src/webapp/res/js/parentReviewTabSchoolReviewForm.js
+++ src/webapp/res/js/parentReviewTabSchoolReviewForm.js
@@ -8,7 +8,7 @@ jQuery(function() {
 
     GS.form.ParentReviewTabSchoolReviewForm.prototype = new GS.form.SchoolReviewForm("frmPRModule");
     GS.form.ParentReviewTabSchoolReviewForm.prototype.postReview = function(email, callerFormId) {
-        var url = GS.uri.Uri.getBaseHostname() + '/school/review/postReview2.page';
+        var url = GS.uri.Uri.getBaseHostname() + '/school/review/postReview.page';
         
         //When this method is called by the "sign in" handler, overwrite review form's email with whatever user signed in with.
         if (email !== undefined && email !== '') {
@@ -67,6 +67,7 @@ jQuery(function() {
                 }
             }
         }).fail(function(data){
+                    console.log(data);
             alert("We're sorry, but we were not able to process your review submission. Please try again soon.");
         });
     }.gs_bind(GS.form.ParentReviewTabSchoolReviewForm.prototype);
