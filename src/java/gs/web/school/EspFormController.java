9d0e4011fa4fe86bb818484bbf4c4315637920e2
aroy <aroy>
2012-01-26 16:29:01 +0000

    GS-12337 Prototyped support for read/write hooks, lots of documentation and reorganization

--- src/java/gs/web/school/EspFormController.java
+++ src/java/gs/web/school/EspFormController.java
@@ -26,6 +26,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -57,6 +59,7 @@ public class EspFormController implements ReadWriteAnnotationController {
     public String showForm(ModelMap modelMap, HttpServletRequest request,
                            @RequestParam(value=PARAM_SCHOOL_ID, required=false) Integer schoolId, 
                            @RequestParam(value=PARAM_STATE, required=false) State state) {
+        // Fetch parameters
         User user = getValidUser(request, state, schoolId);
         if (user == null) {
             UrlBuilder urlBuilder = new UrlBuilder(UrlBuilder.ESP_SIGN_IN);
@@ -75,27 +78,31 @@ public class EspFormController implements ReadWriteAnnotationController {
             UrlBuilder urlBuilder = new UrlBuilder(UrlBuilder.ESP_DASHBOARD);
             return "redirect:" + urlBuilder.asFullUrl(request);
         }
+        // populate basic model data
         modelMap.put("school", school);
         modelMap.put("page", page);
         modelMap.put("maxPage", maxPage);
         modelMap.put("espSuperuser", user.hasRole(Role.ESP_SUPERUSER));
 
-        putResponsesInModel(school, page, modelMap);
+        putResponsesInModel(school, page, modelMap); // fetch responses for school, including external data
+        putPercentCompleteInModel(school, modelMap);
 
         return VIEW;
     }
     
-    protected void putResponsesInModel(School school, int page, ModelMap modelMap) {
-        Map<String, EspResponseStruct> responseMap = new HashMap<String, EspResponseStruct>();
-        
-        // fetch all responses for now to allow page to use ajax page switching if desired.
-        List<EspResponse> responses = _espResponseDao.getResponses(school);
-
+    protected void putPercentCompleteInModel(School school, ModelMap modelMap) {
         Map<Integer, Integer> percentCompleteMap = new HashMap<Integer, Integer>();
         for (int x=1; x <= getMaxPageForSchool(school); x++) {
             percentCompleteMap.put(x, getPercentCompletionForPage(x, school));
         }
         modelMap.put("percentComplete", percentCompleteMap);
+    }
+    
+    protected void putResponsesInModel(School school, int page, ModelMap modelMap) {
+        Map<String, EspResponseStruct> responseMap = new HashMap<String, EspResponseStruct>();
+        
+        // fetch all responses to allow page to use ajax page switching if desired.
+        List<EspResponse> responses = _espResponseDao.getResponses(school);
 
         for (EspResponse response: responses) {
             EspResponseStruct responseStruct = responseMap.get(response.getKey());
@@ -108,33 +115,11 @@ public class EspFormController implements ReadWriteAnnotationController {
 
         fetchExternalValues(responseMap, school); // fetch data that lives outside of esp_response
 
+        // READ HOOKS GO HERE
+        // This is for data that needs to be transformed from the database representation to a view-specific one
+//        handleEndTimeRead(responseMap);
+
         modelMap.put("responseMap", responseMap);
-    }
-
-    protected static class EspResponseStruct {
-        private String _value;
-        private Map<String, String> _valueMap = new HashMap<String, String>();
-
-        public String getValue() {
-            return _value;
-        }
-
-        public Map<String, String> getValueMap() {
-            return _valueMap;
-        }
-        
-        public void addValue(String value) {
-            _value = value;
-            _valueMap.put(value, "1");
-        }
-    }
-
-    protected void saveCensusData(School school, int data) {
-        CensusDataSet dataSet = _dataSetDao.findDataSet(school.getDatabaseState(), CensusDataType.STUDENTS_ENROLLMENT, 0, null, /* breakdown */null);
-        // TODO what if there is no existing data set?
-        if (dataSet != null) {
-            _dataSetDao.addValue(dataSet, school, data, "ESP");
-        }
     }
 
     @RequestMapping(value="form.page", method=RequestMethod.POST)
@@ -142,7 +127,7 @@ public class EspFormController implements ReadWriteAnnotationController {
                          @RequestParam(value=PARAM_SCHOOL_ID, required=false) Integer schoolId,
                          @RequestParam(value=PARAM_STATE, required=false) State state) throws IOException, JSONException {
         response.setContentType("application/json");
-        
+        // Fetch parameters
         User user = getValidUser(request, state, schoolId);
         if (user == null) {
             JSONObject errorObj = new JSONObject();
@@ -165,28 +150,47 @@ public class EspFormController implements ReadWriteAnnotationController {
 
         int page = getPage(request);
 
-        // Deactivate page
+        // The parameter FORM_VISIBLE_KEYS_PARAM is the list of response_keys that are valid for the given
+        // form submission (though they may not be included in the actual post). Parse these into a set that
+        // we can use to deactivate existing data and pull the new data out of the request.
+        // TODO: How will this include end_time?
         String[] visibleKeys = StringUtils.split(request.getParameter(FORM_VISIBLE_KEYS_PARAM), ",");
         Set<String> keysForPage = new HashSet<String>();
         keysForPage.addAll(Arrays.asList(visibleKeys));
-        _espResponseDao.deactivateResponsesByKeys(school, keysForPage);
 
-        Date now = new Date();
         // Save page
         List<EspResponse> responseList = new ArrayList<EspResponse>();
         Set<String> keysForExternalData = getKeysForExternalData(school);
-        // this way saves null for anything not provided
-        // and won't save any extra data that isn't in keysForPage
+
+        // TODO: This is immutable per java api spec, will need to copy to a mutable map to get save hooks working
+        Map<String, String[]> requestParameterMap = (Map<String, String[]>) request.getParameterMap();
+
+        // SAVE HOOKS GO HERE
+        // Any data that needs to be transformed from a view specific format into a database representation
+        // must be handled here. These methods should SET keys in requestParameterMap with the final
+        // values if necessary.
+        // e.g. form posts address_street, address_city, address_zip which get composed by a method here
+        // into a new param "address" with the concatenated values. The DB only knows about "address" while
+        // the page has the fields split out.
+//        handleEndTimeSave(requestParameterMap);
+
+        Date now = new Date(); // consistent time stamp for this save
+        // this won't save any extra data that isn't in keysForPage
         // I'm not yet sure that's a good thing
         for (String key: keysForPage) {
-            // Do not save null values -- these are keys that might be present on a page
-            // but aren't included in the request.
-            String[] responseValues = request.getParameterValues(key);
+            String[] responseValues;
             
+            responseValues = requestParameterMap.get(key);
+
+            // Do not save null values -- these are keys that might be present on a page
+            // but aren't included in the POST (because the controls were disabled, the check boxes were
+            // all deselected, etc.)
             if (responseValues == null || responseValues.length == 0) {
                 continue;
             }
             boolean active = true;
+            // values that live elsewhere get saved out here
+            // these values also go in esp_response but are disabled to clearly mark that they are not sourced from there
             if (ENABLE_EXTERNAL_DATA_SAVING && keysForExternalData.contains(key)) {
                 saveExternalValue(key, responseValues, school);
                 active = false; // data saved elsewhere should be inactive
@@ -203,14 +207,71 @@ public class EspFormController implements ReadWriteAnnotationController {
             }
         }
 
+        // Deactivate existing data first, then save
+        // TODO: This step will obviously need to be skipped on validation or other error above
+        _espResponseDao.deactivateResponsesByKeys(school, keysForPage);
         _espResponseDao.saveResponses(school, responseList);
+
         JSONObject successObj = new JSONObject();
         // let page know new completion percentage
+        // TODO: Will probably need to include global form percentage as well
         successObj.put("percentComplete", getPercentCompletionForPage(page, school));
         successObj.write(response.getWriter());
         response.getWriter().flush();
     }
 
+    /**
+     * Sample read hook. Modifies responseMap in place, adding any new key/value pairs that the view
+     * might need to display the data.
+     */
+    protected void handleEndTimeRead(Map<String, EspResponseStruct> responseMap) {
+        EspResponseStruct responseStruct = responseMap.get("end_time");
+        if (responseStruct != null) {
+            try {
+                SimpleDateFormat sdf = new SimpleDateFormat("militaryTimeFormatStr");
+                Date date = sdf.parse(responseStruct.getValue());
+                sdf.applyPattern("hh:mm:ss");
+                EspResponseStruct textStruct = new EspResponseStruct();
+                textStruct.addValue(sdf.format(date));
+                sdf.applyPattern("ZZ");
+                EspResponseStruct ampmStruct = new EspResponseStruct();
+                ampmStruct.addValue(sdf.format(date));
+                responseMap.put("end_time_text", textStruct);
+                responseMap.put("end_time_ampm", ampmStruct);
+            } catch (ParseException e) {
+                _log.error(e, e);
+                // TODO: ???
+            }
+        }
+    }
+
+    /**
+     * Sample write hook. Modifies requestParameterMap in place, adding any new key/value pairs that the
+     * DB will persist. If value is to be persisted, the key better be in keysOnPage!
+     */
+    protected void handleEndTimeSave(Map<String, String[]> requestParameterMap) {
+        String[] endTimeTextArr = requestParameterMap.get("end_time_text");
+        String[] endTimeAmPmArr = requestParameterMap.get("end_time_ampm");
+        if (endTimeTextArr != null && endTimeAmPmArr != null && endTimeTextArr.length == 1 && endTimeAmPmArr.length == 1) {
+            try {
+                SimpleDateFormat sdf = new SimpleDateFormat("hh:mm ZZ");
+                Date endTimeDate = sdf.parse(endTimeTextArr[0] + " " + endTimeAmPmArr[0]);
+                sdf.applyPattern("hhmm");
+                String endTime = sdf.format(endTimeDate);
+                // TODO: The request map is immutable
+                requestParameterMap.put("end_time", new String[] {endTime});
+            } catch (ParseException e) {
+                _log.error(e, e);
+                // TODO: ??
+            }
+        }
+    }
+
+    /**
+     * Checks if the user has access to the form for the school specified by the given state/schoolId.
+     * Returns false any parameter is null, or if the user does not have an active esp membership
+     * for the given state/schoolId and is not a superuser
+     */
     protected boolean checkUserHasAccess(User user, State state, Integer schoolId) {
         if (user != null && state != null && schoolId > 0) {
             if (user.hasRole(Role.ESP_MEMBER)) {
@@ -227,6 +288,10 @@ public class EspFormController implements ReadWriteAnnotationController {
         return false;
     }
 
+    /**
+     * Pulls the user out of the session context. Returns null if there is no user, or if the user fails
+     * checkUserAccess
+     */
     protected User getValidUser(HttpServletRequest request, State state, Integer schoolId) {
         SessionContext sessionContext = SessionContextUtil.getSessionContext(request);
         User user = null;
@@ -239,6 +304,10 @@ public class EspFormController implements ReadWriteAnnotationController {
         return null;
     }
 
+    /**
+     * Parses the state and schoolId out of the request and fetches the school. Returns null if
+     * it can't parse parameters, can't find school, or the school is inactive
+     */
     protected School getSchool(State state, Integer schoolId) {
         if (state == null || schoolId == null) {
             return null;
@@ -257,6 +326,9 @@ public class EspFormController implements ReadWriteAnnotationController {
         return school;
     }
 
+    /**
+     * Parses the page out of the request, returning -1 in error cases
+     */
     protected int getPage(HttpServletRequest request) {
         int page = -1;
         if (request.getParameter(PARAM_PAGE) != null) {
@@ -268,11 +340,17 @@ public class EspFormController implements ReadWriteAnnotationController {
         }
         return page;
     }
-    
+
+    /**
+     * How many pages are on the form for a given school
+     */
     protected int getMaxPageForSchool(School school) {
         return 2; // TODO: implement
     }
 
+    /**
+     * Percent completion for a page is currently {number of keys that have active values} / {total number of keys}
+     */
     protected int getPercentCompletionForPage(int page, School school) {
         Set<String> keys = getKeysForPercentCompletion(page);
 
@@ -284,9 +362,13 @@ public class EspFormController implements ReadWriteAnnotationController {
             }
         }
 
-        return Integer.valueOf("0");
+        return 0;
     }
 
+    /**
+     * Get the list of keys that count towards percent completion for a particular page.
+     * TODO: This signature might be insufficient. Probably the school needs to get passed in
+     */
     protected Set<String> getKeysForPercentCompletion(int page) {
         Set<String> keys = new HashSet<String>();
         if (page == 1) {
@@ -334,9 +416,19 @@ public class EspFormController implements ReadWriteAnnotationController {
         }
         if (StringUtils.equals("student_enrollment", key)) {
             _log.debug("Saving student_enrollment elsewhere: " + values[0]);
-            saveCensusData(school, Integer.parseInt(values[0]));
+            saveEnrollment(school, Integer.parseInt(values[0]));
         }
+    }
 
+    /**
+     * Save out enrollment data to the db
+     */
+    protected void saveEnrollment(School school, int data) {
+        CensusDataSet dataSet = _dataSetDao.findDataSet(school.getDatabaseState(), CensusDataType.STUDENTS_ENROLLMENT, 0, null, /* breakdown */null);
+        // TODO what if there is no existing data set?
+        if (dataSet != null) {
+            _dataSetDao.addValue(dataSet, school, data, "ESP");
+        }
     }
 
     /**
@@ -350,7 +442,8 @@ public class EspFormController implements ReadWriteAnnotationController {
     }
 
     /**
-     * Fetch external value, e.g. from census or school table.
+     * Fetch external value, e.g. from census or school table. This returns an array of strings
+     * representing the values for that key (e.g. {"K", "1", "2"} for grade_level or {"100"} for enrollment
      */
     protected String[] getExternalValuesForKey(String key, School school) {
         if (StringUtils.equals("student_enrollment", key) && school.getEnrollment() != null) {
@@ -358,5 +451,33 @@ public class EspFormController implements ReadWriteAnnotationController {
             return new String[] {String.valueOf(school.getEnrollment())};
         }
         return new String[0];
+    }
+
+    /**
+     * A simple data structure that presents data to the view in two ways. ${struct.value} fetches a single
+     * string value and should be used for keys with 1-to-1 mappings to values. ${struct.valueMap} returns a
+     * map of answer_key to (irrelevant) which should be used for 1-to-many mappings of key to value. It would
+     * be checked like ${not empty struct.valueMap['K']}.
+     *
+     * Behind the scenes, addValue both sets value and creates a key in valueMap. At this point in development
+     * there is no way of knowing whether any given key is 1-to-1 or 1-to-many, so this structure basically
+     * separates out needing to know that.
+     */
+    protected static class EspResponseStruct {
+        private String _value;
+        private Map<String, String> _valueMap = new HashMap<String, String>();
+
+        public String getValue() {
+            return _value;
+        }
+
+        public Map<String, String> getValueMap() {
+            return _valueMap;
+        }
+
+        public void addValue(String value) {
+            _value = value;
+            _valueMap.put(value, "1");
+        }
     }
 }
