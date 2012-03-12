4042028183b6f439a0ed35cbfbfb8fe77f96d823
rramachandran <rramachandran>
2012-03-12 23:44:07 +0000

    GS-12509 ESP Redesign: Moderation details page.  Added new page for moderation details, controller for displaying the form, and perform approve and save operations. This page can be reached from espModerationForm and espModerationSearch.

---
+++ src/java/gs/web/admin/EspModerationDetailsController.java
@@ -0,0 +1,212 @@
+package gs.web.admin;
+
+import gs.data.community.User;
+import gs.data.school.EspMembership;
+import gs.data.school.School;
+import gs.data.state.State;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.orm.ObjectRetrievalFailureException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rramachandran
+ * Date: 2/29/12
+ * Time: 4:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+@RequestMapping("/admin/espModerationDetails.page")
+public class EspModerationDetailsController extends AbstractEspModerationController {
+    
+    private static final String ESP_MODERATION_DETAILS_VIEW = "admin/espModerationDetails";
+    public static final String ESP_MODERATION_VIEW = "redirect:/admin/espModerationForm.page";
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String showForm(@ModelAttribute("espModerationDetailsCmd") EspModerationDetailsCommand command,
+                           BindingResult bindingResult,
+                           ModelMap modelMap,
+                           HttpServletRequest request,
+                           @RequestParam(value="id", required=false) String id) {
+                
+        int membershipId = getIntMembershipId(id);
+        EspMembership espMembership = getEspMembershipDao().findEspMembershipById(membershipId, false);
+
+        School school = null;
+        try {
+            school = getSchoolDao().getSchoolById(espMembership.getState(), espMembership.getSchoolId());
+        }
+        catch (Exception ex) {
+            /* any other exception - such as nullpointer when non existing id argument is passed */
+            return ESP_MODERATION_VIEW;
+        }
+
+        espMembership.setSchool(school);
+        command.setEspMembership(espMembership);
+
+        modelMap.put("espModerationDetailsCmd", command);
+
+        return getViewName();
+    }
+
+    /* perform any of these actions - approve, reject, deactivate */
+    @RequestMapping(method = RequestMethod.POST, params = "moderatorAction")
+    public String onModeratorAction(@ModelAttribute("espModerationDetailsCmd") EspModerationDetailsCommand command,
+                            BindingResult bindingResult,
+                            ModelMap modelMap,
+                            HttpServletRequest request,
+                            HttpServletResponse response,
+                            @RequestParam(value="moderatorAction", required=true) String moderatorAction,
+                            @RequestParam(value="id", required=false) String id) {
+        int membershipId = getIntMembershipId(id);
+
+        EspModerationCommand moderationCommand = new EspModerationCommand();
+        moderationCommand.setModeratorAction(moderatorAction);
+        List<Integer> espMembershipId = new ArrayList<Integer>();
+        espMembershipId.add(membershipId);
+        moderationCommand.setEspMembershipIds(espMembershipId);
+
+        Map<Integer, String> espMembershipNote = new HashMap<Integer, String>();
+        espMembershipNote.put(membershipId, command.getEspMembership().getNote());
+        moderationCommand.setNotes(espMembershipNote);
+
+        /* call base class method and update status and note entries for that membership in the db table */
+        updateEspMembership(moderationCommand, request, response);
+        return showForm(command, bindingResult, modelMap, request, id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, params = "save")
+    public String onSave(@ModelAttribute("espModerationDetailsCmd") EspModerationDetailsCommand command,
+                         BindingResult bindingResult,
+                         ModelMap modelMap,
+                         HttpServletRequest request,
+                         @RequestParam(value="id", required=false) String id){
+        int membershipId = getIntMembershipId(id);
+        String newStateAbbreviation = request.getParameter("stateName");
+        
+        EspMembership _espMembership = getEspMembershipDao().findEspMembershipById(membershipId, false);
+
+        String newEmail = command.getEspMembership().getUser().getEmail();
+        Integer newSchoolId = command.getEspMembership().getSchoolId();
+        String newJobTitle = command.getEspMembership().getJobTitle();
+        String newFirstName = command.getEspMembership().getUser().getFirstName();
+        String newLastName = command.getEspMembership().getUser().getLastName();
+        String newNote = command.getEspMembership().getNote();
+        State newState = null;
+        if(newStateAbbreviation != null) {
+            newState = State.fromString(newStateAbbreviation);
+            command.getEspMembership().setState(newState);
+        }
+        
+        validate(command, bindingResult, _espMembership, modelMap);
+
+        if(bindingResult.hasErrors()) {
+            return showForm(command, bindingResult, modelMap, request, id);
+        }
+
+        User espUser = _espMembership.getUser();
+
+        espUser.setEmail(newEmail);
+        espUser.setLastName(newLastName);
+        espUser.setFirstName(newFirstName);
+
+        if(StringUtils.isNotBlank(newJobTitle)) {
+            _espMembership.setJobTitle(newJobTitle);
+        }
+
+        if(modelMap.get("newSchool") != null) {
+            School newSchool = (School) modelMap.get("newSchool");
+            if(newSchool != null) {
+                _espMembership.setSchool(newSchool);
+                _espMembership.setSchoolId(newSchoolId);
+                _espMembership.setState(newState);
+            }
+        }
+
+        _espMembership.setNote(newNote);
+
+        Date updateDate = new Date();
+        espUser.setUpdated(updateDate);
+        getUserDao().updateUser(espUser);
+        _espMembership.setUpdated(updateDate);
+        getEspMembershipDao().updateEspMembership(_espMembership);
+
+        command.setEspMembership(_espMembership);
+        modelMap.put("espModerationDetailsCmd", command);
+
+        return getViewName();
+    }
+    
+    private int getIntMembershipId(String id){
+        int membershipId = 0;
+        if(id != null) {
+            try {
+                membershipId = Integer.parseInt(id);
+            }
+            catch (NumberFormatException ex) {
+                /* in case of an exception, the membershipId remains 0 */
+            }
+        }
+        return membershipId;
+    }
+
+    private void validate(EspModerationDetailsCommand command,
+                          BindingResult bindingResult,
+                          EspMembership espMembership,
+                          ModelMap modelMap) {
+        String newEmail = command.getEspMembership().getUser().getEmail();
+        if(StringUtils.isNotBlank(newEmail)) {
+            User userForNewEmail = getUserDao().findUserFromEmailIfExists(newEmail);
+            if(userForNewEmail != null && !userForNewEmail.equals(espMembership.getUser())) {
+                bindingResult.rejectValue("espMembership.user.email", "email_exists", "choose a different email address");
+                modelMap.put("emailError", true);
+            }
+        }
+        else {
+            bindingResult.rejectValue("espMembership.user.email", "invalid_email", "please enter an email address");
+            modelMap.put("emailError", true);
+        }
+
+        String newFirstName = command.getEspMembership().getUser().getFirstName();
+        if(StringUtils.isBlank(newFirstName) || newFirstName.length() < 2 || newFirstName.length() > 24) {
+            bindingResult.rejectValue("espMembership.user.firstName", "invalid_firstname", "please enter a first name");
+            modelMap.put("firstNameError", true);
+        }
+
+        String newLastName = command.getEspMembership().getUser().getLastName();
+        if(StringUtils.isBlank(newLastName) || newLastName.length() > 24) {
+            bindingResult.rejectValue("espMembership.user.lastName", "invalid_lastname", "please enter a last name");
+            modelMap.put("lastNameError", true);
+        }
+
+        State newState = command.getEspMembership().getState();
+        Integer newSchoolId = command.getEspMembership().getSchoolId();
+        if(newSchoolId != null && newState != null) {
+            try {
+                School school = getSchoolDao().getSchoolById(newState, newSchoolId);
+                modelMap.put("newSchool", school);
+            }
+            catch (ObjectRetrievalFailureException ex) {
+                bindingResult.rejectValue("espMembership.state", "invalid_schoolId_state", "Please enter a valid school id for the state");
+                bindingResult.rejectValue("espMembership.schoolId", "invalid_schoolId_state", "Please enter a valid school id for the state");
+                modelMap.put("schoolIdStateError", true);
+            }
+        }
+    }
+
+    @Override
+    protected  String getViewName() {
+        return ESP_MODERATION_DETAILS_VIEW;
+    }
+}
---
+++ src/test/gs/web/admin/EspModerationDetailsControllerTest.java
@@ -0,0 +1,227 @@
+package gs.web.admin;
+
+import gs.data.community.IUserDao;
+import gs.data.community.User;
+import gs.data.school.*;
+import gs.data.security.IRoleDao;
+import gs.data.security.Role;
+import gs.data.state.State;
+import gs.web.BaseControllerTestCase;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.easymock.EasyMock.*;
+import static org.easymock.classextension.EasyMock.createStrictMock;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rramachandran
+ * Date: 3/7/12
+ * Time: 1:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EspModerationDetailsControllerTest extends BaseControllerTestCase {
+    private EspModerationDetailsController _espModerationDetailsController;
+
+    private IEspMembershipDao _espMembershipDao;
+    private ISchoolDao _schoolDao;
+    private IUserDao _userDao;
+    private IRoleDao _roleDao;
+
+    private EspMembership _espMembership, _anotherEspMembership;
+    private School _school, _anotherSchool;
+    private User _user, _anotherUser;
+    private Role _role;
+    private BindingResult _bindingResult;
+    private EspModerationDetailsCommand _command;
+    private ModelMap _modelMap;
+    private String _view;
+
+    private static final String ESP_MODERATION_DETAILS_VIEW = "admin/espModerationDetails";
+    public static final String ESP_MODERATION_VIEW = "redirect:/admin/espModerationForm.page";
+
+    public void setUp() throws Exception {
+        super.setUp();
+        _espModerationDetailsController = new EspModerationDetailsController();
+
+        _espMembershipDao = createStrictMock(IEspMembershipDao.class);
+        _schoolDao = createStrictMock(ISchoolDao.class);
+        _userDao = createStrictMock(IUserDao.class);
+        _roleDao = createStrictMock(IRoleDao.class);
+
+        _espModerationDetailsController.setEspMembershipDao(_espMembershipDao);
+        _espModerationDetailsController.setSchoolDao(_schoolDao);
+        _espModerationDetailsController.setUserDao(_userDao);
+        _espModerationDetailsController.setRoleDao(_roleDao);
+        
+        _role = new Role();
+        _role.setId(1);
+        _role.setKey(Role.ESP_MEMBER);
+        
+        _user =  createUser(456, "ABC", "ZXC", "me@xyz.com", _role);
+        _espMembership = createEspMembership(12345, 987, State.CA, _user, "President");
+        _school = createSchool(987, _espMembership.getState(), "Qwerty Elementary School");
+
+        _anotherUser =  createUser(10, "G", "", "notme@zxc.com", _role);
+        _anotherEspMembership = createEspMembership(98765, 789, State.CA, _anotherUser, "Principal");
+        _anotherSchool = createSchool(789, _anotherEspMembership.getState(), "ASDFGH Elementary School");
+
+        _command = new EspModerationDetailsCommand();
+
+        _bindingResult = new BeanPropertyBindingResult(_command, "espModerationDetailsCmd");
+
+        _modelMap = new ModelMap();
+
+        getRequest().setParameter("id", _espMembership.getId().toString());
+    }
+
+    public void replayAll() {
+        super.replayMocks(_espMembershipDao, _schoolDao, _userDao);
+    }
+
+    public void verifyAll() {
+        super.verifyMocks(_espMembershipDao, _schoolDao, _userDao);
+    }
+
+    public void resetAll() {
+        super.resetMocks(_espMembershipDao, _schoolDao, _userDao);
+    }
+
+    public void testEspModDetailsShowFormInvalidMember() {
+        resetAll();
+        expect(_espMembershipDao.findEspMembershipById(54321, false)).andReturn(null);
+
+        replayAll();
+        _view = _espModerationDetailsController.showForm(_command, _bindingResult, _modelMap, getRequest(), "54321");
+        verifyAll();
+
+        assertEquals(ESP_MODERATION_VIEW, _view);
+    }
+
+    public void testEspModDetailsShowFormValidMember() {
+        Integer id = Integer.parseInt(getRequest().getParameter("id"));
+
+        resetAll();
+
+        expect(_espMembershipDao.findEspMembershipById(id, false)).andReturn(_espMembership);
+        expect(_schoolDao.getSchoolById(_espMembership.getState(), _espMembership.getSchoolId())).andReturn(_school);
+
+        replayAll();
+        _view = _espModerationDetailsController.showForm(_command, _bindingResult, _modelMap, getRequest(),
+                getRequest().getParameter("id"));
+        verifyAll();
+
+        assertEquals(_command.getEspMembership(), _espMembership);
+        assertEquals(_modelMap.get("espModerationDetailsCmd"), _command);
+        assertEquals(ESP_MODERATION_DETAILS_VIEW, _view);
+    }
+    
+    public void testEspModeratorActionApprove() {
+        Integer id = Integer.parseInt(getRequest().getParameter("id"));
+        _espMembership.setStatus(EspMembershipStatus.PROCESSING);
+        _command.setEspMembership(_espMembership);
+        List<EspMembership> espMemberships = new ArrayList<EspMembership>();
+        espMemberships.add(_espMembership);
+
+        resetAll();
+
+        expect(_espMembershipDao.findEspMembershipById(id, false)).andReturn(_espMembership);
+        _espMembershipDao.updateEspMembership(_espMembership);
+        expectLastCall();
+        expect(_espMembershipDao.findEspMembershipsByUserId(new Integer(_user.getId()), true)).andReturn(espMemberships);
+        expect(_espMembershipDao.findEspMembershipById(id, false)).andReturn(_espMembership);
+        expect(_schoolDao.getSchoolById(_espMembership.getState(), _espMembership.getSchoolId())).andReturn(_school);
+
+        replayAll();
+        _view = _espModerationDetailsController.onModeratorAction(_command, _bindingResult, _modelMap, getRequest(),
+                getResponse(), "approve", getRequest().getParameter("id"));
+        verifyAll();
+
+        assertEquals(ESP_MODERATION_DETAILS_VIEW, _view);
+    }
+
+    public void testOnSaveWithErrors() {
+        Integer id = Integer.parseInt(getRequest().getParameter("id"));
+        getRequest().setParameter("stateName", "CA");
+        _command.setEspMembership(_anotherEspMembership);
+
+        resetAll();
+
+        expect(_espMembershipDao.findEspMembershipById(id, false)).andReturn(_espMembership).times(2);
+        expect(_userDao.findUserFromEmailIfExists(_anotherUser.getEmail())).andReturn(_anotherUser);
+        expect(_schoolDao.getSchoolById(_anotherEspMembership.getState(), _anotherEspMembership.getSchoolId())).andReturn(_anotherSchool);
+        expect(_schoolDao.getSchoolById(_espMembership.getState(), _espMembership.getSchoolId())).andReturn(_school);
+
+        replayAll();
+        _view = _espModerationDetailsController.onSave(_command, _bindingResult, _modelMap, getRequest(),
+                getRequest().getParameter("id"));
+        verifyAll();
+
+        assertEquals(_bindingResult.hasErrors(), true);
+        assertEquals(_modelMap.get("emailError"), true);
+        assertEquals(_modelMap.get("firstNameError"), true);
+        assertEquals(_modelMap.get("lastNameError"), true);
+        assertNotSame(_user.getUpdated(), _espMembership.getUpdated());
+        assertEquals(ESP_MODERATION_DETAILS_VIEW, _view);
+    }
+
+    public void testOnSaveWithoutErrors() {
+        Integer id = Integer.parseInt(getRequest().getParameter("id"));
+        getRequest().setParameter("stateName", "DC");
+        _espMembership = createEspMembership(12345, 456, State.DC, _user, "Teacher");
+        _school = createSchool(456, State.DC, "ZXCVB Elementary School");
+        _command.setEspMembership(_espMembership);
+
+        resetAll();
+
+        expect(_espMembershipDao.findEspMembershipById(id, false)).andReturn(_espMembership);
+        expect(_userDao.findUserFromEmailIfExists(_user.getEmail())).andReturn(_user);
+        expect(_schoolDao.getSchoolById(_espMembership.getState(), _espMembership.getSchoolId())).andReturn(_school).atLeastOnce();
+        _userDao.updateUser(_user);
+        expectLastCall();
+        _espMembershipDao.updateEspMembership(_espMembership);
+        expectLastCall();
+
+        replayAll();
+        _view = _espModerationDetailsController.onSave(_command, _bindingResult, _modelMap, getRequest(),
+                getRequest().getParameter("id"));
+        verifyAll();
+
+        assertEquals(_bindingResult.hasErrors(), false);
+        assertEquals(_modelMap.get("newSchool"), _espMembership.getSchool());
+        assertEquals(_user.getUpdated(), _espMembership.getUpdated());
+        assertEquals(ESP_MODERATION_DETAILS_VIEW, _view);
+    }
+    
+    private EspMembership createEspMembership(int id, int schoolId, State state, User user, String jobTitle) {
+        EspMembership espMembership = new EspMembership();
+        espMembership.setId(id);
+        espMembership.setSchoolId(schoolId);
+        espMembership.setState(state);
+        espMembership.setUser(user);
+        espMembership.setJobTitle(jobTitle);
+        return espMembership;
+    }
+
+    private User createUser(int id, String firstName, String lastName, String email, Role role) {
+        User user = new User();
+        user.setId(id);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.addRole(role);
+        return user;
+    }
+    
+    private School createSchool(int id, State state, String name) {
+        School school = new School();
+        school.setId(id);
+        school.setStateAbbreviation(state);
+        school.setName(name);
+        return school;
+    }
+}
