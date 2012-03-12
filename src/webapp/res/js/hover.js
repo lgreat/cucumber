b8263c8dc986f0c5799197fdc263fe3d0c0de35a
ssprouse <ssprouse>
2012-03-12 22:15:15 +0000

    GS-12548 Moved a word out of hyperlink

--- src/webapp/res/js/hover.js
+++ src/webapp/res/js/hover.js
@@ -1110,7 +1110,7 @@ GS.forgotPasswordHover_checkValidationResponse = function(data) {
     if (isOspSignIn && data !== undefined && data.errorCode !== undefined) {
         var espRegistrationUrl = data.ESP_REGISTRATION_URL;
         if (data.errorCode === 'NO_SUCH_ACCOUNT') {
-            GSType.hover.forgotPassword.addMessage('There is no school official\'s account associated with this email address. <a href="' + espRegistrationUrl + '">Please register to gain access</a>.');
+            GSType.hover.forgotPassword.addMessage('There is no school official\'s account associated with this email address. Please <a href="' + espRegistrationUrl + '">register to gain access</a>.');
         } else if (data.errorCode === 'EMAIL_ONLY_ACCOUNT') {
             GSType.hover.forgotPassword.addMessage('You have an email address on file, but still need to create a <a href="' + espRegistrationUrl + '">school official\'s profile</a>.');
         }
