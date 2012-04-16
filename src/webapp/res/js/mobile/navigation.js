a203614f149c074f6122014bd42f09217b2881b9
aroy <aroy>
2012-04-16 23:55:03 +0000

    GS-12633 Add some basic styling to restore global nav usability

--- src/webapp/res/js/mobile/navigation.js
+++ src/webapp/res/js/mobile/navigation.js
@@ -1,12 +1,16 @@
 //when 'navigation' module is "require()d", execution will begin as soon as 'jquery' is available
 define(function() {
-    var $container = $('.js-mobile-header');
-    var $goToButton = $('.top-nav-goto');
+    var $container = null;
+
+    $(function() {
+        $container = $('.js-mobile-header');
+    });
 
     var setupEventHandlers = function() {
         $(function() {
-            $goToButton.on('click', ':visible', function() {
+            $('.top-nav-goto').on('click', ':visible', function() {
                 $('.mobilePage:visible .mobile-sub-nav').toggle();
+                return false;
             });
         })
     };
