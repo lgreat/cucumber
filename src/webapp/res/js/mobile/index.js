a2ebdce24be0f6aa2e9a8e65e7639158cbd78bbb
mseltzer <mseltzer>
2012-05-11 21:40:46 +0000

    GS-12656 adding padding to the right on the textbox, adding in an index file to try and stop zoom on select.

--- src/webapp/res/js/mobile/index.js
+++ src/webapp/res/js/mobile/index.js
@@ -118,6 +118,11 @@ define(['uri', 'geocoder', 'validation', 'geolocation', 'jquery.autocomplete'], 
                     });
                 });
             });
+            var $viewportMeta = $('meta[name="viewport"]');
+            $('input, select, textarea').bind('focus blur', function(event) {
+                $viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
+            });
+
 
             jQuery('#js-gradeLevels input').click(function () {
                 var cssId = jQuery(this).attr('id');
