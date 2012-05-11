945b06e273cc1c9e31d6e4f6ff2399664589d2a7
mseltzer <mseltzer>
2012-05-11 22:06:10 +0000

    GS-12810 added some code to prevent zooming on select list

--- src/webapp/res/js/mobile/mobile.js
+++ src/webapp/res/js/mobile/mobile.js
@@ -14,6 +14,11 @@ define(['tracking','navigation'],function(tracking,navigation) {
                 tracking.successEvents = 'event71';
                 tracking.send();
             });
+            /*  this is to handle the zoom event on iPhone - snaps back to correct size from zoom state - not perfect but better */
+            var $viewportMeta = $('meta[name="viewport"]');
+            $('input, select, textarea').bind('focus blur', function(event) {
+                $viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
+            });
         });
 
         navigation.init();
