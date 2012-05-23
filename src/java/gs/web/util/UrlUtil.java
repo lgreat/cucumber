f7088126f3a980bd8c4af8ebfddbed8579c85dda
mseltzer <mseltzer>
2012-05-23 20:26:04 +0000

    GS-12676 adding mitchtest filter to allow dev configuration on local machine.

--- src/java/gs/web/util/UrlUtil.java
+++ src/java/gs/web/util/UrlUtil.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2005 GreatSchools.org. All Rights Reserved.
- * $Id: UrlUtil.java,v 1.124 2012/05/12 01:54:45 ssprouse Exp $
+ * $Id: UrlUtil.java,v 1.125 2012/05/23 20:26:04 mseltzer Exp $
  */
 
 package gs.web.util;
@@ -490,7 +490,8 @@ public final class UrlUtil {
         "172.21.1.142",
         "172.18.",
         "m.",
-        "samson."
+        "samson.",
+        "mitchtest."
     };
 
     /**
