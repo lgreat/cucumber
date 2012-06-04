70cbb123c510054c507288ef03a53921e1fa4a6d
aroy <aroy>
2012-06-04 20:08:08 +0000

    GS-12899 Pre-configure require to avoid problems with sync vs. async script execution order

--- src/webapp/res/js/mobile/main.js
+++ src/webapp/res/js/mobile/main.js
@@ -1,9 +1,5 @@
 var GS = GS || {};
 
-if (GS.requireConfig) {
-    require.config(GS.requireConfig);
-}
-
 require(['mobile'], function(mobile) {
     GS.log('main mobile module and its dependencies loaded.');
     mobile.init();
