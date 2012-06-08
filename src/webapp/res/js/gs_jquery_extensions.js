1250c345f396f785cd00f7f75c52d177994a8d36
mseltzer <mseltzer>
2012-06-08 18:45:51 +0000

    GS-12676 - added tabs

--- src/webapp/res/js/gs_jquery_extensions.js
+++ src/webapp/res/js/gs_jquery_extensions.js
@@ -491,3 +491,35 @@ function debug(what) {
 
 // end of closure
 })(jQuery);
+
+/* =7 tabs (create tabs using the styles in tabs.css)
+ -------------------------------------------------------------------------------------------*/
+
+(function($) {
+    // extend the core jQuery with gsTabs
+    $.fn.extend({
+        gsTabs : function() {
+            return this.each(function() {
+                var tab = $(this);
+                tab.children('div').hide(); // Hide all content divs
+                var tabNav = tab.find('ul:first'); // get only the first ul not all of the descendents
+                tab.children('div:first').show(); // Show the first div
+                tabNav.find('li:first a').addClass('selected'); // Set the class of the first link to active
+                tabNav.find('li').each(function(){
+                    $(this).find('a').click(function(){ //When any link is clicked
+                        tab.children('div').hide(); // hide all layers
+                        var tabNum = tabNav.find('li').index($(this).parent());// find reference to the content
+                        tab.children('div').eq(tabNum).show();// show the content
+                        tabNav.find('li a').removeClass('selected');// turn all of them off
+                        $(this).addClass('selected');// turn selected on
+                        return false;
+                    });
+                });
+            });
+        }
+    });
+})(jQuery);
+
+jQuery(document).ready(function() {
+    jQuery('.gsTabs').gsTabs();
+});
