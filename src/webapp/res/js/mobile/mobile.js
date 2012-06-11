6ca091563186df330f4b66c6e3a0273f8831dd42
mseltzer <mseltzer>
2012-06-11 22:52:50 +0000

    GS-12914 - added x to clear text field, also changed event bind back as it was failing.

--- src/webapp/res/js/mobile/mobile.js
+++ src/webapp/res/js/mobile/mobile.js
@@ -1,18 +1,23 @@
 define(['tracking','navigation','orient'],function(tracking,navigation,orient) {
-    var handleZoomIOS = function(){
-        if( !( /iPhone|iPad|iPod/.test( navigator.platform ) && navigator.userAgent.indexOf( "AppleWebKit" ) > -1 ) ){
-            return;
-        }
-        var $viewportMeta = $('meta[name="viewport"]');
-        $('body').on('focus blur', 'input, select, textarea', function(event) {
-//            $('input, select, textarea').bind('focus blur', function(event) {
+    var handleFormField = function(){
+        //$('body').on('focus blur', 'input, select, textarea', function(event) {        // doesn't work
+        $('input, select, textarea').bind('focus blur', function(event) {
+
+            if (event.type == 'focus') {
+               // $('.clear_x').css('display', 'none');
+                $(this).siblings().css('display', 'block');
+            }
+            /*  this is to handle the zoom event on iPhone - snaps back to correct size from zoom state - not perfect but better */
+            if ( !( /iPhone|iPad|iPod/.test( navigator.platform ) && navigator.userAgent.indexOf( "AppleWebKit" ) > -1 ) ){
+                return;
+            }
+            var $viewportMeta = $('meta[name="viewport"]');
             $viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
         });
     }
+
     var init = function() {
         $(document).ready(function (){
-
-
             $('#mobile-footer-view-full-site').click( function(){
                 tracking.clear();
                 tracking.successEvents = 'event70';
@@ -39,8 +44,13 @@ define(['tracking','navigation','orient'],function(tracking,navigation,orient) {
                 tracking.successEvents = 'event71';
                 tracking.send();
             });
-            /*  this is to handle the zoom event on iPhone - snaps back to correct size from zoom state - not perfect but better */
-            handleZoomIOS();
+
+            handleFormField();
+
+            $('.clear_x').click( function(){
+                $(this).siblings().val('');
+                $(this).siblings().trigger('focus');
+            });
 
             $('.js-email-school-link').on('click', function() {
                 tracking.clear();
