ce9024061db2c72b987d37a5d11a4d5d0edab482
mseltzer <mseltzer>
2012-06-11 16:46:53 +0000

    GS-12921 added some code to handle double click on android

--- src/webapp/res/js/mobile/mobile.js
+++ src/webapp/res/js/mobile/mobile.js
@@ -1,5 +1,14 @@
 define(['tracking','navigation','orient'],function(tracking,navigation,orient) {
-
+    var handleZoomIOS = function(){
+        if( !( /iPhone|iPad|iPod/.test( navigator.platform ) && navigator.userAgent.indexOf( "AppleWebKit" ) > -1 ) ){
+            return;
+        }
+        var $viewportMeta = $('meta[name="viewport"]');
+        $('body').on('focus blur', 'input, select, textarea', function(event) {
+//            $('input, select, textarea').bind('focus blur', function(event) {
+            $viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
+        });
+    }
     var init = function() {
         $(document).ready(function (){
 
@@ -31,21 +40,13 @@ define(['tracking','navigation','orient'],function(tracking,navigation,orient) {
                 tracking.send();
             });
             /*  this is to handle the zoom event on iPhone - snaps back to correct size from zoom state - not perfect but better */
-            var $viewportMeta = $('meta[name="viewport"]');
-            $('input, select, textarea').bind('focus blur', function(event) {
-                $viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
-            });
+            handleZoomIOS();
 
             $('.js-email-school-link').on('click', function() {
                 tracking.clear();
                 tracking.successEvents = 'event67';
                 tracking.send();
             });
-//            $(window).bind('orientationchange', function(event) {
-//                //alert("event");
-//                //$viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=1');
-//                //$viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=10');
-//            });
         });
 
         navigation.init();
