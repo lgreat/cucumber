72ec6a07c8e10d338242aaf9bd284b931fdb2b7e
ssprouse <ssprouse>
2012-06-12 22:16:56 +0000

    GS-12900 Added barebones profile.js

---
+++ src/webapp/res/js/school/profile.js
@@ -0,0 +1,20 @@
+GS = GS || {};
+GS.school = GS.school || {};
+GS.school.profile = GS.school.profile || (function() {
+
+    var init = function() {
+        registerEventHandlers();
+
+    };
+
+    var registerEventHandlers = function() {
+
+    };
+
+
+    return {
+        init:init
+
+    }
+
+})();
