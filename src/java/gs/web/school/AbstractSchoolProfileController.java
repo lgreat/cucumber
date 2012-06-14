361980f640accb1c74cb0437b3d9b48f197d8ac9
aroy <aroy>
2012-06-14 18:51:19 +0000

    GS-12900 Refactoring

--- src/java/gs/web/school/AbstractSchoolProfileController.java
+++ src/java/gs/web/school/AbstractSchoolProfileController.java
@@ -1,25 +1,30 @@
 package gs.web.school;
 
-
+import gs.data.school.ISchoolDao;
 import gs.data.school.School;
-import gs.data.school.SchoolDaoHibernate;
 import gs.data.state.State;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
 public abstract class AbstractSchoolProfileController {
+    protected final Log _log = LogFactory.getLog(getClass().getName());
 
     @Autowired
-    private SchoolDaoHibernate _schoolDaoHibernate;
+    private ISchoolDao _schoolDao;
 
     public School getSchool(HttpServletRequest request, State state, Integer schoolId) {
 
         School school = (School) request.getAttribute("school");
 
         if (school == null) {
-            school = _schoolDaoHibernate.getSchoolById(state, schoolId);
+            try {
+                school = _schoolDao.getSchoolById(state, schoolId);
+            } catch (Exception e) {
+                _log.error("Cannot find school from request: state=" + state + ";schoolId=" + schoolId);
+            }
         }
 
         return school;
