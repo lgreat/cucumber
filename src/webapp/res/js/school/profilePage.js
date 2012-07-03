ab5c4701cad3e8916811e5d04a725b9a3ec57d72
mseltzer <mseltzer>
2012-07-03 23:41:20 +0000

    GS-12900 - tab arrows eliminated on ie7 and below.  Also reworked some tab url references

--- src/webapp/res/js/school/profilePage.js
+++ src/webapp/res/js/school/profilePage.js
@@ -1,48 +1,53 @@
-GS = GS || {};
-GS.school = GS.school || {};
-GS.school.profile = GS.school.profile || (function() {
-
-    var $tabGroup = null;
-    var $tabBodyGroup = null;
-    var $allTabBodies = null;
-
-    var init = function(tabToSelect) {
-        $tabGroup = $('[data-gs-tab-group=profileTabs]');
-        $tabBodyGroup = $('[data-gs-tab-body-group=profileTabs]');
-        $allTabBodies = $tabBodyGroup.find('[data-gs-tab-body]').hide();
-
-        if (!tabToSelect || tabToSelect == '' || $tabGroup.find('[data-gs-tab=' + tabToSelect + ']').length == 0) {
-            tabToSelect = 'overview';
-        }
-
-        registerEventHandlers();
-        setupTabs(tabToSelect);
-    };
-
-    var registerEventHandlers = function() {
-
-    };
-
-    var setupTabs = function(tabToSelect) {
-        $tabGroup.on('click', '[data-gs-tab]', function() {
-            var $this = $(this);
-            var tab = $this.data('gs-tab');
-
-            var $tabBody = $tabBodyGroup.find('[data-gs-tab-body=' + tab + ']');
-            $allTabBodies.hide();
-            $tabBody.show();
-            $tabGroup.find('li').removeClass('selected');
-            $this.addClass('selected');
-        });
-        // select default tab. This may change depending on URL parameter (or possibly model variable?)
-        $('[data-gs-tab=' + tabToSelect + ']').addClass('selected');
-        $tabBodyGroup.find('[data-gs-tab-body=' + tabToSelect + ']').show();
-    };
-
-
-    return {
-        init:init
-
-    }
-
-})();
+//GS = GS || {};
+//GS.school = GS.school || {};
+//GS.school.profile = GS.school.profile || (function() {
+//
+//    var $tabGroup = null;
+//    var $tabBodyGroup = null;
+//    var $allTabBodies = null;
+//
+//    var init = function(tabToSelect) {
+//        $tabGroup = $('[data-gs-tab-group=profileTabs]');
+//        $tabBodyGroup = $('[data-gs-tab-body-group=profileTabs]');
+//        $allTabBodies = $tabBodyGroup.find('[data-gs-tab-body]').hide();
+//
+//        if (!tabToSelect || tabToSelect == '' || $tabGroup.find('[data-gs-tab=' + tabToSelect + ']').length == 0) {
+//            tabToSelect = 'overview';
+//        }
+//
+//        registerEventHandlers();
+//        setupTabs(tabToSelect);
+//    };
+//
+//    var registerEventHandlers = function() {
+//
+//    };
+//
+//    var setupTabs = function(tabToSelect) {
+//        $tabGroup.on('click', '[data-gs-tab]', function() {
+//            var $this = $(this);
+//            var tab = $this.data('gs-tab');
+//
+//            var $tabBody = $tabBodyGroup.find('[data-gs-tab-body=' + tab + ']');
+//            $allTabBodies.hide();
+//            $tabBody.show();
+//            $tabGroup.find('li').removeClass('selected');
+//            $this.addClass('selected');
+//        });
+//        // select default tab. This may change depending on URL parameter (or possibly model variable?)
+//        $('[data-gs-tab=' + tabToSelect + ']').addClass('selected');
+//        $tabBodyGroup.find('[data-gs-tab-body=' + tabToSelect + ']').show();
+//    };
+//
+//
+//    return {
+//        init:init
+//
+//    }
+//
+//})();
+//<a href="/school/profile.page?tab=programs_extracurriculars&amp;state=ca&amp;id=1" onClick="linkToTabs('extracurriculars');">Link to Programs</a>
+var linkToTabs = function(destination){
+   $("#js_"+destination).triggerHandler('click').stopPropagation();
+   return false;
+}
