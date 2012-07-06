b0298ced0a457237e977c08365a3ac6b16343ebf
ssprouse <ssprouse>
2012-07-06 22:49:04 +0000

    GS-12844 Use RequestAttributeHelper from AbstractSchoolProfileController

--- src/java/gs/web/school/AbstractSchoolProfileController.java
+++ src/java/gs/web/school/AbstractSchoolProfileController.java
@@ -3,6 +3,7 @@ package gs.web.school;
 import gs.data.school.ISchoolDao;
 import gs.data.school.School;
 import gs.data.state.State;
+import gs.web.request.RequestAttributeHelper;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,14 @@ public abstract class AbstractSchoolProfileController {
 
     @Autowired
     private ISchoolDao _schoolDao;
+    @Autowired
+    private RequestAttributeHelper _requestAttributeHelper;
 
     public School getSchool(HttpServletRequest request, State state, Integer schoolId) {
 
-        School school = (School) request.getAttribute("school");
+        School school = _requestAttributeHelper.getSchool(request);
 
+        // allow passing in state and schoolId so that modules can be used standalone
         if (school == null) {
             try {
                 school = _schoolDao.getSchoolById(state, schoolId);
