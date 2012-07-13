7a0a23c86c70a183555b87e5242724c98e7174a4
rramachandran <rramachandran>
2012-07-13 18:27:49 +0000

    GS-12961 Customize the "No Results" message. Display no results message for a new name search in the new template with the search box and the tabs.

--- src/java/gs/web/search/SchoolSearchController2012.java
+++ src/java/gs/web/search/SchoolSearchController2012.java
@@ -107,6 +107,7 @@ public class SchoolSearchController2012  extends AbstractCommandController imple
     public static final String MODEL_CITY_BROWSE = "isCityBrowse";
     public static final String MODEL_DISTRICT_BROWSE = "isDistrictBrowse";
     public static final String MODEL_IS_NEARBY_SEARCH = "isNearbySearch";
+    public static final String MODEL_IS_AJAX_REQUEST = "isAjaxRequest";
 
     public static final int MAX_PAGE_SIZE = 100;
 
@@ -189,6 +190,7 @@ public class SchoolSearchController2012  extends AbstractCommandController imple
         model.put("commandAndFields", commandAndFields);
         model.put(MODEL_STATE, commandAndFields.getState());
         model.put(MODEL_IS_NEARBY_SEARCH, schoolSearchCommand.isNearbySearch());
+        model.put(MODEL_IS_AJAX_REQUEST, schoolSearchCommand.isAjaxRequest());
 
         ModelAndView modelAndView;
         if (commandAndFields.isCityBrowse()) {
