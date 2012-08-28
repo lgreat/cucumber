c0ee1bf3899653bbd328bf80d324116c2c963dbd
rcox <rcox>
2012-08-28 21:45:13 +0000

    GS-12900 Profile Redesign Walton: Master profile redesign - deploy "More" animation on all rows where required.

--- src/webapp/res/js/gs_jquery_extensions.js
+++ src/webapp/res/js/gs_jquery_extensions.js
@@ -287,6 +287,12 @@ jQuery(document).ready(function() {
     jQuery('#js-tile-9-0').popup({distance:0,time:200,hideDelay:50,appear:'right',leftPosition:150,topPosition:5});
     jQuery('#js-tile-9-1-0').popup({distance:0,time:200,hideDelay:50,appear:'right',leftPosition:150,topPosition:5});
     jQuery('#js-tile-9-1-1').popup({distance:0,time:200,hideDelay:50,appear:'right',leftPosition:150,topPosition:5});
+    jQuery('#js-tile-10-0').popup({distance:0,time:200,hideDelay:50,appear:'right',leftPosition:150,topPosition:5});
+    jQuery('#js-tile-10-1').popup({distance:0,time:200,hideDelay:50,appear:'right',leftPosition:150,topPosition:5});
+    jQuery('#js-tile-10-2').popup({distance:0,time:200,hideDelay:50,appear:'right',leftPosition:150,topPosition:5});
+    jQuery('#js-tile-11-0').popup({distance:0,time:200,hideDelay:50,appear:'right',leftPosition:285,topPosition:5});
+    jQuery('#js-tile-11-1').popup({distance:0,time:200,hideDelay:50,appear:'right',leftPosition:150,topPosition:5});
+    jQuery('#js-tile-13-1').popup({distance:0,time:200,hideDelay:50,appear:'right',leftPosition:150,topPosition:5});
 });
 
 /* =3 alternateRowColors (table stripping plugin)
