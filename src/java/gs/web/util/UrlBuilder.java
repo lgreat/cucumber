f9d8322a8b49b2c5c392dc7b8d8dab9e2fc3a5d1
yfan <yfan>
2012-10-11 15:12:14 +0000

    GS-13231 SEO:  Update "City Browse" for SEO purposes - work in progress - got rid of magic number for number of school types

--- src/java/gs/web/util/UrlBuilder.java
+++ src/java/gs/web/util/UrlBuilder.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2005-2006 GreatSchools.org. All Rights Reserved.
- * $Id: UrlBuilder.java,v 1.292 2012/10/10 23:02:52 yfan Exp $
+ * $Id: UrlBuilder.java,v 1.293 2012/10/11 15:12:14 yfan Exp $
  */
 
 package gs.web.util;
@@ -1028,7 +1028,7 @@ public class UrlBuilder {
 
         if (SCHOOLS_IN_CITY.equals(page)) {
             _path = DirectoryStructureUrlFactory.createNewCityBrowseURI(state, cityName, new HashSet<SchoolType>(), null);
-            if (schoolTypes != null && schoolTypes.size() < 3) {
+            if (schoolTypes != null && schoolTypes.size() < SchoolType.NUM_SCHOOL_TYPES) {
                 SortedSet<SchoolType> sortedSchoolTypes = new TreeSet<SchoolType>();
                 sortedSchoolTypes.addAll(schoolTypes);
                 for (SchoolType schoolType : sortedSchoolTypes) {
