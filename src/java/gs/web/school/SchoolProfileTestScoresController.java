d908fa9ebf41bb7be68e03c1c36415467fbe5885
Young Fan <yfan@greatschools.org>
2013-01-18 17:03:02 -0800

    GS-13621 Profile Redesign National: NYC Progress Report - first pass implementing .tagx file

--- src/java/gs/web/school/SchoolProfileTestScoresController.java
+++ src/java/gs/web/school/SchoolProfileTestScoresController.java
@@ -141,11 +141,14 @@ public class SchoolProfileTestScoresController extends AbstractSchoolProfileCont
 
             //If the grade=all then display only the level codes that the school has.
             //Note:This may or may not work with extra data.Re-visit this when working on GS-12963
-            if (Grade.ALL.equals(grade)) {
-                Set<LevelCode.Level> levelCodes = levelCode.getIndividualLevelCodes();
-                for (LevelCode.Level level : levelCodes) {
-                    if (!school.getLevelCode().containsLevelCode(level)) {
-                        levelCode.remove(level);
+            // Do not do this for NYC Progress Report so as not to incorrectly combine data that doesn't belong together
+            if (testDataType.getDisplayType() != TestDataTypeDisplayType.nyc_progress_report_grade) {
+                if (Grade.ALL.equals(grade)) {
+                    Set<LevelCode.Level> levelCodes = levelCode.getIndividualLevelCodes();
+                    for (LevelCode.Level level : levelCodes) {
+                        if (!school.getLevelCode().containsLevelCode(level)) {
+                            levelCode.remove(level);
+                        }
                     }
                 }
             }
