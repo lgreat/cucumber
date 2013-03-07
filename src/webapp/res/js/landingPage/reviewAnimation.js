eaf9225fc65b66b390ee26430ffe791f72da314f
Mitchell Seltzer <mseltzer@greatschools.org>
2013-03-07 11:05:18 -0800

    GS-13763 - made some progress on page two form

--- src/webapp/res/js/landingPage/reviewAnimation.js
+++ src/webapp/res/js/landingPage/reviewAnimation.js
@@ -18,4 +18,66 @@ $(document).ready(function() {
         });
 
     });
+    starRatingInterface("starRatingContainerReview", 16, 5, "overallAsString", "");
 });
+
+/********************************************************************************************************
+ *
+ *  currently created to work with the review page form!!!!
+ *
+ * @param containerS            the id of the layer that contains the star rating.
+ * @param iconW                 icon width needs to have a css component that it is compatible with.
+ * @param starsT                total stars currently set to 5 -- also needs to be changed as default value in the jspx or tagx
+ * @param overallSR             sets the hidden value of a form field
+ * @param divWriteTextValues    show the text value in this div -- the display values are defined in arrStarValuesText
+ */
+
+function starRatingInterface(containerS, iconW, starsT, overallSR, divWriteTextValues){
+    /* star rating */
+    var iconWidth = iconW;
+    var totalStars = starsT;
+    var iconStr =  "i-"+iconWidth+"-star-";
+    var removeClassStr = "";
+    var starsOn = $('#'+containerS+' .starsOn');
+    var starsOff = $('#'+containerS+' .starsOff');
+    var overallStarRating = $("#"+overallSR);
+    var arrStarValuesText = ['Click on stars to rate','Unsatisfactory', 'Below average','Average','Above average','Excellent'];
+    var arrStarValueDefault = 'Click on stars to rate';
+
+    for(var i=1; i<=totalStars; i++){
+        removeClassStr += iconStr+i;
+        if(i != totalStars){
+            removeClassStr += " ";
+        }
+    }
+    $('#'+containerS).mousemove (function(e){
+        var offset = $(this).offset();
+        var x = e.pageX - offset.left;
+        var currentStar = Math.floor(x/iconWidth) +1;
+        if(currentStar > totalStars) currentStar = totalStars;
+        starsOn.removeClass(removeClassStr).addClass(iconStr + currentStar);
+        starsOff.removeClass(removeClassStr).addClass(iconStr+ (totalStars - currentStar));
+        if(divWriteTextValues != ""){
+            $("#"+divWriteTextValues).html(arrStarValuesText[currentStar]);
+        }
+    });
+    $('#'+containerS).click (function(e){
+        var offset = $(this).offset();
+        var x = e.pageX - offset.left;
+        var currentStar = Math.floor(x/iconWidth) +1;
+        if(currentStar > totalStars) currentStar = totalStars;
+        overallStarRating.val(currentStar);
+        overallStarRating.blur();
+        starsOn.removeClass(removeClassStr).addClass(iconStr + currentStar);
+        starsOff.removeClass(removeClassStr).addClass(iconStr+ (totalStars - currentStar));
+
+    });
+    $('#'+containerS).mouseleave (function(e){
+        var currentRating = overallStarRating.val();
+        starsOn.removeClass(removeClassStr).addClass(iconStr + currentRating);
+        starsOff.removeClass(removeClassStr).addClass(iconStr+ (totalStars - currentRating));
+        if(divWriteTextValues != ""){
+            $("#"+divWriteTextValues).html(arrStarValuesText[currentRating]);
+        }
+    });
+}
