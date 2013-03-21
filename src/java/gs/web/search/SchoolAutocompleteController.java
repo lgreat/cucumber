02d6ef99a443b5bcd0790aab4dc231a28413c86c
Samson Sprouse <ssprouse@greatschools.org>
2013-03-20 17:04:39 -0700

    GS-13763 add searchForSchools method to get autocomplete results just from a normal query

--- src/java/gs/web/search/SchoolAutocompleteController.java
+++ src/java/gs/web/search/SchoolAutocompleteController.java
@@ -1,7 +1,19 @@
 package gs.web.search;
 
+import gs.data.geo.City;
+import gs.data.school.district.District;
+import gs.data.search.*;
+import gs.data.search.beans.SolrSchoolSearchResult;
+import gs.data.search.fields.AddressFields;
+import gs.data.search.fields.DocumentType;
+import gs.data.search.fields.SchoolFields;
+import gs.data.search.fields.SolrField;
+import gs.data.search.filters.SchoolFilters;
 import gs.data.search.services.SchoolSearchServiceSolrImpl;
+import gs.data.state.State;
+import gs.web.pagination.RequestedPage;
 import gs.web.util.HttpCacheInterceptor;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,21 +36,30 @@ public class SchoolAutocompleteController {
 
     SchoolSearchServiceSolrImpl _solrSchoolSearchService;
 
+    @Autowired
+    private GsSolrSearcher _gsSolrSearcher;
+
     HttpCacheInterceptor cacheInterceptor = new HttpCacheInterceptor();
+
+    public static int SUGGEST_COUNT = 150;
 
     @RequestMapping(method= RequestMethod.GET)
     public void handleRequestInternal(@RequestParam(value = "q", required = false) String searchString,
                                       @RequestParam(value = "state", required = false) String state,
                                       @RequestParam(value = "schoolDistrict", required = false) Boolean schoolDistrict,
+                                      @RequestParam(value = "schoolCity", required = false) Boolean schoolCity,
                                       HttpServletRequest request, HttpServletResponse response) throws Exception {
 
         try {
             List<String> suggestions = new ArrayList<String>();
             if (!StringUtils.isBlank(state) && !StringUtils.isBlank(searchString)) {
-                if (schoolDistrict != null && schoolDistrict) {
-                    suggestions = _solrSchoolSearchService.suggestSchoolDistrict(StringUtils.trim(searchString), StringUtils.lowerCase(state), 0, 150);
+                if (schoolCity != null && schoolCity) {
+                    suggestions = searchForSchools(state, searchString);
+
+                } else if (schoolDistrict != null && schoolDistrict) {
+                    suggestions = _solrSchoolSearchService.suggestSchoolDistrict(StringUtils.trim(searchString), StringUtils.lowerCase(state), 0, SUGGEST_COUNT);
                 } else {
-                    suggestions = _solrSchoolSearchService.suggest(StringUtils.trim(searchString), StringUtils.lowerCase(state), 0, 150);
+                    suggestions = _solrSchoolSearchService.suggest(StringUtils.trim(searchString), StringUtils.lowerCase(state), 0, SUGGEST_COUNT);
                 }
             }
 
@@ -55,6 +76,43 @@ public class SchoolAutocompleteController {
         } catch (Exception e) {
             _log.warn("Unexpected error when searching for suggestions for school autosuggest.",e);
         }
+    }
+
+    protected List<String> searchForSchools(String state, String searchString) {
+        SearchResultsPage<SolrSchoolSearchResult> searchResultsPage = new SearchResultsPage(0, new ArrayList<SolrSchoolSearchResult>());
+
+        GsSolrQuery q = createGsSolrQuery();
+
+        q.filter(DocumentType.SCHOOL).page(0, SUGGEST_COUNT);
+
+        if (state != null) {
+            q.filter(SchoolFields.SCHOOL_DATABASE_STATE, state.toLowerCase());
+        }
+
+        q.query(searchString);
+
+        List<String> autocompleteResults = new ArrayList<String>();
+
+        try {
+            searchResultsPage = _gsSolrSearcher.search(q, SolrSchoolSearchResult.class, true);
+
+            List<SolrSchoolSearchResult> results = searchResultsPage.getSearchResults();
+
+
+            for(SolrSchoolSearchResult result : results) {
+                String s = result.getId() + "-" + result.getName() + "-" + result.getCity();
+                autocompleteResults.add(s);
+            }
+
+        } catch (SearchException e) {
+            _log.error("Problem occurred while getting schools", e);
+        }
+
+        return autocompleteResults;
+    }
+
+    public GsSolrQuery createGsSolrQuery() {
+        return new GsSolrQuery(QueryType.SCHOOL_SEARCH);
     }
 
     public SchoolSearchServiceSolrImpl getSolrSchoolSearchService() {
