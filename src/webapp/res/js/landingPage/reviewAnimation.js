5922a5d1a2ebaa14cd41bf6d0da21ee4e73871a3
Mitchell Seltzer <mseltzer@greatschools.org>
2013-03-25 16:41:19 -0700

    GS-13763 - added select dropdown js

--- src/webapp/res/js/landingPage/reviewAnimation.js
+++ src/webapp/res/js/landingPage/reviewAnimation.js
@@ -5,11 +5,13 @@
  * Time: 10:07 AM
  * To change this template use File | Settings | File Templates.
  */
+GS = GS || {};
 
 
 $(document).ready(function() {
     $('#js-reviewContent').characterCounter({charLimit:1200});
     $('#js_submitSelectSchool').on("click",function() {
+
         $('.js-pageOneReviewLandingPage').fadeOut('slow', function() {
             $('.js-pageTwoReviewLandingPage').fadeIn('fast', function() {
                 $('.headerBar').show().animate({
@@ -19,54 +21,75 @@ $(document).ready(function() {
         });
 
     });
-    var selectShow = $(".js-selectDropDown"); //notify
-    selectShow.hide();
-    selectShow.click(function(event) {
-        // Handle the click on the notify div so the document click doesn't close it
-        event.stopPropagation();
-    });
 
-    var selectLink = $(".js-selectBox");
-    selectLink.click(showSelect);
+    GS_initializeCustomSelect("js-reviewLandingIAm", GS_selectCallbackReviewsIAm);
+    GS_initializeCustomSelect("js-reviewLandingState");
+    function GS_selectCallbackReviewsIAm(selectValue){
+        hideAllLayers();
+        if(selectValue == "Parent"){
+            $('#js-reviewsLandingStarBox-Parent').show();
+        }
+        if(selectValue == "Student"){
+            $('#js-reviewsLandingStarBox-NoAdd').show();
+        }
+        if(selectValue == "Teacher/Staff member"){
+            $('#js-reviewsLandingStarBox-NoAdd').show();
+        }
+        if(selectValue == "Other"){
+            $('#js-reviewsLandingStarBox-NoAdd').show();
+        }
+        function hideAllLayers(){
+            $('#js-reviewsLandingStarBox-Parent').hide();
+            $('#js-reviewsLandingStarBox-Start').hide();
+            $('#js-reviewsLandingStarBox-NoAdd').hide();
+        }
+    }
+    function GS_initializeCustomSelect(layerContainer, callbackFunction, alignment){
+        var selectContainer = $("#"+layerContainer); //notify
+        var selectBox = selectContainer.find(".js-selectBox");
+        var selectDropDownBox = selectContainer.find(".js-selectDropDown");
+        var selectDropDownItem = selectContainer.find(".js-ddValues");
+        var selectBoxText = selectContainer.find(".js-selectBoxText");
 
-    function showSelect(event) {
-        $(this).unbind('click', showSelect);
+        selectBox.click(showSelect);
 
-        selectShow.show();
+        selectDropDownBox.click(function(event) {
+            // Handle the click on the notify div so the document click doesn't close it
+            event.stopPropagation();
+        });
 
-        $(document).click(hideSelect);
-        $(".js-ddValues").click(showW);
+        function showSelect(event) {
+            $(this).unbind('click', showSelect);
+            selectDropDownBox.show();
+            $(document).click(hideSelect);
+            selectDropDownItem.click(showW);
+            // So the document doesn't immediately handle this same click event
+            event.stopPropagation();
+        };
 
-        // So the document doesn't immediately handle this same click event
-        event.stopPropagation();
-    };
+        function hideSelect(event) {
+            $(this).unbind('click', hideSelect);
+            selectDropDownItem.unbind('click', showW);
+            selectDropDownBox.hide();
+            selectBox.click(showSelect);
+        }
 
-    function hideSelect(event) {
-        $(this).unbind('click', hideSelect);
+        function showW(event) {
+            hideSelect(event);
+            selectBoxText.html($(this).html());
+            callbackFunction($(this).html());
+        }
 
-        selectShow.hide();
+        selectDropDownItem.mouseover(function () {
+            $(this).addClass("ddValuesHighlight");
+        });
 
-        selectLink.click(showSelect);
+        selectDropDownItem.mouseout(function () {
+            $(this).removeClass("ddValuesHighlight");
+        });
     }
 
-    function showW(event) {
-        hideSelect(event);
-        var divValue = $(this).html();
-        selectLink.html(divValue);
-    }
-    $(".js-ddValues").mouseover(function () {
-        $(this).addClass("ddValuesHighlight");
-    });
-    $(".js-ddValues").mouseout(function () {
-        $(this).removeClass("ddValuesHighlight");
-    });
-    $(".js-ddValues").click(function () {
-        var $this = $(this);
-        var selectedValue = $this.html();
-        var displayDropdownValue = $(".selectBox").html(selectedValue);
 
-        $(".js-selectDropDown").toggle();
-    });
 
     starRatingInterface("starRatingContainerReview", 16, 5, "overallAsString", "");
     starRatingInterface("starRatingContainerReviewTeacher", 16, 5, "teacherAsString", "");
