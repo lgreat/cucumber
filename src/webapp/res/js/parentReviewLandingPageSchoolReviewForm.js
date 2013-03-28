14fd12f1f05c6afafcf6416a113221dc761c958d
Samson Sprouse <ssprouse@greatschools.org>
2013-03-27 19:12:32 -0700

    GS-13763 Work on review landing page form submission and validation (cherry picked from commit 7ca55398387192b37053d03672d78994c98e4518)

--- src/webapp/res/js/parentReviewLandingPageSchoolReviewForm.js
+++ src/webapp/res/js/parentReviewLandingPageSchoolReviewForm.js
@@ -10,28 +10,9 @@ jQuery(function() {
     GS.form.ParentReviewLandingPageSchoolReviewForm.prototype.postReview = function(email, callerFormId) {
         var url = '/school/review/postReview.page';
         
-        // first, grab the email from the join/signIn form and use that with the review
-        if (email !== undefined && email !== '') {
-            this.email.getElement().val(email);
-        }
         var formData = this.serialize();
+
         jQuery.post(url, formData, function(data) {
-            if (data.showHover !== undefined && data.showHover === "emailNotValidated") {
-                GSType.hover.emailNotValidated.show();
-                return false;
-            } else if (data.showHover !== undefined && data.showHover === "validateEmailSchoolReview") {
-                subCookie.setObjectProperty("site_pref", "showHover", "validateEmailSchoolReview", 3);
-            } else {
-                if (data.reviewPosted !== undefined) {
-                    if (data.reviewPosted === "true") {
-                        // cookie to show schoolReviewPostedThankYou hover
-                        subCookie.setObjectProperty("site_pref", "showHover", "schoolReviewPostedThankYou", 3);
-                    } else {
-                        // cookie to show schoolReviewNotPostedThankYou hover
-                        subCookie.setObjectProperty("site_pref", "showHover", "schoolReviewNotPostedThankYou", 3);
-                    }
-                }
-            }
 
             var successEvents = "";
             if (data.ratingEvent !== undefined) {
@@ -46,14 +27,39 @@ jQuery(function() {
                 pageTracking.send();
             }
             if (callerFormId) {
-                GSType.hover.signInHover.hide();
-                GSType.hover.joinHover.hide();
                 jQuery('#' + callerFormId).submit();
             } else {
                 window.location.href = '/school/parentReviews.page?id=' + jQuery('#schoolId').val() + '&state=' + jQuery('#schoolState').val();
             }
         }, "json");
     }.gs_bind(GS.form.ParentReviewLandingPageSchoolReviewForm.prototype);
+
+    GS.form.ParentReviewLandingPageSchoolReviewForm.prototype.formValid = function() {
+
+        return true;
+    };
+
+    GS.form.ParentReviewLandingPageSchoolReviewForm.prototype.updateAllErrors = function() {
+
+        return true;
+    };
+
+    /////////// form setup - register event handlers /////////
+    GS.form.ParentReviewLandingPageSchoolReviewForm.prototype.attachEventHandlers = function() {
+
+        $('#js-submitParentReview').click(function() {
+            if (GS.form.parentReviewLandingPageSchoolReviewForm.formValid()) {
+                GS.form.parentReviewLandingPageSchoolReviewForm.postReview();
+            } else {
+                GS.form.parentReviewLandingPageSchoolReviewForm.updateAllErrors();
+            }
+        });
+        return false;
+
+    }.gs_bind(GS.form.ParentReviewLandingPageSchoolReviewForm.prototype);
+
+
     GS.form.parentReviewLandingPageSchoolReviewForm = new GS.form.ParentReviewLandingPageSchoolReviewForm();
+    GS.form.parentReviewLandingPageSchoolReviewForm.attachEventHandlers();
 
 });
