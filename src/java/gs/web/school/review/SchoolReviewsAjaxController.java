8ec4350ef54a33841e58588c2698c9bcc72e074a
Samson Sprouse <ssprouse@greatschools.org>
2013-03-28 16:57:13 -0700

    GS-13763 Revert changes to SchoolReviewsAjaxController to change how review submission works for signed-in users, Add validation to autocomplete search box (cherry picked from commit 4b57bab7064282222b1b9ef3e91316fc330c7d2e)

--- src/java/gs/web/school/review/SchoolReviewsAjaxController.java
+++ src/java/gs/web/school/review/SchoolReviewsAjaxController.java
@@ -175,7 +175,7 @@ public class SchoolReviewsAjaxController extends AbstractCommandController imple
             }
         }
 
-        boolean reviewProvisional = (reviewCommand.isFromReviewLandingPage() || isNewUser || (!userAuthorizedWithPassword && !emailVerifiedRecently));
+        boolean reviewProvisional = (isNewUser || (!userAuthorizedWithPassword && !emailVerifiedRecently));
 
         if (reviewProvisional) {
             if (userIpOnBanList || Poster.STUDENT.equals(poster)) {
@@ -234,7 +234,7 @@ public class SchoolReviewsAjaxController extends AbstractCommandController imple
 
         boolean reviewPosted = !(reviewProvisional || reviewHasReallyBadWords || schoolOnHoldList || userIpOnBanList || Poster.STUDENT.equals(reviewCommand.getPoster()));
         
-        if (reviewPosted && !reviewCommand.isFromReviewLandingPage()) {
+        if (reviewPosted) {
             sendReviewPostedEmail(request, review);
         }
 
@@ -246,7 +246,7 @@ public class SchoolReviewsAjaxController extends AbstractCommandController imple
 
         responseValues.put("userId", user.getId().toString());
 
-        if (reviewProvisional || reviewCommand.isFromReviewLandingPage()) {
+        if (reviewProvisional) {
             responseValues.put("showHover", "validateEmailSchoolReview");
             UrlBuilder urlBuilder = new UrlBuilder(school, UrlBuilder.SCHOOL_PARENT_REVIEWS);
             String redirectUrl = urlBuilder.asFullUrl(request);
