9ab21f5d4658dd2f65379d8d37ba475fbdf6256c
Priyanshi Deshwal <pdeshwal@greatschools.org>
2013-04-26 15:05:23 -0700

    GS-13924: New GS logo for Share with friends dialog

--- src/webapp/res/js/search/schoolSearchResultsPage2012.js
+++ src/webapp/res/js/search/schoolSearchResultsPage2012.js
@@ -161,7 +161,7 @@ GS.schoolSearchResultsPage = GS.schoolSearchResultsPage || (function() {
             var schoolName = $schoolData.data('gs-school-name');
             var autoText = $schoolData.data('gs-school-autotext');
             var link = $this.attr('href');
-            var photoUrl = 'http://www.gscdn.org/res/img/logo/logo_GS_276x50_logo.png'
+            var photoUrl = 'http://www.gscdn.org/res/img/logo/gs_logo_75x75.png'
 
             GS.facebook.postToFeed(link, photoUrl, schoolName, link, autoText);
             return false;
