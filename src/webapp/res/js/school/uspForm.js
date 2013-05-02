fff6575924f51f8b96356136f629d5fd4e003651
Nanditha Patury <npatury@greatschools.org>
2013-05-02 09:58:00 -0700

    GS-13995 added js code for validations

--- src/webapp/res/js/school/uspForm.js
+++ src/webapp/res/js/school/uspForm.js
@@ -1,27 +1,37 @@
-var saveForm = function(form) {
-    var masterDeferred = new jQuery.Deferred();
-    if(!doValidations()) {
-        alert('show error');
-        return masterDeferred.reject().promise();
-    }
+var GS = GS || {};
+GS.form = GS.form || {};
+GS.form.UspForm = function () {
 
-    var data = form.serializeArray();
+    this.saveForm = function (form) {
+        var masterDeferred = new jQuery.Deferred();
+        if (!GS.form.uspForm.doValidations()) {
+            alert('show error');
+            return masterDeferred.reject().promise();
+        }
 
-    jQuery.ajax({type: 'POST', url: document.location, data: data}
-    ).fail(function() {
-            masterDeferred.reject();
-            alert("Sorry! There was an unexpected error saving your form. Please wait a minute and try again.");
-        }).done(function() {});
+        var data = form.serializeArray();
+
+        jQuery.ajax({type:'POST', url:document.location, data:data}
+        ).fail(function () {
+                masterDeferred.reject();
+                alert("Sorry! There was an unexpected error saving your form. Please wait a minute and try again.");
+            }).done(function () {
+            });
+    };
+
+    this.doValidations = function () {
+        return true;
+    };
+
 };
 
-var doValidations = function() {
-    return true;
-};
+GS.form.uspForm = new GS.form.UspForm();
 
-jQuery(function() {
+jQuery(function () {
     var form = jQuery('#js_uspForm');
-    form.on('click', '.js_submit', function() {
-        saveForm(form);
+
+    form.on('click', '.js_submit', function () {
+        GS.form.uspForm.saveForm(form);
         return false;
     });
 });
