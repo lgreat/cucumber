5dd5bb84a71339b3ee12a68f356a04c702ce2708
Ramprasad Ramachandran <rramachandran@greatschools.org>
2013-05-20 16:12:28 -0700

    GS-13972 USP: Create a framework page to start saving data. Sentence case fix for computer animation label

--- src/test/gs/web/school/usp/UspFormControllerTest.java
+++ src/test/gs/web/school/usp/UspFormControllerTest.java
@@ -2,36 +2,47 @@ package gs.web.school.usp;
 
 import gs.data.community.IUserDao;
 import gs.data.community.User;
+import gs.data.school.School;
+import gs.data.state.State;
 import gs.web.BaseControllerTestCase;
 import org.easymock.classextension.EasyMock;
+import org.springframework.ui.ModelMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
 
 public class UspFormControllerTest extends BaseControllerTestCase {
     UspFormController _controller;
     private IUserDao _userDao;
+    private UspFormHelper _uspHelper;
 
     public void setUp() throws Exception {
         super.setUp();
         _controller = new UspFormController();
 
         _userDao = EasyMock.createStrictMock(IUserDao.class);
+        _uspHelper = EasyMock.createStrictMock(UspFormHelper.class);
+
         _controller.setUserDao(_userDao);
+        _controller.setUspFormHelper(_uspHelper);
     }
 
     private void replayAllMocks() {
         replayMocks(_userDao);
+        replayMocks(_uspHelper);
     }
 
     private void verifyAllMocks() {
         verifyMocks(_userDao);
+        verifyMocks(_uspHelper);
     }
 
     private void resetAllMocks() {
         resetMocks(_userDao);
+        resetMocks(_uspHelper);
     }
 
     /**
@@ -126,4 +137,43 @@ public class UspFormControllerTest extends BaseControllerTestCase {
         assertEquals("User is email validated.",expectedJson, getResponse().getContentAsString());
     }
 
+    public void testShowUserForm() {
+        resetAllMocks();
+
+        HttpServletRequest request = getRequest();
+        HttpServletResponse response = getResponse();
+
+        State state = State.CA;
+        Integer schoolId = 1;
+        School school = getSchool(state, schoolId);
+        ModelMap modelMap = new ModelMap();
+
+        expect(_uspHelper.getSchool(null, null)).andReturn(null);
+
+        replayAllMocks();
+        String view = _controller.showUspUserForm(modelMap, request, response, null, null);
+        verifyAllMocks();
+
+        assertEquals("", view);
+
+        resetAllMocks();
+
+        expect(_uspHelper.getSchool(state, schoolId)).andReturn(school);
+        _uspHelper.formFieldsBuilderHelper(modelMap, request, response, school, state, null, false);
+        expectLastCall();
+
+        replayAllMocks();
+        view = _controller.showUspUserForm(modelMap, request, response, schoolId, state);
+        verifyAllMocks();
+
+        assertEquals(UspFormController.FORM_VIEW, view);
+    }
+
+    public School getSchool(State state, Integer schoolId) {
+        School school = new School();
+        school.setId(schoolId);
+        school.setDatabaseState(state);
+        school.setName("QWERT Elementary School");
+        return school;
+    }
 }
