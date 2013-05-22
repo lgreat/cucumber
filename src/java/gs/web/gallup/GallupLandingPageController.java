302887a60f40891bdd75643c9d1ee139011f91b9
Shomi Arora <sarora@greatschools.org>
2013-05-22 14:21:50 -0700

    GS-14036 Fixing the ordering on the page.

--- src/java/gs/web/gallup/GallupLandingPageController.java
+++ src/java/gs/web/gallup/GallupLandingPageController.java
@@ -20,6 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -37,7 +39,29 @@ public class GallupLandingPageController {
     public String handleRequestInternal(HttpServletRequest request, Model model) throws Exception {
         SessionContext sessionContext = SessionContextUtil.getSessionContext(request);
         final ArrayList<GallupSchools> gallupSchools= _gallupDao.findGallupSchoolsList();
+        sortResult(gallupSchools);
         model.addAttribute("gallupSchools",gallupSchools);
         return "/gallup/gallupLanding";
+    }
+
+    private void sortResult(ArrayList<GallupSchools> gallupSchools) {
+        Collections.sort(gallupSchools, new Comparator<GallupSchools>() {
+            public int compare(GallupSchools s1, GallupSchools s2) {
+                 String state1 = s1.getState();
+                String state2 = s2.getState();
+                String schoolName1 = s1.getSchoolName();
+                String schoolName2 = s2.getSchoolName();
+                int stateCompare = state1.compareToIgnoreCase(state2);
+                int valueToReturn;
+                if (stateCompare != 0) {
+                    valueToReturn = stateCompare;
+                } else {
+                    valueToReturn = schoolName1.compareToIgnoreCase(schoolName2);
+
+                }
+                return valueToReturn;
+            }
+
+        });
     }
 }
