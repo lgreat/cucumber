0abaa8323bf62cf23596bf74760f069b17ae7b5a
Bob Raker <rraker@greatschools.org>
2013-05-22 16:11:05 -0700

    GS-13764 Added code to xGAMSetup.tagx to use the same ad for all 300x125 ads

--- src/java/gs/web/ads/AdTagHandler.java
+++ src/java/gs/web/ads/AdTagHandler.java
@@ -239,10 +239,6 @@ public class AdTagHandler extends AbstractDeferredContentTagHandler {
         PageContext pageContext = (PageContext) getJspContext();
         HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
 
-        // GS-13764 - Do not serve any 300x125 ads
-        if (getPosition().endsWith("_300x125")) {
-            return "";
-        }
         // if this is a mobile ad, we will always
         // want to display GPT tag regardless of
         // if GPT is enabled for the desktop
