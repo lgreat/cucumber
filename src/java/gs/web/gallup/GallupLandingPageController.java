1cefe616790a759ad10b52f627d2e118c32954f3
Shomi Arora <sarora@greatschools.org>
2013-05-23 16:56:28 -0700

    GS-14070 Making the changes as needed by design team.

--- src/java/gs/web/gallup/GallupLandingPageController.java
+++ src/java/gs/web/gallup/GallupLandingPageController.java
@@ -9,8 +9,8 @@ package gs.web.gallup;
  */
 
 
+
 import gs.data.gallup.*;
-import java.util.ArrayList;
 import gs.web.util.context.SessionContext;
 import gs.web.util.context.SessionContextUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -38,30 +36,10 @@ public class GallupLandingPageController {
     @RequestMapping(method= RequestMethod.GET)
     public String handleRequestInternal(HttpServletRequest request, Model model) throws Exception {
         SessionContext sessionContext = SessionContextUtil.getSessionContext(request);
-        final ArrayList<GallupSchools> gallupSchools= _gallupDao.findGallupSchoolsList();
-        sortResult(gallupSchools);
-        model.addAttribute("gallupSchools",gallupSchools);
+        List<GallupSchools> gallupSchools= _gallupDao.findGallupSchoolsList();
+         model.addAttribute("gallupSchools",gallupSchools);
         return "/gallup/gallupLanding";
-    }
 
-    private void sortResult(ArrayList<GallupSchools> gallupSchools) {
-        Collections.sort(gallupSchools, new Comparator<GallupSchools>() {
-            public int compare(GallupSchools s1, GallupSchools s2) {
-                 String state1 = s1.getState();
-                String state2 = s2.getState();
-                String schoolName1 = s1.getSchoolName();
-                String schoolName2 = s2.getSchoolName();
-                int stateCompare = state1.compareToIgnoreCase(state2);
-                int valueToReturn;
-                if (stateCompare != 0) {
-                    valueToReturn = stateCompare;
-                } else {
-                    valueToReturn = schoolName1.compareToIgnoreCase(schoolName2);
-
-                }
-                return valueToReturn;
-            }
-
-        });
     }
 }
+
