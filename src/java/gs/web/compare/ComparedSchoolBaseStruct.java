9e17b426f01ecf4290059239da0c1f1ce750e049
Bob Raker <rraker@greatschools.org>
2013-05-23 16:32:56 -0700

    GS-14132 Java 7 jspx/tagx can not use isPrivate because in jspx this becomes ${xxx.private} and private is a reserved word.  Added getter isPrivateSchool to fix

--- src/java/gs/web/compare/ComparedSchoolBaseStruct.java
+++ src/java/gs/web/compare/ComparedSchoolBaseStruct.java
@@ -130,7 +130,7 @@ public class ComparedSchoolBaseStruct {
         return "Private".equals(getType());
     }
 
-    // GS-14132 - Java 7 - Can't use isPrivate in jsp because private is reserved word
+    // GS-14132 - Java 7 - Can't use isPrivate in jsp/tag because private is a reserved word.  Use this in jsp/tag instead of
     public boolean isPrivateSchool() {
         return "Private".equals(getType());
     }
