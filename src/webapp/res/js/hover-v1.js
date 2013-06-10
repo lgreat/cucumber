979d958579362374928db7e0d67a90398671d8c4
Mitchell Seltzer <mseltzer@greatschools.org>
2013-06-10 10:30:01 -0700

    GS-14042 - fix for Chrome Browser issue hash tag was causing scroll to top.

--- src/webapp/res/js/hover-v1.js
+++ src/webapp/res/js/hover-v1.js
@@ -2219,12 +2219,7 @@ GSType.hover.PrintSchoolChooser.prototype = new GSType.hover.HoverDialog('printS
 GSType.hover.printSchoolChooser = new GSType.hover.PrintSchoolChooser();
 
 
-GSType.hover.ModalUspRegistration = function() {
-//    this.show = function() {
-//        GSType.hover.modalUspRegistration.showModal();
-//        return false;
-//    };
-};
+GSType.hover.ModalUspRegistration = function() {};
 GSType.hover.ModalUspRegistration.prototype = new GSType.hover.HoverDialog('js-modalUspRegistration');
 GSType.hover.modalUspRegistration = new GSType.hover.ModalUspRegistration();
 
