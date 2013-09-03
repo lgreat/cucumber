e012bf6c656d0f7168b2b20e1a6d63d2aac3aefc
Shomi Arora <sarora@greatschools.org>
2013-09-03 09:25:40 -0700

    GS-14613 Adding the controller structure for city choose page .

---
+++ src/java/gs/web/geo/CityHubChoosePageController.java
@@ -0,0 +1,89 @@
+package gs.web.geo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sarora
+ * Date: 8/30/13
+ * Time: 3:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import gs.data.hubs.HubConfig;
+import gs.data.state.State;
+import gs.data.url.DirectoryStructureUrlFactory;
+import gs.web.path.DirectoryStructureUrlFields;
+import gs.web.path.IDirectoryStructureUrlController;
+import gs.web.school.review.ReviewFacade;
+import gs.web.util.context.SessionContext;
+import gs.web.util.context.SessionContextUtil;
+import gs.web.util.list.AnchorListModel;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.WordUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * Controller for the City Hub Choose  Pages.
+ * @author sarora@greatschools.org Shomi Arora.
+ */
+
+@Controller
+public class CityHubChoosePageController {
+
+
+    private  static final String PARAM_CITY = "city";
+    @Autowired
+    private CityHubHelper _cityHubHelper;
+
+    @RequestMapping(method= RequestMethod.GET)
+    public ModelAndView handleRequest(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+
+        ModelAndView modelAndView = new ModelAndView("/cityHub/choosePage");
+
+//
+//        SessionContext sessionContext = SessionContextUtil.getSessionContext(request);
+//        final State state = sessionContext.getState();
+//        DirectoryStructureUrlFields fields = (DirectoryStructureUrlFields) request.getAttribute(IDirectoryStructureUrlController.FIELDS);
+//        String city = StringUtils.defaultIfEmpty(request.getParameter(PARAM_CITY), fields.getCityName());
+//        // Validate those inputs and give up if we can't build a reasonable page.
+//        if (state == null) {
+//            // no state name found on city page, so redirect to /
+//            View redirectView = new RedirectView("/");
+//            return new ModelAndView(redirectView);
+//        }
+//
+//        if (StringUtils.isEmpty(city)) {
+//            // no city name found, so redirect to /california or whichever state they did provide
+//            View redirectView = new RedirectView(DirectoryStructureUrlFactory.createNewStateBrowseURIRoot(state));
+//            return new ModelAndView(redirectView);
+//        }
+//
+//        modelAndView.addObject("city", WordUtils.capitalizeFully(city));
+//        modelAndView.addObject("state", state);
+//        modelAndView.addObject("hubId", getCityHubHelper().getHubID(city, state));
+//
+
+
+        return modelAndView;
+
+
+    }
+
+    public CityHubHelper getCityHubHelper() {
+        return _cityHubHelper;
+    }
+
+    public void setCityHubHelper(final CityHubHelper _cityHubHelper) {
+        this._cityHubHelper = _cityHubHelper;
+    }
+}
