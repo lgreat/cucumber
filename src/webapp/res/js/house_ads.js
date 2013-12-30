e72ca27722c037a0602d4b1b544a5549a6cf0bb4
Anthony Roy <aroy@greatschools.org>
2013-12-30 15:50:13 -0800

    PT-128 Remove INT code from k12 traffic driver script, second location

--- src/webapp/res/js/house_ads.js
+++ src/webapp/res/js/house_ads.js
@@ -93,8 +93,7 @@ GS.promo.K12.TrafficDriver = function() {
     };
 
     this.isK12State = function(stateAbbrev) {
-        var k12StatePattern = /AR|AZ|CA|CO|DC|FL|GA|HI|ID|IL|IN|KS|MN|MO|NV|OH|OK|OR|PA|SC|TX|UT|WA|WI|WY/;
-        return (stateAbbrev != null && stateAbbrev.match(k12StatePattern) != null);
+        return true;
     };
 
     this.getK12School = function(gamStateAttr) {
